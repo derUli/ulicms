f7eccf1df646480876523c717ce892eb8a7194a8
derUli <admin@deruli.de>
2012-06-01 07:29:59 +0200

    Dateityp statt Inahltstyp

---
+++ admin/inc/contents.php
@@ -0,0 +1,14 @@
+﻿<?php if(defined("_SECURITY")){
+?>
+
+<h2>Inhalte</h2>
+<strong>Bitte wählen Sie einen Inhaltstyp aus:</strong><br/>
+<a href="index.php?action=news">News</a><br/>
+<a href="index.php?action=pages">Seiten</a><br/>
+
+
+
+
+
+
+<?php }?>
---
+++ admin/inc/media.php
@@ -0,0 +1,15 @@
+﻿<?php if(defined("_SECURITY")){
+?>
+
+<h2>Inhalte</h2>
+<strong>Bitte wählen Sie einen Dateityp aus:</strong><br/>
+<a href="index.php?action=images">Bilder</a><br/>
+<a href="index.php?action=flash">Flash</a><br/>
+<a href="index.php?action=files">Dateien</a><br/>
+
+
+
+
+
+
+<?php }?>
