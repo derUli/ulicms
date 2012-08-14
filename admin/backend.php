0b3c4b3a5f50b2ac2a61ab3310080a73e8540a90
derUli <admin@deruli.de>
2012-08-14 18:05:30 +0200

    Update-Skript wieder hinzugefügt

--- admin/backend.php
+++ admin/backend.php
@@ -33,73 +33,81 @@ if(!$eingeloggt){
 
 
 
-if($_GET["action"]==""||$_GET["action"]=="home"){
+if($_GET["action"] == "" || $_GET["action"] == "home"){
 	require_once "inc/dashboard.php";
 }
-else if($_GET["action"]=="news"){
+else if($_GET["action"] == "news"){
 	require_once "inc/news.php";
 }
-else if($_GET["action"]=="add_news"){
+else if($_GET["action"] == "add_news"){
 	require_once "inc/add_news.php";
 }
-else if($_GET["action"]=="edit_news"){
+else if($_GET["action"] == "edit_news"){
 	require_once "inc/edit_news.php";
 }
-else if($_GET["action"]=="contents"){
+else if($_GET["action"] == "contents"){
 	require_once "inc/contents.php";
 }
 
-else if($_GET["action"]=="pages"){
+else if($_GET["action"] == "pages"){
 	require_once "inc/pages.php";
 }
-else if($_GET["action"]=="pages_edit"){
+else if($_GET["action"] == "pages_edit"){
 	require_once "inc/edit_page.php";
 }
-else if($_GET["action"]=="pages_new"){
+else if($_GET["action"] == "pages_new"){
 	require_once "inc/add_page.php";
 }
-else if($_GET["action"]=="banner"){
+else if($_GET["action"] == "banner"){
 	require_once "inc/banner.php";
 }
-else if($_GET["action"]=="banner_new"){
+else if($_GET["action"] == "banner_new"){
 	require_once "inc/banner_new.php";
 }
-else if($_GET["action"]=="banner_edit"){
+else if($_GET["action"] == "banner_edit"){
 	require_once "inc/banner_edit.php";
 }
-else if($_GET["action"]=="admins"){
+else if($_GET["action"] == "admins"){
 	require_once "inc/admins.php";
 }
-else if($_GET["action"]=="admin_new"){
+else if($_GET["action"] == "admin_new"){
 	require_once "inc/admins_new.php";
 }
-else if($_GET["action"]=="admin_edit"){
+else if($_GET["action"] == "admin_edit"){
 	require_once "inc/admins_edit.php";
 }
-else if($_GET["action"]=="settings"){
+else if($_GET["action"] == "settings"){
 	require_once "inc/settings.php";
 }
-else if($_GET["action"]=="settings_simple"){
+else if($_GET["action"] == "settings_simple"){
 	require_once "inc/settings_simple.php";
 }
-else if($_GET["action"]=="key_new"){
+else if($_GET["action"] == "key_new"){
 	require_once "inc/key_new.php";
 }
-else if($_GET["action"]=="key_edit"){
+else if($_GET["action"] == "key_edit"){
 	require_once "inc/key_edit.php";
 }
 
-else if($_GET["action"]=="templates"){
+else if($_GET["action"] == "templates"){
 	require_once "inc/templates.php";
 }else if($_GET["action"] == "media"){
 	require_once "inc/media.php";
 }
-else if($_GET["action"]=="images" ||$_GET["action"]=="files" ||$_GET["action"]=="flash"){
+else if($_GET["action"] == "images" || $_GET["action"] == "files" || $_GET["action"] == "flash"){
 	require_once "inc/filemanager.php";
 }
 
-else if($_GET["action"]=="info"){
+else if($_GET["action"] == "info" ){
 	require_once "inc/info.php";
+}
+
+else if($_GET["action"] == "info" ){
+	require_once "inc/info.php";
+}
+
+else if($_GET["action"]  == "system_update"){
+	require_once "inc/system_update.php";
 }
 
 }
--- update.php
+++ update.php
@@ -6,10 +6,10 @@ require_once "init.php";
 $config = new config();
 $prefix = $config->mysql_prefix;
 
-// Nothing to do
-
+// Konfigurationsvariable setzen
 setconfig("visitors_can_register", "on");
 
+// Änderungen an der Datenbank durchführen
 mysql_query("UPDATE ".$prefix."admins SET password = MD5(password)");
 mysql_query("ALTER TABLE `".$prefix."content` CHANGE `content` `content` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ");
 mysql_query("ALTER TABLE `".$prefix."news` CHANGE `content` `content` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ");
@@ -23,10 +23,15 @@ mysql_query("UPDATE ".$prefix."content SET valid_from = NOW(), access = 'all'");
 
 mysql_query("INSERT INTO `".$prefix."news` (`id`, `title`, `content`, `date`, `active`, `autor`) VALUES (NULL, 'UliCMS 4.5 Entwicklerversion', '<p>Das hier ist die aktuelle Entwicklerversion von UliCMS 4.5.<br/>
 Beachten Sie bitte, dass diese Software noch nicht 100-prozentig fertig ist und noch Fehler enthalten kann.</p>
-<p>Lesen Sie bitte die news.txt und update.php um Informationen Ã¼ber diese Version zu bekommen.</p>', '1344084710', '1', '1');");
+<p>Lesen Sie bitte die news.txt und update.php um Informationen über diese Version zu bekommen.</p>', '1344084710', '1', '1');");
 
+// Update-Skript löscht sich selbst
+unlink("update.php");
 
+// Redirect zum Dashboard
 header("Location: admin/");
 
+// Schluss
+exit();
 
 ?>
