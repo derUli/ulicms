26f52a20dc672501093f3c5aa9fa67046bc2b49f
derUli <admin@deruli.de>
2012-08-14 15:52:51 +0200

    Installer geändert

--- installer/installer.php
+++ installer/installer.php
@@ -227,7 +227,7 @@ mysql_query("CREATE TABLE IF NOT EXISTS `".$prefix."content` (
 
 mysql_query("INSERT INTO `".$prefix."content` (notinfeed, systemname, title, content, active,
 created, lastchangeby, autor, views, comments_enabled, redirection, menu, position, parent)
-VALUES (1, 'willkommen', 'Willkommen', 
+VALUES (0, 'willkommen', 'Willkommen', 
 '<p>Herzlichen Glückwunsch!<br/>
 UliCMS wurde erfolgreich auf dieser Website installiert.</p>', 1, ".time().",
 1, 1, 0, 0, '', 'top', 0, '-')
@@ -235,7 +235,7 @@ UliCMS wurde erfolgreich auf dieser Website installiert.</p>', 1, ".time().",
 
 mysql_query("INSERT INTO `".$prefix."content` (notinfeed, systemname, title, content, active,
 created, lastchangeby, autor, views, comments_enabled, redirection, menu, position, parent)
-VALUES (0, 'login', 'Anmelden', 
+VALUES (1, 'login', 'Anmelden', 
 '', 1, ".time().",
 1, 1, 0, 0, 'admin/', 'down', 0, '-')
 ");
