5fe6a71b2b63d451a4cfec86f70d572a0cdd7046
derUli <admin@deruli.de>
2012-08-16 15:41:59 +0200

    modules.php hinzugefuegt

---
+++ admin/inc/modules.php
@@ -0,0 +1,19 @@
+<strong>Installierte Module:</strong>
+<p>Hier finden Sie eine Auflistung der installierten Module.<br/>
+<br/>
+Daneben befindet sich der Code, den Sie in eine Seite einfügen müssen,
+um diesen Modul einzubetten</p>
+
+<?php 
+$modules = getAllModules();
+if(count($modules)>0){
+  echo "<ol>";
+  for($i=0; $i<count($modules); $i++){
+    echo "<li>".$modules[$i]." &nbsp;&nbsp;&nbsp<input type='text' value='[module=\"".$modules[$i]."\"]'></li>";
+  }
+  echo "</ol>";
+  
+  }
+?>
+
+<hr/>
