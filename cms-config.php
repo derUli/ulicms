fc05e70285dfebfeaf807e025a0e73a0de287239
derUli <admin@deruli.de>
2012-08-16 17:02:52 +0200

    Updated news.txt

--- cms-config.php
+++ cms-config.php
@@ -5,7 +5,7 @@ var $mysql_server="localhost";
 var $mysql_user="root";
 var $mysql_password="";
 var $mysql_database="ulicms";
-var $mysql_prefix="huihuihui_";
+var $mysql_prefix="rgergtzrgrfgdf_";
 
 }
 ?>
