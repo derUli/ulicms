5be2eb55007a7d77be0dcd958d106a52e882ab37
derUli <admin@deruli.de>
2012-11-12 23:49:01 +0100

    Link "Blogbeitrag einreichen" hinzugefügt.

--- modules/blog/blog_list.php
+++ modules/blog/blog_list.php
@@ -1,5 +1,21 @@
-<?php function blog_list(){
-return "<p><strong>Hier kommt dann die News-Liste mit den Vorschautexten</strong></p>";
+<?php 
+function blog_list(){
+   
+   $html = "";
+   
+   // Wenn der Nutzer mindestens die Berechtigungen
+   // eines Mitarbeiters hat, bekommt er den Link zum 
+   // Anlegen eines neuen Blogbeitrag angezeigt
+   
+   if($_SESSION["group"] >= 20){
+   $html .= "<p><a href='?seite=".
+   get_requested_pagename().
+   "&blog_admin=add'>Blogbeitrag einreichen</a></p>";
+   }
+
+   $html.="<p><strong>Hier kommt dann die News-Liste mit den Vorschautexten</strong></p>";
+
+   return $html;
 
 }
 ?>
