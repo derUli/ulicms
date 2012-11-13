13c5ba6bc7d4bfddd7468f19f7d73a7c4d296b07
derUli <admin@deruli.de>
2012-11-13 21:14:01 +0100

    Pagination im Blogmenü implementiert

--- modules/blog/blog_list.php
+++ modules/blog/blog_list.php
@@ -1,5 +1,8 @@
 <?php 
 function blog_list(){
+
+
+   $posts_per_page = 5;
    
    $html = "";
    
@@ -13,9 +16,114 @@ function blog_list(){
    "&blog_admin=add'>Blogeintrag anlegen</a><hr/></p>";
    }
 
-   $html.="<p><strong>Hier kommt dann die News-Liste mit den Vorschautexten</strong></p>";
+
+
+
+    if(isset($_GET["limit"])){
+      $limit1 = intval($_GET["limit"]);
+      $limit2 = intval($_GET["limit"]) + $posts_per_page;
+      $limit3 = $limit1 - $posts_per_page;
+   
+      
+    }else{
+      $limit1 = 0;
+      $limit2 = $posts_per_page;
+      $limit3 = $limit1 - $posts_per_page;
+      
+    }
+
+    $count_query = mysql_query("SELECT * FROM `".
+    tbname("blog")."` WHERE language='".
+    $_SESSION["language"]."'");
+    $total_entries = mysql_num_rows($count_query);
+
+    $query = mysql_query("SELECT * FROM `".tbname("blog")."` WHERE language='".$_SESSION["language"]."' ORDER by id DESC LIMIT $limit1, $limit2");
+
+
+    $html = "";
+    
+    if(mysql_num_rows($query) > 0){
+          while($post = mysql_fetch_object($query)){
+          $user = getUserById($post->author);    
+          
+          
+          
+          $html.= "<h2 class='blog_headline'><a href='?seite=".get_requested_pagename()."&amp;single=".$post->seo_shortname."'>".$post->title."</a></h2>";
+          $html.= "<hr class='blog_hr'/>";
+          $html.= "<sub><strong>".
+          date(getconfig("date_format"), $post->datum)." - Autor: ". $user["username"].
+          "</strong></sub><br/><br/>";
+          $html.= "<div class='blog_post_content'>".$post->content_preview."</div>";
+       }
+
+
+   $html.= "<br/><div class='page_older_newer'>";
+   
+   
+    if($limit1 > 0){
+                     
+                   
+          
+   $html.= "<a href='?seite=".get_requested_pagename()."&amp;limit=".($limit3)."'>";
+   } 
+   
+   if($_SESSION["language"] == "de"){
+      $html .= "Neuer";
+   
+   }else{
+     $html .= "newer";
+     
+   }   
+   
+   if($limit3 > 0){
+       $html.= "</a>";
+   }
+   
+   $html .= "&nbsp;&nbsp;";
+   
+   
+  
+      $html.= "<a href='?seite=".get_requested_pagename()."&amp;limit=".$limit2."'>";
+   
+   
+   if($_SESSION["language"] == "de"){
+      $html .= "Ältere";
+   
+   }else{
+     $html .= "older";
+     
+   }   
+   
+  
+   
+     $html.= "</a>";
+  
+   
+   
+   
+   $html.= "</div>";
 
    return $html;
+
+}else{
+$html = "<p class='ulicms_error'>";
+
+if($_SESSION["language"] == "de"){
+   $html .= "Es sind keine weiteren Blogeinträge vorhanden";
+   
+}else{
+
+   $html .= "There are no other blog-entries available!";
+}
+
+
+$html.= "</p>";
+
+return $html;
+
+}
+
+
 
 }
 ?>
