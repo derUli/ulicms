63610b63c4dc4addbb24c257c3ceae416c0d9e86
derUli <admin@deruli.de>
2012-11-20 18:03:21 +0100

    Feld für SEO-URL hinzugefügt

--- modules/blog/blog_add.php
+++ modules/blog/blog_add.php
@@ -1,18 +1,33 @@
 <?php 
 function blog_add_form(){
     $html_output = "";
-
+    $all_languages = getAllLanguages();
     $html_output .= "<form action='?seite=".get_requested_pagename()."&blog_admin=submit' method='post'>";
     
     $html_output .= "<table style='width:90%;border:0px;'7>";
+    
     $html_output .= "<tr>";
     $html_output .= "<td>";
     $html_output .= "<strong>Titel dieser News:</strong>";
     $html_output .= "</td>";
     $html_output .= "<td>";
-    $html_output .= "<input name='title' type='text' maxlength=200 size=70>";
+    $html_output .= "<input name='title' type='text' maxlength=200 size=70>
+    <br/><br/>";
     $html_output .= "</td>";
     $html_output .= "</tr>";
+    
+    $html_output .= "<tr>";
+    $html_output .= "<td>";
+    $html_output .= "<strong>SEO-URL:</strong>";
+    $html_output .= "</td>";
+    $html_output .= "<td>";
+    $html_output .= "http://".$_SERVER["SERVER_NAME"].
+    "/pfad/zu/ulicms/".
+    "?seite=".get_requested_pagename().
+    "&single=<input style='font-weight:bold;background-color:rgb(240, 240, 240);border:0px;' name='seo_shortname' type='text' maxlength=200 size=50><br/>";
+    $html_output .= "</td>";
+    $html_output .= "</tr>";
+    
     $html_output .= "<tr>";
     $html_output .= "<td>";
     $html_output .= "</td>";
