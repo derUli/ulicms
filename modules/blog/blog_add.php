ba02abf570dcfbe3f7efe573b407dab0c2c79964
derUli <admin@deruli.de>
2012-11-20 17:50:25 +0100

    Textfeld für News-Titel hinzugefügt

--- modules/blog/blog_add.php
+++ modules/blog/blog_add.php
@@ -4,12 +4,20 @@ function blog_add_form(){
 
     $html_output .= "<form action='?seite=".get_requested_pagename()."&blog_admin=submit' method='post'>";
     
-    $html_output .= "<table>";
+    $html_output .= "<table style='width:90%;border:0px;'7>";
+    $html_output .= "<tr>";
+    $html_output .= "<td>";
+    $html_output .= "<strong>Titel dieser News:</strong>";
+    $html_output .= "</td>";
+    $html_output .= "<td>";
+    $html_output .= "<input name='title' type='text' maxlength=200 size=70>";
+    $html_output .= "</td>";
+    $html_output .= "</tr>";
     $html_output .= "<tr>";
     $html_output .= "<td>";
     $html_output .= "</td>";
     $html_output .= "<td>";
-    $html_output .= "<input type='submit' value='Speichern'>";
+    $html_output .= "<br/><input type='submit' value='Speichern'>";
     $html_output .= "</td>";
     $html_output .= "</tr>";
     
