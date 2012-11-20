fef2f7d2c65ebf7108cfa33a4f71d33f72f0b16c
derUli <admin@deruli.de>
2012-11-20 18:16:45 +0100

    Zeilenabstand / Kommentare aktiviert ja/nein

--- modules/blog/blog_add.php
+++ modules/blog/blog_add.php
@@ -2,17 +2,16 @@
 function blog_add_form(){
     $html_output = "";
     $all_languages = getAllLanguages();
-    $html_output .= "<form action='?seite=".get_requested_pagename()."&blog_admin=submit' method='post'>";
+    $html_output .= "<form action='?seite=".get_requested_pagename()."&blog_admin=submit' method='post' style=''>";
     
-    $html_output .= "<table style='width:90%;border:0px;'7>";
+    $html_output .= "<table class='blog_admin_table' style='width:90%;border:0px;'7>";
     
     $html_output .= "<tr>";
     $html_output .= "<td>";
     $html_output .= "<strong>Titel dieser News:</strong>";
     $html_output .= "</td>";
     $html_output .= "<td>";
-    $html_output .= "<input name='title' type='text' maxlength=200 size=70>
-    <br/><br/>";
+    $html_output .= "<input name='title' type='text' maxlength=200 size=70>";
     $html_output .= "</td>";
     $html_output .= "</tr>";
     
@@ -24,7 +23,7 @@ function blog_add_form(){
     $html_output .= "http://".$_SERVER["SERVER_NAME"].
     "/pfad/zu/ulicms/".
     "?seite=".get_requested_pagename().
-    "&single=<input style='font-weight:bold;background-color:rgb(240, 240, 240);border:0px;' name='seo_shortname' type='text' maxlength=200 size=50><br/>";
+    "&single=<input style='font-weight:bold;background-color:rgb(240, 240, 240);border:0px;' name='seo_shortname' type='text' maxlength=200 size=50>";
     $html_output .= "</td>";
     $html_output .= "</tr>";
     
@@ -46,7 +45,18 @@ function blog_add_form(){
     $html_output .= "</td>";
     $html_output .= "</tr>";
     
-    
+    $html_output .= "<tr>";
+    $html_output .= "<td>";
+    $html_output .= "<strong>Kommentare aktiviert:</strong>";
+    $html_output .= "</td>";
+    $html_output .= "<td>";
+    $html_output .= "
+    <select name='comments_enabled'>
+    <option value='1'>Ja</option>
+    <option value='0'>Nein</option>
+    ";
+    $html_output .= "</td>";
+    $html_output .= "</tr>";    
     
     $html_output .= "<tr>";
     $html_output .= "<td>";
@@ -63,6 +73,12 @@ function blog_add_form(){
     $html_output .= "</table>";
     
     $html_output .= "</form>";
+    
+    $html_output .= "
+    <style type='text/css'>
+    .blog_admin_table tr td{
+    height:50px;
+    }</style>";
     
     
     return $html_output;
