1a5fa28192e807f94e9e1e643518f7c537733043
derUli <admin@deruli.de>
2012-11-20 17:40:03 +0100

    Speichern Button hinzugefügt

--- modules/blog/blog_add.php
+++ modules/blog/blog_add.php
@@ -1,5 +1,26 @@
 <?php 
 function blog_add_form(){
-return "test";
+    $html_output = "";
+
+    $html_output .= "<form action='?seite=".get_requested_pagename()."&blog_admin=submit' method='post'>";
+    
+    $html_output .= "<table>";
+    $html_output .= "<tr>";
+    $html_output .= "<td>";
+    $html_output .= "</td>";
+    $html_output .= "<td>";
+    $html_output .= "<input type='submit' value='Speichern'>";
+    $html_output .= "</td>";
+    $html_output .= "</tr>";
+    
+    
+    $html_output .= "</table>";
+    
+    $html_output .= "</form>";
+    
+    
+    return $html_output;
 }
+
+
 ?>
