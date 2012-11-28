e50ed31875d7a85f56d9583402ab2af30013a4bb
derUli <admin@deruli.de>
2012-11-28 20:30:16 +0100

    Blogmodul Design

--- modules/blog/blog_list.php
+++ modules/blog/blog_list.php
@@ -13,7 +13,7 @@ function blog_list(){
    if($_SESSION["group"] >= 20){
    $html .= "<p><a href='?seite=".
    get_requested_pagename().
-   "&blog_admin=add'>Blogeintrag anlegen</a></p>";
+   "&blog_admin=add'>Blogeintrag anlegen</a></p><br/>";
    }
 
 
