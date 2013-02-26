e166ff94f9f620eccd196ccf22569a5c117cc937
derUli <admin@deruli.de>
2013-02-26 08:14:04 +0100

    Update setzt nun per Default keine Länder mehr auf die Blacklist/Whitelist

--- update.php
+++ update.php
@@ -3,8 +3,8 @@
 require_once "cms-config.php";
 require_once "init.php";
 setconfig("spamfilter_enabled", "yes");
-setconfig("country_blacklist", "cn,br,ru");
-setconfig("country_whitelist", "de,ch,at");
+setconfig("country_blacklist", "");
+setconfig("country_whitelist", "");
 
 
 //@unlink("update.php");
