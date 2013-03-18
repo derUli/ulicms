f723f0e5c1b70e5c83e352dbc7a58df9ec52c1e2
derUli <admin@deruli.de>
2013-03-18 18:38:16 +0100

    CKEditor auf Version 4.0.2 geaddet. Der CKEditor wurde mit CKBuilder generiert. Enthält auch Wortzähler und viel mehr.

--- admin/ckeditor/config.js
+++ admin/ckeditor/config.js
@@ -1,7 +1,7 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
 CKEDITOR.editorConfig = function( config )
 {
