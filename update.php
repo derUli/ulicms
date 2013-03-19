07b48f74d5dec73924bef3693cfa81ad6a74478d
derUli <admin@deruli.de>
2013-03-19 18:31:43 +0100

    Update-Script korrigiert

--- update.php
+++ update.php
@@ -16,9 +16,14 @@ $tpl_unten_filename = "templates/unten.php";
 
 if(file_exists($tpl_unten_filename) and is_writable($tpl_unten_filename)){
 
-   $template_unten = file_get_contents();
-   $template_unten = str_replace('menu("down")', 'menu("bottom")' ,$template_bottom);
-   $template_unten = str_replace("menu('down')", "menu('bottom')" ,$template_bottom);
+   $template_unten = file_get_contents($tpl_unten_filename);
+   $template_unten = str_replace('menu("down")', 'menu("bottom")' ,$template_unten);
+   $template_unten = str_replace("menu('down')", "menu('bottom')" ,$template_unten);
+   
+   
+   $handle = fopen($tpl_unten_filename, "w");
+   fwrite($handle, $template_unten);
+   fclose($handle);
    
    
 }
