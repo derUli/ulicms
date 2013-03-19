bfebf5f940281bd60efde7805c1d6ac9719ef86b
Ulrich Schmidt <u.schmidt@velian.de>
2013-03-19 16:31:34 +0100

    update.php korrigiert menu('down') durch menu('bottom')

--- update.php
+++ update.php
@@ -12,6 +12,18 @@ setconfig("spamfilter_words_blacklist",
 setconfig("empty_trash_days", 30);
 
 
+$tpl_unten_filename = "templates/unten.php";
+
+if(file_exists($tpl_unten_filename) and is_writable($tpl_unten_filename)){
+
+   $template_unten = file_get_contents();
+   $template_unten = str_replace('menu("down")', 'menu("bottom")' ,$template_bottom);
+   $template_unten = str_replace("menu('down')", "menu('bottom')" ,$template_bottom);
+   
+   
+}
+
+
 //@unlink("update.php");
 
 header("Location: admin/");
