6c8f22e28a914f82e3de643e34ade7938d4515cb
derUli <admin@deruli.de>
2013-03-19 21:08:04 +0100

    Newsletter-Modul: Feature "Abonnenten anzeigen" implementiert

--- modules/newsletter/newsletter_admin.php
+++ modules/newsletter/newsletter_admin.php
@@ -7,7 +7,9 @@ if($required_permission === false){
    $required_permission = 20;
 }
 
-define(MODULE_ADMIN_REQUIRED_PERMISSION, $required_permission);
+define("MODULE_ADMIN_REQUIRED_PERMISSION", $required_permission);
+
+define("DATE_FORMAT", getconfig("date_format"));
 
 include getModulePath("newsletter")."newsletter_install.php";
 newsletter_check_install();
@@ -15,14 +17,36 @@ newsletter_check_install();
 
 function newsletter_admin(){
 
-if(!isset($_GET["newsletter_action"])){
 ?>
-<a href="<?php echo getModuleAdminSelfPath()?>&newsletter_action=send_newsletter">Newsletter senden</a>
-<br/>
+<a href="<?php echo getModuleAdminSelfPath()?>&newsletter_action=send_newsletter">Newsletter senden</a> | 
 <a href="<?php echo getModuleAdminSelfPath()?>&newsletter_action=show_subscribers">Abonnenten anzeigen</a>
 <br/>
 <?php
-
+if($_GET["newsletter_action"] == "show_subscribers"){
+   $query = mysql_query("SELECT * FROM ".tbname("newsletter_subscribers"). " ORDER by email");
+   echo "<p>Dieser Newsletter wurde ".mysql_num_rows($query). "x abonniert";
+   if(mysql_num_rows($query) > 0){
+     
+     echo "<table border=1>";
+     echo "<tr style=\"font-weight:bold;\"><td>E-Mail:</td>";
+     echo "<td>Abonnent seit:</td>";
+     echo "</tr>";
+     
+     while($row = mysql_fetch_assoc($query)){
+        echo "<tr>".
+        "<td>".
+        $row["email"].
+        "</td>".
+        "<td>".
+        date(DATE_FORMAT, $row["subscribe_date"]).
+        "</td></tr>";
+             
+     }     
+     
+     echo "</table>";
+   
+   
+   }
 }
 }
  
