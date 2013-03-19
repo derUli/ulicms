b3adc62db32b9753d2a5e6382d7ce1786b1b38de
derUli <admin@deruli.de>
2013-03-19 19:02:08 +0100

    Code refakturiert

--- update.php
+++ update.php
@@ -17,14 +17,14 @@ $tpl_unten_filename = "templates/unten.php";
 if(file_exists($tpl_unten_filename) and is_writable($tpl_unten_filename)){
 
    $template_unten = file_get_contents($tpl_unten_filename);
-   $template_unten = str_replace('menu("down")', 'menu("bottom")' ,$template_unten);
-   $template_unten = str_replace("menu('down')", "menu('bottom')" ,$template_unten);
-   
+   $template_unten = str_replace('menu("down")', 
+   'menu("bottom")' , $template_unten);
+   $template_unten = str_replace("menu('down')", 
+   "menu('bottom')" , $template_unten);
    
    $handle = fopen($tpl_unten_filename, "w");
    fwrite($handle, $template_unten);
    fclose($handle);
-   
    
 }
 
