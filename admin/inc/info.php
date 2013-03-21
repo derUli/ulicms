1ca9b64e06406cd030a99d00650a27fb9c2c86a9
derUli <admin@deruli.de>
2013-03-21 20:32:25 +0100

    Copyright geändert

--- admin/inc/info.php
+++ admin/inc/info.php
@@ -5,8 +5,8 @@
 <br/>
 
 <p>
-<a href="http://www.deruli.de" target="_blank">UliCMS</a> &copy; 2010 - 2013 by Ulrich Schmidt<br/>
-<a href="http://www.ckeditor.com" target="_blank">CKEditor</a> &copy; 2003 - 2012 by CKSource<br/>
+<a href="http://www.deruli.de" target="_blank">UliCMS</a> &copy; 2010 - 2014 by Ulrich Schmidt<br/>
+<a href="http://www.ckeditor.com" target="_blank">CKEditor</a> &copy; 2003 - 2013 by CKSource<br/>
 <a href="http://kcfinder.sunhater.com/" target="_blank">KCFinder</a> Copyright 2010 - 2012 KCFinder Project   <br/ >
 
 <a href="http://codemirror.net/" target="_blank">CodeMirror</a> &copy; 2012 by Marijn Haverbeke</p>
