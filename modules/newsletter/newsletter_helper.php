5a73dbca96dbeddd8304b2e194db7423747056c4
derUli <admin@deruli.de>
2013-03-22 19:31:06 +0100

    send_html_mail Funktion implementiert

--- modules/newsletter/newsletter_helper.php
+++ modules/newsletter/newsletter_helper.php
@@ -16,4 +16,44 @@ function getSubscribers(){
     return $adresses;   
 
 }
+
+
+if(!function_exists("send_html_mail")){
+
+// HTML-Mail senden
+
+
+function send_html_mail($mail_from, $mail_to, 
+                   $subject, $text){
+   $html = "<html>
+    <head>
+        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+        <title>".htmlspecialchars($subject)."</title>
+    </head>
+    <body>
+        $text
+    </body>
+</html>";
+
+   $html = str_replace("\r\n", "\n", $html);
+
+
+   $header  = "MIME-Version: 1.0\n";
+   $header .= "Content-type: text/html; charset=utf-8\n";
+   $header .= "From: $mail_from\n";
+   
+   // $header .= "Reply-To: $replay_to\n";
+   // $header .= "Cc: $cc\n";  // falls an CC gesendet werden soll
+   $header .= "X-Mailer: PHP ". phpversion();
+ 
+   return mail($subject, $betreff, $mailtext, $header);
+
+      
+   
+}
+
+
+}
+
+
 ?>
