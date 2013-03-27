d37faac53f997f958d1ecda8029b42a962707962
derUli <admin@deruli.de>
2013-03-27 23:28:05 +0100

    CKEditor Toolbar korrigiert

--- admin/ckeditor/config.js
+++ admin/ckeditor/config.js
@@ -3,26 +3,22 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	 config.language = 'de';
-	 config.height="400px"
-	 config.width="800px"
-	 config.toolbar = 'cms';
-
-
-
-   if(window.location.href.indexOf("admin/") != -1){
+	 config.height = "500px";
+	 //config.width = "700px";
 	 
+   if(window.location.href.indexOf("admin/") != -1)
+   {
      config.filebrowserBrowseUrl = 'kcfinder/browse.php?type=files';
      config.filebrowserImageBrowseUrl = 'kcfinder/browse.php?type=images';
      config.filebrowserFlashBrowseUrl = 'kcfinder/browse.php?type=flash';
      config.filebrowserUploadUrl = 'kcfinder/upload.php?type=files';
      config.filebrowserImageUploadUrl = 'kcfinder/upload.php?type=images';
      config.filebrowserFlashUploadUrl = 'kcfinder/upload.php?type=flash';
-   } else{
-
+   } else 
+   {
      config.filebrowserBrowseUrl = 'admin/kcfinder/browse.php?type=files';
      config.filebrowserImageBrowseUrl = 'admin/kcfinder/browse.php?type=images';
      config.filebrowserFlashBrowseUrl = 'admin/kcfinder/browse.php?type=flash';
@@ -30,29 +26,6 @@ CKEDITOR.editorConfig = function( config )
      config.filebrowserImageUploadUrl = 'admin/kcfinder/upload.php?type=images';
      config.filebrowserFlashUploadUrl = 'admin/kcfinder/upload.php?type=flash';
    }
-
-
- 
-config.toolbar_cms =
-[
-    ["Source"],
-    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
-    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-    '/',
-    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-    ['BidiLtr', 'BidiRtl'],
-    ['Link','Unlink','Anchor'],
-    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe'],
-    '/',
-    ['Styles','Format','Font','FontSize'],
-    ['TextColor','BGColor'],
-    ['Maximize', 'ShowBlocks']
-];
-
-	 config.uiColor = '#6b94ac';
+   config.entities_latin = false;
+   config.uiColor = '#6b94ac';
 };
-
-CKEDITOR.config.entities_latin = false;
