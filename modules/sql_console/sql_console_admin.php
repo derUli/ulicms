e1fba9bce494440507b225abf6ba7588bdc4d325
Ulrich Schmidt <admin@deruli.de>
2013-04-05 18:11:22 +0200

    sql_console begonnen

---
+++ modules/sql_console/sql_console_admin.php
@@ -0,0 +1,22 @@
+<?php 
+define("MODULE_ADMIN_HEADLINE", "SQL Console");
+
+$required_permission = getconfig("sql_console_required_permission");
+
+if($required_permission === false){
+   $required_permission = 50;
+}
+
+define(MODULE_ADMIN_REQUIRED_PERMISSION, $required_permission);
+
+// Konfiguration checken
+$send_comments_via_email = getconfig("blog_send_comments_via_email") == "yes";
+
+function sql_console_admin(){
+include getModulePath("sql_console")."sql_console_functions.php";
+include getModulePath("sql_console")."sql_console_styles.php";
+include getModulePath("sql_console")."sql_console_form.php";
+}
+
+
+?>
---
+++ modules/sql_console/sql_console_styles.php
@@ -0,0 +1,14 @@
+
+<style type="text/css">
+textarea#sql_code{
+width:98%;
+height:200px;
+margin:auto;
+text-align:left;
+}
+
+form{
+text-align:center;
+}
+
+</style>
