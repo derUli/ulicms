eb23caadf00f3515fe90e8bba805d4883c47a10c
Ulrich Schmidt <admin@deruli.de>
2013-04-26 18:17:49 +0200

    Link zum SS-Feed in head eingefügt

--- modules/blog/blog_head.php
+++ modules/blog/blog_head.php
@@ -3,3 +3,4 @@ if(is_logged_in()){
 ?>
 <script type="text/javascript" src="admin/ckeditor/ckeditor.js"></script>
 <?php }?>
+<link rel="alternate" type="application/rss+xml" title="Blog Newsfeed" href="blog_rss.php" />
