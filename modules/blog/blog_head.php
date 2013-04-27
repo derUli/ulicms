630fe912bf1fd8aaf46b002dc049d8b8314c512f
Ulrich Schmidt <admin@deruli.de>
2013-04-27 16:54:52 +0200

    Unnötiges Leerzeichen entfernt

--- modules/blog/blog_head.php
+++ modules/blog/blog_head.php
@@ -3,4 +3,4 @@ if(is_logged_in()){
 ?>
 <script type="text/javascript" src="admin/ckeditor/ckeditor.js"></script>
 <?php }?>
-<link rel="alternate" type="application/rss+xml" title="Blog Newsfeed" href="blog_rss.php?s=<?php echo get_requested_pagename()?>&lang=<?php echo $_SESSION["language"]?>" />
+<link rel="alternate" type="application/rss+xml" title="Blog Newsfeed" href="blog_rss.php?s=<?php echo get_requested_pagename()?>&lang=<?php echo $_SESSION["language"]?>"/>
