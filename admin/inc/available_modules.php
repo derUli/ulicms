a0339b25a6cd54e825f117bb0c996a96db1cba24
Ulrich Schmidt <admin@deruli.de>
2013-05-02 15:50:03 +0200

    Prüfung ob pkg_src definiert wurde

--- admin/inc/available_modules.php
+++ admin/inc/available_modules.php
@@ -1,7 +1,16 @@
 <?php if(!is_admin()){?>
 <p>Zugriff verweigert</p>
-<?php } else {?>
+<?php } else {
+
+$pkg_src = getconfig("pkg_src");
+
+?>
 
 <h1>Verfügbare Pakete</h1>
+<?php 
+if(!$pkg_src){?>
+<p><strong>Fehler:</strong> <br/> pkg_src wurde nicht definiert!</p>
+<?php }?>
+
 
 <?php }?>
