ae4b2765ebfc150cb2907fcd0b04ac0beab24e68
Ulrich Schmidt <admin@deruli.de>
2013-05-02 15:58:39 +0200

    Ausgabe der Paketliste und Beschreibungen

--- admin/inc/available_modules.php
+++ admin/inc/available_modules.php
@@ -9,8 +9,43 @@ $pkg_src = getconfig("pkg_src");
 <h1>Verfügbare Pakete</h1>
 <?php 
 if(!$pkg_src){?>
-<p><strong>Fehler:</strong> <br/> pkg_src wurde nicht definiert!</p>
-<?php }?>
+<p><strong>Fehler:</strong> <br/>
+pkg_src wurde nicht definiert!</p>
+<?php } else {
+$packageListURL = $pkg_src."list.txt";
+$packageList = @file($packageListURL);
+
+if(!$packageList or count($packageList) === 0){
+?>
+<p><strong>Fehler:</strong> <br/>
+Keine Pakete verfügbar oder Paketquelle nicht erreichbar.</p>
+
+<?php
+} else {
+for($i=0; $i<count($packageList); $i++){
+  $pkg = $packageList[$i];
+  $pkgDescriptionURL = $pkg_src."descriptions/".$pkg.".txt";
+  
+  echo "<p><strong>".$pkg."</strong><br/>";
+  
+  $pkgDescription = @file_get_contents($pkgDescriptionURL);
+  
+  if(!$pkgDescription or strlen($pkgDescription) < 1)
+     echo "Keine Beschreibung verfügbar.";
+  else
+     echo nl2br($pkgDescripiton);
+  
+  
+  echo "</p>";
+
+}
+
+}
+
+?>
 
 
-<?php }?>
+<?php 
+}
+
+}?>
