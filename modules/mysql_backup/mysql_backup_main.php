ba6c9a571972cdbc187a3d410573492a9a6d6e32
Ulrich Schmidt <admin@deruli.de>
2013-05-02 17:17:09 +0200

    mysql_backup Installation entfernt

--- modules/mysql_backup/mysql_backup_main.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include getModulePath("mysql_backup")."mysql_backup_install.php";
-mysql_backup_check_install();
-
-function mysql_backup_render(){  
-   include_once "mysql_backup_cron.php";
-   return "";
-}
-?>
