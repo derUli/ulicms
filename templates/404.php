4a2f7b943391309c2a4d95af1e9258d785eda3ea
derUli <admin@deruli.de>
2013-05-12 00:47:22 +0200

    Theme geandert

---
+++ templates/404.php
@@ -0,0 +1,3 @@
+<h1>Nicht gefunden.</h1>
+<p>Diese Seite existiert leider nicht mehr.
+<img src="templates/schulterzuck.gif" alt=":Ich kann doch auch nichts dafür:"/></p>
--- templates/unten.php
+++ templates/unten.php
@@ -1,6 +1,6 @@
 </div>
 <div id="footer">
-(C) <?php year()?> by <?php homepage_owner()?> | <a href="http://www.ulicms.de" target="_blank">Powered by UliCMS</a>
+<p>(C) <?php year()?> by <?php homepage_owner()?> | <a href="http://www.ulicms.de" target="_blank">Powered by UliCMS</a></p>
 <?php menu("bottom")?>
 </div>
 </div>
