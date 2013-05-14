68b74be4acdea154baf27c524a295282ab8f3052
Ulrich Schmidt <admin@deruli.de>
2013-05-14 21:55:16 +0200

    JQuery Modul

---
+++ modules/jquery/jquery_main.php
@@ -0,0 +1,5 @@
+<?php 
+function jquery_render(){
+   return "";
+}
+?>
---
+++ modules/jquery/post-install.php
@@ -0,0 +1,6 @@
+<p>Variable anlegen (jquery_disabled_on)</p>
+<?php 
+if(!getconfig("jquery_disabled_on")){
+   setconfig("jquery_disabled_on", "");
+}
+?>
