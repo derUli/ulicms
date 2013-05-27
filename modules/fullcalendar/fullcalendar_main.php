cf1e26791e688cc493af78d1dfaf3dafd57ba096
Ulrich Schmidt <admin@deruli.de>
2013-05-27 21:41:59 +0200

    V ersionsummer

--- modules/fullcalendar/fullcalendar_main.php
+++ modules/fullcalendar/fullcalendar_main.php
@@ -1,4 +1,10 @@
 <?php 
+
+
+function fullcalendar_version(){
+   return array(0,0,1);
+}
+
 function fullcalendar_render(){
    return "<div id='loading' style='display:none'>loading...</div>
 <div id='fullcalendar'></div>";
