e5c30d7080c30e46d57253006f0292a1dbb155b3
Ulrich Schmidt <admin@deruli.de>
2013-05-27 19:03:53 +0200

    prefix_events Datenbanktabelle wird wieder angezeigt

--- post-install.php
+++ post-install.php
@@ -1,4 +1,12 @@
 <?php
 // post-install Script für fullcalendar
+mysql_query("CREATE TABLE IF NOT EXISTS `".tbname("events")."` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `start` bigint(20) NOT NULL,
+  `end` bigint(20) NOT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;");
 
 ?>
