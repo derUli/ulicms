f70efb6e5223df12f1ba9d8f52ad8399bdd7845d
Ulrich Schmidt <u.schmidt@velian.de>
2013-05-28 14:36:42 +0200

    utf-8 statt utf8

--- init.php
+++ init.php
@@ -30,7 +30,7 @@ require_once "api.php";
 
 
   if(file_exists("installer/installer.php") or file_exists("../installer/installer.php")){
-     header("Content-type: text/html; charset=utf8");
+     header("Content-Type: text/html; charset=utf-8");
      die("<p>Bitte löschen Sie den Ordner \"installer\" vom Server.<br/>
      Das CMS kann erst betrieben werden, nach dem der Installer gelöscht wurde.
      Dies ist ein Sicherheitsmerkmal von UliCMS.</p>");
