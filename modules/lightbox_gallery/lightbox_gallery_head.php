9f269cd97721c4bd846a3e02c62e82703e7a6c22
Ulrich Schmidt <u.schmidt@velian.de>
2013-05-28 15:49:06 +0200

    lightbox_gallery_0.0.1 package hinzugefügt

--- modules/lightbox_gallery/lightbox_gallery_head.php
+++ modules/lightbox_gallery/lightbox_gallery_head.php
@@ -1,8 +1,8 @@
 <?php 
 if(containsModule(get_requested_pagename(), "lightbox_gallery")) {
 ?>
-<link type="text/css" href="<?php echo getModulePath("lightbox_gallery");?>lightbox/css/lightbox.css" rel="stylesheet" />
 <script type="text/javascript" src="<?php echo getModulePath("lightbox_gallery");?>lightbox/js/lightbox.js"></script>
+<link type="text/css" href="<?php echo getModulePath("lightbox_gallery");?>lightbox/css/lightbox.css" rel="stylesheet" />
 
 <?php 
 }
