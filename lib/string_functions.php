b5f9bc69789177efa3a1a890841c06082b3e3f17
Ulrich Schmidt <admin@deruli.de>
2013-05-30 20:10:44 +0200

    string_functions hinzugefügt

---
+++ lib/string_functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Get excerpt from string
+ * 
+ * @param String $str String to get an excerpt from
+ * @param Integer $startPos Position int string to start excerpt from
+ * @param Integer $maxLength Maximum length the excerpt may be
+ * @return String excerpt
+ */
+function getExcerpt($str, $startPos=0, $maxLength=100) {
+	if(strlen($str) > $maxLength) {
+		$excerpt   = substr($str, $startPos, $maxLength-3);
+		$lastSpace = strrpos($excerpt, ' ');
+		$excerpt   = substr($excerpt, 0, $lastSpace);
+		$excerpt  .= '...';
+	} else {
+		$excerpt = $str;
+	}
+	
+	return $excerpt;
+}
+
+?>
