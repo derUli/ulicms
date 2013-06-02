3a695f0f48465213fcf70a1d7c93312d9e7400dd
Ulrich Schmidt <admin@deruli.de>
2013-06-02 22:43:41 +0200

    update.php alten code entfernt

--- update.php
+++ update.php
@@ -5,62 +5,7 @@ require_once "init.php";
 
 $db_schema_version = getconfig("db_schema_version");
 
-if(!$db_schema_version or $db_schema_version === "6.0"){
-db_query("UPDATE ".tbname("content"). " SET menu='bottom' WHERE menu='down'");
-
-setconfig("spamfilter_words_blacklist", 
-"Casino||Euro Dice||Bingo||Cialis||Viagra||Penis||Enlargement||Drugstore");
-
-setconfig("empty_trash_days", 30);
-
-
-$tpl_unten_filename = "templates/unten.php";
-
-if(file_exists($tpl_unten_filename) and is_writable($tpl_unten_filename)){
-
-   $template_unten = file_get_contents($tpl_unten_filename);
-   $template_unten = str_replace('menu("down")', 
-   'menu("bottom")' , $template_unten);
-   $template_unten = str_replace("menu('down')", 
-   "menu('bottom')" , $template_unten);
-   $template_unten = str_replace("comments()", "", $template_unten);
-   
-   $handle = fopen($tpl_unten_filename, "w");
-   fwrite($handle, $template_unten);
-   fclose($handle);
-   
-   // Dieses Script so lange aufrufen, bis er bei der aktuelle Version angelangt ist
-   header("Location: update.php");
-   exit();
-}
-
-db_query("alter table ".tbname("content")." change `parent` `parent_old` varchar (300);");
-db_query("ALTER TABLE ".tbname("content")." ADD parent int(11);");
-
-$query = db_query("SELECT id, systemname, parent_old FROM ".tbname("content"));
-
-while($row=mysql_fetch_object($query)){
-   
-   if($row->parent_old == "-"){
-      db_query("UPDATE ".tbname("content"). " set parent = NULL where id=".$row->id);
-   } else {
-     $query2 = db_query("SELECT id FROM ".tbname("content")." WHERE systemname='".mysql_real_escape_string($row->parent_old)."'");
-     
-     if(mysql_num_rows($query2) > 0){
-        $results = mysql_fetch_object($query2);
-        db_query("UPDATE ".tbname("content"). " set parent=".$results->id." where id=".$row->id);
-     } else {
-        db_query("UPDATE ".tbname("content"). " set parent = NULL where id=".$row->id);
-     }
-     
-     }
-     // Dieses Script so lange aufrufen, bis er bei der aktuelle Version angelangt ist
-}
-
-// Version des Datenbank-Schemas setzen
-setconfig("db_schema_version", "6.1");
-
-} else if($db_schema_version === "6.1"){
+if($db_schema_version === "6.1"){
 
   // Verbesserung der Systemsicherheit
   // Das Verschlüsselungsverfahren wurde von ungesalzenen MD5
@@ -70,11 +15,13 @@ setconfig("db_schema_version", "6.1");
   
   setconfig("db_schema_version", "6.2");
   @unlink("update.php");
+  
+  header("Location: admin/");
+  exit();
 }
 
 
 
-header("Location: admin/");
-exit();
+
 
 ?>
