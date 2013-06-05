dd12cc1cd7f7990862385d94a92cddd3ab0e7b1b
Ulrich Schmidt <u.schmidt@velian.de>
2013-06-05 15:14:45 +0200

    Update-Skript und news.rft geändert

--- ulicms/update.php
+++ ulicms/update.php
@@ -5,7 +5,60 @@ require_once "init.php";
 
 $db_schema_version = getconfig("db_schema_version");
 
-if($db_schema_version === "6.1"){
+if($db_schema_version === "6.2"){
+   die("Das Update wurde bereits installiert");
+}
+
+mysql_query("UPDATE ".tbname("content"). " SET menu='bottom' WHERE menu='down'");
+
+if(!getconfig("spamfilter_words_blacklist"))
+   setconfig("spamfilter_words_blacklist", 
+   "Casino||Euro Dice||Bingo||Cialis||Viagra||Penis||Enlargement||Drugstore");
+
+if(getconfig("empty_trash_days") === false)
+   setconfig("empty_trash_days", 30);
+
+
+$tpl_unten_filename = "templates/unten.php";
+
+if(file_exists($tpl_unten_filename) and is_writable($tpl_unten_filename)){
+
+   $template_unten = file_get_contents($tpl_unten_filename);
+   $template_unten = str_replace('menu("down")', 
+   'menu("bottom")' , $template_unten);
+   $template_unten = str_replace("menu('down')", 
+   "menu('bottom')" , $template_unten);
+   $template_unten = str_replace("comments()", "", $template_unten);
+   
+   $handle = fopen($tpl_unten_filename, "w");
+   fwrite($handle, $template_unten);
+   fclose($handle);
+   
+}
+
+mysql_query("alter table ".tbname("content")." change `parent` `parent_old` varchar (300);");
+mysql_query("ALTER TABLE ".tbname("content")." ADD parent int(11);");
+
+$query = mysql_query("SELECT id, systemname, parent_old FROM ".tbname("content"));
+
+while($row=mysql_fetch_object($query)){
+   
+   if($row->parent_old == "-"){
+      mysql_query("UPDATE ".tbname("content"). " set parent = NULL where id=".$row->id);
+   } else {
+     $query2 = mysql_query("SELECT id FROM ".tbname("content")." WHERE systemname='".mysql_real_escape_string($row->parent_old)."'");
+     
+     if(mysql_num_rows($query2) > 0){
+        $results = mysql_fetch_object($query2);
+        mysql_query("UPDATE ".tbname("content"). " set parent=".$results->id." where id=".$row->id);
+     } else {
+        mysql_query("UPDATE ".tbname("content"). " set parent = NULL where id=".$row->id);
+     }
+     
+     }
+     
+    
+}
 
   // Verbesserung der Systemsicherheit
   // Das Verschlüsselungsverfahren wurde von ungesalzenen MD5
@@ -18,7 +71,7 @@ if($db_schema_version === "6.1"){
   
   header("Location: admin/");
   exit();
-}
+
 
 
 
