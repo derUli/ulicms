485c53c2d7dd1384bcda69b7871188c7b26a8994
Ulrich Schmidt <admin@deruli.de>
2013-07-06 00:24:21 +0200

    egrgrfg

--- ulicms/cms-config.php
+++ ulicms/cms-config.php
@@ -5,6 +5,6 @@ var $mysql_server="localhost";
 var $mysql_user="root";
 var $mysql_password="";
 var $mysql_database="ulicms";
-var $mysql_prefix="rhrthrtghthrtgfhrtgjuzj_";
+var $mysql_prefix="troll_";
 
 }
