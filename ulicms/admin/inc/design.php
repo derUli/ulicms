278d532b3473003c9f6ff78ab339a0140376dfef
Ulrich Schmidt <admin@deruli.de>
2013-07-06 20:38:24 +0200

    Fonts in Datei ausgelagert, damit man auch eigene Font-Familys hinzufügen kann

--- ulicms/admin/inc/design.php
+++ ulicms/admin/inc/design.php
@@ -57,25 +57,8 @@ if(getconfig("body-background-color")
 } // if submit zu
 
 $allThemes = getThemesList();
-$fonts = Array();
-$fonts["Times New Roman"] = "Times, Times New Roman, serif";
-$fonts["Georgia"] = "Georgia";
-$fonts["Sans Serif"] = "sans-serif";
-$fonts["Arial"] = "arial";
-$fonts["Comic Sans MS"] = "Comic Sans MS";
-$fonts["Helvetica"] = "helvetica";
-$fonts["Tahoma"] = "Tahoma";
-$fonts["Verdana"] = "";
-$fonts["Lucida Sans Unicode"] = "'Lucida Sans Unicode'";
-$fonts["Trebuchet MS"] = "'Trebuchet MS'";
-$fonts["Lucida Sans"] = "'Lucida Sans'";
-$fonts["monospace"] = "monospace";
-$fonts["Courier"] = "Courier";
-$fonts["Courier New"] = "'Courier New', Courier";
-$fonts["Lucida Console"] = "'Lucida Console'";
-$fonts["fantasy"] = "fantasy";
-$fonts["cursive"] = "cursive";
-ksort($fonts);
+include_once "inc/fonts.php";
+$fonts = getFontFamilys();
 
 
 
