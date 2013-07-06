6be91b03d60ec683255b448dea1fe70c86b1b24d
Ulrich Schmidt <admin@deruli.de>
2013-07-06 15:52:57 +0200

    footer

--- ulicms/admin/inc/footer.php
+++ ulicms/admin/inc/footer.php
@@ -1,6 +1,6 @@
 </div>
 <br/>
 <hr/>
-<p>&copy; 2011 - 2013 by <a href="http://www.ulicms.de" target="_blank">UliCMS</a>
+<p id="footer">&copy; 2011 - 2013 by <a href="http://www.ulicms.de" target="_blank">UliCMS</a>
 </body>
 </html>
