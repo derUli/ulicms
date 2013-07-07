cfbea6c62c77fc687c6ad93610e135a013d853d1
Ulrich Schmidt <admin@deruli.de>
2013-07-07 22:38:11 +0200

    Schönere Animation

--- ulicms/admin/inc/available_modules.php
+++ ulicms/admin/inc/available_modules.php
@@ -3,15 +3,18 @@
 <?php } else {
 ?>
 <h1>Verfügbare Pakete</h1>
-<div id="pkglist">
+<div id="loadpkg">
 <img style="margin-right:15px;float:left;" src="gfx/loading.gif" alt="Bitte warten..."> <div style="padding-top:3px;">Daten warten geladen...</div>
 </div>
+<div id="pkglist"></div>
 
 <script type="text/javascript">
 $(window).load(function(){
 
 $.get("index.php?ajax_cmd=available_modules", function(result){
+   $("div#loadpkg").slideUp();
    $("div#pkglist").html(result);
+   $("div#pkglist").slideDown();
 });
 
 });
