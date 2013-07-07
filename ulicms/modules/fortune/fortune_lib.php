b11c2170653ac5fcf7043e787120245de3523ae7
Ulrich Schmidt <admin@deruli.de>
2013-07-08 00:47:32 +0200

    binärdateien gelöscht

--- ulicms/modules/fortune/fortune_lib.php
+++ ulicms/modules/fortune/fortune_lib.php
@@ -5,9 +5,10 @@ function getRandomFortune(){
    do{
    $file = array_rand($fortuneFiles, 1);
    $file = $fortuneFiles[$file];
+   $file = $fortuneDir.$file;
    }while(!is_file($file));
    
-   $fileContent = file_get_contents($fortuneDir.$file);
+   $fileContent = file_get_contents($file);
    $fileContent = trim($fileContent);
    $fileContent = utf8_encode($fileContent);
    $fortunes = explode("%\n", $fileContent);
