5602c38cb6cefa3333843ce4b7a9c782ab6409cb
Ulrich Schmidt <admin@deruli.de>
2013-07-08 08:00:04 +0200

    unnötige Dateien entfernt & .gitignore geändert

--- ulicms/templates/responsive/404.php
+++
@@ -1,3 +0,0 @@
-<h1>Nicht gefunden.</h1>
-<p>Diese Seite existiert leider nicht mehr.
-<img src="<?php echo getTemplateDirPath("responsive");?>schulterzuck.gif" alt=":Ich kann doch auch nichts dafür:"/></p>
