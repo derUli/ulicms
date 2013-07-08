b7b35bbb8ed9327d051875179449e7b66a3b5c16
Ulrich Schmidt <admin@deruli.de>
2013-07-08 21:32:01 +0200

    post-install

--- ulicms/post-install.php
+++ ulicms/post-install.php
@@ -1,12 +1,10 @@
 <?php 
 @mkdir(getModulePath("rss2blog")."etc/", $recursive = true);
-
 if(!is_file(getModulePath("rss2blog")."etc/"."sources.ini")){
   file_put_contents(getModulePath("rss2blog")."etc/sources.ini", 
   "# Hier die URLs zu den RSS-Quellen eintragen.\r\n");
 }
 
 setconfig("rss2blog_bot_user_id", "1");
-
 db_query("ALTER TABLE `".tbname("blog")."` ADD TEXT `src_link` TEXT;")
 ?>
