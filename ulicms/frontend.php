a3174a2eaf2dfd134c1f5c345adc075564934a3e
Ulrich Schmidt <u.schmidt@velian.de>
2013-07-08 11:48:39 +0200

    before_maintenance_message und after_maintenance_message Hook hinzugefügt

--- ulicms/frontend.php
+++ ulicms/frontend.php
@@ -35,7 +35,9 @@ if(getconfig("redirection")!=""&&getconfig("redirection")!=false){
 $theme = getconfig("theme");
 
 if(strtolower(getconfig("maintenance_mode"))=="on"||strtolower(getconfig("maintenance_mode"))=="true"||getconfig("maintenance_mode")=="1"){
+  add_hook("before_maintenance_message");
   require_once getTemplateDirPath($theme)."maintenance.php";
+    add_hook("after_maintenance_message");
   die();
 }
 
