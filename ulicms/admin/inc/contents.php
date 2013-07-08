b551b6d6308e8ef53d267a76617160dd98fdd0a6
Ulrich Schmidt <u.schmidt@velian.de>
2013-07-08 11:35:26 +0200

    <br/> durch <p> ersetzt und content_type_list_entry Hook hinzugefügt

--- ulicms/admin/inc/contents.php
+++ ulicms/admin/inc/contents.php
@@ -2,9 +2,10 @@
 ?>
 
 <h2>Inhalte</h2>
-<strong>Bitte wählen Sie einen Inhaltstyp aus:</strong><br/>
-<a href="index.php?action=pages">Seiten</a><br/>
-<a href="index.php?action=banner">Werbebanner</a>  <br/>
+<p><strong>Bitte wählen Sie einen Inhaltstyp aus:</strong></p>
+<p><a href="index.php?action=pages">Seiten</a></p>
+<p><a href="index.php?action=banner">Werbebanner</a></p>
+<?php add_hook("content_type_list_entry");?>
 
 
 
