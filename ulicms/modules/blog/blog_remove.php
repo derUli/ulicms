e9efa741fcfe9d3737cd8f1d6b1ef1278050a130
Ulrich Schmidt <admin@deruli.de>
2013-07-08 20:48:57 +0200

    Aktueller Stand

---
+++ ulicms/modules/blog/blog_remove.php
@@ -0,0 +1,22 @@
+<?php 
+function blog_remove_post($post_id){
+    if($_SESSION["group"] >= 20) {
+	   db_query("DELETE FROM `".tbname("blog")."` WHERE id = $post_id");
+	   return "<p>Der Blogpost wurde erfolgreich gelöscht!</p>";
+	} else{
+	   return "<p>Zugriff verweigert!</p>";
+	}
+}
+
+
+function blog_remove_comment($post_id){
+    if($_SESSION["group"] >= 40) {
+	   db_query("DELETE FROM `".tbname("blog_comments")."` WHERE id = $post_id");
+	   return "<script type='text/javascript'>
+	   history.back();
+	   </script>";
+	} else{
+	   return "<p>Zugriff verweigert</p>";
+	}
+}
+?>
--- ulicms/modules/rss2blog/rss2blog_cron.php
+++ ulicms/modules/rss2blog/rss2blog_cron.php
@@ -1,5 +1,36 @@
 <?php 
-echo "test";
+
+
+ function rsstotime($rss_time) {
+        $day = substr($rss_time, 5, 2);
+        $month = substr($rss_time, 8, 3);
+        $month = date('m', strtotime("$month 1 2011"));
+        $year = substr($rss_time, 12, 4);
+        $hour = substr($rss_time, 17, 2);
+        $min = substr($rss_time, 20, 2);
+        $second = substr($rss_time, 23, 2);
+        $timezone = substr($rss_time, 26);
+
+        $timestamp = mktime($hour, $min, $second, $month, $day, $year);
+
+        date_default_timezone_set('UTC');
+
+        if(is_numeric($timezone)) {
+            $hours_mod = $mins_mod = 0;
+            $modifier = substr($timezone, 0, 1);
+            $hours_mod = (int) substr($timezone, 1, 2);
+            $mins_mod = (int) substr($timezone, 3, 2);
+            $hour_label = $hours_mod>1 ? 'hours' : 'hour';
+            $strtotimearg = $modifier.$hours_mod.' '.$hour_label;
+            if($mins_mod) {
+                $mins_label = $mins_mod>1 ? 'minutes' : 'minute';
+                $strtotimearg .= ' '.$mins_mod.' '.$mins_label;
+            }
+            $timestamp = strtotime($strtotimearg, $timestamp);
+        }
+
+        return $timestamp;
+}
 
 @include_once "lib/file_get_contents_wrapper.php";
 $srclist = getModulePath("rss2blog")."etc/sources.ini";
@@ -37,7 +68,17 @@ for($i=0; $i < count($srclist); $i++){
                $title = $article["title"];
                $link = $article["link"];
                $description = $article["description"];
-               $pubDate = time();
+               $pubDate = rsstotime($article["pubDate"]);
+               $query = db_query("SELECT * FROM ".tbname("blog"). " WHERE `src_link` = '".mysql_real_escape_string($link)."'");
+               
+
+ 
+               if(mysql_num_rows($query) == 0) {
+  
+                               
+               }
+                
+                                              
                
             }
       }
@@ -46,4 +87,7 @@ for($i=0; $i < count($srclist); $i++){
      
   }
 }
+
+               
+               
 ?>
