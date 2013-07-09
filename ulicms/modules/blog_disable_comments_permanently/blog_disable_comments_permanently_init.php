f7a3679722baebcbf487ea961753bdad947a2e69
Ulrich Schmidt <admin@deruli.de>
2013-07-09 20:45:35 +0200

    Modulordner bereinigt

--- ulicms/modules/blog_disable_comments_permanently/blog_disable_comments_permanently_init.php
+++
@@ -1,3 +0,0 @@
-<?php 
-if(in_array("blog", getAllModules()))
-   db_query("UPDATE ".tbname("blog"). " SET comments_enabled = 0 WHERE comments_enabled = 1");
--- ulicms/post-install.php
+++
@@ -1,7 +0,0 @@
-<?php
-db_query("CREATE TABLE IF NOT EXISTS `".tbname("search_subjects")."` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `subject` text CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
-?>
