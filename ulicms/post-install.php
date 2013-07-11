dad36bd605ac9c3499f7b01ea0e8d1e67fd48954
Ulrich Schmidt <admin@deruli.de>
2013-07-11 02:00:04 +0200

    post-install script angelegt

---
+++ ulicms/post-install.php
@@ -0,0 +1,6 @@
+<?php 
+setconfig("blog2twitter_consumer_key", "XXXXXXXXXXXXXXXXXXX");
+setconfig("blog2twitter_consumer_secret", "XXXXXXXXXXXXXXXXXXX");
+setconfig("blog2twitter_access_token", "XXXXXXXXXXXXXXXXXXX");
+setconfig("blog2twitter_access_token_secret", "XXXXXXXXXXXXXXXXXXX");
+?>
