423b6d2ef4de8eed33f9b09d86ef562ad21cc092
Ulrich Schmidt <admin@deruli.de>
2013-07-11 08:13:19 +0200

    oembed

--- ulicms/modules/oembed/oembed_content_filter.php
+++ ulicms/modules/oembed/oembed_content_filter.php
@@ -2,7 +2,12 @@
 function oembed_content_filter($content){
    include_once getModulePath("oembed")."class_oembed.php";
    $oembed = new OEmbed(); 
-   $content = $oembed->getHTML($content, array('width' => 300));
+   preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $content, $links);
+   for($t=0; $t < count($links); $t++){
+        $html = $oembed->getHTML($links[$t][0], array('width' => 300));
+        $content = str_replace($links[$i][0], $html, $content);
+   }
+   
    return $content;
 
 }
