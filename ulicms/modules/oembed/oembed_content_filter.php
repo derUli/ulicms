ebd9bdae48779ed9327d688ff2a329cbe69f5cd1
Ulrich Schmidt <admin@deruli.de>
2013-07-11 08:21:37 +0200

    args array

--- ulicms/modules/oembed/oembed_content_filter.php
+++ ulicms/modules/oembed/oembed_content_filter.php
@@ -3,18 +3,20 @@ function oembed_content_filter($content){
    include_once getModulePath("oembed")."class_oembed.php";
    $oembed = new OEmbed(); 
    
-   $oembed_width = getconfig("oembed_width");
-   if(!$oembed_width )
-      $oembed_width = 400;
+   $args = array();   
+   
+   $oembed_width = getconfig($oembed_width);
+   if($oembed_width )
+      $args["width"] = $oembed_width;
       
    
-   $oembed_height = getconfig("oembed_height");
-   if(!$oembed_height )
-      $oembed_height = 900;
+   $oembed_height = getconfig($oembed_height);
+   if($oembed_height )
+      $args["height"] = $oembed_height;
    
    preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $content, $links);
    for($t=0; $t < count($links); $t++){
-        $html = $oembed->getHTML($links[$t][0], array('width' => $oembed_width, 'height' => $oembed_height));
+        $html = $oembed->getHTML($links[$t][0], $args);
 
         $content = str_replace($links[$t][0], $html, $content);
    }
