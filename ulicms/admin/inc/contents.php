693302afc8394229ce537d9b52493fdd1a09059d
Ulrich Schmidt <admin@deruli.de>
2013-07-11 22:35:33 +0200

    unnötige Zeilenumbrüche bei contents entfernt

--- ulicms/admin/inc/contents.php
+++ ulicms/admin/inc/contents.php
@@ -2,9 +2,9 @@
 ?>
 
 <h2>Inhalte</h2>
-<p><strong>Bitte wählen Sie einen Inhaltstyp aus:</strong></p>
-<p><a href="index.php?action=pages">Seiten</a></p>
-<p><a href="index.php?action=banner">Werbebanner</a></p>
+<p><strong>Bitte wählen Sie einen Inhaltstyp aus:</strong><br/>
+<a href="index.php?action=pages">Seiten</a><br/>
+<a href="index.php?action=banner">Werbebanner</a></p>
 <?php add_hook("content_type_list_entry");?>
 
 
