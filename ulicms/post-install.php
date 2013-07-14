f611ae096ecdcf78739f4e2133e54ce77ddea7b1
derUli <admin@deruli.de>
2013-07-14 18:43:31 +0200

    post-install Script aendert auch den Datentyp der Blog-Tabelle

--- ulicms/post-install.php
+++ ulicms/post-install.php
@@ -3,6 +3,7 @@
 db_query("ALTER TABLE ".tbname("content")." ADD fulltext(systemname, title, content, meta_description, meta_keywords)");
 
 if(in_array("blog", getAllModules())){
+   db_query("ALTER TABLE ".tbname("blog")." ENGINE=MyISAM;");
    db_query("ALTER TABLE ".tbname("blog")." ADD fulltext(seo_shortname, title, content_full, content_preview)");
 }
 
