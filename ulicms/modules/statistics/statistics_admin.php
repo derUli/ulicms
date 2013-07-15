bcea97eee818a1a9d00a8dc8bcfeefe469bd5a1d
derUli <admin@deruli.de>
2013-07-15 10:46:19 +0200

    Statistiken erweitert

--- ulicms/modules/statistics/statistics_admin.php
+++ ulicms/modules/statistics/statistics_admin.php
@@ -9,8 +9,84 @@ if($required_permission === false){
 
 define(MODULE_ADMIN_REQUIRED_PERMISSION, $required_permission);
 
-function fullcalendar_admin(){
 
+
+function statistics_admin(){
+  if(!setlocale(LC_TIME, "de_DE")){
+     if(!setlocale(LC_TIME, "de_DE.utf8")){
+	   setlocale(LC_TIME, "deu");
+	 }
+  }
+
+  $data = db_query("SELECT * FROM ".tbname("statistics"));
+
+  $visitor_total = mysql_num_rows($data);
+
+  $views_total = 0;
+
+  $gestern  = mktime(0, 0, 0, date("m"), date("d")-1, date("Y"));
+  $heute = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+  $morgen = mktime(0, 0, 0, date("m"), date("d")+1, date("Y"));
+
+  $j = date('Y');
+  $m = date('m');
+  $d = date('d');
+  $monatserster = mktime(0,0,0,$m,1,$j);
+  $monatsletzter = mktime(0,0,0,$m+1,0,$j);
+
+  $visitors_today = 0;
+  $visitors_yesterday = 0;
+  $visitors_month = 0;
+
+  while($row = mysql_fetch_object($data)){
+     $views_total += $row->views;
+     if($row->date >= $heute and $row->date < $morgen){
+        $visitors_today += 1;
+     }
+   
+     if($row->date >= $gestern and $row->date < $heute){
+       $visitors_yesterday += 1;
+     }
+   
+     if($row->date >= monatserster and $row->date < $monatsletzter){
+       $visitors_month  += 1;
+      }
+  }
+
+
+
+
+?>
+<table>
+<tr>
+<td style="width:200px;">
+<strong>Besucher gesamt:</strong></td>
+<td style="text-align:right;"><?php echo intval($visitor_total);?>
+</td>
+</tr>
+<tr>
+<td style="width:200px;">
+<strong>Besucher heute:</strong></td>
+<td style="text-align:right;"><?php echo intval($visitors_today);?>
+</td>
+</tr><tr>
+<td style="width:200px;">
+<strong>Besucher gestern:</strong></td>
+<td style="text-align:right;"><?php echo intval($visitors_yesterday);?>
+</td>
+</tr><tr>
+<td style="width:200px;">
+<strong>Besucher im <?php echo strftime("%B", $monatserster);?>:</strong></td>
+<td style="text-align:right;"><?php echo intval($visitors_yesterday);?>
+</td>
+</tr>
+<td style="width:200px;">
+<strong>Views gesamt:</strong></td>
+<td style="text-align:right;"><?php echo intval($views_total);?>
+</td>
+</tr>
+</table>
+<?php
 
 }
  
--- ulicms/modules/statistics/statistics_init.php
+++ ulicms/modules/statistics/statistics_init.php
@@ -1,16 +1,16 @@
 <?php 
-if(!logged_in()){
+if(!logged_in() and !is_admin_dir()){
 
-$visitorHash = md5($_SERVER["REMOTE_ADDR"].$_SERVER["HTTP_USER_AGENT"]);
+   $visitorHash = md5($_SERVER["REMOTE_ADDR"].$_SERVER["HTTP_USER_AGENT"]);
 
-$query = db_query("SELECT * FROM ".tbname("statistics"). " WHERE hash='$visitorHash'");
+   $query = db_query("SELECT * FROM ".tbname("statistics"). " WHERE hash='$visitorHash'");
 
-if(mysql_num_rows($query) > 0){
-   db_query("UPDATE ".tbname("statistics"). " SET `date` = ".time().", `views` = `views` + 1 WHERE hash ='$visitorHash'");
-} else {
-   db_query("INSERT INTO ".tbname("statistics"). " (hash, date, `views`) VALUES ('$visitorHash',
-   ".time().", 1)");
-}
+   if(mysql_num_rows($query) > 0){
+     db_query("UPDATE ".tbname("statistics"). " SET `date` = ".time().", `views` = `views` + 1 WHERE hash ='$visitorHash'");
+   } else {
+     db_query("INSERT INTO ".tbname("statistics"). " (hash, date, `views`) VALUES ('$visitorHash',
+     ".time().", 1)");
+   }
 
 }
 
