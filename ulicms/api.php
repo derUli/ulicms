e32bd54e971ca83f362207aed81942ba60dcbad5
derUli <admin@deruli.de>
2013-07-16 12:51:59 +0200

    cleanString() Methode zu api.php hinzuefuegt

--- ulicms/api.php
+++ ulicms/api.php
@@ -103,6 +103,25 @@ function getThemesList(){
      return $themes;
      }
 
+  // Make title url safe
+  if(!function_exists("cleanString")){
+function cleanString($string, $separator = '-'){
+	$accents = array('Š' => 'S', 'š' => 's', 'Ð' => 'Dj','Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss','à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y', 'ƒ' => 'f', 'Ä' => 'Ae', 'ä' => 'ae', 
+	'Ö' => 'Oe', 'ö' => 'oe', 'Ü' => 'Ue', 'ü' => 'ue', 'ß' => 'ss'
+	);
+	$string = strtr($string, $accents);
+	$string = strtolower($string);
+	$string = preg_replace('/[^a-zA-Z0-9\s]/', '', $string);
+	$string = preg_replace('{ +}', ' ', $string);
+	$string = trim($string);
+	$string = str_replace(' ', $separator, $string);
+ 
+	return $string;
+}
+
+}
+
+
 function getTemplateDirPath($sub = "default"){
      if(is_admin_dir())
          $templateDir = "../templates/";
