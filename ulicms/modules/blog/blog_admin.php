1db4480e60f35f78d88156816f3a388e6cd8d5f2
derUli <admin@deruli.de>
2013-07-26 18:55:34 +0200

    Option fuer blog_feed_max_items

--- ulicms/modules/blog/blog_admin.php
+++ ulicms/modules/blog/blog_admin.php
@@ -9,31 +9,42 @@ if($required_permission === false){
 
 define(MODULE_ADMIN_REQUIRED_PERMISSION, $required_permission);
 
+
+function blog_admin(){
+
 if(isset($_POST["submit"])){
 
    if($_POST["blog_send_comments_via_email"] == "yes"){
       setconfig("blog_send_comments_via_email", "yes");
    } else{
       setconfig("blog_send_comments_via_email", "no");
-}
+   }
+   if(intval($_POST["blog_feed_max_items"])> 0){
+      setconfig("blog_feed_max_items", intval($_POST["blog_feed_max_items"]));
+   }
 
 }
 
 // Konfiguration checken
 $send_comments_via_email = getconfig("blog_send_comments_via_email") == "yes";
 
-function blog_admin(){
+$blog_feed_max_items = getconfig("blog_feed_max_items");
+if($blog_feed_max_items === false){
+   setconfig("blog_feed_max_items", "10");
+   $blog_feed_max_items = "10";
+}
+
 ?>
 
 <form action="<?php echo getModuleAdminSelfPath()?>" method="post">
-<input type="checkbox" name="blog_send_comments_via_email" value="yes"
+<p><input type="checkbox" name="blog_send_comments_via_email" value="yes"
 <?php if(getconfig("blog_send_comments_via_email") == "yes"){
 echo " checked";
-}?>/> Über neue Kommentare per E-Mail benachrichtigen
-<br/>
-<br/>
+}?>/> Über neue Kommentare per E-Mail benachrichtigen</p>
+<p><input type="number" name="blog_feed_max_items" size=3 maxlength=3 min="5" max="100" value="<?php echo $blog_feed_max_items;?>"/> maximale Anzahl der Einträge im Newsfeed
+</p>
 
-<input type="submit" name="submit" value="Einstellungen speichern"/>
+<p><input type="submit" name="submit" value="Einstellungen speichern"/></p>
 </form>
 <?php
 }
