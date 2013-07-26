c520b90f3141c9182cf45c42a9efe2371617794b
derUli <admin@deruli.de>
2013-07-26 23:23:36 +0200

    last-modified Header wird vom Blogmodul gesendet

---
+++ ulicms/modules/blog/blog_before_html.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET["single"])){
+   $single = mysql_real_escape_string($_GET["single"]);
+   $query = db_query("SELECT * FROM `".tbname("blog")."` WHERE seo_shortname='$single'");
+   
+   if($query){
+  
+   if(mysql_num_rows($query) > 0){
+      $result = mysql_fetch_assoc($query);
+      $datum = $result["datum"];
+   
+ 
+   if(containsModule(get_requested_pagename(), "blog"))
+      header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $datum) . ' GMT' ); 
+   
+   }
+   }
+}
+?>
