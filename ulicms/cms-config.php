15384f493e3c6dfd83f4bcc4b66b68483a0c22be
Ulrich Schmidt <admin@deruli.de>
2013-08-13 00:37:22 +0200

    cms-config bereinigt

--- ulicms/cms-config.php
+++ ulicms/cms-config.php
@@ -1,10 +1,10 @@
 <?php 
 class config{
 
-var $mysql_server="localhost";
-var $mysql_user="admin";
+var $mysql_server="";
+var $mysql_user="";
 var $mysql_password="";
-var $mysql_database="test";
-var $mysql_prefix="blq";
+var $mysql_database="";
+var $mysql_prefix="";
 
 }
