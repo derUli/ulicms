0d008421418996602e8df3b5494aa812e339864e
Ulrich Schmidt <u.schmidt@velian.de>
2013-08-30 11:29:21 +0200

    neues Feature: Alle Einträge aus Menü X in Menü Y verschieben

--- ulicms/admin/inc/other_settings.php
+++ ulicms/admin/inc/other_settings.php
@@ -7,9 +7,15 @@ if(isset($_POST["submit"])){
   if(isset($_POST["mailer"])){
      setconfig("mailer", mysql_real_escape_string($_POST["mailer"]));
   }
+  
+  if($_POST["move_from"] != "-" and $_POST["move_to"] != "-" ){
+     db_query("UPDATE ".tbname("content"). " SET menu='".mysql_real_escape_string($_POST["move_to"])."' WHERE menu='".mysql_real_escape_string($_POST["move_from"])."'");
+  }
 }
 
 $mailer = getconfig("mailer");
+
+$menus = getAllMenus();
 
 ?>
 <h1>Sonstiges</h1>
@@ -19,6 +25,23 @@ $mailer = getconfig("mailer");
 <option value="php-mail"<?php if($mailer == "php-mail"){ echo " selected"; }?>>PHP Mail</option>
 </select>
 </div>
+<h2>Menüeinträge verschieben</h2>
+<p>Verschiebe alle Menüeinträge von <select name="move_from" size="1">
+                                    <option value="-" selected>-</option>
+                                    <?php foreach ($menus as $menu){
+                                    ?>
+                                    <option value="<?php echo $menu?>"><?php echo $menu?></option>
+                                    <?php
+                                    }?>
+                                    </select> nach <select name="move_to" size="1">
+                                    <option value="-" selected>-</option>
+                                    <?php foreach ($menus as $menu){
+                                    ?>
+                                    <option value="<?php echo $menu?>"><?php echo $menu?></option>
+                                    <?php
+                                    }?>
+                                    </select> 
+                                    </p>
 <div class="spacer"></div>
 <input name="submit" type="submit" value="Einstellungen speichern"/>
 </form>
