d674562a264dc455bd796dda5a96f521ca81446e
Ulrich Schmidt <admin@deruli.de>
2013-10-23 00:04:43 +0200

    Carbon removed

--- ulicms/modules/Carbon/Carbon_main.php
+++
@@ -1,5 +0,0 @@
-<?php 
-function carbon_render(){
-  return "<a href=\"https://github.com/briannesbitt/Carbon\">Carbon Homepage</a>";
-}
-?>
--- ulicms/modules/ubench/ubench_main.php
+++
@@ -1,5 +0,0 @@
-<?php 
-function ubench_render(){
-   return "<p>ubench Example Usage:</p>".highlight_file ( getModulePath("ubench")."example.txt", true);
-}
-?>
