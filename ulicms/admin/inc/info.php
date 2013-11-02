89d5263046244d4a5c3d23cc135aa969f21caa3a
derUli <admin@deruli.de>
2013-11-02 23:30:46 +0100

    Infoseite geaendert

--- ulicms/admin/inc/info.php
+++ ulicms/admin/inc/info.php
@@ -12,7 +12,7 @@
 <a href="http://www.deruli.de" target="_blank">UliCMS</a> &copy; 2010 - 2014 by Ulrich Schmidt<br/>
 <a href="http://www.ckeditor.com" target="_blank">CKEditor</a> &copy; 2003 - 2013 by CKSource<br/>
 <a href="http://kcfinder.sunhater.com/" target="_blank">KCFinder</a> Copyright 2010 - 2012 KCFinder Project   <br/ >
-<a href="http://jquery.org" target="_blank">jQuery</a> (c) 2005, 2012 jQuery Foundation, Inc. | jquery.org/license
+<a href="http://jquery.org" target="_blank">jQuery</a> (c) 2005, 2012 jQuery Foundation, Inc.
 <br/>
 
 <a href="http://codemirror.net/" target="_blank">CodeMirror</a> &copy; 2012 by Marijn Haverbeke</p>
