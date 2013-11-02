3b540082cb780d74e9f60b75b8151d8bec4d3e79
derUli <admin@deruli.de>
2013-11-02 19:23:09 +0100

    Infoseite erweitert

--- ulicms/admin/inc/info.php
+++ ulicms/admin/inc/info.php
@@ -12,9 +12,48 @@
 <a href="http://www.deruli.de" target="_blank">UliCMS</a> &copy; 2010 - 2014 by Ulrich Schmidt<br/>
 <a href="http://www.ckeditor.com" target="_blank">CKEditor</a> &copy; 2003 - 2013 by CKSource<br/>
 <a href="http://kcfinder.sunhater.com/" target="_blank">KCFinder</a> Copyright 2010 - 2012 KCFinder Project   <br/ >
+<a href="http://jquery.org" target="_blank">jQuery</a> (c) 2005, 2012 jQuery Foundation, Inc. | jquery.org/license
+<br/>
 
 <a href="http://codemirror.net/" target="_blank">CodeMirror</a> &copy; 2012 by Marijn Haverbeke</p>
 </p>
+<br/>
+<table>
+<tr>
+<td style="width:200px;">
+<strong>PHP-Version: </strong></td>
+<td>
+<?php
+echo phpversion();
+?>
+</td>
+</tr>
+<tr>
+<td style="width:200px;">
+<strong>MySQL-Version: </strong></td>
+<td>
+<?php
+echo phpversion();
+?>
+</td>
+</tr>
+<?php 
+if(function_exists("gd_info")){
+?>
+<tr>
+<td style="width:200px;">
+<strong>libgd Version: </strong></td>
+<td>
+<?php
+$info = gd_info();
+echo $info["GD Version"];
+?>
+</td>
+</tr>
+<?php }?>
+</table>
+
+<br/>
 <br/>
 
 <input type="button" value="UliCMS Portal" onclick='window.open("http://www.ulicms.de");'/>
@@ -23,6 +62,8 @@
 
 <input type="button" value="Feedback" onclick='location.replace("http://www.ulicms.de/?seite=kontakt");'>
 <br/>
+
+
 
 <?php }
 ?>
