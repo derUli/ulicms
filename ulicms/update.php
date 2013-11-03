0fc3c40886beb2331c4126649cd19decdf9ec8fd
Ulrich Schmidt <admin@deruli.de>
2013-11-03 19:41:54 +0100

    update-Skript erhöht db_schema_version

--- ulicms/update.php
+++ ulicms/update.php
@@ -6,6 +6,8 @@ setconfig("cache_type", "file");
 setconfig("registered_user_default_level", "10");
 setconfig("override_shortcuts", "backend");
 
+setconfig("db_schema_version", "6.7");
+
 // Das Script versucht sich selbst zu löschen
 @unlink("update.php");
 
