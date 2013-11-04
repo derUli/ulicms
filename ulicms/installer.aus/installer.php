ef78690c78cf764043de920bcfcadbb9d9692a6a
Ulrich Schmidt <u.schmidt@velian.de>
2013-11-04 16:37:57 +0100

    Installer prüfung auf mysqli

--- ulicms/installer.aus/installer.php
+++ ulicms/installer.aus/installer.php
@@ -42,18 +42,28 @@ Setzen Sie diese Version bitte nicht produktiv ein!<br/>
 <h3>So müssen die Berechtigungen gesetzt sein</h3>
 <p><img src="media/chmod_02.png" alt="FTP Rechtevergabe" title="FTP Rechtevergabe" border=1/></p>
 <br/>
-<br/>
-<form action="index.php" method="post">
-<input type="hidden" name="step" value="1">
-<input type="submit" value="Weiter">
-</form>
-<br/>
 <?php 
 if (!function_exists('gd_info')) {
 ?>
 <p style="color:red;">php5-gd ist nicht installiert. Ohne php5-gd lässt sich UliCMS zwar installieren, jedoch wird der Dateimanager und die Verarbeitung von Grafikdateien nicht funktionieren.</p>
 
 <?php } ?>
+<br/>
+
+<?php 
+if (!function_exists('mysqli_connect')) {
+?>
+<p style="color:red;">php5-mysql ist nicht installiert. Ohne php5-mysql lässt sich UliCMS nicht installieren, da dieses PHP-Modul für die Kommunikation mit dem Datenbankserver benötigt wird.</p>
+
+<?php } else {?>
+
+<form action="index.php" method="post">
+<input type="hidden" name="step" value="1">
+<input type="submit" value="Weiter">
+</form>
+<br/>
+
+<?php }?>
 <?php
      }else{
      ?>
