7b07e34e5296f0c86b09ca4906b9f8bf61ce8804
Ulrich Schmidt <admin@deruli.de>
2013-11-10 00:47:33 +0100

    Update-Skript geändert

--- ulicms/update.php
+++ ulicms/update.php
@@ -17,9 +17,19 @@ $create_table_groups_sql = "CREATE TABLE IF NOT EXISTS `".tbname("groups")."` (
 
 $add_column_group_id = "ALTER TABLE `".tbname("admins")."` ADD `group_id` int NULL";
 
+       
+$insert_group_query = 'INSERT INTO `'.tbname("group").'` (`id`, `name`, `permissions`) VALUES
+(1, \'Administrator\', \'{"banners":true,"cache":true,"dashboard":true,"design":true,"expert_settings":true,"files":true,"flash":true,"groups":true,"images":true,"info":true,"install_packages":true,"languages":true,"list_packages":true,"logo":true,"module_settings":true,"motd":true,"other":true,"pages":true,"pkg_settings":true,"remove_packages":true,"settings_simple":true,"spam_filter":true,"templates":true,"update_system":true,"users":true}\')';
+
+db_query($insert_group_query);
+
+
+
 db_query($create_table_groups_sql);
 db_query($add_column_group_id);
 
+$set_group_id = "UPDATE ".tbname("admins"). " SET `group_id`=1 WHERE group_id = NULL";
+db_query($set_group_id)or die(db_error());
 
 setconfig("db_schema_version", "6.7");
 
