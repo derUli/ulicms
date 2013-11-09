d6c5c4cc908e08f1cb758cbbd4e945ef9a3c65bb
Ulrich Schmidt <admin@deruli.de>
2013-11-09 17:25:09 +0100

    merge

--- ulicms/cms-config.php
+++ ulicms/cms-config.php
@@ -3,11 +3,9 @@ class config extends baseConfig{
 
 var $db_server="localhost";
 var $db_user="root";
-var $db_password="password";
+var $db_password="";
 var $db_database="test";
-var $db_prefix="ulicms_";
+var $db_prefix="blablubb_";
 var $db_type="mysql";
 
 }
-
-define("ULICMS_DEBUG", true);
