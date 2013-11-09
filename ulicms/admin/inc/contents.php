112f999385d4814e5b71cb8026abe5cb4930f986
Ulrich Schmidt <admin@deruli.de>
2013-11-09 17:42:06 +0100

    ACL Abfragen hinzugefugt fuer Inhalte

--- ulicms/admin/inc/contents.php
+++ ulicms/admin/inc/contents.php
@@ -1,10 +1,20 @@
-<?php if(defined("_SECURITY")){
-     ?>
+<?php 
+if(!defined("ULICMS_ROOT"))
+   die("schlechter Hacker");
+   
+$acl = new ACL();
+
+if($acl->hasPermission("pages") or $acl->hasPermission("banners")){
+?>
 
 <h2>Inhalte</h2>
 <p><strong>Bitte wählen Sie einen Inhaltstyp aus:</strong><br/>
+<?php if($acl->hasPermission("pages")){?>
 <a href="index.php?action=pages">Seiten</a><br/>
+<?php }?>
+<?php if($acl->hasPermission("banners")){?>
 <a href="index.php?action=banner">Werbebanner</a></p>
+<?php }?>
 <?php add_hook("content_type_list_entry");
      ?>
 
@@ -13,5 +23,8 @@
 
 
 
-<?php }
+<?php 
+} else {
+  noperms();
+}
 ?>
