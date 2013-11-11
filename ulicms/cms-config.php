2130ea6e9982dd23b316064217bcdf7d9026bb7d
Ulrich Schmidt <admin@deruli.de>
2013-11-11 23:03:54 +0100

    version .php geaendert

--- ulicms/cms-config.php
+++ ulicms/cms-config.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 class config extends baseConfig{
-
-var $db_server="localhost";
-var $db_user="root";
-var $db_password="password";
-var $db_database="test";
-var $db_prefix="blablablubbblablubb_";
-var $db_type="mysql";
-
-}
+    
+    var $db_server = "localhost";
+    var $db_user = "root";
+    var $db_password = "password";
+    var $db_database = "test";
+    var $db_prefix = "blablubb_";
+    var $db_type = "mysql";
+    
+    }
