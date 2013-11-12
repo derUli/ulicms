fbb3d3a5451e1a21734a431278206b1a3f92d06e
Ulrich Schmidt <u.schmidt@velian.de>
2013-11-12 11:50:52 +0100

    Kategorien werden ausgegeben

--- ulicms/admin/inc/categories.php
+++ ulicms/admin/inc/categories.php
@@ -14,3 +14,22 @@ if(!is_admin() and !$acl -> hasPermission("groups")){
     
     
 }
+
+?>
+
+<?php 
+if(count($categories) > 0){
+?>
+<table>
+<tr>
+<td><strong>ID</strong></td>
+<td><strong>Name</strong></td>
+</tr>
+<?php foreach($categories as $category){?>
+<tr>
+<td><?php echo $categories["id"];?></td>
+<td><?php echo real_htmlspecialchars($categories["name"]);?></td>
+</tr>
+<?php }?>
+</table>
+<?php }?>
