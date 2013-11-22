04f3a7cf3f54e1b1f107a3f3cbf95e108f127ddf
Ulrich Schmidt <admin@deruli.de>
2013-11-22 18:40:40 +0100

    Funktion get_ip hinzugefügt

--- ulicms/classes/helper/http_request_helper.php
+++ ulicms/classes/helper/http_request_helper.php
@@ -4,5 +4,24 @@ class httpRequestHelper{
          return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
              || $_SERVER['SERVER_PORT'] == 443);
          }
+         
+         public static function get_ip() 
+         {
+         $ip      = '';
+         $sources = array (
+         'REMOTE_ADDR',
+         'HTTP_X_FORWARDED_FOR',
+         'HTTP_CLIENT_IP');
+         
+         foreach ( $sources as $source ) {
+            if ( isset ( $_SERVER[ $source ] ) )  {
+                $ip = $_SERVER[ $source ];
+            } elseif ( getenv( $source ) ) {
+               $ip = getenv( $source );
+            }
+         }
+
+    return $ip;
+}
     
      }
