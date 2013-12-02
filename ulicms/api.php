97c14565dda12b1a3b7bd1d2aa92da5533a2b1d4
Ulrich Schmidt <admin@deruli.de>
2013-12-02 18:04:15 +0100

    get_mime Funktion zu api.php hinzugefügt

--- ulicms/api.php
+++ ulicms/api.php
@@ -68,6 +68,25 @@ if(!function_exists("get_host")){
     
      }
 
+function get_mime($file){
+     if (function_exists("finfo_file")){
+         $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+         $mime = finfo_file($finfo, $file);
+         finfo_close($finfo);
+         return $mime;
+         }else if (function_exists("mime_content_type")){
+         return mime_content_type($file);
+         }else if (!stristr(ini_get("disable_functions"), "shell_exec")){
+         // http://stackoverflow.com/a/134930/1593459
+        $file = escapeshellarg($file);
+         $mime = shell_exec("file -bi " . $file);
+         return $mime;
+         }else{
+         return false;
+         }
+     }
+
+
  function clearCache(){
      add_hook("before_clear_cache");
      $cache_type = getconfig("cache_type");
