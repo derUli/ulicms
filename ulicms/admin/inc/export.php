f5c97f69cf11ec8b526a26471f1aad781e6c255e
Ulrich Schmidt <u.schmidt@velian.de>
2013-12-11 11:05:50 +0100

    Tabellenliste zu Export hinzugefügt

--- ulicms/admin/inc/export.php
+++ ulicms/admin/inc/export.php
@@ -7,8 +7,20 @@ $acl = new ACL();
 if(!$acl->hasPermission("export")){
    noperms();
 } else {
+
+  $tables = db_get_tables();
 ?>
   <h1>JSON Export</h1>
+  <form action="?action=export" method="post">
+  <p>Exportiere Tabelle:<br/>
+  <select name="table" size="1">
+  <?php foreach($tables as $name){?>
+  <option value="<?php echo $name;?>"><?php echo $name;?></option>
+  <?php }?>
+  </select>
+  </p>
+  <input type="button" name="submit" value="Export">
+  </form>
   
   <?php
   }
