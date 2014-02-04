5ac5eb556d11485e648557a62919d1171f30a6b1
Ulrich Schmidt <uschmidt@velian-imac-07.velian.net>
2014-02-04 13:54:26 +0100

    Label von Checkbox klickbar gemacht

--- ulicms/admin/inc/admins_new.php
+++ ulicms/admin/inc/admins_new.php
@@ -18,7 +18,7 @@
 <input type="text" style="width:300px;" name="admin_email" value=""><br/><br/>
 <strong data-tooltip="Das Passwort des neuen Administrators. Es wird bei der Eingabe im Klartext angezeigt...">Passwort:</strong><br/>
 <input type="text" style="width:300px;" name="admin_password" value=""><br/><br/>
-<input type="checkbox" name="send_mail" value="sendmail"> <label for="send_mail">Die Zugangsdaten per E-Mail senden</label>
+<input type="checkbox" id="send_mail" name="send_mail" value="sendmail"> <label for="send_mail">Die Zugangsdaten per E-Mail senden</label>
 <br/>
 <br/>
 <input type="submit" value="Benutzer anlegen">
