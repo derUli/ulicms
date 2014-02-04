d61bdebdff685516268579b8d5e1646579411333
Ulrich Schmidt <uschmidt@velian-imac-07.velian.net>
2014-02-04 16:44:59 +0100

    update-Skript geändert

--- ulicms/update.php
+++ ulicms/update.php
@@ -3,6 +3,8 @@ include_once "init.php";
 
 db_query("ALTER TABLE ".tbname("content"). " ADD COLUMN `html_file` VARCHAR(255) DEFAULT NULL");
 
+setconfig("backend_style", "green");
+
 setconfig("db_schema", "7.1");
 
 // unlink("update.php");
