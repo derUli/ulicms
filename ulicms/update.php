7af450fb3183a72ec57e238a6e955a0704e00368
Ulrich Schmidt <admin@deruli.de>
2014-02-28 21:27:00 +0100

    Update-Skript legt Config-Variablen für Startseiten in anderen Sprachen an

--- ulicms/update.php
+++ ulicms/update.php
@@ -7,6 +7,14 @@ setconfig("backend_style", "green");
 
 setconfig("db_schema", "7.1");
 
+$languages = getAllLanguages();
+$old_frontpage = getconfig("frontpage");
+
+for($p=0; $p < count($languages); $p++){
+   $lang = $languages[$p];
+   setconfig("frontpage_".$lang, $old_frontpage);
+}
+
 // unlink("update.php");
 header("Location: admin/");
 ?>
