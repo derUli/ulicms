d784344cd2e9c6f79f64e0cc9a4a2f11c259c8e9
Ulrich Schmidt <admin@deruli.de>
2014-03-04 18:00:29 +0100

    Einzahl

--- ulicms/admin/inc/install_method.php
+++ ulicms/admin/inc/install_method.php
@@ -4,7 +4,7 @@ if(!$acl -> hasPermission("install_packages")){
    noperms();
 } else {
 ?>
-<h1>Pakete installieren</h1>
+<h1>Paket installieren</h1>
 <p>
 <a href="?action=upload_package">Datei hochladen</a>
 <br/><a href="?action=available_modules">Aus der Paketquelle</a>
