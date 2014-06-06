35f483d0c888b8816e5b22b71c7732031524f710
Ulrich Schmidt <admin@deruli.de>
2014-06-06 19:49:01 +0200

    Default Empty und db_schema geändert

--- ulicms/update.php
+++ ulicms/update.php
@@ -2,9 +2,11 @@
 include_once "init.php";
 
 if(getconfig("db_schema") == "7.1"){
-   $sql = "ALTER TABLE `" . tbname("content"). "` ADD `alternate_title` VARCHAR(255) DEFAULT NULL";
+   $sql = "ALTER TABLE `" . tbname("content"). "` ADD `alternate_title` VARCHAR(255) DEFAULT ''";
    db_query($sql);
 }
+
+setconfig("db_schema", "7.2");
 
 // unlink("update.php");
 header("Location: admin/");
