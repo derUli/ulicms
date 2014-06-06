09736da4c4c467f34c591cfbfac65f680cce380e
Ulrich Schmidt <admin@deruli.de>
2014-06-06 19:16:59 +0200

    Fehlendes Dollarzeichen

--- ulicms/admin/inc/queries.php
+++ ulicms/admin/inc/queries.php
@@ -360,7 +360,7 @@ if($_POST["edit_page"] == "edit_page" && $acl -> hasPermission("pages")){
      $meta_keywords = db_escape($_POST["meta_keywords"]);
      $language = db_escape($_POST["language"]);
     
-     db_query("UPDATE " . tbname("content") . " SET `html_file` = '$html_file', systemname = '$system_title' , title='$page_title', `alternate_title`='alternate_title', parent=$parent, content='$page_content', active=$activated, lastmodified=" . time() . ", comments_enabled=$comments_enabled, redirection = '$redirection', notinfeed = $notinfeed, menu = '$menu', position = $position, lastchangeby = $user, language='$language', access = '$access', meta_description = '$meta_description', meta_keywords = '$meta_keywords', target='$target', category='$category' WHERE id=$id");
+     db_query("UPDATE " . tbname("content") . " SET `html_file` = '$html_file', systemname = '$system_title' , title='$page_title', `alternate_title`='$alternate_title', parent=$parent, content='$page_content', active=$activated, lastmodified=" . time() . ", comments_enabled=$comments_enabled, redirection = '$redirection', notinfeed = $notinfeed, menu = '$menu', position = $position, lastchangeby = $user, language='$language', access = '$access', meta_description = '$meta_description', meta_keywords = '$meta_keywords', target='$target', category='$category' WHERE id=$id");
     
     
      header("Location: index.php?action=pages");
