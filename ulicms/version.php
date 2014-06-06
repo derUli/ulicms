cd8764beddf3a13c8f81c1ac17ca1acafc153a75
Ulrich Schmidt <u.schmidt@velian.de>
2014-06-06 17:28:06 +0200

    Versionsnummer erhöht

--- ulicms/version.php
+++ ulicms/version.php
@@ -3,7 +3,7 @@ class ulicms_version{
     
      function ulicms_version(){
          $this -> version = "7";
-         $this -> internalVersion = Array(7, 1, 3);
+         $this -> internalVersion = Array(7, 2, 0);
          $this -> developmentVersion = false;
          
          }
