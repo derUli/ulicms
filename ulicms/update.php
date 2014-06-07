1be58979a28b61581f8923125f8c9b8678a52444
derUli <admin@deruli.de>
2014-06-07 10:36:05 +0200

    no db schema

--- ulicms/update.php
+++ ulicms/update.php
@@ -1,8 +1,8 @@
 <?php
 include_once "init.php";
 
-   $sql = "ALTER TABLE `" . tbname("content"). "` ADD `alternate_title` VARCHAR(255) DEFAULT ''";
-   db_query($sql);
+$sql = "ALTER TABLE `" . tbname("content"). "` ADD `alternate_title` VARCHAR(255) DEFAULT ''";
+db_query($sql);
 
 
 setconfig("db_schema", "7.2");
