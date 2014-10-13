db189d2b4ef4fd6172020e3620d6204bbfb38f3d
Ulrich Schmidt <admin@deruli.de>
2014-10-13 21:22:53 +0200

    Version 7.2.1

--- ulicms/version.php
+++ ulicms/version.php
@@ -3,7 +3,7 @@ class ulicms_version{
     
      function ulicms_version(){
          $this -> version = "7";
-         $this -> internalVersion = Array(7, 2, 0);
+         $this -> internalVersion = Array(7, 2, 1);
          $this -> developmentVersion = false;
          
          }
