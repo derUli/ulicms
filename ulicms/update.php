ec8ae3003ff31dac0da2aaf573c5fe7c7e0217e9
Ulrich Schmidt <u.schmidt@velian.de>
2014-10-15 10:33:23 +0200

    use foreign keys

--- ulicms/update.php
+++ ulicms/update.php
@@ -7,6 +7,28 @@ db_query($sql);
 $sql = "ALTER TABLE `" . tbname("banner"). "` ADD `html` TEXT DEFAULT ''";
 db_query($sql);
 
+         $constraint1 = "ALTER TABLE `" .  tbname("admins")."` ADD FOREIGN KEY (`group_id`) REFERENCES `".tbname("groups"). "`(`id`) 
+ON UPDATE CASCADE ON DELETE CASCADE;";
+ db_query($connection, $constraint1);
+         $constraint2 = "ALTER TABLE `" . tbname("content") ."` ADD FOREIGN KEY (`category`) REFERENCES `".tbname("categories") . "`(`id`) 
+ON UPDATE CASCADE ON DELETE CASCADE;";
+ db_query($connection, $constraint2);
+
+
+         $constraint3 = "ALTER TABLE `" . tbname("banner") ."` ADD FOREIGN KEY (`category`) REFERENCES `".tbname("categories"). "`(`id`) 
+ON UPDATE CASCADE ON DELETE CASCADE;";
+ db_query($connection, $constraint3);
+
+
+         $constraint4 = "ALTER TABLE `" . tbname("content") ."` ADD FOREIGN KEY (`autor`) REFERENCES `".tbname("admins") . "`(`id`) 
+ON UPDATE CASCADE ON DELETE CASCADE;";
+ db_query($connection, $constraint4);
+
+
+         $constraint5 = "ALTER TABLE `" . tbname("content") ."` ADD FOREIGN KEY (`language`) REFERENCES `".tbanme("languages"). "`(`language_code`) 
+ON UPDATE CASCADE ON DELETE CASCADE;";
+ db_query($connection, $constraint5);
+
 setconfig("db_schema", "7.2.1");
 
 // unlink("update.php");
