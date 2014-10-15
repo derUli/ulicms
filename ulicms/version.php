9660c11b91e2fe11c90cd3fd5714758c587803aa
Ulrich Schmidt <admin@deruli.de>
2014-10-15 21:16:54 +0200

    development version

--- ulicms/version.php
+++ ulicms/version.php
@@ -4,7 +4,7 @@ class ulicms_version{
      function ulicms_version(){
          $this -> version = "7";
          $this -> internalVersion = Array(7, 2, 1);
-         $this -> developmentVersion = false;
+         $this -> developmentVersion = true;
          
          }
     
