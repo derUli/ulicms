34cf78982e1c9e53a57927d6d727d7d8798355c7
Ulrich Schmidt <admin@deruli.de>
2014-10-23 23:04:38 +0200

    Kein HR mehr im Footer

--- ulicms/admin/inc/footer.php
+++ ulicms/admin/inc/footer.php
@@ -1,6 +1,4 @@
 </div>
-<br/>
-<hr/>
-<p id="footer">&copy; 2011 - 2014 by <a href="http://www.ulicms.de" target="_blank">UliCMS</a>
+<p id="footer">&copy; 2011 - 2014 by <a href="http://www.ulicms.de" target="_blank">UliCMS</a></p>
 </body>
 </html>
