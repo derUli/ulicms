8613b9936275e924799bdccdf8da4390d6815ba4
Ulrich Schmidt <u.schmidt@velian.de>
2014-10-29 15:11:00 +0100

    utf-8

--- services/updatecheck.php
+++ services/updatecheck.php
@@ -1,4 +1,5 @@
 <?php 
+header("Content-Type: text/html; charset=UTF-8");
 $version = $_REQUEST["v"];
 
 
@@ -11,12 +12,12 @@ if($version == "7.1.2" or $version == "7.1.3")
    <a href=\"http://www.ulicms.de/content/files/Releases/Upgrades/ulicms-7.1.2-auf-7.2.0-upgrade.zip\">Download</a></p>");
  
  
-/** 
+/*
 if($version == "7.2.0")
    die("<p>Ein Update auf UliCMS 7.2.1 ist verfügbar.<br/>
    <a href=\"http://www.ulicms.de\">Download</a></p>");
 
-**/
+*/
 
 
 
