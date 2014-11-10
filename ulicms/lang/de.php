d49772b083c2d4491c13087bb11cfdc0d957c36e
Ulrich Schmidt <admin@deruli.de>
2014-11-10 15:02:10 +0100

    Sprachdatei ergänzt

--- ulicms/lang/de.php
+++ ulicms/lang/de.php
@@ -1,4 +1,5 @@
 <?php
+// Dashboard (Willkommen)
 define("TRANSLATION_MOTD", "Nachricht des Tages");
 define("TRANSLATION_STATISTICS", "Statistiken");
 define("TRANSLATION_ONLINE_NOW", "Jetzt online sind");
@@ -19,3 +20,44 @@ define("TRANSLATION_DONE_BY", "Durchgeführt von");
 
 define("TRANSLATION_EDIT_PROFILE", "Profil bearbeiten");
 define("TRANSLATION_HELLO_NAME", "Hallo %firstname% %lastname%!");
+
+// Admin Menü Navigation
+define("TRANSLATION_WELCOME", "Willkommen");
+
+define("TRANSLATION_CONTENTS", "Inhalte");
+define("TRANSLATION_PAGES", "Seiten");
+define("TRANSLATION_ADVERTISEMENTS", "Werbebanner");
+define("TRANSLATION_CATEGORIES", "Kategorien");
+define("TRANSLATION_IMPORT", "Import");
+define("TRANSLATION_EXPORT", "Export");
+
+define("TRANSLATION_MEDIA", "Medien");
+define("TRANSLATION_IMAGES", "Bilder");
+define("TRANSLATION_FLASH", "Flash");
+define("TRANSLATION_FILES", "Dateien");
+
+define("TRANSLATION_USERS", "Benutzer");
+
+define("TRANSLATION_GROUPS", "Gruppen");
+
+define("TRANSLATION_TEMPLATES", "Templates");
+define("TRANSLATION_TOP", "Oben");
+define("TRANSLATION_BOTTOM", "Unten");
+define("TRANSLATION_MAINTENANCE_PAGE", "Wartungsseite");
+define("TRANSLATION_CSS", "CSS");
+
+define("TRANSLATION_PACKAGES", "Pakete");
+define("TRANSLATION_UPDATE", "Update");
+
+define("TRANSLATION_SETTINGS", "Einstellungen");
+define("TRANSLATION_GENERAL_SETTINGS", "Grundinstellungen");
+define("TRANSLATION_DESIGN", "Design");
+define("TRANSLATION_SPAMFILTER", "Spamfilter");
+define("TRANSLATION_MOTD", "MOTD");
+define("TRANSLATION_CACHE", "Cache");
+define("TRANSLATION_CLEAR_CACHE", "Cache leeren");
+define("TRANSLATION_PACKAGE_SOURCE", "Paketquelle");
+define("TRANSLATION_LOGO", "Logo");
+define("TRANSLATION_LANGUAGES", "Sprachen");
+define("TRANSLATION_OTHER", "Sonstiges");
+define("TRANSLATION_DESIGN_SETTINGS", "Design");
