41066780ff0447bf6e83d81aeea80b780d2c9d38
Ulrich Schmidt <admin@deruli.de>
2014-11-10 20:28:17 +0100

    Sprachdatei ergänzt

---
+++ ulicms/modules/block_yasni/block_yasni_init.php
@@ -0,0 +1,20 @@
+<?php
+@$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+if($hostname){
+     $hostname = strtolower($hostname);
+    
+     $list = array("123people.de", "findestars.de", "myonid.de",
+         "peekyou.com", "pipl.com", "rapleaf.com", "snitch.name",
+         "spock.com", "tweepz.com", "wink.com", "yasni.de",
+         "yoname.com", "yourtraces.com", "zoominfo.com", "umsonstleutesuchen.de",
+         "wer-ist.org");
+    
+     for($s = 0; $s < count($list); $s++){
+         if(endsWith($hostname, $list[$s])){
+             die("Respect our Privacy");
+             }
+         }
+    
+     }
+?>
