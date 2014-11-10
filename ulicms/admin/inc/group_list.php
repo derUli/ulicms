5be53b4e6c9c65d8901aee7d26cbbe1aa2656904
Ulrich Schmidt <admin@deruli.de>
2014-11-10 17:39:13 +0100

    Übersetzungen hinzugefügt

--- ulicms/admin/inc/group_list.php
+++ ulicms/admin/inc/group_list.php
@@ -55,18 +55,18 @@ else if($_SESSION["grp_sort"] == "name"){
 <td>
 <?php if($default_acl_group === $id){
              ?>
-<span style="color:green; font-weight:bold;">Ja</a>
+<span style="color:green; font-weight:bold;"><?php echo TRANSLATION_YES;?></a>
 <?php }else{
              ?>
 <a href="?action=groups&standard=<?php echo $id;
-             ?>"<span style="color:red; font-weight:bold;" onclick='return confirm("<?php echo str_ireplace("%name%", $name, TRANSLATION_MAKE_GROUP_DEFAULT);?>")'>Nein</a>
+             ?>"<span style="color:red; font-weight:bold;" onclick='return confirm("<?php echo str_ireplace("%name%", $name, TRANSLATION_MAKE_GROUP_DEFAULT);?>")'><?php echo TRANSLATION_NO;?></a>
 <?php }
          ?>
 </td>
 <td><a href="?action=groups&edit=<?php echo $id;
-         ?>"><img src="gfx/edit.gif" alt="Bearbeiten" title="Bearbeiten"></a></td>
+         ?>"><img src="gfx/edit.gif" alt="<?php echo TRANSLATION_EDIT;?>" title="<?php echo TRANSLATION_EDIT;?>"></a></td>
 <td><a href="?action=groups&delete=<?php echo $id;
-         ?>" onclick="return confirm('<?php echo TRANSLATION_ASK_FOR_DELETE;?>');"><img src="gfx/delete.gif" alt="Löschen" title="Löschen"></a></td>
+         ?>" onclick="return confirm('<?php echo TRANSLATION_ASK_FOR_DELETE;?>');"><img src="gfx/delete.gif" alt="<?php echo TRANSLATION_DELETE;?>" title="<?php echo TRANSLATION_DELETE;?>"></a></td>
 </tr>
 
 
