16de8c0c33b703b7aecda79380cd415ba5e437f9
Ulrich Schmidt <admin@deruli.de>
2014-11-10 19:14:28 +0100

    Übersetzungen hinzugefügt

--- ulicms/admin/inc/cache_settings.php
+++ ulicms/admin/inc/cache_settings.php
@@ -6,23 +6,18 @@ if($acl -> hasPermission("cache")){
 <?php
      if(isset($_GET["clear_cache"])){
          ?>
-<p style="color:green;">Der Cache wurde erfolgreich geleert.</p>
+<p style="color:green;"><?php echo TRANSLATION_CACHE_WAS_CLEARED;?></p>
 <?php }
      ?>
-<p>Um die Performance der Website zu verbessern, 
-bietet das UliCMS eine Cache-Funktion.<br/>
-Statische Seiten, die keine Module enthalten, werden einmalig generiert und dann im cache-Ordner zwischengespeichert.
-Anschließend werden statt die Inhalte immer wieder aus der Datenbank zu laden, die Inhalte aus den gespeicherten HTML-Dateien geladen.</p>
+<?php echo TRANSLATION_CACHE_TEXT1;?>
 <p><strong>Aktueller Status des Caches:</strong><br/>
 <?php if(!getconfig("cache_disabled")){
          ?>
 <span style="color:green;">aktiv</span></p>
 
-<p>Sie können den Cache deaktivieren, in dem Sie in den Einstellungen im Expertenmodus die Konfigurationsvariable<br/>
-<code>cache_disabled</code><br/>
-Mit einem beliebigen Wert anlegen.</p>
+<?php echo TRANSLATION_CACHE_TEXT3;?>
 
-<p>Wenn Änderungen an der Website vorgenommen wurden sind, ist es erforderlich, den Cache zu leeren, damit die Änderungen auch auf der Website angezeigt werden.</p>
+
 <form post="index.php" method="get">
 <input type="hidden" name="action" value="cache"/>
 <input type="hidden" name="clear_cache" value="yes"/>
@@ -40,9 +35,7 @@ Mit einem beliebigen Wert anlegen.</p>
 <?php }else{
          ?>
 <span style="color:red;">deaktiviert</span></p>
-<p>Sie können den Cache aktivieren, in dem Sie in den Einstellungen im Expertenmodus die Konfigurationsvariable<br/>
-<code>cache_disabled</code><br/>
-löschen.</p>
+<?php echo TRANSLATION_CACHE_TEXT2;?>
 <?php }
      ?>
 
