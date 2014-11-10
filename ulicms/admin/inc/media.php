a5200348194902bfbab1a648a38fc5f1147215d7
Ulrich Schmidt <admin@deruli.de>
2014-11-10 16:33:43 +0100

    Übersetzungen

--- ulicms/admin/inc/media.php
+++ ulicms/admin/inc/media.php
@@ -9,21 +9,21 @@ if($acl -> hasPermission("images") or $acl -> hasPermission("flash") or $acl -> 
     
      ?>
 
-<h2>Medien</h2>
+<h2><?php echo TRANSLATION_MEDIA;?></h2>
 <strong>Bitte wählen Sie einen Dateityp aus:</strong><br/>
 <?php if($acl -> hasPermission("images")){
          ?>
-<a href="index.php?action=images">Bilder</a><br/>
+<a href="index.php?action=images"><?php echo TRANSLATION_IMAGES;?></a><br/>
 <?php }
      ?>
 <?php if($acl -> hasPermission("flash")){
          ?>
-<a href="index.php?action=flash">Flash</a><br/>
+<a href="index.php?action=flash"><?php echo TRANSLATION_FLASH;?></a><br/>
 <?php }
      ?>
 <?php if($acl -> hasPermission("files")){
          ?>
-<a href="index.php?action=files">Dateien</a><br/>
+<a href="index.php?action=files"><?php echo TRANSLATION_FILES;?></a><br/>
 <?php }
      ?>
 
