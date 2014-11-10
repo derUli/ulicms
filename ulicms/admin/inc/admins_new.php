94ad00d90e90c1bebf4e37adce72d4a26d8a57d2
Ulrich Schmidt <admin@deruli.de>
2014-11-10 16:39:14 +0100

    Übersetzungen hinzugefügt

--- ulicms/admin/inc/admins_new.php
+++ ulicms/admin/inc/admins_new.php
@@ -6,22 +6,22 @@
              ?>
 <form action="index.php?action=admins" method="post">
 <input type="hidden" name="add_admin" value="add_admin">
-<strong data-tooltip="Dieser Name wird zur Anmeldung im Administrationsbereich benötigt...">Benutzername:</strong><br/>
+<strong><?php echo TRANSLATION_USERNAME;?></strong><br/>
 <input type="text" style="width:300px;" name="admin_username" value="">
 <br/><br/>
-<strong>Nachname:</strong><br/>
+<strong><?php echo TRANSLATION_LASTNAME;?></strong><br/>
 <input type="text" style="width:300px;" name="admin_lastname" value="">
 <br/><br/>
-<strong>Vorname:</strong><br/>
+<strong><?php echo TRANSLATION_FIRSTNAME;?></strong><br/>
 <input type="text" style="width:300px;" name="admin_firstname" value=""><br/><br/>
-<strong>Email:</strong><br/>
+<strong><?php echo TRANSLATION_EMAIL;?></strong><br/>
 <input type="text" style="width:300px;" name="admin_email" value=""><br/><br/>
-<strong data-tooltip="Das Passwort des neuen Administrators. Es wird bei der Eingabe im Klartext angezeigt...">Passwort:</strong><br/>
+<strong><?php echo TRANSLATION_PASSWORD;?></strong><br/>
 <input type="text" style="width:300px;" name="admin_password" value=""><br/><br/>
-<input type="checkbox" id="send_mail" name="send_mail" value="sendmail"> <label for="send_mail">Die Zugangsdaten per E-Mail senden</label>
+<input type="checkbox" id="send_mail" name="send_mail" value="sendmail"> <label for="send_mail"><?php echo TRANSLATION_SEND_LOGINDATA_BY_MAIL;?></label>
 <br/>
 <br/>
-<input type="submit" value="Benutzer anlegen">
+<input type="submit" value="<?php echo TRANSLATION_CREATE_USER;?>">
 <?php
              if(getconfig("override_shortcuts") == "on" || getconfig("override_shortcuts") == "backend"){
                  ?>
