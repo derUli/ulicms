3a433e78bc7a9b8e5712e97f68affd77d0b74e6f
Ulrich Schmidt <admin@deruli.de>
2014-11-15 16:49:00 +0100

    exclude hashes

--- ulicms/api.php
+++ ulicms/api.php
@@ -850,11 +850,11 @@ if(!$lang){
 function getAllSystemNames($lang = null){
 if(!$lang){
    $query = db_query("SELECT systemname,id FROM `" . tbname("content") .
-    "` WHERE `deleted_at` IS NULL ORDER BY systemname");
+    "` WHERE `deleted_at` IS NULL AND redirection NOT LIKE '#%' ORDER BY systemname");
  } else {
  
    $query = db_query("SELECT systemname,id FROM `" . tbname("content") .
-    "` WHERE `deleted_at` IS NULL AND language ='".db_escape($lang)."' ORDER BY systemname");
+    "` WHERE `deleted_at` IS NULL  AND redirection NOT LIKE '#%' AND language ='".db_escape($lang)."' ORDER BY systemname");
  }
  $returnvalues = Array();
  while($row = db_fetch_object($query)){
