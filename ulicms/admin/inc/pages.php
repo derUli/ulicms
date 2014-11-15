b02daa8f41093830fcaa28c3b93543c49bb58908
Ulrich Schmidt <admin@deruli.de>
2014-11-15 22:29:14 +0100

    Domain2Language Mapping wir bei link auf Anzeigen unter Pages verarbeitet

--- ulicms/admin/inc/pages.php
+++ ulicms/admin/inc/pages.php
@@ -184,8 +184,14 @@ $(window).load(function(){
                 if(startsWith($row->redirection, "#")){
                 echo "<td style='text-align:center'></td>";
                 } else {
-                 echo "<td style='text-align:center'><a href=\"../" . $row -> systemname . ".html\" target=\"_blank\"><img src=\"gfx/preview.gif\" alt=\"".TRANSLATION_VIEW."\" title=\"".TRANSLATION_VIEW."\"></a></td>";
-                 
+                 $domain = getDomainByLanguage($row->language);
+                 if(!$domain){
+                    $url = "../".$row -> systemname . ".html";
+                    } else {
+                  $url = "http://".$domain."/". $row -> systemname . ".html";
+                 }
+                 echo "<td style='text-align:center'><a href=\"".$url ."\" target=\"_blank\"><img src=\"gfx/preview.gif\" alt=\"".TRANSLATION_VIEW."\" title=\"".TRANSLATION_VIEW."\"></a></td>";
+                
                  }
                  echo "<td style='text-align:center'>" . '<a href="index.php?action=pages_edit&page=' . $row -> id . '"><img src="gfx/edit.gif" alt="'.TRANSLATION_EDIT.'" title="'.TRANSLATION_EDIT.'"></a></td>';
            
