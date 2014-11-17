295fa5b4ab1c71fc8c278e64453e561f070e0912
Ulrich Schmidt <admin@deruli.de>
2014-11-17 14:40:54 +0100

    Sprachdateien für Installer

---
+++ ulicms/installer.aus/lang/de.php
@@ -0,0 +1,18 @@
+<?php 
+define("TRANSLATION_TITLE", "UliCMS Installation");
+define("TRANSLATION_WELCOME", "Willkommen");
+define("TRANSLATION_WELCOME2", "Willkommen zur Installation von UliCMS.");
+define("TRANSLATION_BETA_VERSION", "Das hier ist eine Vorab-Version von UliCMS.<br/>
+Das bedeutet, diese Version ist noch nicht 100-prozentig fertig und dient nur der Vorschau auf ein neues Release.<br/>
+Setzen Sie diese Version bitte nicht produktiv ein!");
+define("TRANSLATION_FOLLOW_INSTRUCTIONS", "Folgen Sie den Anweisungen um das CMS auf Ihrem Server zu installieren.");
+define("TRANSLATION_CHMOD", "<p>Setzen Sie bitte vorher die Dateirechte der folgenden Dateien auf 0755.<br/>
+<ol>
+<li>Das Hauptverzeichnis des CMS (Ohne die Unterordner)</li>
+<li>templates/ (inklusive Inhalt und Unterordner)</li>
+<li>content/ (inklusive Inhalt und Unterordner)</li>
+<li>modules/ (inklusive Inhalt und Unterordner)</li>
+</ol>
+</p>");
+define("TRANSLATION_PERMISSION", "So müssen die Berechtigungen gesetzt sein");
+define("TRANSLATION_PERMISSIONS2", "FTP Rechtevergabe");
