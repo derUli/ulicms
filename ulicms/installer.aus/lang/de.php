20b06dbf9570d7cc53ede185a91e59b1fdf134bc
Ulrich Schmidt <admin@deruli.de>
2014-11-17 14:49:06 +0100

    Sprachdateien

--- ulicms/installer.aus/lang/de.php
+++ ulicms/installer.aus/lang/de.php
@@ -16,3 +16,16 @@ define("TRANSLATION_CHMOD", "<p>Setzen Sie bitte vorher die Dateirechte der folg
 </p>");
 define("TRANSLATION_PERMISSION", "So müssen die Berechtigungen gesetzt sein");
 define("TRANSLATION_PERMISSIONS2", "FTP Rechtevergabe");
+define("TRANSLATION_GD_MISSING", "<strong>php5-gd</strong> ist nicht installiert.<br/>Ohne <strong>php5-gd</strong> lässt sich UliCMS zwar installieren,<br/>Bitte installieren Sie dieses PHP-Modul und versuchen Sie es erneut.");
+define("TRANSLATION_MYSQLI_MISSING", "<strong>php5-mysql</strong> ist nicht installiert.<br/>Bitte installieren Sie dieses PHP-Modul und versuchen Sie es erneut.");
+define("TRANSLATION_JSON_MISSING", "<strong>php5-json</strong> ist nicht installiert.<br/>Bitte installieren Sie zuerst php5-json und versuchen Sie es dann erneut.");
+define("TRANSLATION_NEXT", "Weiter");
+define("TRANSLATION_MYSQL_LOGIN", "MySQL Logindaten");
+define("TRANSLATION_MYSQL_LOGIN_HELP", "Bitte tragen Sie in das untere Formular die Logindaten für den MySQL-Server ein. Diese bekommen Sie von Ihrem Provider.");
+
+define("TRANSLATION_SERVERNAME", "Servername");
+define("TRANSLATION_LOGINNAME", "Benutzername");
+define("TRANSLATION_PASSWORD", "Passwort");
+define("TRANSLATION_DATABASE_NAME", "Datenbankname");
+define("TRANSLATION_PREFIX", "Prefix");
+define("TRANSLATION_DB_CONNECTION_FAILED", "Die Verbindung mit dem MySQL-Datenbankserver konnte nicht hergestellt werden.<br/>Dies kann z.B. an einem falschen Passwort liegen. Wenn Sie sich sicher sind, dass das Passwort richtig ist, prüfen Sie ob der MySQL-Datenbankserver läuft und erreichbar ist.");
