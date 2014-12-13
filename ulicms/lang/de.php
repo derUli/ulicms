04820106363e4c075eb425bb8388fbb87b37c1a8
Ulrich Schmidt <admin@deruli.de>
2014-12-13 20:01:52 +0100

    fix variable in translation

--- ulicms/lang/de.php
+++ ulicms/lang/de.php
@@ -364,7 +364,7 @@ define("TRANSLATION_TARGET_BLANK", "In einem neuen Fenster");
 define("TRANSLATION_JSON_IMPORT", "JSON Import");
 define("TRANSLATION_IMPORT_INTO_TABLE", "Importe in Tabelle");
 define("TRANSLATION_DO_IMPORT", "Importieren");
-define("TRANSLATION_IMPORT_INTO_TABLE_SUCCESSFULL", "Import in Tabelle %tabelle% erfolgreich!");
+define("TRANSLATION_IMPORT_INTO_TABLE_SUCCESSFULL", "Import in Tabelle %table% erfolgreich!");
 define("TRANSLATION_NOT_A_JSON_FILE", "Diese Datei ist nicht im JSON Format!");
 
 // Export
