cbb15b1b22a3ccb76e6cdd37826db5beac75132b
Ulrich Schmidt <admin@deruli.de>
2015-01-17 18:21:56 +0100

    update Skript legt description an

--- ulicms/update.php
+++ ulicms/update.php
@@ -3,6 +3,7 @@ define("SKIP_TABLE_CHECK", true);
 include_once "init.php";
 
 db_query("ALTER TABLE ".tbname("content")." ADD `theme` varchar(200) null");
+db_query("ALTER TABLE " . tbname("categories") . " ADD `description` varchar(255) NULL DEFAULT ''");
 
 // @unlink("update.php");
 ulicms_redirect("admin/");
