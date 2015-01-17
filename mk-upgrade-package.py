9c9aee8b318fc09589c1276203eab9110a0ab294
Ulrich Schmidt <admin@deruli.de>
2015-01-17 19:45:57 +0100

    Catch KeyboardInterrupt

--- mk-upgrade-package.py
+++ mk-upgrade-package.py
@@ -2,36 +2,40 @@ import shutil
 import argparse
 import os
 import codecs
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', action ="store", dest="target", required = True)
+    args = parser.parse_args()
+    target = os.path.abspath(args.target)
+    source_dir = os.path.dirname(__file__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--target', action ="store", dest="target", required = True)
-args = parser.parse_args()
-target = os.path.abspath(args.target)
+    ignore = ('.git', "doc-src", "phpCB-1.0.1-linux", "*.py", "*.pyc",
+                       "Releases", "cms-config.php", "content", "services",
+                       ".gitignore", ".htaccess", "installer.aus", "installer",
+              "modules", "templates", "fonts.php", "config.php", "contents.css",
+              "config.js")
 
-source_dir = os.path.dirname(__file__)
+    IGNORE_PATTERNS = shutil.ignore_patterns(*ignore)
 
-ignore = ('.git', "doc-src", "phpCB-1.0.1-linux", "*.py", "*.pyc",
-                   "Releases", "cms-config.php", "content", "services",
-                   ".gitignore", ".htaccess", "installer.aus", "installer",
-          "modules", "templates", "fonts.php", "config.php", "contents.css",
-          "config.js")
+    print("Refactoring Code...")
+    execfile('reformat_code.py')
+    print("copying files")
+    shutil.copytree(source_dir, target, ignore=IGNORE_PATTERNS)
+    update_script = os.path.join(target, "ulicms", "update.php")
 
-IGNORE_PATTERNS = shutil.ignore_patterns(*ignore)
-
-
-shutil.copytree(source_dir, target, ignore=IGNORE_PATTERNS)
-
-update_script = os.path.join(target, "ulicms", "update.php")
-
-
-if os.path.exists(update_script):
-    with codecs.open(update_script, 'r+', "utf-8") as f:
-        lines = f.readlines()
-        f.seek(0)
-        f.truncate()
-        for line in lines:
-            if "unlink(\"update.php" in line and line.startswith("//"):
-                line = line = line[2:]
-            line = line.strip()
-            print(line)
-            f.write(line)
+    print("preparing update Script")
+    if os.path.exists(update_script):
+        with codecs.open(update_script, 'r+', "utf-8") as f:
+            lines = f.readlines()
+            f.seek(0)
+            f.truncate()
+            for line in lines:
+                if "unlink(\"update.php" in line and line.startswith("//"):
+                    line = line = line[2:]
+                line = line.strip()
+                print(line)
+                f.write(line)
+try:
+    main()
+except KeyboardInterrupt:
+    pass
