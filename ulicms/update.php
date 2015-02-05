a7dde193d07ab871be46250676b3b7337d6ce637
Ulrich Schmidt <admin@deruli.de>
2015-02-05 21:57:10 +0100

    font size im Backend

--- ulicms/update.php
+++ ulicms/update.php
@@ -5,7 +5,9 @@ include_once "init.php";
 db_query("ALTER TABLE " . tbname("content") . " ADD `theme` varchar(200) null");
 db_query("ALTER TABLE " . tbname("categories") . " ADD `description` TEXT NULL DEFAULT ''");
 
+setconfig("font-size", "medium");
 setconfig('db_schema_version', '8.0.0');
+
 
 //  @unlink ("update.php");
 ulicms_redirect("admin/");
