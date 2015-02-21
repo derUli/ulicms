701cbce995bb9031c091c7e68152ed4a45396fe8
Ulrich Schmidt <ulrich1992@web.de>
2015-02-22 00:29:13 +0100

    en_US

--- ulicms/installer.aus/installer.php
+++ ulicms/installer.aus/installer.php
@@ -473,7 +473,7 @@ Eine Dokumentation finden Sie unter <a href=\"http://www.ulicms.de\" target=\"_b
 (49, 'cache_disabled', 'disabled'),
 (50, 'locale', 'de_DE.UTF-8'),
 (51, 'locale_de', 'de_DE.UTF-8'),
-(52, 'locale_en', 'en_GB.UTF-8'),
+(52, 'locale_en', 'en_US.UTF-8'),
 (53, 'session_timeout', '60');")or die(mysqli_error($connection));
         
          mysqli_query($connection, "UPDATE `" . $prefix . "content` SET parent=NULL")or die(mysqli_error($connection));
