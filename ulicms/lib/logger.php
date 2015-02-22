464b3aa63af02b974f6f97d8cfddeb776ecc8e4b
Ulrich Schmidt <ulrich1992@web.de>
2015-02-22 16:09:50 +0100

    falschen kommentar entfernt

--- ulicms/lib/logger.php
+++ ulicms/lib/logger.php
@@ -1,10 +1,7 @@
 <?php
 define("LOG_TIME_FORMAT", "H:i:s");
 function log_db_query($query){
-     // Das DB Logging kann man deaktivieren, durch anlegen der Konfigurationsvariable disable_query_log in der cms_config.php;
-    include_once ULICMS_ROOT . DIRECTORY_SEPERATOR . "cms-config.php";
-    
-    
+    include_once ULICMS_ROOT . DIRECTORY_SEPERATOR . "cms-config.php";    
      $config = new config();
      if(!isset($config -> query_logging)){
          return false;
