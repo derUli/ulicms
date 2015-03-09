fc83023a2ffce86719aadc64c22e77a9f11b9de8
Ulrich Schmidt <u.schmidt@velian.de>
2015-03-09 12:07:31 +0100

    pkg

--- ulicms/classes/package_manager.php
+++ ulicms/classes/package_manager.php
@@ -6,7 +6,7 @@ class packageManager{
          $this -> package_source = $cfg -> getVar("pkg_src");
          $this -> package_source = $this -> replacePlaceHolders($this -> package_source);
          }
-    
+
      public function splitPackageName($name){
          $name = str_ireplace(".tar.gz", "", $name);
          $name = str_ireplace(".zip", "", $name);
@@ -14,40 +14,43 @@ class packageManager{
          $version = array_pop($splitted);
          $name = $splitted;
          return array(join("-", $name), $version);
-        
+
          }
-         
-         
+
+
      public function getInstalledPatchNames(){
         $query = db_query("SELECT name from ".tbname("installed_patches"));
         $retval = array();
         while($row = db_fetch_object($query)) {
-              $retval[] = $row->name;    
+              $retval[] = $row->name;
         }
         return $retval;
      }
-     
+
      // @FIXME: Delete temporary files after install a patch
      public function installPatch($name, $description, $url){
          $test = $this->getInstalledPatchNames();
          if(in_array($name, $test))
 	   return false;
-	   
-	   $tmp_dir = ULICMS_TMP . "/" .uniqid()."/";
-           
-           $download = file_get_contents_wrapper($url, true);
 
-           $download_tmp = $tmp_dir."patch.zip";
-           if(!$download_tmp)
-              return false;
-           file_put_contents($download_tmp, $url);
+	   $tmp_dir = ULICMS_TMP . "/" .uniqid()."/";
+     if(!file_exists($tmp_dir))
+        mkdir($tmp_dir);
+
+
+    $download = file_get_contents_wrapper($url, true);
+
+    $download_tmp = $tmp_dir."patch.zip";
+    if(!$download)
+       return false;
+
+     file_put_contents($download_tmp, $download);
 	   $zip = new ZipArchive;
-	   
+
 	   // @FIXME: open zip from url funktioniert nicht
-	   
+
            if ($zip->open($download_tmp) === TRUE) {
-              if(!file_exists($tmp_dir))
-                 mkdir($tmp_dir);
+
                  $zip->extractTo($tmp_dir);
                  $patch_dir = $tmp_dir."patch";
                  $zip->close();
@@ -56,35 +59,35 @@ class packageManager{
                      $name = db_escape($name);
 	             $description = db_escape($description);
 	             $url = db_escape($url);
-	  db_query("INSERT INTO ".tbname("installed_patches"). " (name, description, url) VALUES ('$name', '$description', '$url')");  
-	  return true;      
+	  db_query("INSERT INTO ".tbname("installed_patches"). " (name, description, url) VALUES ('$name', '$description', '$url')");
+	  return true;
                  }
            }
-           
+
            return false;
-	   
-	 
+
+
      }
-     
+
      public function getInstalledPatches(){
         $query = db_query("SELECT * from ".tbname("installed_patches"));
         $retval = array();
         while($row = db_fetch_object($query)) {
-              $retval[$row->name] = $row;    
+              $retval[$row->name] = $row;
         }
         return $retval;
      }
-    
+
      public function installPackage($file){
          try{
              // Paket entpacken
             $phar = new PharData($file);
              $phar -> extractTo(ULICMS_ROOT, null, true);
-            
+
              $post_install_script1 = ULICMS_ROOT . DIRECTORY_SEPARATOR . "post-install.php";
-            
+
              $post_install_script2 = ULICMS_TMP . DIRECTORY_SEPARATOR . "post-install.php";
-            
+
              // post_install_script ausführen und anschließend
             // entfernen, sofern vorhanden;
             if(file_exists($post_install_script1)){
@@ -100,9 +103,9 @@ class packageManager{
          catch (Exception $e){
              return false;
              }
-        
+
          }
-    
+
      private function replacePlaceHolders($url){
          $cfg = new config();
          $version = new ulicms_version();
@@ -111,37 +114,37 @@ class packageManager{
          $url = str_replace("{version}", $internalVersion, $url);
          return $url;
          }
-    
+
      public function getInstalledModules(){
          $module_folder = ULICMS_ROOT . DIRECTORY_SEPERATOR . "modules" . DIRECTORY_SEPERATOR;
-        
-        
+
+
          $available_modules = Array();
          $directory_content = scandir($module_folder);
-        
+
          natcasesort($directory_content);
          for($i = 0;$i < count($directory_content);$i++){
              if(is_dir($module_folder . $directory_content[$i])){
                  $module_init_file = $module_folder . $directory_content[$i] . "/" .
                  $directory_content[$i] . "_main.php";
-                
-                
+
+
                  if($directory_content[$i] != ".." and $directory_content[$i] != "."){
                      if(is_file($module_init_file)){
                          array_push($available_modules, $directory_content[$i]);
                          }
                      }
                  }
-            
+
              }
          natcasesort($available_modules);
          return $available_modules;
          }
-    
+
      public function getInstalledThemes(){
          $themes = Array();
          $templateDir = ULICMS_ROOT . DIRECTORY_SEPERATOR . "templates" . DIRECTORY_SEPERATOR;
-        
+
          $folders = scanDir($templateDir);
          natcasesort($folders);
          for($i = 0; $i < count($folders); $i++){
@@ -150,18 +153,18 @@ class packageManager{
                  if(is_file($f . "oben.php") and is_file($f . "unten.php")
                          and is_file($f . "style.css"))
                      array_push($themes, $folders[$i]);
-                
+
                  }
              }
-        
+
          natcasesort($themes);
-        
+
          return $themes;
-        
+
          }
-    
+
      public function getInstalledPackages($type = 'modules'){
-        
+
          if($type === 'modules'){
              return $this -> getInstalledModules();
              }else if($type === 'themes'){
@@ -169,14 +172,14 @@ class packageManager{
              }else{
              return null;
              }
-        
-        
+
+
          }
-    
+
      public function getPackageSource(){
          return $this -> package_source;
          }
-    
+
      public function setPackageSource($url){
          $this -> package_source = $url;
          }
