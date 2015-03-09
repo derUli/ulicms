97d7dee7e34c56a0ce7bb1f436260043f9b1a68f
Ulrich Schmidt <u.schmidt@velian.de>
2015-03-09 13:51:34 +0100

    install patches

---
+++ ulicms/admin/inc/install_patches.php
@@ -0,0 +1,23 @@
+<?php
+if(defined("_SECURITY")){
+     $acl = new ACL();
+     if(is_admin() or $acl -> hasPermission("update_system")){
+
+$pkg = new PackageManager();
+
+foreach($patches as $patch){
+   $splitted = explode("|", $patch);
+   $success = $pkg->installPatch($splitted[0], $splitted[1], $splitted[2]);
+   if($success){
+      echo '<p style="color:green">'.htmlspecialchars($splitted[0])." ".get_translation("was_successfully_installed").'</p>';
+   } else {
+     echo '<p style="color:red">'.get_translation("installation_of")." ".htmlspecialchars($splitted[0])." ".get_Translation("was_failed")."</p>";
+}
+
+}
+
+} else {
+noperms();
+}
+
+}
