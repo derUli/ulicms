60bd78af2b14c0077110cc2e55e81b24310517c4
Ulrich Schmidt <admin@deruli.de>
2015-03-16 22:22:55 +0100

    link to upload video form

--- ulicms/admin/inc/videos.php
+++ ulicms/admin/inc/videos.php
@@ -5,7 +5,8 @@ $all_videos = db_query("SELECT * FROM ".tbname("videos")." ORDER by id");
 if($acl -> hasPermission("videos")){
 ?>
 <h1><?php translate("videos");?></h1>
-Coming Soon!
+<p><a href="index.php?action=add_video">[<?php translate("upload_video");?>]</a></p>
+<p>Coming Soon!</p>
 
 
 <?php 
