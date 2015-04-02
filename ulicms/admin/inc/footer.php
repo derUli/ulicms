eb0d407bf716444b08b6cfa1bdd61b0cc553ea8f
Ulrich Schmidt <admin@deruli.de>
2015-04-02 07:28:14 +0200

    Zeige Scrollup nur wenn User eingeloggt ist

--- ulicms/admin/inc/footer.php
+++ ulicms/admin/inc/footer.php
@@ -1,4 +1,6 @@
+<?php if(is_logged_in()){?>
 <a style="float:right" href="#" class="scrollup" name="scrollup" onclick="scrollToAnchor('top'); return false;"><img class="mobile-big-image" src="gfx/up.png" alt="Nach Oben" title="Nach oben"></a>
+<?php }?>
 </div>
 <p id="footer">&copy; 2011 - 2015 by <a href="http://www.ulicms.de" target="_blank">UliCMS</a></p>
 </div>
