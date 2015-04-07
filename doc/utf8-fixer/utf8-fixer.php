dafff250b9101209179a2a8a4e89923533443fdc
Ulrich Schmidt <admin@deruli.de>
2015-04-08 00:46:02 +0200

    utf8fixer

---
+++ doc/utf8-fixer/utf8-fixer.php
@@ -0,0 +1,23 @@
+<?php header("Content-Type: text/html; charset = utf-8");
+?>
+<!doctype HTML>
+<html>
+<head>
+<title>UTF-8 Fixer</title
+</head>
+<body>
+<form action="utf8-fixer.php" method="post">
+<strong>Broken</strong><br/>
+<textarea name="original" cols=80 rows=25></textarea>
+<br/><br/>
+<strong>Fixed</strong><br/>
+<textarea cols=80 rows=25><?php if(isset($_POST["original"])){
+include "Encoding.php";
+header("Content-Type: text/plain; charset=utf-8");
+$file = file_get_contents("dump-2015-02-24.sql");
+echo htmlspecialchars(ForceUTF8\Encoding::fixUTF8($file));
+}
+?></textarea>
+<br/><br/>
+<input type="submit" value="Fix!">
+</form>
