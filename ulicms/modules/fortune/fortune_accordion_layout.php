6cd02215c855ee28739b25c36f439083760a85f5
Ulrich Schmidt <admin@deruli.de>
2015-04-14 08:14:02 +0200

    code refakturiert

--- ulicms/modules/fortune/fortune_accordion_layout.php
+++ ulicms/modules/fortune/fortune_accordion_layout.php
@@ -1,9 +1,11 @@
-<h2 class="accordion-header"><?php translate("fortune");
+<h2 class="accordion-header"><?php
+
+translate ( "fortune" );
 ?></h2>
 <div class="accordion-content">
 <?php
- include_once getModulePath("fortune") . "fortune_lib.php";
- $fortune = getRandomFortune();
- echo nl2br($fortune);
+include_once getModulePath ( "fortune" ) . "fortune_lib.php";
+$fortune = getRandomFortune ();
+echo nl2br ( $fortune );
 ?>
 </div>
--- ulicms/modules/fortune/fortune_register_actions.php
+++ ulicms/modules/fortune/fortune_register_actions.php
@@ -1,3 +1,3 @@
 <?php
-$path = getModulePath("fortune") . "show_fortune.php";
-register_action("fortune", $path);
+$path = getModulePath ( "fortune" ) . "show_fortune.php";
+register_action ( "fortune", $path );
