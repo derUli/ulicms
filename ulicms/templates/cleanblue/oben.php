dcd5dbcc6944284bcda09d88cba51fa73736ccf3
Ulrich Schmidt <admin@deruli.de>
2015-04-14 08:14:22 +0200

    code refakturiert

--- ulicms/templates/cleanblue/oben.php
+++ ulicms/templates/cleanblue/oben.php
@@ -1,38 +1,48 @@
 <!doctype html>
-<html lang="<?php echo getCurrentLanguage();
+<html lang="<?php
+
+echo getCurrentLanguage ();
 ?>">
 <head>
-<meta name="viewport" content="width=1024"/>
+<meta name="viewport" content="width=1024" />
 <?php base_metas()?>
 </head>
 <body>
-<div id="root-container">
-<header>
-<section id="logo">
-<a href="./">
+	<div id="root-container">
+		<header>
+			<section id="logo">
+				<a href="./">
 <?php
-if(getconfig("logo_disabled") == "no")
-    {
-     logo();
-     ?>
-<br/>  
+if (getconfig ( "logo_disabled" ) == "no") {
+	logo ();
+	?>
+<br />  
 <?php
-     }
-else{
-     ?><strong><?php homepage_title();
-     ?></strong>
-<?php }
+} else {
+	?><strong><?php
+	
+homepage_title ();
+	?></strong>
+<?php
+
+}
 ?>
 </a>
-</section>
-<nav><?php menu("top");
-?></nav>
-</header>
-<main>
-<?php if(!containsModule()){
-     ?>
-<h1><?php headline();
-     ?></h1>
+			</section>
+			<nav><?php
+			
+menu ( "top" );
+			?></nav>
+		</header>
+		<main>
+<?php
+
+if (! containsModule ()) {
+	?>
+<h1><?php
+	
+headline ();
+	?></h1>
 <?php
      }
 ?>
