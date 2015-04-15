828894f9298df1804ee53078c77d8381beb00e2b
Ulrich Schmidt <u.schmidt@velian.de>
2015-04-15 11:43:40 +0200

    Kommentare

--- ulicms/admin/inc/cron.php
+++ ulicms/admin/inc/cron.php
@@ -1,4 +1,7 @@
 <?php
+// Es kann eingestellt werden, dass die Anwender ihr Passwort alle X Tage ändern müssen.
+// Hier wird require_password_change = 1 gesetzt, bei allen Accounts wo die letzte Passwortänderung länger als X Tage her ist.
+// wenn force_password_change_every_x_days 0 oder nicht gesetzt ist, ist diese Funktionalität deaktiviert.
 $days = getconfig("force_password_change_every_x_days");
 $days = intval($days);
 if($days > 0){
