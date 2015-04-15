2f9edd19eb2f098451d8d074101f25a9ce5f0768
Ulrich Schmidt <u.schmidt@velian.de>
2015-04-15 11:41:03 +0200

    reihenfolge von datediff parametern korrigiert

--- ulicms/admin/inc/cron.php
+++ ulicms/admin/inc/cron.php
@@ -2,5 +2,5 @@
 $days = getconfig("force_password_change_every_x_days");
 $days = intval($days);
 if($days > 0){
-	db_query ( "UPDATE " . tbname ( "users" ) . " SET require_password_change = 1 where DATEDIFF(password_changed, NOW()) > ". $days);
+	db_query ( "UPDATE " . tbname ( "users" ) . " SET require_password_change = 1 where DATEDIFF(NOW(), password_changed) > ". $days);
 }
