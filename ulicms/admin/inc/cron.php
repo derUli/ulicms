a08eb8e990dd7628bdae1884f24c2aeff9e71737
Ulrich Schmidt <admin@deruli.de>
2015-04-16 18:58:42 +0200

    in-page editing

--- ulicms/admin/inc/cron.php
+++ ulicms/admin/inc/cron.php
@@ -7,3 +7,5 @@ $days = intval($days);
 if($days > 0){
 	db_query ( "UPDATE " . tbname ( "users" ) . " SET require_password_change = 1 where DATEDIFF(NOW(), password_changed) >= ". $days);
 }
+
+add_hook("admin_cron");
