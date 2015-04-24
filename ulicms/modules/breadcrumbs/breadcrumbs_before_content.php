b552d00bdc6b3c6d8edc7d16cae5eb2f9dee43eb
Ulrich Schmidt <ulrich1992@web.de>
2015-04-24 22:45:29 +0200

    breadcrumbs

--- ulicms/modules/breadcrumbs/breadcrumbs_before_content.php
+++ ulicms/modules/breadcrumbs/breadcrumbs_before_content.php
@@ -1,17 +1,30 @@
 <?php
-$page = get_page();
-$parent = $page["parent"];
-   $parent_name = getPageSystemnameByID($parent);
-$html = "<a href=\"".buildSEOUrl($page["systemname"])."\">".htmlspecialchars($page["title"]). "</a>";
-while($parent != null){
+if(!function_exists("breadcrumbs")){
+function get_breadcrumbs(){
+  $page = get_page();
+  $parent = $page["parent"];
+  $parent_name = getPageSystemnameByID($parent);
+  $html = "<a href=\"".buildSEOUrl($page["systemname"])."\">".htmlspecialchars($page["title"]). "</a>";
+  while($parent != null){
 
-   $page = get_page();
-   $parent = $page["parent"];
-   $parent_name = getPageSystemnameByID($parent);
+    $page = get_page($parent_name);
+    $parent = $page["parent"];
+    $parent_name = getPageSystemnameByID($parent);
 
-   $html = "<a href=\"".buildSEOUrl($page["systemname"])."\">".htmlspecialchars($page["title"]). "</a>". " &gt; ".$html;
+    $html = "<a href=\"".buildSEOUrl($page["systemname"])."\">".htmlspecialchars($page["title"]). "</a>". " &gt; ".$html;
 }
 
-$html = '<div class="breadcrumb_nav">'.$html."</div>";
+    $html = "<a href=\"".buildSEOUrl(get_frontpage())."\">".get_translation("frontpage"). "</a>". " &gt; ".  $html;
 
-echo $html;
+  $html = '<div class="breadcrumb_nav">'.$html."</div>";
+  return $html;
+}
+
+
+}
+
+
+if(!function_exists("breadcrumbs")){
+function breadcrumbs(){
+  return get_breadcrumbs();
+}
