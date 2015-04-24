b9332f53216520490d4c8c9f0a2db0aa365b5697
Ulrich Schmidt <u.schmidt@velian.de>
2015-04-24 15:41:01 +0200

    kcfinder übersetzt

--- ulicms/admin/kcfinder/config.php
+++ ulicms/admin/kcfinder/config.php
@@ -1,107 +1,123 @@
 <?php
 
-/**
- * * This file is part of KCFinder project
- * 
- * @desc Base configuration file
- * @package KCFinder
- * @version 2.41
- * @author Pavel Tzonkov <pavelc@users.sourceforge.net> 
- * @copyright 2010, 2011 KCFinder Project
- * @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
- * @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
- * @link http://kcfinder.sunhater.com
- */
+/** This file is part of KCFinder project
+  *
+  *      @desc Base configuration file
+  *   @package KCFinder
+  *   @version 2.54
+  *    @author Pavel Tzonkov <sunhater@sunhater.com>
+  * @copyright 2010-2014 KCFinder Project
+  *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+  *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
+  *      @link http://kcfinder.sunhater.com
+  */
 
 // IMPORTANT!!! Do not remove uncommented settings in this file even if
 // you are using session configuration.
 // See http://kcfinder.sunhater.com/install for setting descriptions
-$_CONFIG = array (
-		'_sessionVar' => & $_SESSION ['KCFINDER'],
-		'_sessionLifetime' => 1440,
-		'disabled' => true,
-		'denyZipDownload' => false,
-		'denyUpdateCheck' => false,
-		'denyExtensionRename' => false,
-		
-		'theme' => "oxygen",
-		
-		'uploadURL' => "/content",
-		'uploadDir' => "",
-		
-		'dirPerms' => 0755,
-		'filePerms' => 0644,
-		
-		'access' => array (
-				
-				'files' => array (
-						'upload' => true,
-						'delete' => true,
-						'copy' => true,
-						'move' => true,
-						'rename' => true 
-				),
-				
-				'dirs' => array (
-						'create' => true,
-						'delete' => true,
-						'rename' => true 
-				) 
-		),
-		
-		'deniedExts' => "exe com msi bat php phps phtml php3 php4 cgi pl",
-		
-		'types' => array (
-				
-				// CKEditor & FCKEditor types
-				'files' => "",
-				'flash' => "swf",
-				'images' => "*img",
-				
-				// TinyMCE types
-				'file' => "",
-				'media' => "swf flv avi mpg mpeg qt mov wmv asf rm",
-				'image' => "*img" 
-		),
-		
-		'filenameChangeChars' => array ()/**
-		 * ' ' => "_",
-		 * ':' => "."
-		 */
-		,
-		
-		'dirnameChangeChars' => array ()/**
-		 * ' ' => "_",
-		 * ':' => "."
-		 */
-		,
-		
-		'mime_magic' => "",
-		
-		'maxImageWidth' => 0,
-		'maxImageHeight' => 0,
-		
-		'thumbWidth' => 100,
-		'thumbHeight' => 100,
-		
-		'thumbsDir' => ".thumbs",
-		
-		'jpegQuality' => 100,
-		
-		'cookieDomain' => "",
-		'cookiePath' => "",
-		'cookiePrefix' => 'KCFINDER_',
-		
-		// THE FOLLOWING SETTINGS CANNOT BE OVERRIDED WITH SESSION CONFIGURATION
-		'_check4htaccess' => true,
-		// '_tinyMCEPath' => "/tiny_mce",
-		'_sessionVar' => & $_SESSION ['KCFINDER'] 
-)
-// '_sessionLifetime' => 30,
-// '_sessionDir' => "/full/directory/path",
-// '_sessionDomain' => ".mysite.com",
-// '_sessionPath' => "/my/path",
-;
+
+$_CONFIG = array(
+
+
+// GENERAL SETTINGS
+
+    'disabled' => true,
+    'theme' => "oxygen",
+    'uploadURL' => "/content",
+    'uploadDir' => "",
+
+    'types' => array(
+
+    // (F)CKEditor types
+        'files'   =>  "",
+        'flash'   =>  "swf",
+        'images'  =>  "*img",
+
+    // TinyMCE types
+        'file'    =>  "",
+        'media'   =>  "swf flv avi mpg mpeg qt mov wmv asf rm",
+        'image'   =>  "*img",
+    ),
+
+
+// IMAGE SETTINGS
+
+    'imageDriversPriority' => "imagick gmagick gd",
+    'jpegQuality' => 90,
+    'thumbsDir' => ".thumbs",
+
+    'maxImageWidth' => 0,
+    'maxImageHeight' => 0,
+
+    'thumbWidth' => 100,
+    'thumbHeight' => 100,
+
+    'watermark' => "",
+
+
+// DISABLE / ENABLE SETTINGS
+
+    'denyZipDownload' => false,
+    'denyUpdateCheck' => true,
+    'denyExtensionRename' => false,
+
+
+// PERMISSION SETTINGS
+
+    'dirPerms' => 0755,
+    'filePerms' => 0644,
+
+    'access' => array(
+
+        'files' => array(
+            'upload' => true,
+            'delete' => true,
+            'copy'   => true,
+            'move'   => true,
+            'rename' => true
+        ),
+
+        'dirs' => array(
+            'create' => true,
+            'delete' => true,
+            'rename' => true
+        )
+    ),
+
+    'deniedExts' => "exe com msi bat php phps phtml php3 php4 cgi pl",
+
+
+// MISC SETTINGS
+
+    'filenameChangeChars' => array(/*
+        ' ' => "_",
+        ':' => "."
+    */),
+
+    'dirnameChangeChars' => array(/*
+        ' ' => "_",
+        ':' => "."
+    */),
+
+    'mime_magic' => "",
+
+    'cookieDomain' => "",
+    'cookiePath' => "",
+    'cookiePrefix' => 'KCFINDER_',
+
+
+// THE FOLLOWING SETTINGS CANNOT BE OVERRIDED WITH SESSION SETTINGS
+
+    '_check4htaccess' => true,
+    //'_tinyMCEPath' => "/tiny_mce",
+
+    '_sessionVar' => &$_SESSION['KCFINDER'],
+    //'_sessionLifetime' => 30,
+    //'_sessionDir' => "/full/directory/path",
+
+    //'_sessionDomain' => ".mysite.com",
+    //'_sessionPath' => "/my/path",
+);
 
 include_once "../../init.php";
 
