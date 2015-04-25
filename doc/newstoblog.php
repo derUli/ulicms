bfb873504a52281121c2e5ca85fbae05f0901600
Ulrich Schmidt <ulrich1992@web.de>
2015-04-25 19:05:31 +0200

    code refakturiert

--- doc/newstoblog.php
+++ doc/newstoblog.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once "init.php";
 $old_table = "alt_news";
 $new_table = "neu_blog";
@@ -7,11 +7,11 @@ $new_table = "neu_blog";
 
 $query = db_query("SELECT * FROM `".$old_table."`");
 while($row = db_fetch_object($query)){
-$date = $row->date;
-$title = db_escape(utf8_decode($row->title));
-$content = db_escape(utf8_decode($row->content));
-$shortname = md5($title);
-   $insert = "INSERT INTO `".$new_table."` (`datum`, `title`, `seo_shortname`, `language`, `entry_enabled`, `author`, `content_full`, `content_preview`, `views`) 
+	$date = $row->date;
+	$title = db_escape(utf8_decode($row->title));
+	$content = db_escape(utf8_decode($row->content));
+	$shortname = md5($title);
+	$insert = "INSERT INTO `".$new_table."` (`datum`, `title`, `seo_shortname`, `language`, `entry_enabled`, `author`, `content_full`, `content_preview`, `views`)
    VALUES ($date, '$title', '$shortname', 'de', 1, 1, '$content', '$content', 0);";   db_query($insert)or die(db_error());
 }
 ?>
--- doc/utf8-fixer/utf8-fixer.php
+++ doc/utf8-fixer/utf8-fixer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 header("Content-Type: text/html; charset = utf-8");
 include "Encoding.php";
 ?>
@@ -6,19 +6,22 @@ include "Encoding.php";
 <html>
 <head>
 <title>UTF-8 Fixer</title
+
 </head>
 <body>
-<form action="utf8-fixer.php" method="post">
-<strong>Broken</strong><br/>
-<textarea name="original" cols=80 rows=25><?php if(isset($_POST["original"])) echo htmlspecialchars($_POST["original"]);?></textarea>
-<br/><br/>
-<strong>Fixed</strong><br/>
-<textarea cols=80 rows=25><?php if(isset($_POST["original"])){
-$file = $_POST["original"];
-$file = ForceUTF8\Encoding::fixUTF8($file);
-echo htmlspecialchars($file);
-}
-?></textarea>
-<br/><br/>
-<input type="submit" value="Fix!">
-</form>
+	<form action="utf8-fixer.php" method="post">
+		<strong>Broken</strong><br />
+		<textarea name="original" cols=80 rows=25>
+		<?php if(isset($_POST["original"])) echo htmlspecialchars($_POST["original"]);?>
+		</textarea>
+		<br /> <br /> <strong>Fixed</strong><br />
+		<textarea cols=80 rows=25>
+		<?php if(isset($_POST["original"])){
+			$file = $_POST["original"];
+			$file = ForceUTF8\Encoding::fixUTF8($file);
+			echo htmlspecialchars($file);
+		}
+		?>
+		</textarea>
+		<br /> <br /> <input type="submit" value="Fix!">
+	</form>
