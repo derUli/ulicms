a66b6406f266da217b5866ce26020efb70fd523b
Ulrich Schmidt <admin@deruli.de>
2015-04-29 00:20:16 +0200

    refakturiert

--- ulicms/users_api.php
+++ ulicms/users_api.php
@@ -8,7 +8,7 @@ function getUsers() {
 	while ( $row = db_fetch_object ( $query ) ) {
 		array_push ( $users, $row->username );
 	}
-
+	
 	return $users;
 }
 function getUsersOnline() {
@@ -28,17 +28,17 @@ function resetPassword($username, $length = 8) {
 	$new_pass = rand_string ( $length );
 	$user = getUserByName ( $username );
 	if (! $user)
-	return false;
+		return false;
 	$uid = intval ( $user ["id"] );
 	changePassword ( $new_pass, $uid );
-
+	
 	db_query ( "UPDATE " . tbname ( "users" ) . " SET require_password_change = 1 where id = $uid" );
 	$message = TRANSLATION_RESET_PASSWORD_MAIL_BODY;
 	$message = str_replace ( "%host%", get_http_host (), $message );
 	$message = str_replace ( "%ip%", get_ip (), $message );
 	$message = str_replace ( "%password%", $new_pass, $message );
 	$message = str_replace ( "%username%", $user ["username"], $message );
-
+	
 	$headers = "From: " . getconfig ( "email" ) . "\n" . "Content-type: text/plain; charset=UTF-8";
 	@ulicms_mail ( $user ["email"], TRANSLATION_RESET_PASSWORD_SUBJECT, $message, $headers );
 	return true;
@@ -64,38 +64,38 @@ function adduser($username, $lastname, $firstname, $email, $password, $group, $s
 	$lastname = db_escape ( $lastname );
 	$firstname = db_escape ( $firstname );
 	$email = db_escape ( $email );
-	$admin = intval($admin);
+	$admin = intval ( $admin );
 	$password = $password;
 	$require_password_change = intval ( $require_password_change );
 	// legacy group
 	$group = intval ( $group );
 	// Default ACL Group
 	if (! $acl_group)
-	$acl_group = getconfig ( "default_acl_group" );
+		$acl_group = getconfig ( "default_acl_group" );
 	if (! $acl_group)
-	$acl_group = "NULL";
-
+		$acl_group = "NULL";
+	
 	if (is_null ( $acl_group ))
-	$acl_group = "NULL";
-
+		$acl_group = "NULL";
+	
 	add_hook ( "before_create_user" );
-
+	
 	db_query ( "INSERT INTO " . tbname ( "users" ) . "
 (username,lastname, firstname, email, password, `group`, `group_id`, `require_password_change`, `password_changed`, `admin`) VALUES ('$username', '$lastname','$firstname','$email','" . db_escape ( hash_password ( $password ) ) . "',$group, " . $acl_group . ", $require_password_change, NOW(), $admin)" ) or die ( db_error () );
 	$message = "Hallo $firstname,\n\n" . "Ein Administrator hat auf http://" . $_SERVER ["SERVER_NAME"] . " für dich ein neues Benutzerkonto angelegt.\n\n" . "Die Zugangsdaten lauten:\n\n" . "Benutzername: $username\n" . "Passwort: $password\n";
 	$header = "From: " . getconfig ( "email" ) . "\n" . "Content-type: text/plain; charset=utf-8";
-
+	
 	if ($sendMessage) {
 		@ulicms_mail ( $email, "Dein Benutzer-Account bei " . $_SERVER ["SERVER_NAME"], $message, $header );
 	}
-
+	
 	add_hook ( "after_create_user" );
 }
 function get_user_id() {
 	if (isset ( $_SESSION ["login_id"] ))
-	return intval ( $_SESSION ["login_id"] );
+		return intval ( $_SESSION ["login_id"] );
 	else
-	return 0;
+		return 0;
 }
 function user_exists($name) {
 	$query = db_query ( "SELECT * FROM " . tbname ( "users" ) . " WHERE username = '" . db_escape ( $name ) . "'" );
@@ -110,43 +110,43 @@ function register_session($user, $redirect = true) {
 	$_SESSION ["require_password_change"] = $user ["require_password_change"];
 	// Soll durch group_id und eine ACL ersetzt werden
 	$_SESSION ["group"] = $user ["group"];
-
+	
 	// Group ID
 	$_SESSION ["group_id"] = $user ["group_id"];
-
+	
 	$_SESSION ["logged_in"] = true;
 	if (is_null ( $_SESSION ["group_id"] ))
-	$_SESSION ["group_id"] = 0;
-
+		$_SESSION ["group_id"] = 0;
+	
 	$_SESSION ["session_begin"] = time ();
-
+	
 	if ($user ["notify_on_login"]) {
 		$subject = "Login auf \"" . getconfig ( "homepage_title" ) . "\" als " . $user ["username"];
 		$text = "Von der IP " . $_SERVER ["REMOTE_ADDR"] . " hat sich jemand um " . date ( "r" ) . " erfolgreich in das Benutzerkonto " . $user ["username"] . " auf dem Server " . $_SERVER ["HTTP_HOST"] . " eingeloggt.";
 		$headers = "From: " . getconfig ( "email" );
 		ulicms_mail ( $user ["email"], $subject, $text, $headers );
 	}
-
+	
 	if (! $redirect)
-	return;
-
+		return;
+	
 	if (isset ( $_REQUEST ["go"] ))
-	header ( "Location: " . $_REQUEST ["go"] );
+		header ( "Location: " . $_REQUEST ["go"] );
 	else
-	header ( "Location: index.php" );
-
+		header ( "Location: index.php" );
+	
 	return;
 }
 function validate_login($user, $password) {
 	include_once ULICMS_ROOT . "/lib/encryption.php";
 	$user = getUserByName ( $user );
-
+	
 	if ($user) {
 		if ($user ["old_encryption"])
-		$password = md5 ( $password );
+			$password = md5 ( $password );
 		else
-		$password = hash_password ( $password );
-
+			$password = hash_password ( $password );
+		
 		if ($user ["password"] == $password) {
 			return $user;
 		}
