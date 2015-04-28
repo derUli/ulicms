e53b8507b3f9c5ced6976c23c26cee3be0833376
Ulrich Schmidt <admin@deruli.de>
2015-04-29 00:40:21 +0200

    code refakturiert

--- ulicms/cron.php
+++ ulicms/cron.php
@@ -5,9 +5,9 @@ flush ();
 $empty_trash_days = getconfig ( "empty_trash_days" );
 
 if ($empty_trash_days === false)
-$empty_trash_days = 30;
-
-// Papierkorb für Seiten Cronjob
+	$empty_trash_days = 30;
+	
+	// Papierkorb für Seiten Cronjob
 $empty_trash_timestamp = $empty_trash_days * (60 * 60 * 24);
 db_query ( "DELETE FROM " . tbname ( "content" ) . " WHERE " . time () . " -  `deleted_at` > $empty_trash_timestamp" ) or die ( db_error () );
 
@@ -19,21 +19,21 @@ $version = new ulicms_version ();
 $developmentVersion = "";
 
 if ($version->getDevelopmentVersion ())
-$developmentVersion = " Vorabversion";
-
-// Start Call Home //
+	$developmentVersion = " Vorabversion";
+	
+	// Start Call Home //
 $cfg_script = "UliCMS " . $version->getVersion () . " (v" . join ( ".", $version->getInternalVersion () ) . $developmentVersion . ")";
 $cfg_url = "http://www.ulicms.de/chs/api.php";
 
 $urlfrom = $_SERVER ['HTTP_HOST'];
 
 if (! is_file ( "init.php" ) and ! is_dir ( "libs" ))
-exit ();
+	exit ();
 
 $folderfrom = str_replace ( "\\", "/", dirname ( $_SERVER ['SCRIPT_NAME'] ) );
 
 if (! endsWith ( $folderfrom, "/" ))
-$folderfrom .= "/";
+	$folderfrom .= "/";
 
 $var_url = get_site_protocol () . $urlfrom . $folderfrom;
 
