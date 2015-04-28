0611f95a016890719e5cb87ff727f6ba106ee713
Ulrich Schmidt <admin@deruli.de>
2015-04-29 00:44:54 +0200

    code refactured

--- ulicms/admin/inc/categories.php
+++ ulicms/admin/inc/categories.php
@@ -3,28 +3,28 @@ $acl = new ACL ();
 if (! is_admin () and ! $acl->hasPermission ( "categories" )) {
 	noperms ();
 } else {
-
+	
 	// Create
 	if (isset ( $_REQUEST ["create"] )) {
 		if (! empty ( $_REQUEST ["name"] )) {
 			categories::addCategory ( $_REQUEST ["name"], $_REQUEST ["description"] );
 		}
 	}
-
+	
 	// Create
 	if (isset ( $_REQUEST ["update"] )) {
 		if (! empty ( $_REQUEST ["name"] ) and ! empty ( $_REQUEST ["id"] )) {
 			categories::updateCategory ( intval ( $_REQUEST ["id"] ), $_REQUEST ["name"], $_REQUEST ["description"] );
 		}
 	}
-
+	
 	// Delete
 	if (isset ( $_GET ["del"] )) {
 		$del = intval ( $_GET ["del"] );
 		if ($del != 1)
-		categories::deleteCategory ( $del );
+			categories::deleteCategory ( $del );
 	}
-
+	
 	include_once ULICMS_ROOT . DIRECTORY_SEPERATOR . "lib" . DIRECTORY_SEPERATOR . "string_functions.php";
 	if (isset ( $_GET ["order"] ) and in_array ( $_GET ["order"], array (
 			"id",
@@ -32,64 +32,63 @@ if (! is_admin () and ! $acl->hasPermission ( "categories" )) {
 			"description",
 			"created",
 			"updated" 
-			) ))
-			$order = db_escape ( $_GET ["order"] );
-			else
-			$order = "id";
-
-			$categories = categories::getAllCategories ( $order );
-
-			?>
+	) ))
+		$order = db_escape ( $_GET ["order"] );
+	else
+		$order = "id";
+	
+	$categories = categories::getAllCategories ( $order );
+	
+	?>
 
 			<?php
-			if (! isset ( $_GET ["add"] ) and ! isset ( $_GET ["edit"] )) {
-				?>
+	if (! isset ( $_GET ["add"] ) and ! isset ( $_GET ["edit"] )) {
+		?>
 
 <h2>
 <?php
-
-echo TRANSLATION_CATEGORIES;
-?>
+		
+		echo TRANSLATION_CATEGORIES;
+		?>
 </h2>
 <p>
 <?php
-
-echo TRANSLATION_CATEGORIES_INFOTEXT;
-?>
+		
+		echo TRANSLATION_CATEGORIES_INFOTEXT;
+		?>
 </p>
 
 <p>
 	<a href="?action=categories&add"><?php
-
-	echo TRANSLATION_CREATE_CATEGORY;
-	?> </a>
+		
+		echo TRANSLATION_CREATE_CATEGORY;
+		?> </a>
 </p>
-	<?php
-
-			}
-			?>
+<?php
+	}
+	?>
 
 			<?php
-			if (count ( $categories ) > 0 and ! isset ( $_GET ["add"] ) and ! isset ( $_GET ["edit"] )) {
-				?>
+	if (count ( $categories ) > 0 and ! isset ( $_GET ["add"] ) and ! isset ( $_GET ["edit"] )) {
+		?>
 <table class="tablesorter">
 
 	<thead>
 		<tr>
 			<th style="min-width: 50px;"><a href="?action=categories&order=id"><?php
-
-			echo TRANSLATION_ID;
-			?> </a></th>
+		
+		echo TRANSLATION_ID;
+		?> </a></th>
 
 			<th style="min-width: 200px;"><a href="?action=categories&order=name"><?php
-
-			echo TRANSLATION_NAME;
-			?> </a></th>
+		
+		echo TRANSLATION_NAME;
+		?> </a></th>
 			<th style="min-width: 200px;"><a
 				href="?action=categories&order=description"><?php
-
-				echo TRANSLATION_DESCRIPTION;
-				?> </a></th>
+		
+		echo TRANSLATION_DESCRIPTION;
+		?> </a></th>
 			<td></td>
 			<td></td>
 		</tr>
@@ -97,60 +96,57 @@ echo TRANSLATION_CATEGORIES_INFOTEXT;
 	
 	<tbody>
 	<?php
-
-	foreach ( $categories as $category ) {
-		?>
+		
+		foreach ( $categories as $category ) {
+			?>
 		<tr>
 			<td><?php
-
+			
 			echo $category ["id"];
 			?></td>
 			<td style="padding-right: 20px;"><?php
-
+			
 			echo real_htmlspecialchars ( $category ["name"] );
 			?></td>
 			<td style="padding-right: 20px;"><?php
-
+			
 			echo nl2br ( real_htmlspecialchars ( $category ["description"] ) );
 			?></td>
 			<td style="text-align: center;"><a
 				href="?action=categories&edit=<?php
 			
-echo $category ["id"];
+			echo $category ["id"];
 			?>"><img src="gfx/edit.png" class="mobile-big-image"
 					alt="<?php
 			
-echo TRANSLATION_EDIT;
+			echo TRANSLATION_EDIT;
 			?>"
 					title="<?php
 			
-echo TRANSLATION_EDIT;
-			?>">
-			
-			</td>
+			echo TRANSLATION_EDIT;
+			?>"></td>
 			<?php
-
+			
 			if ($category ["id"] != 1) {
 				?>
 
 			<td style="text-align: center;"><a
 				href="?action=categories&del=<?php
 				
-echo $category ["id"];
+				echo $category ["id"];
 				?>"
 				onclick="return confirm('Wirklich Löschen?')"><img
 					class="mobile-big-image" src="gfx/delete.gif"
 					alt="<?php
 				
-echo TRANSLATION_DELETE;
+				echo TRANSLATION_DELETE;
 				?>"
 					title="<?php
 				
-echo TRANSLATION_DELETE;
+				echo TRANSLATION_DELETE;
 				?>"> </a></td>
 
 				<?php
-
 			} else {
 				?>
 			<td style="text-align: center;"><a href="#"
@@ -158,44 +154,41 @@ echo TRANSLATION_DELETE;
 					class="mobile-big-image" src="gfx/delete.gif"
 					alt="<?php
 				
-echo TRANSLATION_DELETE;
+				echo TRANSLATION_DELETE;
 				?>"
 					title="<?php
 				
-echo TRANSLATION_DELETE;
+				echo TRANSLATION_DELETE;
 				?>"> </a></td>
 				<?php
-
 			}
 			?>
 
 		</tr>
 		<?php
-
-	}
-	?>
+		}
+		?>
 	</tbody>
 </table>
-	<?php
-
-			} else if (isset ( $_GET ["add"] )) {
-				?>
+<?php
+	} else if (isset ( $_GET ["add"] )) {
+		?>
 <h2>
 <?php
-
-echo TRANSLATION_CREATE_CATEGORY;
-?>
+		
+		echo TRANSLATION_CREATE_CATEGORY;
+		?>
 </h2>
 <form action="?action=categories" method="post">
 <?php
-
-csrf_token_html ();
-?>
+		
+		csrf_token_html ();
+		?>
 	<p>
 	<?php
-
-	echo TRANSLATION_NAME;
-	?>
+		
+		echo TRANSLATION_NAME;
+		?>
 		<input type="text" required="true" name="name" required="true"
 			value="">
 
@@ -203,71 +196,74 @@ csrf_token_html ();
 
 	<p>
 	<?php
-
-	echo TRANSLATION_DESCRIPTION;
-	?>
-		<br />
-		<textarea cols="50" name="description" rows="5" maxlength="255"></textarea>
+		
+		echo TRANSLATION_DESCRIPTION;
+		?>
+		<br /> <textarea cols="50" name="description" rows="5" maxlength="255"></textarea>
 	</p>
 	<p>
 		<input type="submit" name="create"
 			value="<?php
 		
-echo TRANSLATION_CREATE;
+		echo TRANSLATION_CREATE;
 		?>">
 	</p>
 
 
 </form>
 
-		<?php
-
-			} else if (isset ( $_GET ["edit"] )) {
-				?>
+<?php
+	} else if (isset ( $_GET ["edit"] )) {
+		?>
 <h2>
 <?php
-
-echo TRANSLATION_EDIT_CATEGORY;
-?>
+		
+		echo TRANSLATION_EDIT_CATEGORY;
+		?>
 </h2>
 <form action="?action=categories" method="post">
 <?php
-
-csrf_token_html ();
-?>
+		
+		csrf_token_html ();
+		?>
 	<input type="hidden" name="id"
 		value="<?php echo intval($_GET["edit"])?>">
 	<p>
 	<?php
-
-	echo TRANSLATION_NAME;
-	?>
+		
+		echo TRANSLATION_NAME;
+		?>
 		<input type="text" name="name" required="true"
 			value="<?php
 		
-echo categories::getCategoryById ( intval ( $_GET ["edit"] ) );
+		echo categories::getCategoryById ( intval ( $_GET ["edit"] ) );
 		?>">
 	</p>
 
 	<p>
 	<?php
-
-	echo TRANSLATION_DESCRIPTION;
-	?>
-		<br />
-		<textarea cols="50" name="description" rows="5" maxlength="255"><?php
-
+		
+		echo TRANSLATION_DESCRIPTION;
+		?>
+		<br /> <textarea cols="50" name="description" rows="5" maxlength="255"><?php
+		
 		echo htmlspecialchars ( categories::getCategoryDescriptionById ( intval ( $_GET ["edit"] ) ) );
 		?></textarea>
 	</p>
 	<p>
 		<input type="submit" name="update"
-			value="<?php echo TRANSLATION_SAVE;
-         ?>">
+			value="<?php
+		
+echo TRANSLATION_SAVE;
+		?>">
 	</p>
 </form>
 
-         <?php }
-         ?>
-         <?php }
-         ?>
+<?php
+	
+}
+	?>
+         <?php
+
+}
+?>
