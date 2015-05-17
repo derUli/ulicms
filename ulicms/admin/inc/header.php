ce0dc23fbb370ed46b69455703888a2d93222b7c
Ulrich Schmidt <admin@deruli.de>
2015-05-18 00:48:32 +0200

    apple-touch icon hinzugefügt

--- ulicms/admin/inc/header.php
+++ ulicms/admin/inc/header.php
@@ -51,9 +51,15 @@ if (is_logged_in ()){
 <script src="scripts/vallenato/vallenato.js" type="text/javascript"></script>
 <link rel="stylesheet" href="scripts/vallenato/vallenato.css"
 	type="text/css">
-<link rel="icon" href="gfx/favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="gfx/favicon.ico" type="image/x-icon">
-
+  <link rel="shortcut icon" href="gfx/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="gfx/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="gfx/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="gfx/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="gfx/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="gfx/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="gfx/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="gfx/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="gfx/apple-touch-icon-152x152.png" />
 <link rel="stylesheet" type="text/css"
 	href="codemirror/lib/codemirror.css">
 <script src="codemirror/lib/codemirror.js" type="text/javascript"></script>
