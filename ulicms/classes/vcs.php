5dd5f8acb3718eecaae8950250dfaca3ad1c5ed7
Ulrich Schmidt <admin@deruli.de>
2015-05-26 19:22:45 +0200

    createRevision

--- ulicms/classes/vcs.php
+++ ulicms/classes/vcs.php
@@ -1,2 +1,10 @@
 <?php
-class VCS
+// Version Control System for pages
+class VCS{
+   public static function createRevision($content_id, $content, $user_id){
+       $content_id = intval($content_id);
+       $content = db_escape($content);       
+       $user_id = intval($user_id);
+       db_query("INSERT INTO `".tbname("history")."` (content_id, content, user_id) VALUES($content_id, '$content', $user_id)");
+         }
+}
