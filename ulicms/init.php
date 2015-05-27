52f86fee0ec7bab6817c5a4bf5a11681bc73a759
Ulrich Schmidt <admin@deruli.de>
2015-05-27 20:49:31 +0200

    poweredby

--- ulicms/init.php
+++ ulicms/init.php
@@ -227,11 +227,7 @@ if (! defined ("SKIP_TABLE_CHECK")){
      tbname ("settings"),
     );
 
-if(isset($_GET["output_scripts"])){
-  getCombinedScripts();
-} else if(isset($_GET["output_stylesheets"])){
-  getCombinedStylesheets();
-}
+
 
  for($i = 0; $i < count ($required_tables); $i ++){
      $table = $required_tables [$i];
@@ -247,6 +243,12 @@ if(isset($_GET["output_scripts"])){
 
 if (! getconfig ("hide_meta_generator"))
  @header ('X-Powered-By: UliCMS Release ' . cms_version ());
+
+if(isset($_GET["output_scripts"])){
+  getCombinedScripts();
+} else if(isset($_GET["output_stylesheets"])){
+  getCombinedStylesheets();
+}
 
 $memory_limit = getconfig ("memory_limit");
 
