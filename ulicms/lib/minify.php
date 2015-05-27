7a515224783e889646e2ff9751536d135d02c6fa
Ulrich Schmidt <admin@deruli.de>
2015-05-27 20:23:15 +0200

    minify

--- ulicms/lib/minify.php
+++ ulicms/lib/minify.php
@@ -67,3 +67,69 @@ function getCombinedScriptURL(){
 }
 
 // Ab hier Stylesheet Funktionen
+
+function resetStylesheetQueue(){
+   $_SERVER["stylesheet_queue"] = array();
+}
+
+function enqueueStylesheetFile($path){
+   if(!isset($_SERVER["stylesheet_queue"])){
+         $_SERVER["stylesheet_queue"] = array();
+   }
+   $_SERVER["stylesheet_queue"][] = $path;
+   
+}
+
+function getCombinedStylesheets(){
+   $output = "";
+   if(isset($_GET["output_stylesheets"])){
+      $stylesheets = explode(";", $_GET["output_stylesheets"]);
+      foreach($stylesheets as $stylesheet){
+          if(is_file($stylesheet)){
+             $ext = pathinfo($stylesheet, PATHINFO_EXTENSION);
+             if($ext == "css"){
+                $content = @file_get_contents($stylesheet);
+ 
+                if($content){
+                   $content = str_replace("\r\n", "\n", $content);
+                   $content = str_replace("\r", "\n", $content);
+                   $content = str_replace("\n", "\r\n", $content);
+                   $content = trim($content);
+                   $output .= "/* Stylesheet ".$stylesheet." */\r\n";
+                   $output .= $content;
+                   $output .= "\r\n";
+                   $output .= "\r\n";
+                }
+             }
+             
+          }
+      }
+   }
+   header("Content-Type: text/css");
+   $len = mb_strlen($content , 'binary');
+   header("Content-Length". $len);
+   
+   echo $output;
+   exit();
+}
+
+function combined_stylesheet_html(){
+  echo get_combined_stylesheet_html();
+}
+
+function get_combined_stylesheet_html(){
+   $html = '<link rel="stylesheet" type="text/css" href="'.getCombinedStylesheetURL().'"/>';
+   return $html;
+}
+
+function getCombinedStylesheetURL(){
+  $output = "";
+  if(isset($_SERVER["stylesheet_queue"]) and is_array($_SERVER["stylesheet_queue"])){
+  $files = implode(";", $_SERVER["stylesheet_queue"]);
+     $url = "?output_stylesheets=".$files;
+  } else {
+     $url="index.php?stylesheets=";
+  }
+   
+   return $url;
+}
