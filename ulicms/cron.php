42272316fa5c766d6d4f7762665cc2528316a368
Ulrich Schmidt <ulrich1992@web.de>
2015-07-04 18:08:25 +0200

    typo

--- ulicms/cron.php
+++ ulicms/cron.php
@@ -3,7 +3,7 @@ require_once "init.php";
 fcflush ();
 
 if(getconfig("delete_ips_after_48_hours")){
-   db_query("Update ".tbname("log")." SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2")
+   db_query("Update ".tbname("log")." SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2");
 }
 
 $empty_trash_days = getconfig ( "empty_trash_days" );
