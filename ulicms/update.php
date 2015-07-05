e78749bf271139ac1d87087509470e5888cfde8c
Ulrich Schmidt <ulrich1992@web.de>
2015-07-05 15:51:09 +0200

    update auch von 9.0.0 möglich

--- ulicms/update.php
+++ ulicms/update.php
@@ -1,8 +1,40 @@
 <?php
-define ("SKIP_TABLE_CHECK", true);
 include_once "init.php";
+define("SKIP_TABLE_CHECK", true);
+
+// Änderungen in der Datenbank von 9.0.1
+db_query ( "CREATE TABLE IF NOT EXISTS `" . tbname ( "mails" ) . "` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `headers` TEXT NOT NULL,
+  `to` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `body` mediumtext NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;" );
+
+db_query ( "CREATE TABLE IF NOT EXISTS `" . tbname ( "history" ) . "` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content_id` int(11) NOT NULL,
+  `content` longtext NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;" );
+
+
+// comments Ordner weggelöschen, sofern er noch existiert, da dieser nicht mehr benötigt wird.
+$comments_dir = ULICMS_ROOT . "/comments";
+
+if(is_dir($comments_dir)){
+   @SureRemoveDir ( $comments_dir, true );
+}
+
+setconfig ( "db_schema_version", "9.0.1" );
 
 $pkg = new PackageManager ();
 $pkg->truncateInstalledPatches ();
+
 // @unlink ("update.php");
 ulicms_redirect ( "admin/" );
+
