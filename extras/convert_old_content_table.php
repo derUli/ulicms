2e08f87ab78d4f44883c5e5767e1a86bd058b487
Ulrich Schmidt <admin@deruli.de>
2015-07-15 21:48:53 +0200

    value != "-"

--- extras/convert_old_content_table.php
+++ extras/convert_old_content_table.php
@@ -1,14 +1,14 @@
 <?php
 include_once "init.php";
-$old_table = "old_table";
-$new_table = "new_table";
+$old_table = "old_cotent";
+$new_table = "new_content";
 
 $old_content = db_query("SELECT * FROM $old_table");
 while($row = db_fetch_assoc($old_content)){
   
   foreach($row as $key=>$value){
       if($key == "parent"){
-         if(!is_null($value) and !empty($value)){
+         if(!is_null($value) and !empty($value) and $value != "-"){
          $sub_query = db_query("select id from $old_table where systemname='".db_escape($value)."'");
          $result = db_fetch_assoc($sub_query);
          $row["parent"] = $result["id"];
@@ -23,10 +23,10 @@ while($row = db_fetch_assoc($old_content)){
 created, lastchangeby, autor, views, comments_enabled, redirection, menu, position, parent, lastmodified)
 
 VALUES (".$row["id"].",0, '".db_escape(utf8_decode($row["systemname"]))."', 
-'".db_escape($row["title"])."', 
-".db_escape($row["content"])."',
+'".db_escape(utf8_decode($row["title"]))."', 
+'".db_escape(utf8_decode($row["content"]))."',
  1, ".time().",
 1, 1, 0, 0, 
 '', '".db_escape($row["menu"])."', 
-".$row["position"].", ".$row["parent"].", ".time().")");
+".$row["position"].", ".$row["parent"].", ".time().")")or die (db_error());
 }
