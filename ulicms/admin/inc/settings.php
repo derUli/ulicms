ff63f675ed0924342b38dbcb05fedfb78af33190
Ulrich Schmidt <admin@deruli.de>
2015-07-20 21:51:09 +0200

    settings

--- ulicms/admin/inc/settings.php
+++ ulicms/admin/inc/settings.php
@@ -29,12 +29,10 @@ if(defined("_SECURITY")){
 <?php
                  echo '<tr>';
                  echo "<td style=\"width:40px;\">--></td>";
-                 echo "<td>" . htmlspecialchars($row -> name, ENT_QUOTES, "UTF-8") . "</td>";
-                 echo "<td style=\"word-break:break-all;\">" . nl2br(htmlspecialchars($row -> value)) . "</td>";
+                 echo "<td>" . htmlspecialchars ($row -> name, ENT_QUOTES, "UTF-8") . "</td>";
+                 echo "<td style=\"word-break:break-all;\">" . nl2br (htmlspecialchars ($row -> value)) . "</td>";
                  echo "<td style=\"text-align:center\">" . '<a href="index.php?action=key_edit&key=' . $row -> id . '"><img src="gfx/edit.png" class="mobile-big-image" alt="' . TRANSLATION_EDIT . '" title="' . TRANSLATION_EDIT . '"></a></td>';
-                 echo "<td style=\"text-align:center;\">" . '<a href="index.php?action=key_delete&key=' .
-                 htmlspecialchars($row -> name,
-                     ENT_QUOTES) . '" onclick="return confirm(\'' . TRANSLATION_ASK_FOR_DELETE . '\');"><img src="gfx/delete.gif" class="mobile-big-image" alt="' . TRANSLATION_DELETE . '" title="' . TRANSLATION_DELETE . '"></a></td>';
+                 echo "<td style=\"text-align:center;\">" . '<form action="index.php?action=key_delete&key=' . htmlspecialchars ($row -> name, ENT_QUOTES) . '" onsubmit="return confirm(\'' . TRANSLATION_ASK_FOR_DELETE . '\');" method="post"><input type="image" src="gfx/delete.gif" class="mobile-big-image" alt="' . TRANSLATION_DELETE . '" title="' . TRANSLATION_DELETE . '">'.get_csrf_token_html().'</form></td>';
                  echo '</tr>';
                 
                  }
