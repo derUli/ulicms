9799669e78c91c69bdc1dd4c12aa6e5f8c278ba0
Ulrich Schmidt <admin@deruli.de>
2015-07-21 11:16:40 +0200

    fi

--- extras/convert_old_content_table.php
+++ extras/convert_old_content_table.php
@@ -28,10 +28,10 @@ while($row = db_fetch_assoc($old_content)){
   db_query("INSERT INTO `".$new_table."` (id, notinfeed, systemname, title, content, active,
 created, lastchangeby, autor, views, comments_enabled, menu, position, parent, lastmodified, language, redirection, access)
 
-VALUES (".$row["id"].",0, '".db_escape(utf8_decode($row["systemname"]))."', 
-'".db_escape(utf8_decode($row["title"]))."', 
-'".db_escape(utf8_decode($row["content"]))."',
+VALUES (".$row["id"].",0, '".db_escape($row["systemname"])."', 
+'".db_escape($row["title"])."', 
+'".db_escape($row["content"])."',
  1, ".time().",
 1, 1, 0, 0, '".db_escape($row["menu"])."', 
-".$row["position"].", ".$row["parent"].", ".time().", 'de', '".db_escape(utf8_decode($row["redirection"]))."', 'all')")or die (db_error());
+".$row["position"].", ".$row["parent"].", ".time().", 'de', '".db_escape($row["redirection"])."', 'all')")or die (db_error());
 }
