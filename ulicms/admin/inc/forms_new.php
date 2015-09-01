f3ee3e2a76706c5d13715916e9cc91bcbe389dcf
Ulrich Schmidt <admin@deruli.de>
2015-09-01 02:11:23 +0200

    create form

--- ulicms/admin/inc/forms_new.php
+++ ulicms/admin/inc/forms_new.php
@@ -7,7 +7,21 @@ if (!$acl -> hasPermission ("forms")){
 	$forms = Forms::getAllForms();
 	?>
 	<h1><?php translate("create_form");?></h1>
+	<form action="index.php?action=forms" method="post">
+	<p><strong><?php translate("name");?></strong><br/>
+	<input type="text" value="" name="name" required="true"/></p>
 	
+	<p><strong><?php translate("email_to");?></strong><br/>
+	<input type="email" value="" name="email_to" required="true"/></p>
+	
+	<p><strong><?php translate("subject");?></strong><br/>
+	<input type="text" value="" name="subject" required="true"/></p>
+	<p><strong><?php translate("category");?></strong><br/>
+	<?php
+	echo categories::getHTMLSelect ();
+	?></p>
+	
+	</form>
 <?php 
 }
 ?>
