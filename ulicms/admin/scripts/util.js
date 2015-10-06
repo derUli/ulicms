9e11193400f96cd72e0e7f26fab18f75e7454f30
Ulrich Schmidt <u.schmidt@velian.de>
2015-10-06 15:59:15 +0200

    Shake Effekt zu util.js hinzugegefügt

--- ulicms/admin/scripts/util.js
+++ ulicms/admin/scripts/util.js
@@ -4,3 +4,18 @@ function scrollToAnchor(aid) {
 		scrollTop : aTag.offset().top
 	}, 'slow');
 }
+
+function shake(div){                                                                                                                                                                                            
+    var interval = 100;                                                                                                 
+    var distance = 10;                                                                                                  
+    var times = 4;                                                                                                      
+
+    $(div).css('position','relative');                                                                                  
+
+    for(var iter=0;iter<(times+1);iter++){                                                                              
+        $(div).animate({ left: ((iter%2==0 ? distance : distance*-1))},interval);                                   
+    }//for                                                                                                              
+
+    $(div).animate({ left: 0},interval);                                                                                
+
+}//shake   
