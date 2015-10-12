4e2627d76f499804e9b5cf3517529d5d799f381d
Ulrich Schmidt <u.schmidt@velian.de>
2015-10-12 13:17:24 +0200

    vernünftige urls in newstoblog.php

--- extras/newstoblog.php
+++ extras/newstoblog.php
@@ -1,5 +1,6 @@
 <?php
 include_once "init.php";
+include_once ULICMS_ROOT."/lib/string_functions.php";
 $old_table = "alt_news";
 $new_table = "neu_blog";
 
@@ -10,7 +11,7 @@ while ( $row = db_fetch_object ( $query ) ) {
 	$date = $row->date;
 	$title = db_escape ( utf8_decode ( $row->title ) );
 	$content = db_escape ( utf8_decode ( $row->content ) );
-	$shortname = md5 ( $title );
+	$shortname = cleanString ( $title );
 	$insert = "INSERT INTO `" . $new_table . "` (`datum`, `title`, `seo_shortname`, `language`, `entry_enabled`, `author`, `content_full`, `content_preview`, `views`, `comments_enabled`)
    VALUES ($date, '$title', '$shortname', 'de', 1, 1, '$content', '$content', 0, 1);";
 	db_query ( $insert ) or die ( db_error () );
