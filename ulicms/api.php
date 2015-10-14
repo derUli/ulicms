b5b9b4ea7e73ee8de49758aacb5a8de7e548998d
Ulrich Schmidt <u.schmidt@velian.de>
2015-10-14 12:52:23 +0200

    prefered_language(array , )

--- ulicms/api.php
+++ ulicms/api.php
@@ -1,5 +1,31 @@
 <?php
 
+function get_prefered_language(array $available_languages, $http_accept_language) {
+
+    $available_languages = array_flip($available_languages);
+
+    $langs;
+    preg_match_all('~([\w-]+)(?:[^,\d]+([\d.]+))?~', strtolower($http_accept_language), $matches, PREG_SET_ORDER);
+    foreach($matches as $match) {
+
+        list($a, $b) = explode('-', $match[1]) + array('', '');
+        $value = isset($match[2]) ? (float) $match[2] : 1.0;
+
+        if(isset($available_languages[$match[1]])) {
+            $langs[$match[1]] = $value;
+            continue;
+        }
+
+        if(isset($available_languages[$a])) {
+            $langs[$a] = $value - 0.1;
+        }
+
+    }
+    arsort($langs);
+
+    return $langs;
+}
+
 function get_google_fonts(){
     $retval = array();
     $file = ULICMS_ROOT."/lib/webFontNames.opml";
