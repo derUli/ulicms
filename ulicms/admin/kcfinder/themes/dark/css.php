5c727b8c980ac1915e91900bebf10160db03fc7b
Ulrich Schmidt <admin@deruli.de>
2015-10-14 23:25:21 +0200

    Revert "KCFinder auf KCFinder 3.20 Test 2 aktualisiert"
    This reverts commit 7c370b3d304ed3999c5d0bb3cd9eb1a9a9cbd1cc.

--- ulicms/admin/kcfinder/themes/dark/css.php
+++ ulicms/admin/kcfinder/themes/dark/css.php
@@ -7,4 +7,6 @@ chdir("..");
 require "core/autoload.php";
 $theme = basename(dirname(__FILE__));
 $min = new minifier("css");
-$min->minify("cache/theme_$theme.css");
+$min -> minify("cache/theme_$theme.css");
+
+?>
