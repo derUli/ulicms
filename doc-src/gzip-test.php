29e7b800edb644b6a428e5722dec10d22cb2673d
Ulrich Schmidt <u.schmidt@velian.de>
2015-10-21 10:38:41 +0200

    gzip

---
+++ doc-src/gzip-test.php
@@ -0,0 +1,11 @@
+<?php
+// Bevor der erste Content ausgegeben wird.
+if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+    {
+      ob_start("ob_gzhandler");
+    }else{
+        ob_start(); 
+    }
+
+// Nach der letzten Ausgabe
+   ob_end_flush();
