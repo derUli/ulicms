75cb648715cefb5fb4856caeaf66ecada52ea769
Ulrich Schmidt <u.schmidt@velian.de>
2015-10-22 14:30:01 +0200

    todo

--- doc-src/gzip-test.php
+++ doc-src/gzip-test.php
@@ -4,8 +4,9 @@ if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
     {
       ob_start("ob_gzhandler");
     }else{
-        ob_start(); 
+        ob_start();
     }
+  }
 
 // Nach der letzten Ausgabe
    ob_end_flush();
