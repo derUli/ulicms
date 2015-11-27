da6f5dd544df25761db3fb493daadb877e2afe20
Ulrich Schmidt <ulrich1992@web.de>
2015-11-27 17:36:43 +0100

    keine development version

--- ulicms/version.php
+++ ulicms/version.php
@@ -10,7 +10,7 @@ if (! class_exists ( "ulicms_version" )) {
 					0 
 			);
 			$this->update = "";
-			$this->developmentVersion = true;
+			$this->developmentVersion = false;
 		}
 		public function getReleaseYear() {
 			return strval ( $this->releaseYear );
