24895ace144779d3e688b0268a350351e7ec6f9c
Ulrich Schmidt <admin@deruli.de>
2015-12-03 20:01:10 +0100

    package

--- ulicms/update.php
+++ ulicms/update.php
@@ -4,8 +4,6 @@ include_once "init.php";
 
 @set_time_limit ( 0 );
 
-setconfig ( "db_schema_version", "9.8.1" );
-
 // Patch Manager zurücksetzen
 $pkg = new PackageManager ();
 $pkg->truncateInstalledPatches ();
