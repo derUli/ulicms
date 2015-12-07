20872def03aa29580b7c0ae2d22fc80e125b44dd
Ulrich Schmidt <u.schmidt@velian.de>
2015-12-07 10:21:04 +0100

    copy paste fehler korrigiert

--- ulicms/admin/inc/modules.php
+++ ulicms/admin/inc/modules.php
@@ -6,13 +6,13 @@ if (! $acl->hasPermission ( "list_packages" )) {
 	if(isset($_POST["truncate_installed_patches"]) and $acl->hasPermission("patch_management")){
 	    db_query("TRUNCATE TABLE ".tbname("installed_patches"));
 	}
-	
+
 	if ($acl->hasPermission ( "remove_packages" )) {
-		
+
 		// Modul deinstallieren
 		if (isset ( $_GET ["remove"] )) {
 			$remove = basename ( $_GET ["remove"] );
-			
+
 			$type = $_GET ["type"];
 			$uninstalled = uninstall_module ( $remove, $type );
 			if ($uninstalled)
@@ -27,7 +27,7 @@ if (! $acl->hasPermission ( "list_packages" )) {
 		?>
 <p style="margin-bottom: 30px;">
 	<a href="?action=install_method">[<?php
-		
+
 		echo TRANSLATION_INSTALL_PACKAGE;
 		?>]</a>
 </p>
@@ -35,13 +35,13 @@ if (! $acl->hasPermission ( "list_packages" )) {
 	}
 	?>
 <strong><?php
-	
+
 	echo TRANSLATION_INSTALLED_MODULES;
 	?>
 </strong>
 <p>
 <?php
-	
+
 	echo TRANSLATION_INSTALLED_MODULES_INFO;
 	?>
 </p>
@@ -54,17 +54,17 @@ if (! $acl->hasPermission ( "list_packages" )) {
 		echo "<ol style=\"margin-bottom:30px;\">";
 		for($i = 0; $i < count ( $modules ); $i ++) {
 			echo "<li style=\"margin-bottom:10px;border-bottom:solid #cdcdcd 1px;\" id=\"dataset-module-".$modules[$i]."\"><strong>";
-			
+
 			$module_has_admin_page = file_exists ( getModuleAdminFilePath ( $modules [$i] ) );
-			
+
 			echo getModuleName ( $modules [$i] );
 			$version = getModuleMeta($modules [$i], "version");
 			$color = null;
-			
+
 			if($version != null){
-			
+
 			$status = $pkg->checkForNewerVersionOfPackage($modules[$i]);
-			
+
 			if($status){
 			   if (version_compare($status, $version, '>')) {
 			       $color = "red";
@@ -72,38 +72,38 @@ if (! $acl->hasPermission ( "list_packages" )) {
 			} else{
 			      $color = "green";
 			}
-			
+
 			}
-			
+
 			if($color){
 			   echo '<span style="color: '.$color.'">';
 			}
-			
+
 			   echo " ".$version;
-			   
-			   
+
+
 			if($color){
 			   echo "</span>";
 			}
-			
+
 			}
-			
+
 			echo "</strong>";
-			
+
 			echo "<div style=\"float:right\">";
-			
+
 			if ($module_has_admin_page) {
 				echo "<a style=\"font-size:0.8em;\" href=\"?action=module_settings&module=" . $modules [$i] . "\">";
 				echo "[" . TRANSLATION_SETTINGS . "]";
 				echo "</a>";
 			}
-			
+
 			if ($acl->hasPermission ( "remove_packages" )) {
 				echo " <a style=\"font-size:0.8em;\" href=\"?action=modules&remove=" . $modules [$i] . "&type=module\" onclick=\"return uninstallModule(this.href, '".$modules[$i]."');\">";
 				echo " [" . TRANSLATION_DELETE . "]";
 				echo "</a>";
 			}
-			
+
 			echo "</div>";
 			$noembed_file1 = getModulePath ( $modules [$i] ) . ".noembed";
 			$noembed_file2 = getModulePath ( $modules [$i] ) . "noembed.txt";
@@ -123,14 +123,14 @@ if (! $acl->hasPermission ( "list_packages" )) {
 
 <p>
 	<strong><?php
-	
+
 	echo TRANSLATION_INSTALLED_DESIGNS;
 	?>
 	</strong>
 </p>
 <p>
 <?php
-	
+
 	echo TRANSLATION_INSTALLED_DESIGNS_INFO;
 	?>
 </p>
@@ -142,16 +142,16 @@ if (! $acl->hasPermission ( "list_packages" )) {
 		echo "<ol>";
 		for($i = 0; $i < count ( $themes ); $i ++) {
 			echo "<li style=\"margin-bottom:20px;padding-bottom:10px;border-bottom:solid #cdcdcd 1px;\" id=\"dataset-theme-".$themes[$i]."\"><strong>";
-			
+
 			echo $themes [$i];
-			
+
 			$version = getThemeMeta($themes [$i], "version");
 			$color = null;
-			
+
 			if($version != null){
-			
-			$status = $pkg->checkForNewerVersionOfPackage($modules[$i]);
-			
+
+			$status = $pkg->checkForNewerVersionOfPackage($themes[$i]);
+
 			if($status){
 			   if (version_compare($status, $version, '>')) {
 			       $color = "red";
@@ -159,40 +159,40 @@ if (! $acl->hasPermission ( "list_packages" )) {
 			} else{
 			      $color = "green";
 			}
-			
+
 			}
-			
+
 			if($color){
 			   echo '<span style="color: '.$color.'">';
 			}
-			
+
 			   echo " ".$version;
-			   
-			   
+
+
 			if($color){
 			   echo "</span>";
 			}
-			
+
 			}
-			   
+
 			echo "</strong>";
-			
+
 			echo "<div style=\"float:right\">";
-			
+
 			if ($acl->hasPermission ( "remove_packages" ) and $themes [$i] != $ctheme) {
 				echo " <a style=\"font-size:0.8em;\" href=\"?action=modules&remove=" . $themes [$i] . "&type=theme\" onclick=\"return uninstallTheme(this.href, '".$themes[$i]."');\">";
-				
+
 				echo " [" . TRANSLATION_DELETE . "]";
 				echo "</a>";
 			} else if ($acl->hasPermission ( "remove_packages" )) {
-				
+
 				echo " <a style=\"font-size:0.8em;\" href=\"#\" onclick=\"alert('Das Theme kann nicht gelöscht werden, da es gerade aktiv ist.')\">";
 				echo " [" . TRANSLATION_DELETE . "]";
 				echo "</a>";
 			}
-			
+
 			echo "</div>";
-			
+
 			"</li>";
 		}
 		echo "</ol>";
@@ -202,11 +202,11 @@ if (! $acl->hasPermission ( "list_packages" )) {
 ?>
 <a name="installed_patches_a" id="installed_patches_a"></a>
 <p><strong><?php
-	
+
 	translate("installed_patches");
 	?>
 </strong></p>
-<p><?php 
+<p><?php
 	translate("installed_patches_help");
 ?>
 	</p>
@@ -235,16 +235,16 @@ if(count($patches) > 0){
 var ajax_options = {
   success : function(responseText, statusText, xhr, $form){
   $("div#inst_patch_slide_container").slideUp();
-  
+
   }
-  
+
 
 }
 
-$("form#truncate_installed_patches").ajaxForm(ajax_options); 
+$("form#truncate_installed_patches").ajaxForm(ajax_options);
 </script>
 
-<?php 
+<?php
 }
 } ?>
 
@@ -253,13 +253,13 @@ function uninstallModule(url, name){
    if(confirm("Möchten Sie das Modul " + name + " wirklich deinstallieren?")){
    $.ajax({
       url: url,
-      success: function(){      
-     
+      success: function(){
+
          $("li#dataset-module-" + name).slideUp();
-         
+
       }
 });
-} 
+}
   return false;
 }
 
@@ -270,15 +270,15 @@ function uninstallTheme(url, name){
       success: function(){
 
          $("li#dataset-theme-" + name).slideUp();
-         
+
       }
 });
-} 
+}
   return false;
 }
 
-</script>	
-	
+</script>
+
 	<?php
 }
 ?>
