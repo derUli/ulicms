917a53bf0da364620f8aea1bd4afffb17092680f
Ulrich Schmidt <u.schmidt@velian.de>
2015-12-10 10:22:16 +0100

    typo

--- extras/forms_antispam/content/modules/forms_antispam/forms_antispam_before_http_header.php
+++ extras/forms_antispam/content/modules/forms_antispam/forms_antispam_before_http_header.php
@@ -3,6 +3,6 @@ $antispam_field_name = getconfig("antispam_field_name");
 if(!$antispam_field_name){
    $antispam_field_name = "fax";
 }
-if(isset($_GET["submit-cms-form"]) and !empty($_GET["submit-cms-form"]) and get_request_method() === "POST" and !empty($_POST["fax"])){
+if(isset($_GET["submit-cms-form"]) and !empty($_GET["submit-cms-form"]) and get_request_method() === "POST" and !empty($_POST[$antispam_field_name])){
    die("Spam detected!");
 }
