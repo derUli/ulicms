5d5002bdbecca7cf43dff50c73d3273ba8effffb
Ulrich Schmidt <u.schmidt@velian.de>
2015-12-10 14:57:10 +0100

    count blocked mails

--- extras/forms_antispam/content/modules/forms_antispam/forms_antispam_before_http_header.php
+++ extras/forms_antispam/content/modules/forms_antispam/forms_antispam_before_http_header.php
@@ -4,5 +4,7 @@ if(!$antispam_field_name){
    $antispam_field_name = "fax";
 }
 if(isset($_GET["submit-cms-form"]) and !empty($_GET["submit-cms-form"]) and get_request_method() === "POST" and !empty($_POST[$antispam_field_name])){
-   die("Spam detected!");
+  $count = intval(getconfig("contact_form_refused_spam_mails")) + 1;
+  setconfig("contact_form_refused_spam_mails", $count);
+  die("Spam detected!");
 }
