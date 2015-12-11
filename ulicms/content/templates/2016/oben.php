be1fdafcbc838558f485abde2683b31d4cabfe98
Ulrich Schmidt <admin@deruli.de>
2015-12-11 08:15:00 +0100

    2016

---
+++ ulicms/content/templates/2016/oben.php
@@ -0,0 +1,40 @@
+<?php 
+  html5_doctype();
+  og_html_prefix();
+?>
+<head>
+<?php 
+  base_metas();
+  og_tags();
+  ?>
+<meta name="viewport" content="width=device-width"/>
+<link rel="stylesheet" type="text/css" href="<?php echo getTemplateDirPath(get_theme());?>mobile.css"/>
+<style type="text/css">
+header, footer{
+   background-color: <?php echo getconfig("header-background-color"); ?>;
+}
+
+h1, h2, h3, h4, h5, h6{
+   color: <?php echo getconfig("header-background-color"); ?>;
+}
+</style>
+<script>
+	$(function(){
+		$('ul.menu_top').slicknav({
+           "prependTo" : "div#mobile-menu",
+           "label" : "<?php translate("pages");?>",
+           "allowParentLinks" : false
+        });
+	});
+</script>
+</head>
+<body class="<?php body_classes();?>">
+<header>
+<a href="./"><?php logo();?></a>
+</header>
+<div id="root-container">
+<nav><?php menu("top");?></nav>
+<div id="mobile-menu"></div>
+<div id="header-image"></div>
+<main>
+<h1><?php headline();?></h1>
---
+++ ulicms/content/templates/2016/unten.php
@@ -0,0 +1,7 @@
+<?php random_banner();?>
+</main>
+</div>
+<nav><?php menu("bottom");?></nav>
+<footer><p>&copy; 2015 by <?php homepage_owner();?> | </p><?php poweredByUliCMS();?></footer>
+</body>
+</html>
