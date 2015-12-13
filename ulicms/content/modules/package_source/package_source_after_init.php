0526bb844b8c01e0dc0376e5e581345cc52863db
Ulrich Schmidt <admin@deruli.de>
2015-12-13 19:10:27 +0100

    -filter

--- ulicms/content/modules/package_source/package_source_after_init.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("PACKAGE_SOURCE_BASE_PATH", ULICMS_ROOT."/packages");
-define("PACKAGE_SOURCE_BAS_URL", "http://packages.ulicms.de");
--- ulicms/content/modules/package_source/package_source_lang_de.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("TRANSLATION_DOWNLOAD_PACKAGE_FOR", "Download");
-define("TRANSLATION_SELECT_ULICMS_VERSION", "Bitte wählen Sie Ihre UliCMS Version");
-define("TRANSLATION_ALL_PACKAGES_FOR", "Alle Pakete für UliCMS %version%");
-define("TRANSLATION_PACKAGE_FOR_VERSION", "%paket% für UliCMS %version%");
