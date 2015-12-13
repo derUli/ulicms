6afc9a79ee3824f88ab74c607dde93c70ea6a8e9
Ulrich Schmidt <admin@deruli.de>
2015-12-13 19:02:25 +0100

    source

--- services/package_source/package_source_main.php
+++ services/package_source/package_source_main.php
@@ -1,7 +1,4 @@
 <?php
-define("PACKAGE_SOURCE_BASE_PATH", ULICMS_ROOT."/packages");
-define("PACKAGE_SOURCE_BAS_URL", "http://packages.ulicms.de");
-
 function isNotExcluded($file){
    return($file != "." and $file != ".." and $file != "logs" and $file != "usage");
 }
