888f11df9936d1b04f8b750c99c3c1a643ab0c07
Ulrich Schmidt <ulrich1992@web.de>
2015-12-18 20:52:49 +0100

    test.php

--- ulicms/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once "init.php";
-var_dump(Settings::get("blubb"));
-var_dump(Settings::init("blubb", "bla bla"));
-var_dump(Settings::get("blubb"));
-var_dump(Settings::set("blubb", "Ulis löbliche Heimseite"));
-var_dump(Settings::get("blubb"));
-var_dump(Settings::delete("blubb"));
-var_dump(Settings::delete("blubb"));
