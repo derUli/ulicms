2edb946bb7c5c4b90a4a8602806a0aea979aefe9
Ulrich Schmidt <admin@deruli.de>
2015-12-18 07:17:10 +0100

    test.php

--- ulicms/test.php
+++ ulicms/test.php
@@ -1,7 +1,9 @@
 <?php 
 include_once "init.php";
-var_dump(Settings::get("homepage_title"));
-var_dump(Settings::init("homepage_title", "bla bla"));
-var_dump(Settings::get("homepage_title"));
-var_dump(Settings::set("homepage_title", "Ulis löbliche Heimseite"));
-var_dump(Settings::get("homepage_title"));
+var_dump(Settings::get("blubb"));
+var_dump(Settings::init("blubb", "bla bla"));
+var_dump(Settings::get("blubb"));
+var_dump(Settings::set("blubb", "Ulis löbliche Heimseite"));
+var_dump(Settings::get("blubb"));
+var_dump(Settings::delete("blubb"));
+var_dump(Settings::delete("blubb"));
