041a67c968b8a6f51f350090bcbc1b6dc69ec930
Ulrich Schmidt <ulrich1992@web.de>
2015-12-19 10:04:47 +0100

    cron.php

--- ulicms/cron.php
+++ ulicms/cron.php
@@ -2,16 +2,17 @@
 require_once "init.php";
 fcflush ();
 
-if(getconfig("delete_ips_after_48_hours")){
-   db_query("Update ".tbname("log")." SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2");
+if (getconfig ( "delete_ips_after_48_hours" )) {
+	db_query ( "Update " . tbname ( "log" ) . " SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2" );
 }
 
 $empty_trash_days = getconfig ( "empty_trash_days" );
 
-if ($empty_trash_days === false)
+if ($empty_trash_days === false) {
 	$empty_trash_days = 30;
-	
-	// Papierkorb für Seiten Cronjob
+}
+
+// Papierkorb für Seiten Cronjob
 $empty_trash_timestamp = $empty_trash_days * (60 * 60 * 24);
 db_query ( "DELETE FROM " . tbname ( "content" ) . " WHERE " . time () . " -  `deleted_at` > $empty_trash_timestamp" ) or die ( db_error () );
 
@@ -26,22 +27,25 @@ $version = new ulicms_version ();
 
 $developmentVersion = "";
 
-if ($version->getDevelopmentVersion ())
+if ($version->getDevelopmentVersion ()) {
 	$developmentVersion = " Vorabversion";
-	
-	// Start Call Home //
+}
+
+// Start Call Home //
 $cfg_script = "UliCMS " . $version->getVersion () . " (v" . join ( ".", $version->getInternalVersion () ) . $developmentVersion . ")";
 $cfg_url = "http://www.ulicms.de/chs/api.php";
 
 $urlfrom = $_SERVER ['HTTP_HOST'];
 
-if (! is_file ( "init.php" ) and ! is_dir ( "libs" ))
+if (! is_file ( "init.php" ) and ! is_dir ( "lib" )) {
 	exit ();
+}
 
 $folderfrom = str_replace ( "\\", "/", dirname ( $_SERVER ['SCRIPT_NAME'] ) );
 
-if (! endsWith ( $folderfrom, "/" ))
+if (! endsWith ( $folderfrom, "/" )) {
 	$folderfrom .= "/";
+}
 
 $var_url = get_site_protocol () . $urlfrom . $folderfrom;
 
