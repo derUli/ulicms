2ad53b5770424a5e8d83b8e1a0bac9fc5a126f30
Ulrich Schmidt <admin@deruli.de>
2016-02-15 23:29:54 +0100

    code refakturiert

--- ulicms/classes/objects/custom_data.php
+++ ulicms/classes/objects/custom_data.php
@@ -1,9 +1,9 @@
 <?php
 class CustomData {
 	public static function get($page = null) {
-		if (! $page)
+		if (! $page) {
 			$page = get_requested_pagename ();
-		
+		}
 		$sql = "SELECT `custom_data` FROM " . tbname ( "content" ) . " WHERE systemname='" . Database::escapeValue ( $page ) . "'  AND language='" . Database::escapeValue ( $_SESSION ["language"] ) . "'";
 		$query = Database::query ( $sql );
 		if (Database::getNumRows ( $query ) > 0) {
@@ -13,32 +13,35 @@ class CustomData {
 		return null;
 	}
 	public static function set($var, $value, $page = null) {
-		if (! $page)
+		if (! $page) {
 			$page = get_requested_pagename ();
-		
+		}
 		$data = self::get ( $page );
-		if (is_null ( $data ))
+		if (is_null ( $data )) {
 			$data = array ();
-		
+		}
 		$data [$var] = $value;
 		$json = json_encode ( $data );
 		
 		return Database::query ( "UPDATE " . tbname ( "content" ) . " SET custom_data = '" . Database::escapeValue ( $json ) . "' WHERE systemname='" . Database::escapeValue ( $page ) . "'" );
 	}
 	public static function delete($var = null, $page = null) {
-		if (! $page)
+		if (! $page) {
 			$page = get_requested_pagename ();
+		}
 		$data = self::get ( $page );
-		if (is_null ( $data ))
+		if (is_null ( $data )) {
 			$data = array ();
-			// Wenn $var gesetzt ist, nur $var aus custom_data löschen
+		}
+		// Wenn $var gesetzt ist, nur $var aus custom_data löschen
 		if ($var) {
 			if (isset ( $data [$var] )) {
 				unset ( $data [$var] );
 			}
-		}  // Wenn $var nicht gesetzt ist, alle Werte von custom_data löschen
-else {
-			
+		} 
+
+		else {
+			// Wenn $var nicht gesetzt ist, alle Werte von custom_data löschen
 			$data = array ();
 		}
 		
