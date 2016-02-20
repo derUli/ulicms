459d36ba70684273b845e9f06b29ed1d6cb4be77
Ulrich Schmidt <admin@deruli.de>
2016-02-20 11:24:06 +0100

    page Klasse weiter entwickelt

--- ulicms/classes/objects/content/content.php
+++ ulicms/classes/objects/content/content.php
@@ -1,7 +1,7 @@
 <?php
 abstract class Content {
 	abstract protected function loadByID($id);
-	abstract protected function loadByNameAndLanguage($name, $language = null);
+	abstract protected function loadBySystemnameAndLanguage($name, $language);
 	abstract protected function update();
 	abstract protected function create();
 	abstract protected function save();
--- ulicms/classes/objects/content/page.php
+++ ulicms/classes/objects/content/page.php
@@ -36,15 +36,53 @@ class Page extends Content {
 		$this->systemname = $result->systemname;
 		$this->title = $result->title;
 		$this->alternate_title = $result->alternate_title;
+		$this->target = $result->target;
+		$this->category = $result->category;
+		$this->content = $result->content;
+		$this->language = $result->language;
+		$this->menu_image = $result->menu_image;
+		$this->active = $result->active;
+		$this->created = $result->created;
+		$this->lastmodified = $result->lastmodified;
+		$this->autor = $result->autor;
+		$this->lastchangeby = $result->lastchangeby;
+		$this->views = $result->views;
+		$this->redirection = $result->redirection;
+		$this->menu = $result->menu;
+		$this->position = $result->position;
+		$this->parent = $result->parent;
+		$this->access = $result->access;
+		$this->meta_description = $result->meta_description;
+		$this->meta_keywords = $result->meta_keywords;
+		$this->deleted_at = $result->deleted_at;
+		$this->html_file = $result->html_file;
+		$this->theme = $result->theme;
+		$this->custom_data = json_decode ( $result->custom_data, true );
+		$this->type = "page";
+		$this->og_title = $result->og_title;
+		$this->og_type = $result->og_type;
+		$this->og_image = $result->og_image;
+		$this->og_description = $result->og_description;
 	}
 	public function loadByID($id) {
 		$id = intval ( $id );
-		$query = DB::query ( "SELECT * FROM `" . tbname ( "content" ) . "` where id = " . $id );
+		$query = DB::query ( "SELECT * FROM `" . tbname ( "content" ) . "` where id = " . $id . " and `type` = 'page'" );
 		if (DB::getNumRows ( $query ) > 0) {
 			$result = DB::fetchObject ( $query );
 			$this->fillVarsByResult ( $result );
 		} else {
 			throw new Exception ( "No page with id $id" );
+		}
+	}
+	public function loadBySystemnameAndLanguage($name, $language) {
+		$name = DB::escapeValue ( $name );
+		$language = DB::escapeValue ( $language );
+		$query = DB::query ( "SELECT * FROM `" . tbname ( "content" ) . "` where `systemname` = '$name' and `language` = '$language' and `type` = 'page'" );
+		if (DB::getNumRows ( $query ) > 0) {
+			$result = DB::fetchObject ( $query );
+			$this->fillVarsByResult ( $result );
+		} else {
+			throw new Exception ( "No such page" );
 		}
 	}
 }
