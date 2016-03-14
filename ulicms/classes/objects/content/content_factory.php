db99a279fc3b706b6a54dcca506aac0abae161c8
Ulrich Schmidt <admin@deruli.de>
2016-03-14 22:36:41 +0100

    fix

--- ulicms/classes/objects/content/content_factory.php
+++ ulicms/classes/objects/content/content_factory.php
@@ -5,11 +5,7 @@ class ContentFactory {
 		$query = DB::query ( "SELECT `type` FROM `" . tbname ( "content" ) . "` where id = " . $id );
 		if (DB::getNumRows ( $query ) > 0) {
 			$result = DB::fetchObject ( $query );
-			if ($result->type == "page") {
-				$page = new Page ();
-				$page->loadByID ( $id );
-				return $page;
-			}
+			return self::getContentObjectByID ( $result );
 		} else {
 			throw new Exception ( "No page with id $id" );
 		}
@@ -20,32 +16,41 @@ class ContentFactory {
 		$query = DB::query ( "SELECT id, `type` FROM `" . tbname ( "content" ) . "` where `systemname` = '$name' and `language` = '$language'" );
 		if (DB::getNumRows ( $query ) > 0) {
 			$result = DB::fetchObject ( $query );
-			if ($result->type == "page") {
-				$page = new Page ();
-				$page->loadByID ( $result->$id );
-				return $page;
-			}
+			return self::getContentObjectByID ( $result );
 		} else {
 			throw new Exception ( "No page with this combination of $name and $language" );
 		}
+	}
+	private static function getContentObjectByID($row) {
+		$retval = null;
+		if ($row->type == "page") {
+			$retval = new Page ();
+			$retval->loadByID ( $row->id );
+		}
+		return $retval;
 	}
 	public static function getAll($order = "id") {
 		$result = array ();
 		$sql = "SELECT id, `type` FROM " . tbname ( "content" ) . " ORDER BY $order";
 		$query = DB::query ( $sql );
 		while ( $row = DB::fetchObject ( $query ) ) {
-			if ($result->type == "page") {
-				$page = new Page ();
-				$page->loadByID ( $row->id );
-				$result [] = $page;
-			}
+			$result [] = self::getContentObjectByID ( $row );
+		}
+		return $result;
+	}
+	public static function getByLanguage($language, $order = "id") {
+		$language = DB::escapeValue ( $language );
+		$result = array ();
+		$sql = "SELECT id, `type` FROM " . tbname ( "content" ) . " where `language` = '$language' ORDER BY $order";
+		$query = DB::query ( $sql );
+		while ( $row = DB::fetchObject ( $query ) ) {
+			$result [] = self::getContentObjectByID ( $row );
 		}
 		return $result;
 	}
 	
 	// @TODO: Funktionen für Abfrage von mehreren Datensätzen
 	// z.B.
-	// getByLanguage()
 	// getByMenu()
 	// getByMenuAndLanguage()
 	// usw.
--- ulicms/test.php
+++ ulicms/test.php
@@ -1,9 +1,3 @@
 <?php
 include_once "init.php";
-$ad = new Banner ();
-$ad->setType ( "html" );
-$ad->html = "neues blubb";
-$ad->save ();
-$ad->html = "geändertes Blubb";
-$ad->save ();
-$ad->delete();
+var_dump(ContentFactory::getAll());
