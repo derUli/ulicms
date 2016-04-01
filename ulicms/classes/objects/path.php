55e3c78360b0b7f91f7d079ed0d0d8aa8623bcfd
Ulrich Schmidt <admin@deruli.de>
2016-04-01 17:43:05 +0200

    code refakturiert

--- ulicms/classes/objects/path.php
+++ ulicms/classes/objects/path.php
@@ -1,13 +1,13 @@
 <?php
-if(!defined("KCFINDER_PAGE")){
-class Path {
-	public static function resolve($path) {
-		$path = str_ireplace ( "ULICMS_ROOT", ULICMS_ROOT, $path );
-		$path = str_ireplace ( "ULICMS_TMP", ULICMS_TMP, $path );
-		$path = str_ireplace ( "ULICMS_CACHE", ULICMS_CACHE, $path );
-		$path = str_ireplace ( "\\", "/", $path );
-		$path = rtrim ( $path, "/" );
-		return $path;
+if (! defined ( "KCFINDER_PAGE" )) {
+	class Path {
+		public static function resolve($path) {
+			$path = str_ireplace ( "ULICMS_ROOT", ULICMS_ROOT, $path );
+			$path = str_ireplace ( "ULICMS_TMP", ULICMS_TMP, $path );
+			$path = str_ireplace ( "ULICMS_CACHE", ULICMS_CACHE, $path );
+			$path = str_ireplace ( "\\", "/", $path );
+			$path = rtrim ( $path, "/" );
+			return $path;
+		}
 	}
-}
 }
