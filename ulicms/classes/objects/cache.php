11930bafd0f7868891a31301a81cc553de0298bb
Ulrich Schmidt <admin@deruli.de>
2016-04-09 15:46:14 +0200

    cache

--- ulicms/classes/objects/cache.php
+++ ulicms/classes/objects/cache.php
@@ -32,4 +32,37 @@ class Cache {
 		apc_clear_cache ( 'user' );
 		apc_clear_cache ( 'opcode' );
 	}
+	public static function store($data, $requestUri = null) {
+		if (! $request_uri) {
+			$requestUri = get_request_uri ();
+		}
+		$cacheFile = self::buildCacheFilePath ( $requestUri );
+		file_put_contents ( $cacheFile, $data );
+	}
+	public static function get($requestUri) {
+		$retval = null;
+		if (! $request_uri) {
+			$requestUri = get_request_uri ();
+		}
+		$cacheFile = self::buildCacheFilePath ( $requestUri );
+		
+		if (file_exists ( $cacheFile )) {
+			$lastModified = filemtime ( $cacheFile );
+			if (time () - $lastModified < CACHE_PERIOD) {
+				$retval = file_get_contents ( $cacheFile );
+			}
+		}
+		return $retval;
+	}
+	public static function buildCacheFilePath($request_uri) {
+		$language = $_SESSION ["language"];
+		if (! $language) {
+			$language = Settings::get ( "default_language" );
+		}
+		$unique_identifier = $request_uri . $language . strbool ( is_mobile () );
+		if (function_exists ( "apply_filter" )) {
+			$unique_identifier = apply_filter ( $unique_identifier, "unique_identifier" );
+		}
+		return "content/cache/" . md5 ( $unique_identifier ) . ".tmp";
+	}
 }
