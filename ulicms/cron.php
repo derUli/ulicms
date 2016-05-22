f9e3f529eeed04db3fa88671e13f5bf3a8ccc4b5
Ulrich Schmidt <admin@deruli.de>
2016-05-22 20:57:20 +0200

    Database::query

--- ulicms/cron.php
+++ ulicms/cron.php
@@ -3,7 +3,7 @@ require_once "init.php";
 fcflush ();
 
 if (Settings::get ( "delete_ips_after_48_hours" )) {
-	db_query ( "Update " . tbname ( "log" ) . " SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2" );
+	Database::query ( "Update " . tbname ( "log" ) . " SET ip = NULL WHERE DATEDIFF(NOW(), zeit) >= 2" );
 }
 
 $empty_trash_days = Settings::get ( "empty_trash_days" );
@@ -14,11 +14,11 @@ if ($empty_trash_days === false) {
 
 // Papierkorb für Seiten Cronjob
 $empty_trash_timestamp = $empty_trash_days * (60 * 60 * 24);
-db_query ( "DELETE FROM " . tbname ( "content" ) . " WHERE " . time () . " -  `deleted_at` > $empty_trash_timestamp" ) or die ( db_error () );
+Database::query ( "DELETE FROM " . tbname ( "content" ) . " WHERE " . time () . " -  `deleted_at` > $empty_trash_timestamp" ) or die ( db_error () );
 
 // Alle Revisionen von bereits gelöschten Seiten entfernen
-db_query ( "DELETE FROM " . tbname ( "history" ) . " WHERE content_id NOT IN (
+Database::query ( "DELETE FROM " . tbname ( "history" ) . " WHERE content_id NOT IN (
             SELECT id from " . tbname ( "content" ) . ");" );
 
 // Cronjobs der Module
-add_hook ( "cron" );
+Database::query ( "cron" );
