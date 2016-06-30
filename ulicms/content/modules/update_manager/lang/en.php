d8652e5349514e38eae4edc36ac2a5c72c8531a2
Ulrich Schmidt <admin@deruli.de>
2016-06-30 18:00:55 +0200

    ajax gedöns

--- ulicms/content/modules/update_manager/lang/en.php
+++ ulicms/content/modules/update_manager/lang/en.php
@@ -2,4 +2,7 @@
 define ( "TRANSLATION_UPDATE_MANAGER", "Update Manager" );
 define ( "TRANSLATION_INSTALL_UPDATES", "Install Updates" );
 define ( "TRANSLATION_PLEASE_SELECT_PACKAGES", "Please select packages to upgrade." );
+
 define ( "TRANSLATION_NO_UPDATES_AVAILABLE", "No updates available." );
+define ( "TRANSLATION_UPDATES_AVAILABLE_SINGULAR", "An update is available." );
+define ( "TRANSLATION_UPDATES_AVAILABLE_MULTIPLE", "There are %count% updates available." );
