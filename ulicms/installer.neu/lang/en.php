bfc7495ee104b28f4f8237240b56ddba7027eb99
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-08 15:19:48 +0200

    translation changed

--- ulicms/installer.neu/lang/en.php
+++ ulicms/installer.neu/lang/en.php
@@ -1,7 +1,8 @@
 <?php
 define ( "TRANSLATION_INSTALLATION", "Installation" );
 define ( "TRANSLATION_TITLE_STEP_1", "Select Language" );
-define ( "TRANSLATION_TITLE_STEP_2", "change file permissions" );
-define ( "TRANSLATION_TITLE_STEP_3", "connect to database" );
-define ( "TRANSLATION_TITLE_STEP_4", "create administrator account" );
-define ( "TRANSLATION_TITLE_STEP_5", "select optional components" );
+define ( "TRANSLATION_TITLE_STEP_2", "Change File Permissions" );
+define ( "TRANSLATION_TITLE_STEP_3", "Connect to Database" );
+define ( "TRANSLATION_TITLE_STEP_4", "Create administrator account" );
+define ( "TRANSLATION_TITLE_STEP_5", "Select optional components" );
+define ( "TRANSLATION_TITLE_STEP_6", "Install" );
