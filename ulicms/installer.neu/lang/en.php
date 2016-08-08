94d1156ec6ec564fa2a7bde0146fdeb183050b96
Ulrich Schmidt <admin@deruli.de>
2016-08-08 18:15:05 +0200

    CHANGE FILE permissions

--- ulicms/installer.neu/lang/en.php
+++ ulicms/installer.neu/lang/en.php
@@ -7,3 +7,11 @@ define ( "TRANSLATION_TITLE_STEP_4", "Create administrator account" );
 define ( "TRANSLATION_TITLE_STEP_5", "Select optional components" );
 define ( "TRANSLATION_TITLE_STEP_6", "Install" );
 define ( "TRANSLATION_SELECT_LANGUAGE", "Please select your language." );
+define ( "TRANSLATION_FOLLOW_INSTRUCTIONS", "Please follow instructions, to install UliCMS on this Server." );
+define ( "TRANSLATION_CHMOD", "<p>Please chmod the following files to 0755.<br/>
+<ol>
+<li>The main directory of UliCMS (recursive, including all files and subfolders)</li>
+</ol>
+</p>" );
+define ( "TRANSLATION_PERMISSION", "Please set the file permissions like in this screenshot" );
+define ( "TRANSLATION_PERMISSIONS2", "FTP Permission changes" );
