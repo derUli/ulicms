21f1d226e9661a732acb80cbb0b04440326d5eb4
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-08 16:14:59 +0200

    select_language

--- ulicms/installer.neu/lang/en.php
+++ ulicms/installer.neu/lang/en.php
@@ -6,3 +6,4 @@ define ( "TRANSLATION_TITLE_STEP_3", "Connect to Database" );
 define ( "TRANSLATION_TITLE_STEP_4", "Create administrator account" );
 define ( "TRANSLATION_TITLE_STEP_5", "Select optional components" );
 define ( "TRANSLATION_TITLE_STEP_6", "Install" );
+define ( "TRANSLATION_SELECT_LANGUAGE", "Please select your language" );
