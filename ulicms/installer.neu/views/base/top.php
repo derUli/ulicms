805d5db1f5ca6974c3024c46b2ee05e36cc7fd7b
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-08 14:59:24 +0200

    Show nav

--- ulicms/installer.neu/views/base/top.php
+++ ulicms/installer.neu/views/base/top.php
@@ -4,10 +4,16 @@
 <title><?php echo Installer::getTitle();?></title>
 <link rel="stylesheet" type="text/css"
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/href" href="media/style.css"/>
+<link rel="stylesheet" type="text/href" href="media/style.css" />
 </head>
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-4" id="steps">{List of Steps}</div>
+			<div class="col-sm-4" id="steps">
+				<ol id="navigation">
+				<?php for($i=1; $i++ <= 5; $i++){?>
+					<li><?php echo constant("TRANLATION_TITLE_STEP_".$i);?></li>
+				<?php }?>
+</ol>
+			</div>
 			<div class="col-sm-8" id="main">
