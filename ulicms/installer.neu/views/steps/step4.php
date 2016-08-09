370ac0e63e3a51ab16776dc5db8ed25ddb391891
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-09 10:25:08 +0200

    refactor

--- ulicms/installer.neu/views/steps/step4.php
+++ ulicms/installer.neu/views/steps/step4.php
@@ -3,26 +3,30 @@
 	<div class="form-group">
 		<label for="text"><?php echo TRANSLATION_ADMIN_USER;?></label> <input
 			type="text" class="form-control" name="admin_user" id="admin_user"
-			value="<?php echo htmlspecialchars($_SESSION["admin_user"]);?>" required>
+			value="<?php echo htmlspecialchars($_SESSION["admin_user"]);?>"
+			required>
 	</div>
 
 	<div class="form-group">
 		<label for="text"><?php echo TRANSLATION_ADMIN_PASSWORD;?></label> <input
 			type="password" class="form-control" name="admin_password"
 			id="admin_password"
-			value="<?php echo htmlspecialchars($_SESSION["admin_password"]);?>" required>
+			value="<?php echo htmlspecialchars($_SESSION["admin_password"]);?>"
+			required>
 	</div>
 
 	<div class="form-group">
 		<label for="text"><?php echo TRANSLATION_ADMIN_PASSWORD_REPEAT;?></label>
 		<input type="password" class="form-control"
-			name="admin_password_repeat" id="admin_password_repeat" value="" required>
+			name="admin_password_repeat" id="admin_password_repeat" value=""
+			required>
 	</div>
 
 	<div class="form-group">
 		<label for="text"><?php echo TRANSLATION_ADMIN_EMAIL;?></label> <input
 			type="email" class="form-control" name="admin_email" id="admin_email"
-			value="<?php echo htmlspecialchars($_SESSION["admin_email"]);?>" required>
+			value="<?php echo htmlspecialchars($_SESSION["admin_email"]);?>"
+			required>
 	</div>
 
 
@@ -30,14 +34,16 @@
 		<label for="text"><?php echo TRANSLATION_ADMIN_LASTNAME;?></label> <input
 			type="text" class="form-control" name="admin_lastname"
 			id="admin_lastname"
-			value="<?php echo htmlspecialchars($_SESSION["admin_lastname"]);?>" required>
+			value="<?php echo htmlspecialchars($_SESSION["admin_lastname"]);?>"
+			required>
 	</div>
 
 	<div class="form-group">
 		<label for="text"><?php echo TRANSLATION_ADMIN_FIRSTNAME;?></label> <input
 			type="text" class="form-control" name="admin_firstname"
 			id="admin_firstname"
-			value="<?php echo htmlspecialchars($_SESSION["admin_firstname"]);?>" required>
+			value="<?php echo htmlspecialchars($_SESSION["admin_firstname"]);?>"
+			required>
 	</div>
 
 	<button type="submit" class="btn btn-default"><?php echo TRANSLATION_APPLY;?></button>
