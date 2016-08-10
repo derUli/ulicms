a4beb5cd59b727d5c7a40115cb8912bb0edb3b94
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-10 11:52:39 +0200

    sql script für settings tabelle inhalt hinzugefügt

---
+++ ulicms/installer.neu/sql/009.sql
@@ -0,0 +1,66 @@
+INSERT INTO `{prefix}settings` (`id`, `name`, `value`) VALUES
+(1, 'homepage_title', 'Sample Page'),
+(2, 'maintenance_mode', '0'),
+(3, 'redirection', ''),
+(4, 'homepage_owner', '{firstname} {lastname}'),
+(5, 'email', '{email}'),
+(6, 'motto', 'Where do you want to go today?'),
+(7, 'date_format', 'd.m.Y H:i'),
+(8, 'autor_text', 'Diese Seite wurde verfasst von Vorname Nachname'),
+(9, 'robots', 'index,follow'),
+(10, 'meta_keywords', 'Stichwort 1, Stichwort 2, Stichwort 3'),
+(11, 'meta_description', 'Eine kurzer Beschreibungstext'),
+(12, 'logo_disabled', 'no'),
+(13, 'logo_image', '67cc042b9ee9eb28cdc81ae7d7420d8a.png'),
+(14, 'motd', '<p>Willkommen bei <strong>UliCMS</strong>!<br/>
+Eine Dokumentation finden Sie unter <a href=\"http://www.ulicms.de\" target=\"_blank\">www.ulicms.de</a>.</p>'),
+(15, 'visitors_can_register', 'off'),
+(16, 'frontpage', 'willkommen'),
+(17, 'contact_form_refused_spam_mails', '0'),
+(18, 'default_language', '{language}'),
+(19, 'system_language', '{language}'),
+(20, 'country_blacklist', 'ru, cn, in'),
+(21, 'spamfilter_enabled', 'yes'),
+(22, 'comment_mode', 'off'),
+(23, 'facebook_id', ''),
+(24, 'disqus_id', ''),
+(25, 'spamfilter_words_blacklist',
+'viagra
+vicodin
+cialis
+xanax
+mortgage
+refinance
+pharm
+diploma
+enlargement
+pills'),
+(26, 'empty_trash_days', '30'),
+(27, 'password_salt', '{salt}'),
+(28, 'timezone', 'Europe/Berlin'),
+(29, 'db_schema_version', '9.8.5'),
+(30, 'pkg_src', 'http://packages.ulicms.de/{version}/'),
+(31, 'theme', '2016'),
+(32, 'zoom', '100'),
+(33, 'default-font', 'Arial, \'Helvetica Neue\', Helvetica, sans-serif'),
+(34, 'font-size', 'medium'),
+(35, 'header-background-color', '#E8912A'),
+(36, 'body-background-color', '#FFFFFF'),
+(37, 'body-text-color', '#000000'),
+(38, 'disable_html_validation', 'disable'),
+(39, 'title_format', '%homepage_title% > %title%'),
+(40, 'cache_type', 'file'),
+(41, 'registered_user_default_level', '10'),
+(42, 'override_shortcuts', 'backend'),
+(43, 'domain_to_language', ''),
+(44, 'frontpage_de', 'willkommen'),
+(45, 'frontpage_en', 'welcome'),
+(46, 'email_mode', 'internal'),
+(47, 'ckeditor_skin', 'moono'),
+(48, 'installed_at', '" . time () . "'),
+(49, 'cache_disabled', 'disabled'),
+(50, 'locale', 'de_DE.UTF-8; de_DE; deu_deu'),
+(51, 'locale_de', 'de_DE.UTF-8; de_DE; deu_deu'),
+(52, 'locale_en', 'en_US.UTF-8; en_GB.UTF-8; en_US; en_GB; english-uk; eng; uk'),
+(53, 'session_timeout', '60'),
+(54, 'og_type', 'article');"
