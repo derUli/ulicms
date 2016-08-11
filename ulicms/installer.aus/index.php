96c00de27cdb86708b5af67725a933f87606261d
Ulrich Schmidt <admin@deruli.de>
2016-08-11 23:26:43 +0200

    installer ordner umbenannt

--- ulicms/installer.aus/index.php
+++ ulicms/installer.aus/index.php
@@ -1,3 +1,32 @@
 <?php
-require_once "installer.php";
-?>
+session_start ();
+setcookie ( session_name (), session_id () );
+error_reporting ( E_ALL ^ E_NOTICE );
+include_once "controllers/installer_controller.php";
+
+InstallerController::initSessionVars ();
+if (isset ( $_GET ["language"] )) {
+	$languages = array (
+			"en" 
+	);
+	if (in_array ( $_GET ["language"], $languages )) {
+		$_SESSION ["language"] = $_GET ["language"];
+	}
+}
+$lang = InstallerController::getLanguage ();
+
+InstallerController::loadLanguageFile ( $lang );
+
+if (isset ( $_REQUEST ["submit_form"] )) {
+	
+	if (method_exists ( InstallerController, "submit" . $_REQUEST ["submit_form"] )) {
+		call_user_func ( "InstallerController::submit" . $_REQUEST ["submit_form"] );
+	}
+	die ();
+}
+
+$step = InstallerController::getStep ();
+
+include_once "views/base/top.php";
+include_once "views/steps/step" . $step . ".php";
+include_once "views/base/bottom.php";
