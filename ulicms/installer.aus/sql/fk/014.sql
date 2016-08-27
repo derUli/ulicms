3c2aec8c18cfb6aef643921050ec662ad7ca1af9
Ulrich Schmidt <ulrich1992@web.de>
2016-08-27 13:23:54 +0200

    refactor

--- ulicms/installer.aus/sql/fk/014.sql
+++ ulicms/installer.aus/sql/fk/014.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `{prefix}lists`
-  ADD CONSTRAINT fk_language
+  ADD CONSTRAINT fk_lists_language
   FOREIGN KEY (language) 
   REFERENCES `{prefix}languages`(language_code)
   ON DELETE SET NULL;
--- ulicms/installer.aus/sql/fk/015.sql
+++ ulicms/installer.aus/sql/fk/015.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `{prefix}lists`
-  ADD CONSTRAINT fk_category
+  ADD CONSTRAINT fk_lists_category
   FOREIGN KEY (category_id) 
   REFERENCES `{prefix}categories`(id)
   ON DELETE SET NULL;
