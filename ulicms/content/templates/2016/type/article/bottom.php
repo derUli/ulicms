b44ce6a1dfcc9ce974f6e86337cc9b7b3427e370
Ulrich Schmidt <admin@deruli.de>
2016-09-03 17:19:42 +0200

    email adresse wird ausgegeben

--- ulicms/content/templates/2016/type/article/bottom.php
+++ ulicms/content/templates/2016/type/article/bottom.php
@@ -6,14 +6,22 @@ if (isNotNullOrEmpty ( $article_meta )) {
 	<small><?php
 	
 	if (isNotNullOrEmpty ( $article_meta->article_author_name )) {
-		echo Template::escape ( $article_meta->article_author_name );
+		if (isNotNullOrEmpty ( $article_meta->article_author_email )) {
+			echo '<a class="author-email-link" href="mailto:"' . Template::getEscape ( $article_meta->article_author_email ) . ">";
+			echo Template::escape ( $article_meta->article_author_name );
+			echo "</a>";
+		} else {
+			echo Template::escape ( $article_meta->article_author_name );
+		}
 	} else {
 		translate ( "guest" );
 	}
 	if (isNotNullOrEmpty ( $article_meta->article_date )) {
 		?>
 	<?php translate("on_time");?>
-		<?php echo strftime ( "%A, %x %X", strtotime ( $article_meta->article_date ) );
+		<?php
+		
+		echo strftime ( "%A, %x %X", strtotime ( $article_meta->article_date ) );
 		?>
 		
 		<?php translate("o_clock");?>
