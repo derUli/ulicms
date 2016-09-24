a1e86bd8082fb17700c201d7ae56973a2f1f6516
Ulrich Schmidt <ulrich1992@web.de>
2016-09-24 11:15:12 +0200

    version 9.8.5

--- ulicms/version.php
+++ ulicms/version.php
@@ -7,7 +7,7 @@ if (! class_exists ( "ulicms_version" )) {
 			$this->internalVersion = Array (
 					9,
 					8,
-					5
+					6
 			);
 			$this->update = "";
 			$this->developmentVersion = false;
