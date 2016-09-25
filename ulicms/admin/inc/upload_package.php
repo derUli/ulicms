9d85b92c261c51d7cee71ccb21c1fe620685b678
Ulrich Schmidt <admin@deruli.de>
2016-09-25 22:58:38 +0200

    Übersetzung gefixt

--- ulicms/admin/inc/upload_package.php
+++ ulicms/admin/inc/upload_package.php
@@ -12,9 +12,13 @@ if (! $acl->hasPermission ( "install_packages" )) {
 					$pkg = new PackageManager ();
 					if ($pkg->installPackage ( $file_in_tmp )) {
 						@unlink ( $file_in_tmp );
-						echo "<p style='color:green'>" . str_ireplace ( "%file%", $_FILES ['file'] ['name'], get_translation ( "PACKAGE_SUCCESSFULL_UPLOADED " ) ) . "</p>";
+						echo "<p style='color:green'>" . get_translation ( "PACKAGE_SUCCESSFULL_UPLOADED", array (
+								"%file%" => $_FILES ['file'] ['name'] 
+						) ) . "</p>";
 					} else {
-						echo "<p style='color:red'>" . str_ireplace ( "%file%", $_FILES ['file'] ['name'], get_translation ( "installation_failed" ) ) . "</p>";
+						echo "<p style='color:red'>" . get_translation ( "installation_failed", array (
+								"%file%" => $_FILES ['file'] ['name'] 
+						) ) . "</p>";
 					}
 				} else if (endsWith ( $file_in_tmp, ".sin" )) {
 					$url = "?action=pkginfo&file=" . basename ( $file_in_tmp );
