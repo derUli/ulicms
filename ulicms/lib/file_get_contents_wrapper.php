97a21e3443c6323b7e4f560930c179a2e46aeb2f
Ulrich Schmidt <u.schmidt@velian.de>
2016-10-21 10:14:19 +0200

    changelog

--- ulicms/lib/file_get_contents_wrapper.php
+++ ulicms/lib/file_get_contents_wrapper.php
@@ -7,13 +7,13 @@ function file_get_contents_curl($url) {
 	curl_setopt ( $ch, CURLOPT_HEADER, 0 );
 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 ); // Set curl to return the data instead of printing it to the browser.
 	curl_setopt ( $ch, CURLOPT_URL, $url );
-
+	
 	$data = curl_exec ( $ch );
-
-	if (curl_getinfo ( $ch, CURLINFO_HTTP_CODE ) != 200 and curl_getinfo ( $ch, CURLINFO_HTTP_CODE ) != 304) {
+	
+	if (curl_getinfo ( $ch, CURLINFO_HTTP_CODE ) != 200 and curl_getinfo ( $ch, CURLINFO_HTTP_CODE ) != 304 and curl_getinfo ( $ch, CURLINFO_HTTP_CODE ) != 302) {
 		$data = false;
 	}
-
+	
 	curl_close ( $ch );
 	return $data;
 }
@@ -21,7 +21,7 @@ function is_url($url) {
 	if (substr_compare ( $url, 'http://', 0, 7 ) > 0 or substr_compare ( $url, 'https://', 0, 8 ) > 0 or substr_compare ( $url, 'ftp://', 0, 8 ) > 0) {
 		return true;
 	}
-
+	
 	return false;
 }
 
@@ -34,19 +34,19 @@ function file_get_contents_wrapper($url, $no_cache = false) {
 	$cache_name = md5 ( $url ) . "-" . basename ( $url );
 	$cache_folder = ULICMS_ROOT . "/content/cache";
 	$cache_path = $cache_folder . "/" . $cache_name;
-	if (file_exists ( $cache_path ) && is_url ( $url ) && ! $no_cache)
+	if (file_exists ( $cache_path ) && is_url ( $url ) && ! $no_cache) {
 		return file_get_contents ( $cache_path );
-
-		if (function_exists ( "curl_init" ) and is_url ( $url )) {
-			$content = file_get_contents_curl ( $url );
-		}
-		else if (ini_get ( "allow_url_fopen" )) {
-			$content = file_get_contents ( $url );
+	}
+	
+	if (function_exists ( "curl_init" ) and is_url ( $url )) {
+		$content = file_get_contents_curl ( $url );
+	} else if (ini_get ( "allow_url_fopen" )) {
+		$content = file_get_contents ( $url );
 	}
 	if ($content) {
 		if (is_dir ( $cache_folder ) and is_url ( $url ) and ! $no_cache)
 			file_put_contents ( $cache_path, $content );
-
+		
 		return $content;
 	}
 	return false;
