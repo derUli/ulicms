12040fa51f8d2998d6840b800e4a1c6a07f473a1
Ulrich Schmidt <admin@deruli.de>
2016-10-23 01:58:35 +0200

    performance

--- ulicms/templating.php
+++ ulicms/templating.php
@@ -553,7 +553,7 @@ function get_title($ipage = null, $headline = false) {
 	$ipage = db_escape ( $_GET ["seite"] );
 	$query = db_query ( "SELECT alternate_title, title FROM " . tbname ( "content" ) . " WHERE systemname='$ipage' AND language='" . db_escape ( $_SESSION ["language"] ) . "'", $connection );
 	if ($ipage == "") {
-		$query = db_query ( "SELECT * FROM " . tbname ( "content" ) . " ORDER BY id LIMIT 1" );
+		$query = db_query ( "SELECT title, alternate_title FROM " . tbname ( "content" ) . " ORDER BY id LIMIT 1" );
 	}
 	if (db_num_rows ( $query ) > 0) {
 		while ( $row = db_fetch_object ( $query ) ) {
