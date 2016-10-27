9fd82e92a921d47cdfbf29240f55cea0608dc9bd
Ulrich Schmidt <admin@deruli.de>
2016-10-27 14:47:19 +0200

    jumbotron zwischenstand

--- ulicms/content/templates/jumbotron/top.php
+++ ulicms/content/templates/jumbotron/top.php
@@ -5,12 +5,12 @@ og_html_prefix ();
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="<?php echo getModulePath("bootstrap");?>css/bootstrap.min.css">
   <?php
 		base_metas ();
 		og_tags ();
 		?>
-<link rel="stylesheet"
-	href="<?php echo getModulePath("bootstrap");?>css/bootstrap.min.css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
