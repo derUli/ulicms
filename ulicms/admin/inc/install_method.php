e801b9de29b5a38fd1f36bb197755d8a63f7dc38
Ulrich Schmidt <admin@deruli.de>
2016-11-17 20:37:16 +0100

    Link auf UliCMS eXtend

--- ulicms/admin/inc/install_method.php
+++ ulicms/admin/inc/install_method.php
@@ -10,6 +10,8 @@ if (! $acl->hasPermission ( "install_packages" )) {
 <p>
 	<a href="?action=upload_package"><?php translate("upload_file");?>
 	</a> <br /> <a href="?action=available_modules"><?php translate ( "from_the_package_source" );?>
+	</a> <br /> 
+	<a href="http://extend.ulicms.de" target="_blank">UliCMS eXtend</a>
 	</a>
 </p>
 
