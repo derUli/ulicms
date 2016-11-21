d01b207b8be1f1c3ae8bc99390fbcf295c6cc5a4
Ulrich Schmidt <admin@deruli.de>
2016-11-21 22:04:10 +0100

    funktion SecurityHelper::securePath implementiert

--- ulicms/classes/helper/security_helper.php
+++ ulicms/classes/helper/security_helper.php
@@ -13,4 +13,18 @@ class securityHelper {
 		
 		return sha1 ( $salt . $password );
 	}
+	public static function securePath($path) {
+		$securedPath = array ();
+		$path = explode ( "/", $path );
+		foreach ( $path as $key => $value ) {
+			if ($value != "." and $value != "..") {
+				$securedPath [] = $value;
+			}
+		}
+		$securedPath = array_map ( 'trim', $securedPath );
+		$securedPath = array_filter ( $securedPath, 'strlen' );
+		$securedPath = array_filter ( $securedPath, 'is_null' );
+		$securedPath = "/" . implode ( "/", $securedPath );
+		return $securedPath;
+	}
 }
