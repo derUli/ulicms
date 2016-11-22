943c0fa52b2f868956774ae4a0caf1b082048444
Ulrich Schmidt <admin@deruli.de>
2016-11-23 00:00:01 +0100

    fcall parameter wird akzeptiert

--- ulicms/classes/objects/controller.php
+++ ulicms/classes/objects/controller.php
@@ -1,3 +1,11 @@
 <?php
 class Controller {
+	public function __construct() {
+		if (isset ( $_REQUEST ["fcall"] ) and isNotNullOrEmpty ( $_REQUEST ["fcall"] )) {
+			$fcall = $_REQUEST ["fcall"];
+			if (method_exists ( $this, $fcall ) and ! startsWith ( $fcall, "__" )) {
+				$this->$fcall ();
+			}
+		}
+	}
 }
