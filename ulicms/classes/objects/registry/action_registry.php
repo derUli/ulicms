1bcb4483c63eb2072d0dc472f60ae760ac648ef0
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-23 16:48:17 +0100

    fix

--- ulicms/classes/objects/registry/action_registry.php
+++ ulicms/classes/objects/registry/action_registry.php
@@ -2,24 +2,26 @@
 class ActionRegistry {
 	private static $assignedControllers = array ();
 	public static function loadModuleActions() {
-		global $actions;
-		$modules = getAllModules ();
-		foreach ( $modules as $module ) {
-			$cActions = getModuleMeta ( $module, "actions" );
-			if ($cActions) {
-				foreach ( $cActions as $key => $value ) {
-					$path = getModulePath ( $module ) . trim ( $value, "/" );
-					if (! endsWith ( $path, ".php" )) {
-						$path .= ".php";
-					}
-					
-					if (file_exists ( $path ) and is_file ( $path )) {
-						$actions [$key] = $path;
+		if(!defined("KCFINDER_PAGE")){
+			global $actions;
+			$modules = getAllModules ();
+			foreach ( $modules as $module ) {
+				$cActions = getModuleMeta ( $module, "actions" );
+				if ($cActions) {
+					foreach ( $cActions as $key => $value ) {
+						$path = getModulePath ( $module ) . trim ( $value, "/" );
+						if (! endsWith ( $path, ".php" )) {
+							$path .= ".php";
+						}
+
+						if (file_exists ( $path ) and is_file ( $path )) {
+							$actions [$key] = $path;
+						}
 					}
 				}
 			}
-		}
-		self::loadModuleActionAssignment ();
+			self::loadModuleActionAssignment ();
+	}
 	}
 	public static function loadModuleActionAssignment() {
 		$modules = getAllModules ();
