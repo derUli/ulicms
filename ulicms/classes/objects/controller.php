04d92becc8c966ea1b7003ef91d14a64f7db8f60
Ulrich Schmidt <admin@deruli.de>
2016-11-23 07:15:05 +0100

    smethod

--- ulicms/classes/objects/controller.php
+++ ulicms/classes/objects/controller.php
@@ -1,12 +1,14 @@
 <?php
 class Controller {
 	public function __construct() {
-		if (isset ( $_REQUEST ["fcall"] ) and isNotNullOrEmpty ( $_REQUEST ["fcall"] )) {
-			$fcall = $_REQUEST ["fcall"];
-			if (method_exists ( $this, $fcall ) and ! startsWith ( $fcall, "_" )) {
-				$this->$fcall ();
+		if (isset ( $_REQUEST ["sMethod"] ) and isNotNullOrEmpty ( $_REQUEST ["sMethod"] )) {
+			$sMethod = $_REQUEST ["sMethod"];
+			$reflection = new ReflectionMethod ( $this, $sMethod );
+			
+			if (method_exists ( $this, $sMethod ) and ! startsWith ( $sMethod, "_" ) and $reflection->isPublic ()) {
+				$this->$sMethod ();
 			} else {
-				throw new NotImplementedException ( "method " . htmlspecialchars ( $fcall ) . " is not available" );
+				throw new NotImplementedException ( "method " . htmlspecialchars ( $sMethod ) . " is not available" );
 			}
 		}
 	}
