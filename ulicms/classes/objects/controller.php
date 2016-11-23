79a7556e8f885dd9f2b1f85814ab62350fd8a589
Ulrich Schmidt <admin@deruli.de>
2016-11-23 07:11:12 +0100

    funktion ist nicht aufrufbar, wenn der Name mit einem Unterstrich beginnt

--- ulicms/classes/objects/controller.php
+++ ulicms/classes/objects/controller.php
@@ -3,7 +3,7 @@ class Controller {
 	public function __construct() {
 		if (isset ( $_REQUEST ["fcall"] ) and isNotNullOrEmpty ( $_REQUEST ["fcall"] )) {
 			$fcall = $_REQUEST ["fcall"];
-			if (method_exists ( $this, $fcall ) and ! startsWith ( $fcall, "__" )) {
+			if (method_exists ( $this, $fcall ) and ! startsWith ( $fcall, "_" )) {
 				$this->$fcall ();
 			} else {
 				throw new NotImplementedException ( "method " . htmlspecialchars ( $fcall ) . " is not available" );
