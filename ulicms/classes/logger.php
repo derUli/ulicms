1cada1fdfb0b1f39558506aee7844d3da7f468df
Ulrich Schmidt <admin@deruli.de>
2016-12-03 11:12:16 +0100

    refactor

--- ulicms/classes/logger.php
+++ ulicms/classes/logger.php
@@ -6,41 +6,38 @@ class logger {
 	}
 	public function log_db_query($query) {
 		// Das DB Logging kann man deaktivieren, durch anlegen der Konfigurationsvariable disable_query_log in der cms_config.php;
-		if (is_admin_dir ())
+		if (is_admin_dir ()){
 			include_once "../cms-config.php";
-		else
+		}
+		else{
 			include_once "cms-config.php";
+		}
 		$config = new config ();
 		if (! isset ( $config->query_logging )) {
 			return false;
 		} else {
-			
-			if (! $config->query_logging)
+			if (! $config->query_logging){
 				return false;
+			}
 		}
-		
 		$logdir = "";
 		
-		if (is_admin_dir ())
+		if (is_admin_dir ()){
 			$logdir .= "../";
-		
+		}
 		$logdir .= "content/log/db/";
-		
 		if (! is_dir ( $logdir )) {
 			@mkdir ( $logdir, 0755, true );
 			if (! is_dir ( $logdir )) {
 				return false;
 			}
 		}
-		
 		@$date = date ( "Y-m-d" );
 		@$time = date ( $log_date_format );
 		$logfile = $logdir . $date . ".log";
 		$query = trim ( $query );
-		
 		// Make all Line Endings Windows Style
 		$query = preg_replace ( '~\r\n?~', "\r\n", $query );
-		
 		$handle = fopen ( $logfile, "a" );
 		fwrite ( $handle, $time . "\t" );
 		fwrite ( $handle, $query );
@@ -49,4 +46,3 @@ class logger {
 		return true;
 	}
 }
-?>
