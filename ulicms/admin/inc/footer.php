65ab527a15e1893c5c0eb1131dcd30230041ba16
Ulrich Schmidt <u.schmidt@velian.de>
2017-01-20 13:41:46 +0100

    ajax

--- ulicms/admin/inc/footer.php
+++ ulicms/admin/inc/footer.php
@@ -1,3 +1,9 @@
+<div id="msgcontainer">
+	<img id="loading" src="gfx/loading.gif" alt="Bitte warten..."
+		style="display: none;">
+</div>
+<div id="message"><br/></div>
+</div>
 <p id="footer">
 	&copy; 2011 - <?php cms_release_year();?> by <a
 		href="http://www.ulicms.de" target="_blank">UliCMS</a>
