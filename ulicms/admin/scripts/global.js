1b76d6586bdafb3b84d51373cec9891fb94e8bb1
Ulrich Schmidt <ulrich1992@web.de>
2017-01-20 22:28:07 +0100

    Revert "menü merkt sich nun auch nach page reload ob es geöffnet war"
    This reverts commit c00c14f0975efde3d3c161443bb93da15055d1ff.

--- ulicms/admin/scripts/global.js
+++ ulicms/admin/scripts/global.js
@@ -1,16 +1,5 @@
 $(function() {
-	if(sessionStorage.getItem("openMenu") === null){
-		sessionStorage.setItem("openMenu", "yes" );
-	}
-	if(sessionStorage.getItem("openMenu") == "yes"){
-			$(".mainmenu").slideDown();
-	}
 	$("#menu-toggle").click(function() {
 		$(".mainmenu").slideToggle();
-			if(sessionStorage.getItem("openMenu") == "yes"){
-					sessionStorage.setItem("openMenu", "no" );
-				} else {
-						sessionStorage.setItem("openMenu", "yes" );
-				}
 	});
 });
