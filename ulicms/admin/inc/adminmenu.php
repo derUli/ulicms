80d5feccc9e75c979abaa312457d0ddf3846f52b
Ulrich Schmidt <u.schmidt@velian.de>
2017-01-20 10:01:58 +0100

    adminmenu

--- ulicms/admin/inc/adminmenu.php
+++ ulicms/admin/inc/adminmenu.php
@@ -7,11 +7,11 @@ $entries [] = new MenuEntry ( get_translation ( "contents" ), "?action=contents"
 $entries [] = new MenuEntry ( get_translation ( "media" ), "?action=media", "media" );
 $entries [] = new MenuEntry ( get_translation ( "users" ), "?action=admins", "admins" );
 $entries [] = new MenuEntry ( get_translation ( "groups" ), "?action=groups", "groups" );
-$entries [] = new MenuEntry ( get_translation ( "modules" ), "?action=modules", "modules" );
+$entries [] = new MenuEntry ( get_translation ( "packages" ), "?action=modules", "modules" );
 $entries [] = new MenuEntry ( get_translation ( "settings" ), "?action=settings", "settings" );
 $entries [] = new MenuEntry ( get_translation ( "info" ), "?action=info", "info" );
 $entries [] = new MenuEntry ( get_translation ( "logout" ), "?action=destroy", "destroy" );
-$entries = apply_filter($entries, "admin_menu_entries");
+$entries = apply_filter ( $entries, "admin_menu_entries" );
 $menu->setChildren ( $entries );
 $menu->render ();
 ?>
