18cea1ca81803024e22bada6b8d10c7ec941d9fe
Ulrich Schmidt <u.schmidt@velian.de>
2017-03-22 10:49:25 +0100

    theme-2018: wenn excerpt leer ist, wird der vollständige artikel angezeigt

--- ulicms/content/templates/2018/list.php
+++ ulicms/content/templates/2018/list.php
@@ -28,34 +28,48 @@ if ($id !== null) {
 				$next_start = $start + $limit;
 			}
 			
-
 			?>
-			
-				<div class="container">
+
+<div class="container">
 	<?php
 			
 			foreach ( $entries as $entry ) {
-				$article_image = getTemplateDirPath("2018") . "images/nopic.jpg";
-				$meta = get_article_meta($entry->systemname);
+				$article_image = getTemplateDirPath ( "2018" ) . "images/nopic.jpg";
+				$meta = get_article_meta ( $entry->systemname );
+				$excerpt = strip_tags ( $meta->excerpt, "<img><iframe><embed><object>" );
+				$excerpt = trim ( $excerpt );
+				$excerpt = isNotNullOrEmpty ( $excerpt ) ? $excerpt : $entry->content;
 				?>
   <div class="row article-list-row">
-	<p><strong><a
-		href="<?php Template::escape(buildSEOUrl($entry->systemname));?>"><?php Template::escape($entry->title);?></a></strong></p>
-		
-    <div class="col-sm-5">
-		<?php if($meta and !empty($meta->article_image)){
-			$article_image = $meta->article_image;
-		}
-			?>
-		<p><a href="<?php Template::escape(buildSEOUrl($entry->systemname));?>" class="list-article-image"><img src="<?php echo $article_image;?>" alt="Screenshot <?php Template::escape($entry->title);?>" class="article-image"></a></p>
-		
+		<p>
+			<strong><a
+				href="<?php Template::escape(buildSEOUrl($entry->systemname));?>"><?php Template::escape($entry->title);?></a></strong>
+		</p>
+
+		<div class="col-sm-5">
+		<?php
+				
+				if ($meta and ! empty ( $meta->article_image )) {
+					$article_image = $meta->article_image;
+				}
+				?>
+		<p>
+				<a href="<?php Template::escape(buildSEOUrl($entry->systemname));?>"
+					class="list-article-image"><img src="<?php echo $article_image;?>"
+					alt="Screenshot <?php Template::escape($entry->title);?>"
+					class="article-image"></a>
+			</p>
+
 		</div>
-		
-    <div class="col-sm-7">
+
+		<div class="col-sm-7">
 	
-	<?php echo $meta->excerpt;?>
-	<p><a href="<?php Template::escape(buildSEOUrl($entry->systemname));?>">Mehr Informationen</a></p>
-	</div>
+	<?php echo $excerpt;?>
+	<p>
+				<a href="<?php Template::escape(buildSEOUrl($entry->systemname));?>">Mehr
+					Informationen</a>
+			</p>
+		</div>
 	</div>
 	<?php }?>
 	
@@ -64,19 +78,20 @@ if ($id !== null) {
 <div class="page_older_newer">
 <?php if($start > 0 and $use_pagination){?>
 <span class="blog_pagination_newer"><a
-		href="<?php Template::escape(buildSEOUrl());?>?start=<?php echo $previous_start;?>"><?php Template::escape("<<");?></a></span>
+			href="<?php Template::escape(buildSEOUrl());?>?start=<?php echo $previous_start;?>"><?php Template::escape("<<");?></a></span>
 <?php }?>
 	<?php if($start + $limit < $entries_count_total and $use_pagination){?>
 <span class="blog_pagination_older"><a
-		href="<?php Template::escape(buildSEOUrl());?>?start=<?php echo $next_start;?>"><?php Template::escape(">>");?></a></span>
+			href="<?php Template::escape(buildSEOUrl());?>?start=<?php echo $next_start;?>"><?php Template::escape(">>");?></a></span>
 
 <?php }?>
 </div>
 
-                <div class="text-right rss-icon">
-                <a class="fa fa-rss fa-4x" href="<?php Template::escape(buildSEOUrl(false, null, "rss"));?>"
-                   title="Newsfeed"></a>
-                </div>
+	<div class="text-right rss-icon">
+		<a class="fa fa-rss fa-4x"
+			href="<?php Template::escape(buildSEOUrl(false, null, "rss"));?>"
+			title="Newsfeed"></a>
+	</div>
 <?php }?>
 	<?php
 		}
