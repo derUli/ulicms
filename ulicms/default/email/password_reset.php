94096982fd163e246ae89040b2fa6e3d04281069
Ulrich Schmidt <admin@deruli.de>
2017-04-12 18:01:02 +0200

    email template korrigiert

--- ulicms/default/email/password_reset.php
+++ ulicms/default/email/password_reset.php
@@ -1,8 +1,13 @@
 <?php translate("hello_name", array("%firstname%" => ViewBag::get("firstname"), "%lastname%"=>ViewBag::get("lastname")));?>
-
-<?php translate("password_reset_part1", array("%ip%" => ViewBag::get("ip"), "%domain%"=>get_domain()));?>
-
-<?php translate("password_reset_part2");?>
+ 
+ 
+<?php translate("password_reset1", array("%ip%" => ViewBag::get("ip"), "%domain%"=>get_domain()));?>
+ 
+ 
+<?php translate("password_reset2");?>
+ 
+ 
 <?php Template::escape(ViewBag::get("url"));?>
-
-<?php translate("password_reset_part3");?>
+ 
+ 
+<?php translate("password_reset3");?>
