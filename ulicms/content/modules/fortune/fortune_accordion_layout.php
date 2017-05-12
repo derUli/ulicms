e1411420febd1ee51a554d862be3fa8ba2986bd5
Ulrich Schmidt <u.schmidt@velian.de>
2017-05-12 14:41:20 +0200

    fortune

--- ulicms/content/modules/fortune/fortune_accordion_layout.php
+++ ulicms/content/modules/fortune/fortune_accordion_layout.php
@@ -1,10 +1,9 @@
+<?php
+$controller = ControllerRegistry::get ( getModuleMeta ( "fortune", "main_class" ) );
+?>
 <h2 class="accordion-header"><?php
 translate ( "fortune" );
 ?></h2>
 <div class="accordion-content">
-<?php
-include_once getModulePath ( "fortune" ) . "fortune_lib.php";
-$fortune = getRandomFortune ();
-echo nl2br ( $fortune );
-?>
+<?php echo $controller->render();?>
 </div>
