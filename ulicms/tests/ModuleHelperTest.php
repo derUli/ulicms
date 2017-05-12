c279bfa97fda9ca53dac3e6ec66a14d0e3105930
Ulrich Schmidt <ulrich1992@web.de>
2017-05-12 23:06:31 +0200

    ModuleHelperTest hinzugefügt

--- ulicms/tests/ModuleHelperTest.php
+++ ulicms/tests/ModuleHelperTest.php
@@ -9,4 +9,19 @@ class ModuleHelperTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ( "content/modules/my_module/js/coolscript.js", ModuleHelper::buildModuleRessourcePath ( "my_module", "js/coolscript.js" ) );
 		$this->assertEquals ( "content/modules/other_module/test.css", ModuleHelper::buildModuleRessourcePath ( "other_module", "test.css" ) );
 	}
+	public function testBuildAdminURL() {
+		$this->assertEquals ( "?action=module_settings&module=my_module&var1=hallo&var2=welt", ModuleHelper::buildAdminURL ( "my_module", "var1=hallo&var2=welt" ) );
+		$this->assertEquals ( "?action=module_settings&module=other_module", ModuleHelper::buildAdminURL ( "other_module" ) );
+	}
+	public function testGetFirstPageWithModule() {
+		$this->assertEquals ( 13, ModuleHelper::getFirstPageWithModule ()->id );
+		$this->assertEquals ( 14, ModuleHelper::getFirstPageWithModule ( "pfbc_sample" )->id );
+		$this->assertEquals ( 13, ModuleHelper::getFirstPageWithModule ( "fortune" )->id );
+		$this->assertEquals ( 7, ModuleHelper::getFirstPageWithModule ( "pfbc_sample", "de" )->id );
+		$this->assertEquals ( 6, ModuleHelper::getFirstPageWithModule ( "fortune" , "de" )->id);
+		$this->assertEquals ( 14, ModuleHelper::getFirstPageWithModule ( "pfbc_sample", "en" )->id );
+		$this->assertEquals ( 13, ModuleHelper::getFirstPageWithModule ( "fortune" , "en" )->id);
+		$this->assertEquals ( 6, ModuleHelper::getFirstPageWithModule ( null , "de" )->id);
+		$this->assertEquals ( 13, ModuleHelper::getFirstPageWithModule ( null , "en" )->id);
+	}
 }
