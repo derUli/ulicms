02791800101cae78b98f0cdc0fa7e98431afe607
Ulrich Schmidt <admin@deruli.de>
2017-05-12 17:15:08 +0200

    settings

--- ulicms/content/modules/fortune2/lang/en.php
+++ ulicms/content/modules/fortune2/lang/en.php
@@ -1,4 +1,4 @@
 <?php
 define ( "TRANSLATION_FORTUNE", "Fortune Cookie" );
-define ( "TRANSLATION_MY_SETTINGS_PAGE", "My Settings" );
+define ( "TRANSLATION_MY_SETTINGS_PAGE", "My Settings Page" );
 define ( "TRANSLATION_HELLO_WORLD", "Hello World!" );
