ac97df76cfae160f51b28774e0450807c98c24dd
Ulrich Schmidt <u.schmidt@velian.de>
2017-05-12 14:38:49 +0200

    fortune theme angepasst

--- ulicms/content/modules/fortune/templates/default.php
+++ ulicms/content/modules/fortune/templates/default.php
@@ -1,6 +1,9 @@
+<?php
+$controller = ControllerRegistry::get ( getModuleMeta ( "fortune", "main_class" ) );
+?>
 <div class="fortune">
 <?php
-$fortune = getRandomFortune ();
+$fortune = $controller->getRandomFortune ();
 echo nl2br ( $fortune );
 ?>
 	</div>
