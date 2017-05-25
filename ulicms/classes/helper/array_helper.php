5d2bf535f96a3bda4207fd3cb375029adbafb574
Ulrich Schmidt <admin@deruli.de>
2017-05-25 13:17:27 +0200

    array_helper hinzugefügt

---
+++ ulicms/classes/helper/array_helper.php
@@ -0,0 +1,37 @@
+<?php
+class ArrayHelper extends Helper {
+	public static function insertBefore($input, $index, $element) {
+		if (! array_key_exists ( $index, $input )) {
+			throw new Exception ( "Index not found" );
+		}
+		$tmpArray = array ();
+		$originalIndex = 0;
+		foreach ( $input as $key => $value ) {
+			if ($key === $index) {
+				$tmpArray [] = $element;
+				break;
+			}
+			$tmpArray [$key] = $value;
+			$originalIndex ++;
+		}
+		array_splice ( $input, 0, $originalIndex, $tmpArray );
+		return $input;
+	}
+	public static function insertAfter($input, $index, $element) {
+		if (! array_key_exists ( $index, $input )) {
+			throw new Exception ( "Index not found" );
+		}
+		$tmpArray = array ();
+		$originalIndex = 0;
+		foreach ( $input as $key => $value ) {
+			$tmpArray [$key] = $value;
+			$originalIndex ++;
+			if ($key === $index) {
+				$tmpArray [] = $element;
+				break;
+			}
+		}
+		array_splice ( $input, 0, $originalIndex, $tmpArray );
+		return $input;
+	}
+}
