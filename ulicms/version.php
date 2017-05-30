12ef4a3356c20d339e09a3c1628fc1f25ff7f83d
Ulrich Schmidt <admin@deruli.de>
2017-05-30 21:37:03 +0200

    versionsnummer angepasst

--- ulicms/version.php
+++ ulicms/version.php
@@ -7,7 +7,7 @@ if (! class_exists ( "ulicms_version" )) {
 			$this->internalVersion = Array (
 					2017,
 					3,
-					7
+					8
 			);
 			$this->update = "";
 			$this->developmentVersion = false;
