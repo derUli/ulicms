b0e1555c725fe49b79ec2114c0f6c6d4f7d11768
Ulrich Schmidt <admin@deruli.de>
2017-06-07 00:12:15 +0200

    spalte default_language zur datenbank hinzugefügt

--- ulicms/version.php
+++ ulicms/version.php
@@ -7,7 +7,7 @@ if (! class_exists ( "ulicms_version" )) {
 			$this->internalVersion = Array (
 					2017,
 					3,
-					8
+					9
 			);
 			$this->update = "";
 			$this->developmentVersion = false;
