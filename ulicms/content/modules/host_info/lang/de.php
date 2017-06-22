f6f338d51aaf00c9be910801200d0cb7fb0ae0e5
Ulrich Schmidt <admin@deruli.de>
2017-06-23 00:50:36 +0200

    typo fix

--- ulicms/content/modules/host_info/lang/de.php
+++ ulicms/content/modules/host_info/lang/de.php
@@ -4,7 +4,7 @@ define ( "TRANSLATION_UPTIME", "Betriebszeit" );
 define ( "TRANSLATION_OPERATING_SYSTEM", "Betriebssystem" );
 define ( "TRANSLATION_MEMORY_USAGE_MB", "Arbeitsspeicher Nutzung in MB" );
 define ( "TRANSLATION_DISK_USAGE", "Festplattenbelegung" );
-define ( "TRANSLATION_CPU_INFO", "CPU INfo" );
+define ( "TRANSLATION_CPU_INFO", "CPU Info" );
 define ( "TRANSLATION_PHP_VERSION", "PHP Version" );
 define ( "TRANSLATION_MYSQL_CLIENT_VERSION", "MySQL Client Version" );
 define ( "TRANSLATION_MYSQL_SERVER_VERSION", "MySQL Server Version" );
