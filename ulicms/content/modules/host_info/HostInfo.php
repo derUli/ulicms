0f00d8594f652cdacd3f94c7ccc47db1acc009c4
Ulrich Schmidt <admin@deruli.de>
2017-06-23 00:52:50 +0200

    settings headline

--- ulicms/content/modules/host_info/HostInfo.php
+++ ulicms/content/modules/host_info/HostInfo.php
@@ -1,8 +1,8 @@
 <?php
 class HostInfo extends Controller {
 	private $moduleName = "host_info";
-	public function getSettingsHeadline(){
-		return get_translation("host_info");
+	public function getSettingsHeadline() {
+		return get_translation ( "host_info" );
 	}
 	public function settings() {
 		return Template::executeModuleTemplate ( $this->moduleName, "view.php" );
