e562a6d39251bf79f8ac1a030b4646d3c05d3500
Ulrich Schmidt <u.schmidt@velian.de>
2017-06-23 16:12:45 +0200

    unnötige funktionen entfernt

--- ulicms/classes/objects/abstract/controller.php
+++ ulicms/classes/objects/abstract/controller.php
@@ -3,8 +3,6 @@ abstract class Controller {
 	protected $blacklist = array (
 			"runCommand" 
 	);
-	public function __construct() {
-	}
 	public function runCommand() {
 		if (isset ( $_REQUEST ["sMethod"] ) and StringHelper::isNotNullOrEmpty ( $_REQUEST ["sMethod"] ) and ! faster_in_array ( $_REQUEST ["sMethod"], $this->blacklist )) {
 			$sMethod = $_REQUEST ["sMethod"];
