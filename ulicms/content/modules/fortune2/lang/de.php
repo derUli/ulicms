f88c6a3d4becb2270a743344b03fde12792a205e
Ulrich Schmidt <admin@deruli.de>
2017-06-25 16:46:41 +0200

    Link zu Adminseite von Modul Anpassbar machen

--- ulicms/content/modules/fortune2/lang/de.php
+++ ulicms/content/modules/fortune2/lang/de.php
@@ -5,3 +5,4 @@ define ( "TRANSLATION_HELLO_WORLD", "Hallo Welt!" );
 define ( "TRANSLATION_UNKNOWN_REQUEST_TYPE", "Unbekannte HTTP-Methode" );
 define ( "TRANSLATION_POST_REQUEST_TYPE", "Das ist eine POST Antwort." );
 define ( "TRANSLATION_GET_REQUEST_TYPE", "Das ist eine GET Antwort." );
+define ( "TRANSLATION_SHOW_SAMPLES", "Beispiele anzeigen" );
--- ulicms/content/modules/fortune2/lang/en.php
+++ ulicms/content/modules/fortune2/lang/en.php
@@ -5,3 +5,4 @@ define ( "TRANSLATION_HELLO_WORLD", "Hello World!" );
 define ( "TRANSLATION_UNKNOWN_REQUEST_TYPE", "Unkwown Request Method" );
 define ( "TRANSLATION_POST_REQUEST_TYPE", "This is POST answer." );
 define ( "TRANSLATION_GET_REQUEST_TYPE", "This is GET answer." );
+define ( "TRANSLATION_SHOW_SAMPLES", "Show Examples" );
