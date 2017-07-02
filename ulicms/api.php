c23b8bd5e74c264362bb7c4c9dedc7b9ed0101eb
Ulrich Schmidt <admin@deruli.de>
2017-07-02 23:12:26 +0200

    Bestimmte Typen dürfen nicht mehr als Startseite auswählbar sein

--- ulicms/api.php
+++ ulicms/api.php
@@ -852,7 +852,7 @@ function getModulePath($module, $abspath = false) {
 	// Frontend Directory
 	if (is_file ( "cms-config.php" )) {
 		$module_folder = "content/modules/";
-	}  // Backend Directory
+	} // Backend Directory
 else {
 		$module_folder = "../content/modules/";
 	}
@@ -1108,7 +1108,7 @@ function getAllPages($lang = null, $order = "systemname", $exclude_hash_links = 
 	}
 	$returnvalues = Array ();
 	while ( $row = db_fetch_assoc ( $query ) ) {
-		if (! ($exclude_hash_links and startsWith ( $row ["redirection"], "#" ))) {
+		if (! ($exclude_hash_links and startsWith ( $row ["redirection"], "#" )) and $row ["type"] != "link" and $row ["type"] != "node" and $row["type"] <> "language_link") {
 			array_push ( $returnvalues, $row );
 		}
 	}
