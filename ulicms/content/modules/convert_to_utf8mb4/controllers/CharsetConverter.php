87d95dd87e9a77aa49fb442d7b44bb196cc91894
Ulrich Schmidt <admin@deruli.de>
2017-07-07 18:05:02 +0200

    convert_to_utf8mb4 in eigenes Repository ausgelagert

--- ulicms/content/modules/convert_to_utf8mb4/controllers/CharsetConverter.php
+++
@@ -1,6 +0,0 @@
-<?php
-class CharsetConverter extends Controller {
-	public function convertTOUTF8MB4($table) {
-		return Database::pQuery ( "ALTER TABLE $table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" );
-	}
-}
