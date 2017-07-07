5503b002609ec12e37f803921e85d9129d488444
Ulrich Schmidt <admin@deruli.de>
2017-07-07 14:39:28 +0200

    übersetzung hinzugefügt

--- ulicms/content/modules/convert_to_utf8mb4/lang/en.php
+++ ulicms/content/modules/convert_to_utf8mb4/lang/en.php
@@ -1,3 +1,4 @@
 <?php
 define ( "TRANSLATION_START_CONVERSION", "Konversion starten" );
 define ( "TRANSLATION_CONVERT_TO_UTF8MB4", "Convert to utf8mb4" );
+define ( "TRANSLATION_CONVERT_TO_UTF8MB4_HELP_TEXT", "This module enables support for UliCMS by converting all database tables to utf8mb4 charset." );
