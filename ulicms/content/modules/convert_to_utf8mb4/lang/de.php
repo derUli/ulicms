4183d7c13f3b20297c4b08edac6689adfa735bd8
Ulrich Schmidt <admin@deruli.de>
2017-07-07 17:35:58 +0200

    Deutsche Sprachdatei hinzugefügt

---
+++ ulicms/content/modules/convert_to_utf8mb4/lang/de.php
@@ -0,0 +1,5 @@
+<?php
+define ( "TRANSLATION_START_CONVERSION", "Konversion starten" );
+define ( "TRANSLATION_CONVERT_TO_UTF8MB4", "Zu utf8mb4 konvertieren" );
+define ( "TRANSLATION_CONVERT_TO_UTF8MB4_HELP_TEXT", "Dieses Modul aktiviert Unterstützung für Emojis durch Konvertierung der Datenbanktabellen in den Zeichensatz utf8mb4." );
+define ( "TRANSLATION_CONVERSION_FINISHED", "Konvertierung des Zeichensatzes abgeschlossen." );
--- ulicms/content/modules/convert_to_utf8mb4/lang/en.php
+++ ulicms/content/modules/convert_to_utf8mb4/lang/en.php
@@ -1,5 +1,5 @@
 <?php
-define ( "TRANSLATION_START_CONVERSION", "Konversion starten" );
+define ( "TRANSLATION_START_CONVERSION", "Start Conversion" );
 define ( "TRANSLATION_CONVERT_TO_UTF8MB4", "Convert to utf8mb4" );
 define ( "TRANSLATION_CONVERT_TO_UTF8MB4_HELP_TEXT", "This module enables support for UliCMS by converting all database tables to utf8mb4 charset." );
 define ( "TRANSLATION_CONVERSION_FINISHED", "Table charset conversion finished." );
