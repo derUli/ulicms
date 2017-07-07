559de50e0ee34dcd5041939f2884cb9cce54af3b
Ulrich Schmidt <admin@deruli.de>
2017-07-07 12:57:50 +0200

    zwischenstand

--- ulicms/content/modules/convert_to_utf8mb4/templates/form.php
+++ ulicms/content/modules/convert_to_utf8mb4/templates/form.php
@@ -1,8 +1,7 @@
 <div id="convert-container">
-	<form
-		action="<?php Template::escape(ModuleHelper::buildAdminURL("convert_to_utf8mb4", ModuleHelper::buildMethodCall("ConvertToUTF8MB4", "convertTable")));?>"
-		method="post">
-	<?php csrf_token_html();?>
+	<form action="#"
+		data-action-url="<?php Template::escape(ModuleHelper::buildAdminURL("convert_to_utf8mb4", ModuleHelper::buildMethodCall("ConvertToUTF8MB4", "convertTable")));?>"
+		method="get">
 	<p><?php translate("convert_to_utf8mb4_help_text")?></p>
 		<input type="submit" name="submit"
 			value="<?php translate("start_conversion");?>">
