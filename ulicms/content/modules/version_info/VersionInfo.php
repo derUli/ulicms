b6a04dfbe4ca247221e9ae1c912bbd7ef54e191c
Ulrich Schmidt <admin@deruli.de>
2017-07-08 11:42:34 +0200

    modul version_info in eigenes repo ausgelagert

--- ulicms/content/modules/version_info/VersionInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-class VersionInfo extends Controller {
-	public function adminCopyrightFooterLeft() {
-		return "<small>";
-	}
-	public function adminCopyrightFooterRight() {
-		echo Template::executeModuleTemplate ( "version_info", "version_info" );
-	}
-}
