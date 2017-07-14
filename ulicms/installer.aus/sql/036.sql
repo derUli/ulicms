f8e1704656d04f745c9c620df2724f5199f762d9
Ulrich Schmidt <admin@deruli.de>
2017-07-14 23:14:59 +0200

    alle tabellen haben nun das charset utf8mb4

--- ulicms/installer.aus/sql/036.sql
+++ ulicms/installer.aus/sql/036.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `{prefix}custom_fields` ( `id` INT NOT NULL AUTO_INCREMENT , `content_id` INT NOT NULL , `name` VARCHAR(100) NOT NULL , `value` TEXT NOT NULL , PRIMARY KEY (`id`) ) ENGINE = InnoDB;
+CREATE TABLE `{prefix}custom_fields` ( `id` INT NOT NULL AUTO_INCREMENT , `content_id` INT NOT NULL , `name` VARCHAR(100) NOT NULL , `value` TEXT NOT NULL , PRIMARY KEY (`id`) )  ENGINE=InnoDB DEFAULT charset=utf8mb4;
