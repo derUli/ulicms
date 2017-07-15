82d02e745267d55c253f0416c74ce6104e1281cf
Ulrich Schmidt <admin@deruli.de>
2017-07-15 20:29:31 +0200

    move_menu_items in eigenes Repository verschoben

--- ulicms/content/modules/move_menu_items/lang/de.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ( "TRANSLATION_MOVE", "Verschieben" );
-define ( "TRANSLATION_MOVE_MENU_ITEMS", "Menüeinträge verschieben" );
-define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_FROM", "Verschiebe alle Menüeinträge von" );
-define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_TO", "nach" );
-define ( "TRANSLATION_X_DATASET_MOVED", "%count% Datensatz verschoben." );
-define ( "TRANSLATION_X_DATASETS_MOVED", "%count% Datensätze verschoben." );
--- ulicms/content/modules/move_menu_items/lang/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ( "TRANSLATION_MOVE", "Move" );
-define ( "TRANSLATION_MOVE_MENU_ITEMS", "Move menu entries" );
-define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_FROM", "Move all menu entries from " );
-define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_TO", "to" );
-define ( "TRANSLATION_X_DATASET_MOVED", "%count% datasets moved." );
-define ( "TRANSLATION_X_DATASETS_MOVED", "%count% dataset moved." );
