99d5c36287ca6c0bbaf42821d8bdf65cd0073271
Ulrich Schmidt <admin@deruli.de>
2017-07-15 14:24:05 +0200

    space entfernt

--- ulicms/content/modules/move_menu_items/lang/de.php
+++ ulicms/content/modules/move_menu_items/lang/de.php
@@ -1,6 +1,5 @@
 <?php
 define ( "TRANSLATION_MOVE", "Verschieben" );
-
 define ( "TRANSLATION_MOVE_MENU_ITEMS", "Menüeinträge verschieben" );
 define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_FROM", "Verschiebe alle Menüeinträge von" );
 define ( "TRANSLATION_MOVE_ALL_MENU_ITEMS_TO", "nach" );
