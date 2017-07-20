ab97e1b1eca62423564041e08f1715e5d55a9f88
Ulrich Schmidt <admin@deruli.de>
2017-07-20 15:44:22 +0200

    simple_md5_generator in eigenes repo verschoben

--- ulicms/content/modules/simple_md5_generator/lang/de.php
+++
@@ -1,6 +0,0 @@
-<?php
-define ( "TRANSLATION_SIMPLE_MD5_GENERATOR", "Einfacher MD5 Generator" );
-define ( "TRANSLATION_ENTER_TEXT", "Text eingeben" );
-define ( "TRANSLATION_GENERATE", "Generieren" );
-define ( "TRANSLATION_RESULT", "Ergebnis" );
-?>
--- ulicms/content/modules/simple_md5_generator/lang/en.php
+++
@@ -1,6 +0,0 @@
-<?php
-define ( "TRANSLATION_SIMPLE_MD5_GENERATOR", "Simple MD5 Generator" );
-define ( "TRANSLATION_ENTER_TEXT", "Enter Text" );
-define ( "TRANSLATION_GENERATE", "Generate" );
-define ( "TRANSLATION_RESULT", "Result:" );
-?>
