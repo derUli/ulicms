bc6ec379366d31bb6823f21283fb3891c7c3c9d2
Ulrich Schmidt <admin@deruli.de>
2017-07-28 17:03:00 +0200

    Revert "secure cookies"
    This reverts commit e7c9085a02c7f6e9c1b0ad2aaf1d4b363946a3ec.

--- ulicms/content/modules/security_http_headers/SecurityHTTPHeaders.php
+++ ulicms/content/modules/security_http_headers/SecurityHTTPHeaders.php
@@ -8,16 +8,5 @@ class SecurityHTTPHeaders extends Controller {
 		header ( 'X-XSS-Protection: 1; mode=block' );
 		header ( 'X-Content-Type-Options: nosniff' );
 		header ( "Referrer-Policy: no-referrer-when-downgrade" );
-		
-		// **PREVENTING SESSION HIJACKING**
-		// Prevents javascript XSS attacks aimed to steal the session ID
-		ini_set ( 'session.cookie_httponly', 1 );
-		
-		// **PREVENTING SESSION FIXATION**
-		// Session ID cannot be passed through URLs
-		ini_set ( 'session.use_only_cookies', 1 );
-		
-		// Uses a secure connection (HTTPS) if possible
-		ini_set ( 'session.cookie_secure', 1 );
 	}
 }
