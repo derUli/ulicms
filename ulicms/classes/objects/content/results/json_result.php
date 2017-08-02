c4f86d68cf4f01e33ca8fdce1de89b4f57a93359
Ulrich Schmidt <u.schmidt@velian.de>
2017-08-02 11:49:21 +0200

    http/1.0 davor

--- ulicms/classes/objects/content/results/json_result.php
+++ ulicms/classes/objects/content/results/json_result.php
@@ -1,12 +1,12 @@
 <?php
 function JSONResult($data, $status = 200) {
-	header ( Request::getStatusCodeByNumber ( $status ) );
+	header ( "HTTP/1.0 " . Request::getStatusCodeByNumber ( $status ) );
 	header ( 'Content-Type: application/json' );
 	echo json_encode ( $data );
 	exit ();
 }
 function RawJSONResult($data, $status = 200) {
-	header ( Request::getStatusCodeByNumber ( $status ) );
+	header ( "HTTP/1.0 " . Request::getStatusCodeByNumber ( $status ) );
 	header ( 'Content-Type: application/json' );
 	echo $data;
 	exit ();
