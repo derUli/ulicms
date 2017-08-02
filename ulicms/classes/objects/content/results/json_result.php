567edf26296f9afd919db6ed1bf571d53b99ebb4
Ulrich Schmidt <u.schmidt@velian.de>
2017-08-02 14:48:20 +0200

    JSON Result setzt nun die content-length

--- ulicms/classes/objects/content/results/json_result.php
+++ ulicms/classes/objects/content/results/json_result.php
@@ -1,13 +1,19 @@
 <?php
 function JSONResult($data, $status = 200) {
 	header ( "HTTP/1.0 " . Request::getStatusCodeByNumber ( $status ) );
+	$json = json_encode ( $data );
+	// get string size in Byte
+	$size = ini_get ( 'mbstring.func_overload' ) ? mb_strlen ( $json, '8bit' ) : strlen ( $json );
 	header ( 'Content-Type: application/json' );
-	echo json_encode ( $data );
+	header ( "Content-length: $size" );
+	echo $json;
 	exit ();
 }
 function RawJSONResult($data, $status = 200) {
 	header ( "HTTP/1.0 " . Request::getStatusCodeByNumber ( $status ) );
+	$size = ini_get ( 'mbstring.func_overload' ) ? mb_strlen ( $data, '8bit' ) : strlen ( $data );
 	header ( 'Content-Type: application/json' );
+	header ( "Content-length: $size" );
 	echo $data;
 	exit ();
 }
