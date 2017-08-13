df7f5f29c3a2660bdf6a012b049316f3e1db051f
Ulrich Schmidt <admin@deruli.de>
2017-08-13 11:18:14 +0200

    Modul gebaut, welches die Anzahl der Module je Kategorie anzeigt

---
+++ services/count_by_category/main.php
@@ -0,0 +1,27 @@
+<?php
+function count_by_category_render() {
+	$sql = "select c.name as Kategorie, count(e.id) as Anzahl from {prefix}content e inner join {prefix}categories c on c.id = category where type = 'article'  group by e.category order by Kategorie desc;";
+	$query = Database::query ( $sql, true );
+	?>
+<table class="tablesorter">
+
+	<thead>
+		<tr>
+			<th>Kategorie</th>
+			<th>Anzahl</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php while($row = Database::fetchObject($query)){?>
+	<tr>
+			<td><?php Template::escape($row->Kategorie);?></td>
+			<td class="text-right"><?php  Template::escape(strval($row->Anzahl));?></td>
+		</tr>
+	<?php }?>
+	</tbody>
+
+</table>
+
+<?php
+
+}
