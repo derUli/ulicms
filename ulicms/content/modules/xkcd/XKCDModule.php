bab6f6b6333dca0766b7cee78bbdc0340738b5f4
Ulrich Schmidt <admin@deruli.de>
2017-08-20 12:16:09 +0200

    nicht mehr zutreffenden Kommentar entfernt

--- ulicms/content/modules/xkcd/XKCDModule.php
+++ ulicms/content/modules/xkcd/XKCDModule.php
@@ -3,7 +3,7 @@ class XKCDModule extends Controller {
 	public function render() {
 		$xkcd = new xkcd ();
 		$html = '<div class="random-xkcd">';
-		$comic = $xkcd->random (); // get the comic #327, Exploits of a Mom.
+		$comic = $xkcd->random ();
 		$html .= '<h3>' . $comic->safe_title . ' - xkcd</h3>'; // prints the title
 		$html .= "<img class=\"responsive-image\" src=\"{$comic->img}\" title=\"{$comic->alt}\"/>"; // prints the image (don't miss the hover text!)
 		$html .= '</div>';
@@ -12,7 +12,7 @@ class XKCDModule extends Controller {
 	}
 	public function accordionLayout() {
 		$xkcd = new xkcd ();
-		$comic = $xkcd->random (); // get the comic #327, Exploits of a Mom.
+		$comic = $xkcd->random ();
 		$html .= '<h2 class="accordion-header">' . $comic->safe_title . ' - xkcd</h2>';
 		$html .= '<div class="accordion-content">';
 		$html .= "<img class=\"responsive-image\" src=\"{$comic->img}\" title=\"{$comic->alt}\"/>"; // prints the image (don't miss the hover text!)
