a36424c3ce373c7d33ac64a0f8c37c183b33d223
Ulrich Schmidt <admin@deruli.de>
2017-08-28 07:34:23 +0200

    sql Migrations hinzugefügt

---
+++ ulicms/content/modules/highlight_php_code/sql/up/001.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `{prefix}php_code`
+( `id` INT NOT NULL AUTO_INCREMENT ,
+`name` VARCHAR(140) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci 
+NOT NULL , `code` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci 
+NOT NULL , PRIMARY KEY (`id`), UNIQUE (`name`)) ENGINE = InnoDB;
