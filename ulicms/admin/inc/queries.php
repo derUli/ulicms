40a091162e617f67386f3621106a7b823ffd5535
Ulrich Schmidt <admin@deruli.de>
2017-09-09 11:58:00 +0200

    Grundeinstellungen in Core Modul ausgelagert

--- ulicms/admin/inc/queries.php
+++ ulicms/admin/inc/queries.php
@@ -5,7 +5,6 @@ add_hook ( "query" );
 include_once ULICMS_ROOT . "/classes/objects/content/vcs.php";
 
 if ($acl->hasPermission ( "pages" ) and Request::getVar ( "toggle-show-core-modules" )) {
-	
 	$_SESSION ["show_core_modules"] = ! $_SESSION ["show_core_modules"];
 	Request::redirect ( ModuleHelper::buildActionURL ( Request::getVar ( "action" ) ) );
 }
@@ -19,28 +18,6 @@ if ($_REQUEST ["action"] == "install-sin-package" and StringHelper::isNotNullOrE
 		@unlink ( $path );
 	}
 	Request::redirect ( "index.php?action=sin_package_install_ok&file=$file" );
-}
-if ($_GET ["action"] == "save_settings" && isset ( $_POST ["save_settings"] ) && $acl->hasPermission ( "settings_simple" )) {
-	add_hook ( "before_safe_simple_settings" );
-	setconfig ( "homepage_owner", db_escape ( $_POST ["homepage_owner"] ) );
-	setconfig ( "language", db_escape ( $_POST ["language"] ) );
-	setconfig ( "visitors_can_register", intval ( isset ( $_POST ["visitors_can_register"] ) ) );
-	setconfig ( "maintenance_mode", intval ( isset ( $_POST ["maintenance_mode"] ) ) );
-	setconfig ( "email", db_escape ( $_POST ["email"] ) );
-	setconfig ( "max_news", ( int ) $_POST ["max_news"] );
-	setconfig ( "logo_disabled", db_escape ( $_POST ["logo_disabled"] ) );
-	setconfig ( "timezone", db_escape ( $_POST ["timezone"] ) );
-	setconfig ( "robots", db_escape ( $_POST ["robots"] ) );
-	
-	if (! isset ( $_POST ["disable_password_reset"] )) {
-		setconfig ( "disable_password_reset", "disable_password_reset" );
-	} else {
-		Settings::delete ( "disable_password_reset" );
-	}
-	
-	add_hook ( "after_safe_simple_settings" );
-	header ( "Location: index.php?action=settings_simple" );
-	exit ();
 }
 
 if ($_GET ["action"] == "view_website" or $_GET ["action"] == "frontpage") {
