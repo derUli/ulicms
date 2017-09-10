d2ace44f5270a19b9a617642763854410f5952d1
Ulrich Schmidt <admin@deruli.de>
2017-09-10 13:06:09 +0200

    Design einstellungen in das core_settings Modul verschoben

--- ulicms/content/modules/core_expert_settings/lang/de.php
+++
@@ -1,4 +0,0 @@
-<?php
-define ( "TRANSLATION_CREATE_OPTION", "Konfigurationsvariable erstellen" );
-define ( "TRANSLATION_OPTION", "Option" );
-define ( "TRANSLATION_VALUE", "Wert" );
--- ulicms/content/modules/core_expert_settings/lang/en.php
+++
@@ -1,4 +0,0 @@
-<?php
-define ( "TRANSLATION_CREATE_OPTION", "Create configuration variable" );
-define ( "TRANSLATION_OPTION", "Variable" );
-define ( "TRANSLATION_VALUE", "Value" );
