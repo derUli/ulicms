f446e686be2b6b6a37f9530f1f63ed1afdeff61b
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-12 13:43:49 +0200

    settings template hinzugefügt

--- ulicms/content/modules/select_google_fonts/templates/settings.php
+++ ulicms/content/modules/select_google_fonts/templates/settings.php
@@ -5,8 +5,7 @@ $fonts = $controller->getGoogleFonts();
 <ol>
 <?php foreach($fonts as $font){
   ?>
-<li><span style="font-family:<?php echo Template::escape($font);?>">Franz jagt im komplett verwahrlosten Taxi quer durch Bayern
-.</span></li>
+<li><span style="font-family:<?php echo Template::escape($font);?>">Franz jagt im komplett verwahrlosten Taxi quer durch Bayern.</span></li>
 <?php
 }
 ?>
