49d691062be52d5f7128d98ce072076b5f2cdb32
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-12 13:47:56 +0200

    settings template hinzugefügt

--- ulicms/content/modules/select_google_fonts/templates/settings.php
+++ ulicms/content/modules/select_google_fonts/templates/settings.php
@@ -2,10 +2,11 @@
 $controller = ControllerRegistry::get("DesignSettingsController");
 $fonts = $controller->getGoogleFonts();
 ?>
-<ol>
+<!-- @TODO: CSS in Datei auslagern-->
+<ol style="list-style:none">
 <?php foreach($fonts as $font){
   ?>
-<li><span style="font-family:<?php echo Template::escape($font);?>">Franz jagt im komplett verwahrlosten Taxi quer durch Bayern.</span></li>
+<li><input type="checkbox" name="fonts"> <span style="font-family:<?php echo Template::escape($font);?>; font-size:110%;">Franz jagt im komplett verwahrlosten Taxi quer durch Bayern.</span></li>
 <?php
 }
 ?>
