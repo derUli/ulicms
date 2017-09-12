ad18fc0a020ab85d786dc8989797f7e2defe6186
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-12 15:09:31 +0200

    Bugfix: Administratoren-Gruppe wird wieder angelegt

--- ulicms/installer.aus/sql/002.sql
+++ ulicms/installer.aus/sql/002.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `{prefix}groups` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(255) NOT NULL,
 `permissions` mediumtext NOT NULL,
+`allowable_tags` TEXT NULL DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT charset=utf8mb4 AUTO_INCREMENT=1 ;
