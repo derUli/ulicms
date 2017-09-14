8434e7c9325cba679861af1ed6f7305dd98bddb7
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-14 14:23:44 +0200

    unit test, noch ohne funktion

--- ulicms/content/modules/is_absolute_path/tests/IsAbsolutePathTest.php
+++ ulicms/content/modules/is_absolute_path/tests/IsAbsolutePathTest.php
@@ -1,5 +1,15 @@
 <?php
 class isAbsolutePathTest extends PHPUnit_Framework_TestCase {
-	public function testIsAbsolutePath() {
+	public function testIsAbsolutePathUnix() {
+		$this->assertTrue ( is_absolute_path ( "/" ) );
+		$this->assertTrue ( is_absolute_path ( "/var/www/html/index.html" ) );
+		$this->assertTrue ( is_absolute_path ( "/var/www/html/index" ) );
+		$this->assertTrue ( is_absolute_path ( "/var/www/html/index/" ) );
+		$this->assertFalse ( is_absolute_path ( "test.html" ) );
+		$this->assertFalse ( is_absolute_path ( "./test.html" ) );
+		$this->assertFalse ( is_absolute_path ( "../test.html" ) );
+		$this->assertFalse ( is_absolute_path ( "../test/test.html" ) );
+		$this->assertFalse ( is_absolute_path ( "mein/test.html" ) );
 	}
+	// @TODO: Implement Test for windows
 }
