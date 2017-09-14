1e51380f79aeb8995c4cd2665c5cdc01955f3c18
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-14 13:37:30 +0200

    PrependSchemaTest hinzugefügt, noch leer

--- ulicms/content/modules/prepend_scheme/tests/PrependSchemaTest.php
+++ ulicms/content/modules/prepend_scheme/tests/PrependSchemaTest.php
@@ -1,3 +1,10 @@
 <?php
 class PrependSchemaTest extends PHPUnit_Framework_TestCase {
+	public function testPrependSchema() {
+		$this->assertEquals ( "http://www.google.de", prepend_scheme ( "www.google.de" ) );
+		$this->assertEquals ( "http://www.google.de", prepend_scheme ( "http://www.google.de" ) );
+		$this->assertEquals ( "https://www.google.de", prepend_scheme ( "https://www.google.de" ) );
+		$this->assertEquals ( "ftp://www.google.de", prepend_scheme ( "ftp://www.google.de" ) );
+		$this->assertEquals ( "ftps://www.google.de", prepend_scheme ( "ftps://www.google.de" ) );
+	}
 }
