6c778af2168df8181325b7efae0e4b8556070217
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-14 14:14:44 +0200

    Modul is_absolute_path hinzugefügt

---
+++ ulicms/content/modules/is_absolute_path/is_absolute_path.php
@@ -0,0 +1,6 @@
+<?php
+function is_absolute_path($path) {
+	if ($path === null || $path === '')
+		throw new Exception ( "Empty path" );
+	return $path [0] === DIRECTORY_SEPARATOR || preg_match ( '~\A[A-Z]:(?![^/\\\\])~i', $path ) > 0;
+}
