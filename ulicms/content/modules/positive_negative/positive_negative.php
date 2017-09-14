75d3fa326d5216e07b7fd020909c6eb0869b8553
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-14 10:36:52 +0200

    positive_negativ

--- ulicms/content/modules/positive_negative/positive_negative.php
+++ ulicms/content/modules/positive_negative/positive_negative.php
@@ -1,6 +1,9 @@
 <?php
-function is_positive($value) {
-	return (is_numeric ( $value ) and $value >= 0);
+function is_positive($value, $zeroPositive = false) {
+	if ($zeroPositive) {
+		return (is_numeric ( $value ) and $value >= 0);
+	}
+	return (is_numeric ( $value ) and $value > 0);
 }
 function is_negative($value) {
 	return (is_numeric ( $value ) and $value < 0);
