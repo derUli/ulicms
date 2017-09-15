b61a5dee01ac8919875ec5eb864d5b8d61b3b657
Ulrich Schmidt <admin@deruli.de>
2017-09-15 18:02:57 +0200

    switch_users zeigt nun die Gruppe an

--- ulicms/content/modules/select_google_fonts/lang/en.php
+++
@@ -1,3 +0,0 @@
-<?php
-define ( "TRANSLATION_FONT_PREVIEW_TEXT", "The quick brown fox jumps over the lazy dog." );
-define ( "TRANSLATION_USE_THIS_FONT", "Use this font" );
--- ulicms/content/modules/switch_users/templates/user_select.php
+++ ulicms/content/modules/switch_users/templates/user_select.php
@@ -5,12 +5,18 @@ echo ModuleHelper::buildMethodCallForm ( "SwitchUsers", "switchUser", array (
 		"url" => getCurrentURL () 
 ), "post" );
 ?>
-<strong><?php translate("switch_user")?></strong>
+<strong><?php translate("switch_user");?></strong>
 <br />
 <select name="user_id" id="user_switch">
 <?php foreach($users as $user){?>
+<?php
+	$group = new Group ( $user->getGroupId () );
+	$groupName = $group->getName () ? $group->getName () : "-";
+	?>
 <option value="<?php echo $user->getId();?>"
-		<?php if(get_user_id() == $user->getId()){ echo "selected";}?>><?php Template::escape($user->getUsername());?></option>
+		<?php if(get_user_id() == $user->getId()){ echo "selected";}?>><?php Template::escape($user->getUsername());?>
+		[<?php Template::escape($groupName);?>]
+		</option>
 <?php }?>
 </select>
 </form>
