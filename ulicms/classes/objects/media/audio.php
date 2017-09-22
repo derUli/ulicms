c058d23d2e37c0f67235fd1e01d06f75246b6e08
Ulrich Schmidt <admin@deruli.de>
2017-09-22 07:55:53 +0200

    fillVars implementiert

--- ulicms/classes/objects/media/audio.php
+++ ulicms/classes/objects/media/audio.php
@@ -18,7 +18,30 @@ class Audio extends Model {
 		$query = Database::pQuery ( "select * from `{prefix}audio` where id = ?", array (
 				intval ( $id ) 
 		), true );
+		if (! Database::any ( $query )) {
+			$query = null;
+		}
 		$this->fillVars ( $query );
+	}
+	protected function fillVars($query = null) {
+		if ($query) {
+			$result = Database::fetchSingle ( $query );
+			$this->setID ( $result->id );
+			$this->setName ( $result->name );
+			$this->setMP3File ( $result->mp3_file );
+			$this->setOGGFile ( $result->ogg_file );
+			$this->setCategoryId ( $result->category_id );
+			$this->created = $result->created;
+			$this->updated = $result->updated;
+		} else {
+			$this->setID ( null );
+			$this->setName ( null );
+			$this->setMP3File ( null );
+			$this->setOGGFile ( null );
+			$this->setCategoryId ( null );
+			$this->created = null;
+			$this->updated = null;
+		}
 	}
 	public function getName() {
 		return $this->name;
@@ -43,6 +66,9 @@ class Audio extends Model {
 	}
 	public function setMP3File($val) {
 		$this->mp3_file = StringHelper::isNotNullOrWhitespace ( $val ) ? strval ( $val ) : null;
+	}
+	public function setOGGFile($val) {
+		$this->ogg_file = StringHelper::isNotNullOrWhitespace ( $val ) ? strval ( $val ) : null;
 	}
 	public function setCategoryId($val) {
 		$this->category_id = is_numeric ( $val ) ? intval ( $val ) : null;
