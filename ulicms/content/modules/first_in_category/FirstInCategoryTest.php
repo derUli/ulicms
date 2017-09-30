08fe077111f8377c99f1bd76918f4e026cf7f784
Ulrich Schmidt <admin@deruli.de>
2017-09-30 10:01:32 +0200

    unit test grundgerüst erstellt

---
+++ ulicms/content/modules/first_in_category/FirstInCategoryTest.php
@@ -0,0 +1,10 @@
+<?php
+class ApiTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		// Delete all data created by this test
+	}
+	public function tearDown() {
+		$this->setUp ();
+	}
+	// TODO: Tests implementieren
+}
