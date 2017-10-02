77c37b6f9ac4b672d19f38ebf4a98ff39b7204e6
Ulrich Schmidt <admin@deruli.de>
2017-10-02 22:16:02 +0200

    Deutsche Sprachdatei erstellt

---
+++ ulicms/content/modules/wordpress_xml_importer/lang/de.php
@@ -0,0 +1,9 @@
+<?php
+idefine ( "TRANSLATION_FROM", "Von" );
+idefine ( "TRANSLATION_TO", "Nach" );
+
+idefine ( "TRANSLATION_IMPORT_FROM", "Import von" );
+idefine ( "TRANSLATION_IMPORT_TO", "Import nach" );
+
+idefine ( "TRANSLATION_REPLACE", "Ersetzen" );
+idefine ( "TRANSLATION_DEFAULT_CATEGORY", "Standardkategorie" );
