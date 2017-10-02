d667d08bef911c65aa5d09e9bd78c2d0d9863057
Ulrich Schmidt <admin@deruli.de>
2017-10-02 21:01:58 +0200

    Form wird angezeigt

--- ulicms/content/modules/wordpress_xml_importer/controllers/WordpressXmlImporterHooks.php
+++ ulicms/content/modules/wordpress_xml_importer/controllers/WordpressXmlImporterHooks.php
@@ -1,3 +1,13 @@
 <?php
 class WordpressXmlImporterHooks extends Controller {
+	private $moduleName = "wordpress_xml_importer";
+	public function getSettingsHeadline() {
+		return "Wordpress XML Importer";
+	}
+	public function getSettingsText() {
+		return get_translation ( "open" );
+	}
+	public function settings() {
+		return Template::executeModuleTemplate ( $this->moduleName, "form.php" );
+	}
 }
