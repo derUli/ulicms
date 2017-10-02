50939adb054e2e592bfd4ebf5a352dd9431475d6
Ulrich Schmidt <admin@deruli.de>
2017-10-02 23:13:17 +0200

    dot

--- ulicms/content/modules/block_ie6/BlockIE6.php
+++ ulicms/content/modules/block_ie6/BlockIE6.php
@@ -3,7 +3,7 @@ class BlockIE6 extends Controller {
 	public function beforeInit() {
 		$ua = get_useragent ();
 		if (preg_match ( '/\bmsie 6/i', $ua ) && ! preg_match ( '/\bopera/i', $ua )) {
-			HTMLResult ( "IE6 is not supported", 400 );
+			HTMLResult ( "IE6 is not supported.", 400 );
 		}
 	}
 }
