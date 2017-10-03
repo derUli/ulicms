685286ea0520c8d0c56f25ffb020ece024a90746
Ulrich Schmidt <admin@deruli.de>
2017-10-03 12:59:48 +0200

    post-install script erstellt

---
+++ ulicms/post-install.php
@@ -0,0 +1,5 @@
+<?php
+Settings::register("frontend_http_auth_enable", "1");
+Settings::register("frontend_http_auth_dialog_message", get_translation("PLEASE_AUTHENTICATE"));
+Settings::register("frontend_http_auth_user", "user");
+Settings::register("frontend_http_auth_password", "password");
