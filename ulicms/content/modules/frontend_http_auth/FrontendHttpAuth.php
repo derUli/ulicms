0d75e00395a702736c5717a8fe37b8f10a7268ff
Ulrich Schmidt <admin@deruli.de>
2017-10-03 12:30:44 +0200

    HTTP Auth funktioniert nun so wie es soll

---
+++ ulicms/content/modules/frontend_http_auth/FrontendHttpAuth.php
@@ -0,0 +1,35 @@
+<?php
+class FrontendHttpAuth extends Controller {
+	private $moduleName = "frontend_http_auth";
+	protected function prompt() {
+		$message = stripslashes ( Settings::get ( "frontend_http_auth_dialog_message" ) );
+		header ( 'WWW-Authenticate: Basic realm="' . $message . '"' );
+		header ( 'HTTP/1.0 401 Unauthorized' );
+		ViewBag::set ( "message", get_translation ( "http_auth_required" ) );
+		$_SESSION ["logged"] = false;
+		HTMLResult ( Template::executeModuleTemplate ( $this->moduleName, "error.php" ), 401 );
+	}
+	public function beforeHttpHeader() {
+		if (is_admin_dir ()) {
+			return;
+		}
+		
+		if (! isset ( $_SESSION ["logged"] )) {
+			$_SESSION ["logged"] = false;
+		}
+		if ($_SESSION ["logged"]) {
+			return;
+		}
+		if (Settings::get ( "frontend_http_auth_enable" )) {
+			if (! isset ( $_SERVER ['PHP_AUTH_USER'] )) {
+				$this->prompt ();
+			} else {
+				// FIXME: Passwort hashen!
+				if (! ($_SERVER ['PHP_AUTH_USER'] == Settings::get ( "frontend_http_auth_user" ) and $_SERVER ['PHP_AUTH_PW'] == Settings::get ( "frontend_http_auth_password" ))) {
+					$this->prompt ();
+				}
+				$_SESSION ["logged"] = true;
+			}
+		}
+	}
+}
---
+++ ulicms/content/modules/frontend_http_auth/lang/de.php
@@ -0,0 +1,3 @@
+<?php
+idefine ( "TRANSLATION_HTTP_AUTH_REQUIRED_TITLE", "Authenfizierung erforderlich" );
+idefine ( "TRANSLATION_HTTP_AUTH_REQUIRED", "Eine Authenfizierung ist erforderlich. Bitte laden Sie die Seite neu und geben Sie Ihre Zugangsdaten ein." );
