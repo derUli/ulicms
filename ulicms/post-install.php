8e863eb41ef805902a8aeb586f72954c7c8fee06
Ulrich Schmidt <admin@deruli.de>
2017-10-04 17:22:35 +0200

    frontend_http_auth in eigenes Repository verschoben

--- ulicms/post-install.php
+++
@@ -1,5 +0,0 @@
-<?php
-Settings::register("frontend_http_auth_enable", "1");
-Settings::register("frontend_http_auth_dialog_message", get_translation("PLEASE_AUTHENTICATE"));
-Settings::register("frontend_http_auth_user", "user");
-Settings::register("frontend_http_auth_password", "password");
