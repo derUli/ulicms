785e7ef0fc61ca55fa606032c35f441bf9b469e7
Ulrich Schmidt <u.schmidt@velian.de>
2017-10-06 14:33:30 +0200

    htaccess reverted, fromHeader implementiert

--- ulicms/content/modules/remote_auth/objects/Authorization.php
+++ ulicms/content/modules/remote_auth/objects/Authorization.php
@@ -9,6 +9,16 @@ class Authorization {
 		}
 	}
 	public function fromHeader($header) {
+		$header = trim ( $header );
+		$typeAndToken = explode ( " ", $header );
+		$type = $typeAndToken [0];
+		$token = base64_decode ( $typeAndToken [1] );
+		$usernamePassword = base64_decode ( $token );
+		$usernamePassword = explode ( ":", $usernamePassword );
+		$this->user = $usernamePassword [0];
+		// Config-Array Wert "plain_text_password_auth_types" auswerten.
+		// wenn $type darin enthalten ist, new PlainTextPasswort() statt new Password() verwenden
+		$this->password = new Password ( $usernamePassword [1] );
 	}
 	public function __toString() {
 	}
