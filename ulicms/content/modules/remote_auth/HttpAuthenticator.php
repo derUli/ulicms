0f9bc4c1f735e44c57dce585b79ca03b4277728b
Ulrich Schmidt <admin@deruli.de>
2017-10-06 07:45:57 +0200

    umbenannt

---
+++ ulicms/content/modules/remote_auth/HttpAuthenticator.php
@@ -0,0 +1,47 @@
+<?php
+class HttpAuthenticator extends Controller {
+	public function getConfig() {
+		$cfg = new config ();
+		if (isset ( $cfg->remote_auth_config ) and is_array ( $cfg->remote_auth_config )) {
+			return $cfg->remote_auth_config;
+		}
+		return null;
+	}
+	public function getEnvVars() {
+		$env_vars = array (
+				"REMOTE_USER",
+				"REDIRECT_REMOTE_USER" 
+		);
+		$cfg = $this->getConfig ();
+		if (isset ( $cfg ["env_vars"] )) {
+			if (is_array ( $cfg ["env_vars"] )) {
+				$env_vars = $cfg ["env_vars"];
+			} else if (is_string ( $cfg ["env_vars"] )) {
+				$env_vars = array (
+						$cfg ["env_vars"] 
+				);
+			}
+		}
+		return $env_vars;
+	}
+	public function getRemoteUser() {
+		$vars = $this->getEnvVars ();
+		foreach ( $vars as $var ) {
+			if (isset ( $_SERVER [$var] ) and StringHelper::isNotNullOrWhitespace ( $_SERVER [$var] )) {
+				return $_SERVER [$var];
+			}
+		}
+		return null;
+	}
+	public function auth($username = null) {
+		if (! $username) {
+			$username = $this->getRemoteUser ();
+		}
+		$user = getUserByName ( $username );
+		$cfg = $this->getConfig ();
+		if (! $user and isset ( $cfg ["create_user"] ) and $cfg ["create_user"]) {
+			// TODO: User anlegen
+		}
+		return $user;
+	}
+}
--- ulicms/content/modules/remote_auth/RemoteAuth.php
+++ ulicms/content/modules/remote_auth/RemoteAuth.php
@@ -1,47 +1,21 @@
 <?php
-class HttpAuthenticator extends Controller {
-	public function getConfig() {
-		$cfg = new config ();
-		if (isset ( $cfg ["remote_auth_config"] ) and is_array ( $cfg ["remote_auth_config"] )) {
-			return $cfg ["remote_auth_config"];
-		}
-		return null;
-	}
-	public function getEnvVars() {
-		$env_vars = array (
-				"REMOTE_USER",
-				"REDIRECT_REMOTE_USER" 
-		);
-		$cfg = $this->getConfig ();
-		if (isset ( $cfg ["env_vars"] )) {
-			if (is_array ( $cfg ["env_vars"] )) {
-				$env_vars = $cfg ["env_vars"];
-			} else if (is_string ( $cfg ["env_vars"] )) {
-				$env_vars = array (
-						$cfg ["env_vars"] 
-				);
+class RemoteAuth extends Controller {
+	public function beforeBackendHeader() {
+		if (! is_logged_in ()) {
+			$authenticator = ControllerRegistry::get ( "HttpAuthenticator" );
+			$cfg = $authenticator->getConfig ();
+			$user = $authenticator->auth ();
+			if ($user) {
+				// sync modules folder with database at first login
+				if (! Settings::get ( "sys_initialized" )) {
+					clearCache ();
+					Settings::set ( "sys_initialized", "true" );
+				}
+				add_hook ( "login_ok" );
+				register_session ( $sessionData, true );
+			} else if (isset ( $cfg ["login_url"] ) and StringHelper::isNotNullOrWhitespace ( $cfg ["login_url"] )) {
+				Request::redirect ( $cfg ["login_url"] );
 			}
 		}
-		return $env_vars;
-	}
-	public function getRemoteUser() {
-		$vars = $this->getEnvVars ();
-		foreach ( $vars as $var ) {
-			if (isset ( $_SERVER [$var] ) and StringHelper::isNotNullOrWhitespace ( $_SERVER [$var] )) {
-				return $_SERVER [$var];
-			}
-		}
-		return null;
-	}
-	public function auth($username = null) {
-		if (! $username) {
-			$username = $this->getRemoteUser ();
-		}
-		$user = getUserByName ( $username );
-		$cfg = $this->getConfig ();
-		if (! $user and isset ( $cfg ["create_user"] ) and $cfg ["create_user"]) {
-			// TODO: User anlegen
-		}
-		return $user;
 	}
 }
