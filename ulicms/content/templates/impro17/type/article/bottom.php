78f4f85229f65f73b164aaf505910e10f7317fea
Ulrich Schmidt <admin@deruli.de>
2017-10-10 19:25:42 +0200

    commit

--- ulicms/content/templates/impro17/type/article/bottom.php
+++ ulicms/content/templates/impro17/type/article/bottom.php
@@ -1,15 +1,15 @@
 <?php $meta = get_article_meta ();?>
 <?php if(StringHelper::isNotNullOrEmpty($meta->article_author_name)){?>
 <p class="author-info"><?php
-
+	
 	if (StringHelper::isNullOrEmpty ( $meta->article_author_email )) {
 		translate ( "ARTICLE_WRITTEN_BY_X", array (
-				"%author%" => Template::getEscape ( $meta->article_author_name )
+				"%author%" => Template::getEscape ( $meta->article_author_name ) 
 		) );
 	} else {
 		translate ( "ARTICLE_WRITTEN_BY_X_WITH_LINK", array (
 				"%author%" => Template::getEscape ( $meta->article_author_name ),
-				"%email%" => Template::getEscape ( $meta->article_author_email )
+				"%email%" => Template::getEscape ( $meta->article_author_email ) 
 		) );
 	}
 	?></p>
