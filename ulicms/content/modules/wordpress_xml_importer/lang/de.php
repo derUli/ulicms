28a5d574933f853622714bb84f5a3824dccdf11c
Ulrich Schmidt <admin@deruli.de>
2017-10-10 07:28:15 +0200

    wordpress xml importer in ein eigenes repository verschoben

--- ulicms/content/modules/wordpress_xml_importer/lang/de.php
+++
@@ -1,9 +0,0 @@
-<?php
-idefine ( "TRANSLATION_FROM", "Von" );
-idefine ( "TRANSLATION_TO", "Nach" );
-
-idefine ( "TRANSLATION_IMPORT_FROM", "Import von" );
-idefine ( "TRANSLATION_IMPORT_TO", "Import nach" );
-
-idefine ( "TRANSLATION_REPLACE", "Ersetzen" );
-idefine ( "TRANSLATION_DEFAULT_CATEGORY", "Standardkategorie" );
