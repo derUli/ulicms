fcc7f2a6bd01db7e7611b865a8260ec2d039b2de
Ulrich Schmidt <admin@deruli.de>
2017-10-14 09:51:12 +0200

    aufgeräumt

--- ulicms/content/modules/wordpress_xml_importer/lang/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-idefine ( "TRANSLATION_FROM", "From" );
-idefine ( "TRANSLATION_TO", "To" );
-
-idefine ( "TRANSLATION_IMPORT_FROM", "Import from" );
-idefine ( "TRANSLATION_IMPORT_TO", "Import to" );
-
-idefine ( "TRANSLATION_REPLACE", "Replace" );
-idefine ( "TRANSLATION_DEFAULT_CATEGORY", "Default Category" );
