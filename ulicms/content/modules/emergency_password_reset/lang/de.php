0ab8623f0b997b9ce8bf21b74907a630651d0394
Ulrich Schmidt <admin@deruli.de>
2017-10-18 19:29:40 +0200

    Sprachdateien erstellt, Berechtigungsprüfung hinzugefügt

---
+++ ulicms/content/modules/emergency_password_reset/lang/de.php
@@ -0,0 +1,4 @@
+<?php
+add_translation ( "emergency_password_reset", "Notfall Passwort Zurücksetzung" );
+add_translation ( "reset_all_passwords", "Alle Passwörter zurücksetzen" );
+add_translation ( "all_password_were_resetted", "Alle Passwörter wurden zurückgesetzt. Es wurden Links zum Setzen eines neuen Passworts versendet." );
---
+++ ulicms/content/modules/emergency_password_reset/lang/en.php
@@ -0,0 +1,4 @@
+<?php
+add_translation ( "emergency_password_reset", "Emergency Password Reset" );
+add_translation ( "reset_all_passwords", "Reset all passwords" );
+add_translation ( "all_password_were_resetted", "All passwords were reset. Links to set a new passwords were mailed to users." );
