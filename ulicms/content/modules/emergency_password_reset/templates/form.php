49329f8ece3a4aeff5280383c8b4376041a2e960
Ulrich Schmidt <u.schmidt@velian.de>
2017-10-19 09:48:22 +0200

    moved emergency_password_reset in it's own repo

--- ulicms/content/modules/emergency_password_reset/templates/form.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo ModuleHelper::buildMethodCallForm ( "EmergencyPasswordReset", "resetAllPasswords" );
-?>
-<button type="submit"><?php translate("reset_all_passwords");?></button>
-</form>
