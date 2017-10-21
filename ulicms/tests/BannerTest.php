cf596bd77b085dc6c672b95b837eae98c872aed2
Ulrich Schmidt <ulrich1992@web.de>
2017-10-21 15:23:12 +0200

    unit tests für das erstellen von Bannern implementiert

--- ulicms/tests/BannerTest.php
+++ ulicms/tests/BannerTest.php
@@ -2,6 +2,12 @@
 class BannerTest extends PHPUnit_Framework_TestCase {
 	const HTML_TEXT1 = "My first Banner HTML";
 	const HTML_TEXT2 = "My second Banner HTML";
+	const NAME_TEXT1 = "My first Gif Banner";
+	const NAME_TEXT2 = "My second Gif Banner";
+	const IMAGE_URL_TEXT1 = "http://firma.de/bild.gif";
+	const IMAGE_URL_TEXT2 = "http://firma.de/bild2.gif";
+	const LINK_URL_TEXT1 = "http://www.google.de";
+	const LINK_URL_TEXT2 = "http://www.yahoo.com";
 	public function setUp() {
 		Database::pQuery ( "DELETE FROM `{prefix}banner` where html in (? , ?)", array (
 				self::HTML_TEXT1,
@@ -60,5 +66,68 @@ class BannerTest extends PHPUnit_Framework_TestCase {
 		$banner = new Banner ();
 		$this->assertNull ( $banner->id );
 	}
-	// TODO: Tests für Gif-Banner anlegen mit Sprache und ohne Sprache implementieren
+	public function testGifBannerWithoutLanguage() {
+		$banner = new Banner ();
+		$banner->setType ( "gif" );
+		$banner->name = self::NAME_TEXT1;
+		$banner->image_url = self::IMAGE_URL_TEXT1;
+		$banner->link_url = self::LINK_URL_TEXT1;
+		$banner->save ();
+		$this->assertNotNull ( $banner->id );
+		$id = intval ( $banner->id );
+		$banner = new Banner ( $id );
+		$this->assertNotNull ( $banner->id );
+		$this->assertEquals ( "gif", $banner->getType () );
+		$this->assertEquals ( self::NAME_TEXT1, $banner->name );
+		$this->assertEquals ( self::IMAGE_URL_TEXT1, $banner->image_url );
+		$this->assertEquals ( self::LINK_URL_TEXT1, $banner->link_url );
+		$this->assertNull ( $banner->language );
+		$banner->name = self::NAME_TEXT2;
+		$banner->image_url = self::IMAGE_URL_TEXT2;
+		$banner->link_url = self::LINK_URL_TEXT2;
+		$banner->save ();
+		$banner = new Banner ( $id );
+		$this->assertNotNull ( $banner->id );
+		$this->assertEquals ( "gif", $banner->getType () );
+		$this->assertEquals ( self::NAME_TEXT2, $banner->name );
+		$this->assertEquals ( self::IMAGE_URL_TEXT2, $banner->image_url );
+		$this->assertEquals ( self::LINK_URL_TEXT2, $banner->link_url );
+		$this->assertNull ( $banner->language );
+		$banner->delete ();
+		$banner = new Banner ();
+		$this->assertNull ( $banner->id );
+	}
+	public function testGifBannerWithLanguage() {
+		$banner = new Banner ();
+		$banner->setType ( "gif" );
+		$banner->language = "de";
+		$banner->name = self::NAME_TEXT1;
+		$banner->image_url = self::IMAGE_URL_TEXT1;
+		$banner->link_url = self::LINK_URL_TEXT1;
+		$banner->save ();
+		$this->assertNotNull ( $banner->id );
+		$id = intval ( $banner->id );
+		$banner = new Banner ( $id );
+		$this->assertNotNull ( $banner->id );
+		$this->assertEquals ( "gif", $banner->getType () );
+		$this->assertEquals ( self::NAME_TEXT1, $banner->name );
+		$this->assertEquals ( self::IMAGE_URL_TEXT1, $banner->image_url );
+		$this->assertEquals ( self::LINK_URL_TEXT1, $banner->link_url );
+		$this->assertEquals ( "de", $banner->language );
+		$banner->language = "en";
+		$banner->name = self::NAME_TEXT2;
+		$banner->image_url = self::IMAGE_URL_TEXT2;
+		$banner->link_url = self::LINK_URL_TEXT2;
+		$banner->save ();
+		$banner = new Banner ( $id );
+		$this->assertNotNull ( $banner->id );
+		$this->assertEquals ( "gif", $banner->getType () );
+		$this->assertEquals ( self::NAME_TEXT2, $banner->name );
+		$this->assertEquals ( self::IMAGE_URL_TEXT2, $banner->image_url );
+		$this->assertEquals ( self::LINK_URL_TEXT2, $banner->link_url );
+		$this->assertEquals ( "en", $banner->language );
+		$banner->delete ();
+		$banner = new Banner ();
+		$this->assertNull ( $banner->id );
+	}
 }
