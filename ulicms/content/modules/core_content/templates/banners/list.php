c09fa9dc831816abec62eba4bef2e5b1937ebfb3
Ulrich Schmidt <ulrich1992@web.de>
2017-10-21 13:50:46 +0200

    sql code aus banners/list.php entfernt, funktion Banners::getByCategory hinzugefügt

--- ulicms/content/modules/core_content/templates/banners/list.php
+++ ulicms/content/modules/core_content/templates/banners/list.php
@@ -1,5 +1,4 @@
 <?php
-// @FIXME: Das SQL hier muss in einen Controller verschoben werden.
 $acl = new ACL ();
 if ($acl->hasPermission ( "banners" )) {
 	if (! isset ( $_SESSION ["filter_category"] )) {
@@ -8,14 +7,11 @@ if ($acl->hasPermission ( "banners" )) {
 	if (isset ( $_GET ["filter_category"] )) {
 		$_SESSION ["filter_category"] = intval ( $_GET ["filter_category"] );
 	}
-	$sql = "SELECT * FROM " . tbname ( "banner" ) . " ";
 	if ($_SESSION ["filter_category"] == 0) {
-		$sql .= "WHERE 1=1 ";
+		$banners = Banners::getAll();
 	} else {
-		$sql .= "WHERE category=" . $_SESSION ["filter_category"] . " ";
+		$banners = Banners::getByCategory($_SESSION ["filter_category"]);
 	}
-	$sql .= "ORDER BY id";
-	$query = db_query ( $sql );
 	?>
 <script type="text/javascript">
 $(window).load(function(){
@@ -60,27 +56,27 @@ $(window).load(function(){
 		</thead>
 		<tbody>
 	<?php
-	if (db_num_rows ( $query ) > 0) {
-		while ( $row = db_fetch_object ( $query ) ) {
+	if (count ( $banners ) > 0) {
+		foreach($banners as $banner){
 			?>
 			<?php
-			echo '<tr id="dataset-' . $row->id . '">';
-			if ($row->type == "gif") {
-				$link_url = Template::getEscape ( $row->link_url );
-				$image_url = Template::getEscape ( $row->image_url );
-				$name = Template::getEscape ( $row->name );
+			echo '<tr id="dataset-' . $banner->id . '">';
+			if ($banner->getType() == "gif") {
+				$link_url = Template::getEscape ( $banner->link_url );
+				$image_url = Template::getEscape ( $banner->image_url );
+				$name = Template::getEscape ( $banner->name );
 				echo '<td><a href="' . $link_url . '" target="_blank"><img src="' . $image_url . '" title="' . $name . '" alt="' . $name . '" border=0></a></td>';
 			} else {
-				echo '<td>' . Template::getEscape ( $row->html ) . '</td>';
+				echo '<td>' . Template::getEscape ( $banner->html ) . '</td>';
 			}
-			if ($row->language == "all") {
+			if ($banner->language == "all") {
 				echo '<td>Alle</td>';
 			} else {
-				echo '<td>' . getLanguageNameByCode ( $row->language ) . "</td>";
+				echo '<td>' . getLanguageNameByCode ( $banner->language ) . "</td>";
 			}
 			if ($acl->hasPermission ( "banners_edit" )) {
-				echo "<td style='text-align:center;'>" . '<a href="index.php?action=banner_edit&banner=' . $row->id . '"><img class="mobile-big-image" src="gfx/edit.png" alt="' . get_translation ( "edit" ) . '" title="' . get_translation ( "edit" ) . '"></a></td>';
-				echo "<td style='text-align:center;'>" . '<form action="index.php?sClass=BannerController&sMethod=delete&banner=' . $row->id . '" method="post" onsubmit="return confirm(\'Wirklich löschen?\');" class="delete-form">' . get_csrf_token_html () . '<input type="image" class="mobile-big-image" src="gfx/delete.gif" title="' . get_translation ( "delete" ) . '"></form></td>';
+				echo "<td style='text-align:center;'>" . '<a href="index.php?action=banner_edit&banner=' . $banner->id . '"><img class="mobile-big-image" src="gfx/edit.png" alt="' . get_translation ( "edit" ) . '" title="' . get_translation ( "edit" ) . '"></a></td>';
+				echo "<td style='text-align:center;'>" . '<form action="index.php?sClass=BannerController&sMethod=delete&banner=' . $banner->id . '" method="post" onsubmit="return confirm(\'Wirklich löschen?\');" class="delete-form">' . get_csrf_token_html () . '<input type="image" class="mobile-big-image" src="gfx/delete.gif" title="' . get_translation ( "delete" ) . '"></form></td>';
 			}
 			echo '</tr>';
 		}
