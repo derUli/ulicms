d42b442235ac131b011f6af97af16d09e4e5059f
Ulrich Schmidt <admin@deruli.de>
2017-10-23 22:57:53 +0200

    User Locking Cronjob funktioniert nun

--- ulicms/content/modules/expire_users/ExpireUsers.php
+++ ulicms/content/modules/expire_users/ExpireUsers.php
@@ -1,8 +1,22 @@
 <?php
 class ExpireUsers extends controller {
+	// lock all expired users in a pseudo cronjob
+	// expire_users executes this every 12 hours
 	public function cron() {
 		BetterCron::hours ( "module/expire_users/lock_expired_users", 12, function () {
-			// TODO: Iterate over all users and lock expired users.
+			$manager = new UserManager ();
+			// OPTIMIZE: Load only not locked users to improve performance
+			$users = $manager->getAllUsers ();
+			foreach ( $users as $user ) {
+				// expire_date is a timestamp
+				// when expire_date is set and the timestamp is in the past
+				// user will be locked
+				$expire_date = UserSettings::get ( "expire_date", "int", $user->getId () );
+				if ($expire_date and time () >= $expire_date and ! $user->getLocked ()) {
+					$user->setLocked ( true );
+					$user->save ();
+				}
+			}
 		} );
 	}
 }
