b3fe32928074698c783ea933370598adab2f8624
Ulrich Schmidt <admin@deruli.de>
2017-11-14 20:51:03 +0100

    release notes abschnitt hinzugefügt

--- ulicms/admin/inc/queries.php
+++ ulicms/admin/inc/queries.php
@@ -4,7 +4,7 @@ add_hook ( "query" );
 
 include_once ULICMS_ROOT . "/classes/objects/content/vcs.php";
 
-if ($acl->hasPermission ( "modules" ) and Request::getVar ( "toggle-show-core-modules" )) {
+if ($acl->hasPermission ( "module_settings" ) and Request::getVar ( "toggle-show-core-modules" )) {
 	$_SESSION ["show_core_modules"] = ! $_SESSION ["show_core_modules"];
 	Request::redirect ( ModuleHelper::buildActionURL ( Request::getVar ( "action" ) ) );
 }
