5874cc4edbd168283f1635cf0685ea9205f37e5c
Ulrich Schmidt <admin@deruli.de>
2017-11-22 07:50:32 +0100

    Arbeit an modstarter new form begonnen

--- ulicms/content/modules/modstarter/lang/en.php
+++ ulicms/content/modules/modstarter/lang/en.php
@@ -1,3 +1,8 @@
 <?php
 add_translation ( "create_module", "Create Module" );
 add_translation ( "cancel", "Cancel" );
+add_translation ( "source", "Source" );
+add_translation ( "embeddable", "Embeddable" );
+add_translation ( "shy", "Shy" );
+add_translation ( "main_class", "Main Class" );
+add_translation ( "create_post_install_script", "Create post-install Script" );
--- ulicms/content/modules/modstarter/templates/new.php
+++ ulicms/content/modules/modstarter/templates/new.php
@@ -1,8 +1,52 @@
-
+<?php
+$sources = array (
+		"",
+		"pkgsrc",
+		"extend",
+		"local",
+		"core" 
+);
+// TODO: List of all hooks
+$hooks = array (
+		"uninstall",
+		"clearCache" 
+);
+?>
 <h1><?php translate("create_module");?></h1>
+<?php echo ModuleHelper::buildMethodCallForm("ModStarter", "save");?>
 <p>
 	<a href="<?php echo ModuleHelper::buildAdminURL("modstarter");?>"
 		class="btn btn-default"><?php translate("cancel");?></a>
 </p>
 
-Not implemented yet
+<p>
+	<strong><?php translate("source");?></strong><br /> <select
+		name="source">
+		<?php foreach($sources as $source){?>
+		<option value="<?php esc($source);?>"><?php esc($source);?></option>
+		<?php }?></select>
+</p>
+<p>
+	<strong><?php translate("version")?></strong> <br /> <input type="text"
+		name="version" maxlength="20" value="" required>
+</p>
+<p>
+	<input type="checkbox" name="embeddable" id="embeddable" value=""> <label
+		for="embeddable"><?php translate("embeddable");?></label>
+</p>
+<p>
+	<input type="checkbox" name="shy" id="shy" value=""> <label for="shy"><?php translate("shy");?></label>
+</p>
+<p>
+	<strong><?php translate("main_class")?></strong><br /> <input
+		type="text" name="main_class" value="" required>
+</p>
+<p>
+	<input type="checkbox" name="create_post_install_script"
+		id="create_post_install_script" value=""> <label
+		for="create_post_install_script"><?php translate("create_post_install_script");?></label>
+</p>
+<p>
+	<button type="submit" class="btn btn-success"><?php translate("save")?></button>
+</p>
+</form>
