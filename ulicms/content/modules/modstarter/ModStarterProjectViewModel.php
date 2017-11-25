4797c5c1d862b1fb462b56998e8e94d91e2b85f7
Ulrich Schmidt <admin@deruli.de>
2017-11-25 15:10:14 +0100

    ModStarterProjectViewModel hinzugefügt

---
+++ ulicms/content/modules/modstarter/ModStarterProjectViewModel.php
@@ -0,0 +1,11 @@
+<?php
+class ModStarterProjectViewModel {
+	public $module_folder = "";
+	public $source = "";
+	public $version = "";
+	public $embeddable = false;
+	public $shy = false;
+	public $main_class = "";
+	public $create_post_install_script = false;
+	public $hooks = array ();
+}
