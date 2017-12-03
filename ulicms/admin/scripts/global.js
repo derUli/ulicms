9b64150e57f417a3a91dd4214d200689f19161ea
Ulrich Schmidt <admin@deruli.de>
2017-12-03 17:52:11 +0100

    fix

--- ulicms/admin/scripts/global.js
+++ ulicms/admin/scripts/global.js
@@ -6,7 +6,7 @@ $(function() {
 		$(this).hide();
 		$("#menu-clear-cache-loading").show()
 		var url = $("#menu-clear-cache").data("url");
-		$.get("index.php?clear_cache=clear_cache", function(result) {
+		$.get(url, function(result) {
 			$("#menu-clear-cache").show();
 			$("#menu-clear-cache-loading").hide();
 		});
