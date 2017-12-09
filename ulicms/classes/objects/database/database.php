b632f906cff1e764270788bef290f4cb3324f084
Ulrich Schmidt <admin@deruli.de>
2017-12-09 20:24:36 +0100

    Funktion verschoben

--- ulicms/classes/objects/database/database.php
+++ ulicms/classes/objects/database/database.php
@@ -1,6 +1,22 @@
 <?php
 class Database {
 	private static $connection = null;
+	
+	// Connect with database server
+	public static function connect($server, $user, $password, $socket = null) {
+		self::$connection = mysqli_connect ( $server, $user, $password, "", ini_get ( "mysqli.default_port" ), $socket );
+		if (! self::$connection) {
+			return false;
+		}
+		self::query ( "SET NAMES 'utf8mb4'" );
+		// sql_mode auf leer setzen, da sich UliCMS nicht im strict_mode betreiben lässt
+		self::query ( "SET SESSION sql_mode = '';" );
+		
+		return self::$connection;
+	}
+	public static function close() {
+		mysqli_close ( self::$connection );
+	}
 	// Abstraktion für Ausführen von SQL Strings
 	public static function query($sql, $replacePrefix = false) {
 		Logger::logDbQuery ( $sql );
@@ -182,22 +198,7 @@ class Database {
 		
 		return $retval;
 	}
-	public static function close() {
-		mysqli_close ( self::$connection );
-	}
 	
-	// Connect with database server
-	public static function connect($server, $user, $password, $socket = null) {
-		self::$connection = mysqli_connect ( $server, $user, $password, ini_get( "mysqli.default_port" ), $socket );
-		if (! self::$connection) {
-			return false;
-		}
-		self::query ( "SET NAMES 'utf8mb4'" );
-		// sql_mode auf leer setzen, da sich UliCMS nicht im strict_mode betreiben lässt
-		self::query ( "SET SESSION sql_mode = '';" );
-		
-		return self::$connection;
-	}
 	// Datenbank auswählen
 	public static function select($schema) {
 		return mysqli_select_db ( self::$connection, $schema );
