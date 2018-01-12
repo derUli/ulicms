e6e20f2db717224103b3de71af7a7e941d7e8095
Ulrich Schmidt <admin@deruli.de>
2018-01-12 23:05:10 +0100

    deutsche Sprachdatei für ip_login hinzugefügt

---
+++ ulicms/content/modules/ip_login/lang/de.php
@@ -0,0 +1,8 @@
+<?php
+add_translation ( "ip_login_settings", "IP Login Einstellungen" );
+add_translation ( "ip_user_mapping", "IP-zu-Benutzer Mapping" );
+add_translation ( "ip_user_mapping_help", "Ein Benutzer wird automatisch eingeloggt, wenn der Zugriff auf UliCMS von einer bestimmten IP-Adresse erfolgt.
+Mit folgender Zeile beispielsweise können Sie festlegen, dass Ihr Benutzerkonto automatisch eingeloggt wird, wenn der Zugriff von ihrer aktuellen IP-Adresse erfolgt.
+
+%example%
+" );
