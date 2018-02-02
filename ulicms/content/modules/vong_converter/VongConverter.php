248a8de39197e6e555fd6745d153ea7e44cc6247
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-02 11:50:06 +0100

    vong_converter für frontend und backend abstellbar

--- ulicms/content/modules/vong_converter/VongConverter.php
+++ ulicms/content/modules/vong_converter/VongConverter.php
@@ -1,17 +1,22 @@
 <?php
 class VongConverter extends Controller{
-   // Todo: Übersetzung soll seperat für Frontend und Backend
-   // eingeschalten und deaktiviert werden können.
+
+   protected function isNidVong($vong){
+     $cfg = new config();
+     $varName = "nid_vong_".strtolower($vong);
+     return is_true($cfg->$varName);
+   }
+
    public function frontendFooter(){
-     if(getCurrentLanguage() !== "de"){
+     if(getCurrentLanguage() !== "de" || $this->isNidVong("frontend")){
        return;
      }
       return Template::executeModuleTemplate("vong_converter", "vong.php");
   }
   public function adminFooter(){
-    if(getSystemLanguage() !== "de"){
+    if(getSystemLanguage() !== "de"|| $this->isNidVong("backend")){
       return;
     }
-    return $this->frontendFooter();
+     return Template::executeModuleTemplate("vong_converter", "vong.php");
   }
 }
