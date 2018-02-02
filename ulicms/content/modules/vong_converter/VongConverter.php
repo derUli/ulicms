57d09e6880b94163a8172b48eeda3d2c6070e095
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-02 11:43:04 +0100

    vong converter nur noch in deutsch

--- ulicms/content/modules/vong_converter/VongConverter.php
+++ ulicms/content/modules/vong_converter/VongConverter.php
@@ -4,9 +4,15 @@ class VongConverter extends Controller{
    // Übersetzung soll seperat für Frontend und Backend
    // eingeschalten und deaktiviert werden können.
    public function frontendFooter(){
+     if(getCurrentLanguage() !== "de"){
+       return;
+     }
       return Template::executeModuleTemplate("vong_converter", "vong.php");
   }
   public function adminFooter(){
+    if(getSystemLanguage() !== "de"){
+      return;
+    }
     return $this->frontendFooter();
   }
 }
