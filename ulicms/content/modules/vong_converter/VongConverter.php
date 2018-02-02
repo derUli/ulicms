b7964c35618ca217dcf36dd2c1d1119c90dfe53e
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-02 11:29:01 +0100

    vong_converter hinzugefügt

---
+++ ulicms/content/modules/vong_converter/VongConverter.php
@@ -0,0 +1,12 @@
+<?php
+class VongConverter extends Controller{
+   // Todo: Nur deutsch übersetzen
+   // Übersetzung soll seperat für Frontend und Backend
+   // eingeschalten und deaktiviert werden können.
+   public function frontendFooter(){
+      return Template::executeModuleTemplate("vong_converter", "vong.php");
+  }
+  public function backendFooter(){
+    return $this->frontendFooter();
+  }
+}
---
+++ ulicms/content/modules/vong_converter/templates/vong.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="<?php echo ModuleHelper::buildModuleRessourcePath("vong_converter", "js/konvertasation.js");?>"></script>
