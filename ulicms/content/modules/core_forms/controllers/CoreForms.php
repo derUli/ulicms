2bda6261b9f202d25adf93ebf14600289d5b8bb6
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-05 12:06:16 +0100

    spam honeypot bei formular system funktioniert nun

--- ulicms/content/modules/core_forms/controllers/CoreForms.php
+++ ulicms/content/modules/core_forms/controllers/CoreForms.php
@@ -5,11 +5,12 @@ class CoreForms extends Controller {
 
 		if (StringHelper::isNotNullOrWhitespace ( Request::getVar ( "submit-cms-form" ) )) {
 			// apply spam filter if disabled
-			if($_POST ["spamfilter_enabled"] == "yes"){
+			if(Settings::get("spamfilter_enabled") == "yes"){
 				// check if honeypot field is filled
 				if(!empty($_POST["business_fax"])){
 					Settings::set("contact_form_refused_spam_mails", Settings::get("contact_form_refused_spam_mails") + 1);
 					HTMLResult(get_translation("spam_trapped"), 403);
+					// TODO: Vollständigen Spamfilter implementieren
 				}
 			}
 			$form_id = Request::getVar ( "submit-cms-form", null, "int" );
