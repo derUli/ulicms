b1cc97593cc7082b9f3dad1c26ad5248194fe999
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-05 14:43:48 +0100

    chinesisch und kyrillisch spamfilter für forms implementiert

--- ulicms/content/modules/core_forms/controllers/CoreForms.php
+++ ulicms/content/modules/core_forms/controllers/CoreForms.php
@@ -2,7 +2,6 @@
 class CoreForms extends Controller {
 	private $moduleName = "core_forms";
 	public function beforeHttpHeader() {
-
 		if (StringHelper::isNotNullOrWhitespace ( Request::getVar ( "submit-cms-form" ) ) and Request::isPost()) {
 			// apply spam filter if disabled
 			if(Settings::get("spamfilter_enabled") == "yes"){
@@ -11,7 +10,6 @@ class CoreForms extends Controller {
 					Settings::set("contact_form_refused_spam_mails", Settings::get("contact_form_refused_spam_mails") + 1);
 					HTMLResult(get_translation( "honeypot_is_not_empty" ), 403);
 				}
-				// TODO: Vollständigen Spamfilter implementieren
 				if(Settings::get("disallow_chinese_chars") or Settings::get("disallow_cyrillic_chars")){
 						foreach($_POST as $key=>$value){
 							  if(Settings::get("disallow_chinese_chars") and AntiSpamHelper::isChinese($_POST[$key])){
@@ -22,6 +20,7 @@ class CoreForms extends Controller {
  								}
 						}
 				}
+				// TODO: Vollständigen Spamfilter implementieren
 			}
 			$form_id = Request::getVar ( "submit-cms-form", null, "int" );
 			Forms::submitForm ( $form_id );
