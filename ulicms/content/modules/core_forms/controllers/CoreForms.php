20342ead451f57bb049f418ed9393ab7e1b5ff33
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-05 15:38:42 +0100

    inc spam count

--- ulicms/content/modules/core_forms/controllers/CoreForms.php
+++ ulicms/content/modules/core_forms/controllers/CoreForms.php
@@ -1,25 +1,31 @@
 <?php
 class CoreForms extends Controller {
 	private $moduleName = "core_forms";
+	protected function incSpamCount(){
+			Settings::set("contact_form_refused_spam_mails", Settings::get("contact_form_refused_spam_mails") + 1);
+	}
 	public function beforeHttpHeader() {
 		if (StringHelper::isNotNullOrWhitespace ( Request::getVar ( "submit-cms-form" ) ) and Request::isPost()) {
 			// apply spam filter if disabled
 			if(Settings::get("spamfilter_enabled") == "yes"){
 				// check if honeypot field is filled
 				if(!empty($_POST["business_fax"])){
-					Settings::set("contact_form_refused_spam_mails", Settings::get("contact_form_refused_spam_mails") + 1);
+					$this->incSpamCount();
 					HTMLResult(get_translation( "honeypot_is_not_empty" ), 403);
 				}
 						foreach($_POST as $key=>$value){
 							  if(Settings::get("disallow_chinese_chars") and AntiSpamHelper::isChinese($_POST[$key])){
+									  $this->incSpamCount();
 										HTMLResult(get_translation( "chinese_chars_not_allowed" ), 403);
 								}
 								if(Settings::get("disallow_cyrillic_chars") and AntiSpamHelper::isCyrillic($_POST[$key])){
+								  	$this->incSpamCount();
  										HTMLResult(get_translation( "cyrillic_charts_not_allowed" ), 403);
  								}
 
 								$badwordsCheck = AntispamHelper::containsBadwords($_POST[$key]);
 								if($badwordsCheck){
+									  $this->incSpamCount();
 								   	HTMLResult(get_translation( "request_contains_badword", array("%word%"=>$badWordsCheck) ), 403);
 								}
 						}
