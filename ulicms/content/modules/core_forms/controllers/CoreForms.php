94953c3b3b5d2548b2c10af3fe700387623b67d1
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-05 15:36:51 +0100

    core forms

--- ulicms/content/modules/core_forms/controllers/CoreForms.php
+++ ulicms/content/modules/core_forms/controllers/CoreForms.php
@@ -10,7 +10,6 @@ class CoreForms extends Controller {
 					Settings::set("contact_form_refused_spam_mails", Settings::get("contact_form_refused_spam_mails") + 1);
 					HTMLResult(get_translation( "honeypot_is_not_empty" ), 403);
 				}
-				if(Settings::get("disallow_chinese_chars") or Settings::get("disallow_cyrillic_chars")){
 						foreach($_POST as $key=>$value){
 							  if(Settings::get("disallow_chinese_chars") and AntiSpamHelper::isChinese($_POST[$key])){
 										HTMLResult(get_translation( "chinese_chars_not_allowed" ), 403);
@@ -18,8 +17,13 @@ class CoreForms extends Controller {
 								if(Settings::get("disallow_cyrillic_chars") and AntiSpamHelper::isCyrillic($_POST[$key])){
  										HTMLResult(get_translation( "cyrillic_charts_not_allowed" ), 403);
  								}
+
+								$badwordsCheck = AntispamHelper::containsBadwords($_POST[$key]);
+								if($badwordsCheck){
+								   	HTMLResult(get_translation( "request_contains_badword", array("%word%"=>$badWordsCheck) ), 403);
+								}
 						}
-				}
+
 				// TODO: Vollständigen Spamfilter implementieren
 			}
 			$form_id = Request::getVar ( "submit-cms-form", null, "int" );
