a25c72c0a92100cadae397c9ba6f75d6cdbb6d47
Ulrich Schmidt <u.schmidt@velian.de>
2018-02-14 14:57:19 +0100

    footer_html gehört nicht ins release

--- ulicms/content/modules/footer_html/lang/de.php
+++
@@ -1,4 +0,0 @@
-<?php
-add_translation ( "footer_html", "Frontend HTML" );
-add_translation ( "footer_html_help", "In dem unten stehenden Textfeld können Sie HTML-Codes wie z.B. Tracking Codes eingeben,
-die in der frontend_footer Hook ausgegeben werden.");
