ae1723dfc9d2b03edd694e51eee66f5682de71ee
Ulrich Schmidt <admin@deruli.de>
2018-02-17 20:07:22 +0100

    deprecated Funktionsaufruf ersetzt

--- ulicms/content/modules/evil_css/EvilCss.php
+++ ulicms/content/modules/evil_css/EvilCss.php
@@ -2,6 +2,6 @@
 class EvilCss extends Controller {
 	public function frontendFooter() {
 		enqueueStylesheet ( ModuleHelper::buildRessourcePath ( "evil_css", "evil.css" ) );
-		return get_combined_stylesheet_html ();
+		return getCombinedStylesheetHtml ();
 	}
 }
