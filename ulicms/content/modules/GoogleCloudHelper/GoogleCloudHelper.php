df826dba690d694c63b4a192d8a002acc45853de
Ulrich Schmidt <admin@deruli.de>
2018-02-25 12:25:10 +0100

    Hilfsfunktionen fürs setzen der Datei-ACL

--- ulicms/content/modules/GoogleCloudHelper/GoogleCloudHelper.php
+++ ulicms/content/modules/GoogleCloudHelper/GoogleCloudHelper.php
@@ -16,4 +16,24 @@ class GoogleCloudHelper {
 	public static function isDevelopment() {
 		return isset ( $_SERVER ['SERVER_SOFTWARE'] ) && strpos ( $_SERVER ['SERVER_SOFTWARE'], 'Development/' ) === 0;
 	}
+	public static function writeCloudFile($filename, $content, $acl = "public-read") {
+		$options = [ 
+				'gs' => [ 
+						'acl' => $acl 
+				] 
+		];
+		$context = stream_context_create ( $options );
+		file_put_contents ( $filename, $content, 0, $context );
+	}
+	public static function changeFileVisiblity($filename, $public) {
+		if (! file_exists ( $filename )) {
+			return false;
+		}
+		$content = file_get_contents ( $filename );
+		if (! $content) {
+			return false;
+		}
+		self::writeCloudFile ( $filename, $content, $public ? "public-read" : "private" );
+		return true;
+	}
 }
