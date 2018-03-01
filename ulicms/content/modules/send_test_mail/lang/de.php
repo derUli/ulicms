790ae2307c07d5eda43821c56720c489823953d0
Ulrich Schmidt <admin@deruli.de>
2018-03-01 16:08:50 +0100

    send_test_mail entfernt

--- ulicms/content/modules/send_test_mail/lang/de.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ( "TRANSLATION_SEND_TEST_MAIL", "Test E-Mail senden" );
-define ( "TRANSLATION_TEST_MAIL_SUBJECT", "Test E-Mail" );
-define ( "TRANSLATION_TEST_MAIL_BODY", "Hallo Welt!\r\n" . "Dies ist eine Test-E-Mail von %domain%.\r\n\r\n" . "Wenn Sie diese E-Mail empfangen, bedeutet das, dass Ihre UliCMS Instanz E-Mails versenden kann." );
-define ( "TRANSLATION_SEND", "Absenden" );
-define ( "TRANSLATION_RECEIVER", "Kopfzeilen" );
-define ( "TRANSLATION_MAIL_SENT", "Die Test E-Mail wurde versandt." );
--- ulicms/content/modules/send_test_mail/lang/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-define ( "TRANSLATION_SEND_TEST_MAIL", "Send test mail" );
-define ( "TRANSLATION_TEST_MAIL_SUBJECT", "Test Mail" );
-define ( "TRANSLATION_TEST_MAIL_BODY", "Hello World!\r\n" . "This is a test mail from %domain%.\r\n\r\n" . "If you receive this mail that means that your UliCMS instance is able to send mails." );
-define ( "TRANSLATION_SEND", "Send" );
-define ( "TRANSLATION_RECEIVER", "Receiver" );
-define ( "TRANSLATION_HEADERS", "Headers" );
-define ( "TRANSLATION_MAIL_SENT", "Mail sent." );
