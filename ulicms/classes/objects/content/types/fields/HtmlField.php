643419d849dd019f9ff481236536268728677012
Ulrich Schmidt <u.schmidt@velian.de>
2018-03-05 17:07:13 +0100

    custom types feature repariert

--- ulicms/classes/objects/content/types/fields/HtmlField.php
+++ ulicms/classes/objects/content/types/fields/HtmlField.php
@@ -9,7 +9,7 @@ class HtmlField extends CustomField {
 		}
 		ViewBag::set ( "field", $this );
 		ViewBag::set ( "field_value", $value );
-		ViewBag::set ( "field_name", ! is_null ( $this->$contentType ) ? $this->$contentType . "_" . $this->name : $this->name );
+		ViewBag::set ( "field_name", ! is_null ( $this->contentType ) ? $this->contentType . "_" . $this->name : $this->name );
 		
 		return Template::executeDefaultOrOwnTemplate ( "fields/htmlfield.php" );
 	}
