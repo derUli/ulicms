14a984f4dccc8d5b182fe5aef2a8422c4c60541b
Ulrich Schmidt <admin@deruli.de>
2018-03-08 19:21:28 +0100

    backslash in unit test escapt

--- ulicms/tests/YaLinqoTest.php
+++ ulicms/tests/YaLinqoTest.php
@@ -5,6 +5,6 @@ class YaLinqoTest extends PHPUnit_Framework_TestCase {
      // Test if YaLinqo Framework is correctly installed
      // Yet Another LINQ to Objects for PHP
      public function testYaLinqoInstalled(){
-       $this->assertTrue(class_exists("\\YaLinqo\Enumerable"));
+       $this->assertTrue(class_exists("\\YaLinqo\\Enumerable"));
      }
 }
