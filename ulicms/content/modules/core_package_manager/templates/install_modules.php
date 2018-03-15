b17ecfac05d9594dbab3d70e271dae97261d29a9
Ulrich Schmidt <support@ulicms.de>
2018-03-15 17:59:54 +0100

    Performance der Installation von mehreren Paketen und Patches optimeirt

--- ulicms/content/modules/core_package_manager/templates/install_modules.php
+++ ulicms/content/modules/core_package_manager/templates/install_modules.php
@@ -1,93 +1,95 @@
 <?php
-$acl = new ACL ();
-$pkg = new PackageManager ();
-if (! $acl->hasPermission ( "install_packages" )) {
-	noperms ();
+$acl = new ACL();
+$pkg = new PackageManager();
+if (! $acl->hasPermission("install_packages")) {
+    noperms();
 } else {
-	$pkg_src = Settings::get ( "pkg_src" );
-	@set_time_limit ( 0 );
-	?>
+    $pkg_src = Settings::get("pkg_src");
+    @set_time_limit(0);
+    ?>
 <p>
 	<a href="<?php echo ModuleHelper::buildActionURL("modules");?>"
 		class="btn btn-default btn-back"><?php translate("back")?></a>
 </p>
 <h1><?php translate("install");?></h1>
 <?php
-	if (! $pkg_src) {
-		?>
+    if (! $pkg_src) {
+        ?>
 <p>
 	<strong><?php translation ( "error" );?></strong> <br />
 	<?php translate ( "pkgsrc_not_defined" );?></p>
 <?php
-	} else if (! class_exists ( "PharData" )) {
-		?>
+    } else if (! class_exists("PharData")) {
+        ?>
 <p>
 	<strong><?php translate ( "error" );?></strong> <br />
 	<?php translate ( "phardata_not_available" );?></p>
 <?php
-	} else {
-		include_once "../version.php";
-		$version = new UliCMSVersion ();
-		$internalVersion = implode ( ".", $version->getInternalVersion () );
-		$pkg_src = str_replace ( "{version}", $internalVersion, $pkg_src );
-
-		$packageArchiveFolder = $pkg_src . "archives/";
-		$packagesToInstall = explode ( ",", $_REQUEST ["packages"] );
-
-		$post_install_script = ULICMS_DATA_STORAGE_ROOT . "/post-install.php";
-		if (file_exists ( $post_install_script )) {
-			unlink ( $post_install_script );
-		}
-
-		if (count ( $packagesToInstall ) === 0 or empty ( $_REQUEST ["packages"] )) {
-			?>
+    } else {
+        include_once "../version.php";
+        $version = new UliCMSVersion();
+        $internalVersion = implode(".", $version->getInternalVersion());
+        $pkg_src = str_replace("{version}", $internalVersion, $pkg_src);
+        
+        $packageArchiveFolder = $pkg_src . "archives/";
+        $packagesToInstall = explode(",", $_REQUEST["packages"]);
+        
+        $post_install_script = ULICMS_DATA_STORAGE_ROOT . "/post-install.php";
+        if (file_exists($post_install_script)) {
+            unlink($post_install_script);
+        }
+        
+        if (count($packagesToInstall) === 0 or empty($_REQUEST["packages"])) {
+            ?>
 <p>
 	<strong><?php translate("error");?></strong> <br />
 	 <?php translate("nothing_to_do");?>
 </p>
 
 <?php
-		} else {
-			for($i = 0; $i < count ( $packagesToInstall ); $i ++) {
-				if (! empty ( $packagesToInstall [$i] )) {
-					$pkgURL = $packageArchiveFolder . basename ( $packagesToInstall [$i] ) . ".tar.gz";
-					$pkgContent = @file_get_contents_wrapper ( $pkgURL );
-
-					// Wenn Paket nicht runtergeladen werden konnte
-					if (! $pkgContent or strlen ( $pkgContent ) < 1) {
-						echo "<p style='color:red;'>" . str_ireplace ( "%pkg%", $packagesToInstall [$i], get_translation ( "download_failed" ) ) . "</p>";
-					} else {
-						$tmpdir = "../content/tmp/";
-						if (! is_dir ( $tmpdir )) {
-							mkdir ( $tmpdir, 0777 );
-						}
-
-						$tmpFile = $tmpdir . $packagesToInstall [$i] . ".tar.gz";
-
-						// write downloaded tarball to file
-						$handle = fopen ( $tmpFile, "wb" );
-						fwrite ( $handle, $pkgContent );
-						fclose ( $handle );
-
-						if (file_exists ( $tmpFile )) {
-							// Paket installieren
-							if ($pkg->installPackage ( $tmpFile )) {
-								echo "<p style='color:green;'>" . str_ireplace ( "%pkg%", $packagesToInstall [$i], get_translation ( "INSTALLATION_SUCCESSFULL" ) ) . "</p>";
-							} else {
-								echo "<p style='color:red;'>" . str_ireplace ( "%pkg%", $packagesToInstall [$i], get_translation ( "EXTRACTION_OF_PACKAGE_FAILED" ) ) . "</p>";
-							}
-						}
-						@unlink ( $tmpFile );
-					}
-				}
-			}
-			?>
+        } else {
+            for ($i = 0; $i < count($packagesToInstall); $i ++) {
+                if (! empty($packagesToInstall[$i])) {
+                    $pkgURL = $packageArchiveFolder . basename($packagesToInstall[$i]) . ".tar.gz";
+                    $pkgContent = @file_get_contents_wrapper($pkgURL);
+                    
+                    // Wenn Paket nicht runtergeladen werden konnte
+                    if (! $pkgContent or strlen($pkgContent) < 1) {
+                        echo "<p style='color:red;'>" . str_ireplace("%pkg%", $packagesToInstall[$i], get_translation("download_failed")) . "</p>";
+                    } else {
+                        $tmpdir = "../content/tmp/";
+                        if (! is_dir($tmpdir)) {
+                            mkdir($tmpdir, 0777);
+                        }
+                        
+                        $tmpFile = $tmpdir . $packagesToInstall[$i] . ".tar.gz";
+                        
+                        // write downloaded tarball to file
+                        $handle = fopen($tmpFile, "wb");
+                        fwrite($handle, $pkgContent);
+                        fclose($handle);
+                        
+                        if (file_exists($tmpFile)) {
+                            // Paket installieren
+                            if ($pkg->installPackage($tmpFile, false)) {
+                                echo "<p style='color:green;'>" . str_ireplace("%pkg%", $packagesToInstall[$i], get_translation("INSTALLATION_SUCCESSFULL")) . "</p>";
+                            } else {
+                                echo "<p style='color:red;'>" . str_ireplace("%pkg%", $packagesToInstall[$i], get_translation("EXTRACTION_OF_PACKAGE_FAILED")) . "</p>";
+                            }
+                        }
+                        @unlink($tmpFile);
+                    }
+                }
+            }
+            
+            clearCache();
+            ?>
 <p>
 	<a
 		href="<?php echo ModuleHelper::buildActionURL("available_modules");?>"
 		class="btn btn-default"><?php translate("install_another_package")?></a>
 </p>
 <?php
-		}
-	}
+        }
+    }
 }
