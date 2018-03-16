d01605e84cbc99c5248055be65ae94868413a2aa
Ulrich Schmidt <ulrich1992@web.de>
2018-03-16 22:48:51 +0100

    gulli_ticker verschoben

--- ulicms/content/modules/gulli_ticker/GulliTicker.php
+++
@@ -1,7 +0,0 @@
-<?php
-class GulliTicker extends MainClass {
-	const MODULE_NAME = "gulli_ticker";
-	public function render() {
-		return Template::executeModuleTemplate ( GulliTicker::MODULE_NAME, "ticker.php" );
-	}
-}
--- ulicms/content/modules/gulli_ticker/templates/ticker.php
+++
@@ -1,2 +0,0 @@
-<script type="text/javascript"
-	src="http://ticker.gulli.com/feed/ticker/view/ticker/width/480/speed/1/newscount/10/target/_blank/"></script>
