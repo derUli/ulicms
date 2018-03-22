8bd36cddd2392a311498b7eee4b9753f4c6d9003
Ulrich Schmidt <support@ulicms.de>
2018-03-22 06:24:03 +0100

    Meldung wenn ein Download fehlschlägt

--- ulicms/content/modules/core_package_manager/templates/install_patches.php
+++ ulicms/content/modules/core_package_manager/templates/install_patches.php
@@ -1,10 +1,10 @@
 <?php
-$acl = new ACL ();
-if ($acl->hasPermission ( "update_system" )) {
-	// No time limit
-	@set_time_limit ( 0 );
-	
-	?>
+$acl = new ACL();
+if ($acl->hasPermission("update_system")) {
+    // No time limit
+    @set_time_limit(0);
+    
+    ?>
 <p>
 	<a
 		href="<?php echo ModuleHelper::buildActionURL("available_patches");?>"
@@ -12,26 +12,32 @@ if ($acl->hasPermission ( "update_system" )) {
 </p>
 <h1><?php translate ( "install_patches" );?></h1>
 <?php
-	$patches = $_POST ["patches"];
-	$pkg = new PackageManager ();
-	
-	if (count ( $patches ) <= 0) {
-		translate ( "no_patches_selected" );
-	} else {
-		foreach ( $patches as $patch ) {
-			$splitted = explode ( "|", $patch );
-
-			$checksum = count($splitted) >= 4 ?  $splitted [3] : null;
-			$success = $pkg->installPatch ( $splitted [0], $splitted [1], $splitted [2], false, $checksum );
-			
-			if ($success) {
-				echo '<p style="color:green">' . htmlspecialchars ( $splitted [0] ) . " " . get_translation ( "was_successfully_installed" ) . '</p>';
-			} else {
-				echo '<p style="color:red">' . get_translation ( "installation_of" ) . " " . htmlspecialchars ( $splitted [0] ) . " " . get_Translation ( "is_failed" ) . "</p>";
-			}
-			fcflush ();
-		}
-	}
+    $patches = $_POST["patches"];
+    $pkg = new PackageManager();
+    
+    if (count($patches) <= 0) {
+        translate("no_patches_selected");
+    } else {
+        foreach ($patches as $patch) {
+            $splitted = explode("|", $patch);
+            
+            $checksum = count($splitted) >= 4 ? $splitted[3] : null;
+            try {
+                $success = $pkg->installPatch($splitted[0], $splitted[1], $splitted[2], false, $checksum);
+                
+                if ($success) {
+                    echo '<p style="color:green">' . htmlspecialchars($splitted[0]) . " " . get_translation("was_successfully_installed") . '</p>';
+                } else {
+                    echo '<p style="color:red">' . get_translation("installation_of") . " " . htmlspecialchars($splitted[0]) . " " . get_Translation("is_failed") . "</p>";
+                }
+            } catch (CorruptDownloadException $e) {
+                echo '<p>' . get_secure_translation("download_of_x_failed", array(
+                    "%item%" => $splitted[0]
+                )) . '</p>';
+            }
+            fcflush();
+        }
+    }
 } else {
-	noperms ();
+    noperms();
 }
