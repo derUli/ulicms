47bde0d736ec6136a3497cc504eef29520a9bc0c
Ulrich Schmidt <support@ulicms.de>
2018-03-24 15:51:42 +0100

    Abwärtskompatiblität zu alten Modulen hinzugefügt

--- ulicms/init.php
+++ ulicms/init.php
@@ -453,5 +453,12 @@ session_name(Settings::get("session_name"));
 // PHPUnit 6 introduced a breaking change that
 // removed PHPUnit_Framework_TestCase as a base class,
 // and replaced it with \PHPUnit\Framework\TestCase
-if (! class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+if (! class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')){
     class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
+// Backwards compatiblity for modules using the old config class name
+if(class_exists("CMSConfig") and !class_exists("config")){
+    class_alias("CMSConfig", "config");
+}
+   
