fabfa250de19cf96114c78223d5e012838a6b299
Ulrich Schmidt <support@ulicms.de>
2018-03-24 19:39:59 +0100

    CMSConfig aus Deploy-Scripts excludet

--- mk-upgrade-package.py
+++ mk-upgrade-package.py
@@ -37,7 +37,7 @@ def main():
               "modules", "templates", "contents.css",
               "config.js", "comments", "*~", ".settings", ".project", ".buildpath",
               "tests", "run-tests.sh", "run-tests.bat",
-              "run-tests.xampp.mac.sh", ".pydevproject")
+              "run-tests.xampp.mac.sh", ".pydevproject", "CMSConfig.php")
 
     IGNORE_PATTERNS = shutil.ignore_patterns(*ignore)
     if args.delete and os.path.exists(target):
