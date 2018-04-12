7523caba7faaf71601c9fdbb3876302ded6d423f
Ulrich Schmidt <support@ulicms.de>
2018-04-12 18:40:21 +0200

    Option cookie_expires wird ausgewertet

--- ulicms/admin/backend.php
+++ ulicms/admin/backend.php
@@ -1,56 +1,61 @@
 <?php
 // required because my local XAMPP is sometimes in wrong folder, so includes will fail
-chdir ( dirname ( __FILE__ ) );
+chdir(dirname(__FILE__));
 
 require_once "../init.php";
-@session_start ();
-$acl = new acl ();
 
-if ($acl->hasPermission ( $_REQUEST ["type"] ) and ($_REQUEST ["type"] == "images" or $_REQUEST ["type"] == "files" or $_REQUEST ["type"] == "flash")) {
-	$_CONFIG ["disabled"] = false;
-	$_SESSION ['KCFINDER'] = array ();
-	$_SESSION ['KCFINDER'] ['disabled'] = false;
+@session_start();
+$cfg = new CMSConfig();
+$cookie_expires = (isset($cfg->cookie_expires) and $cfg->cookie_expires > 0) ? (time() + intval($cfg->cookie_expires)) : 0;
+if (! isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), session_id(), $cookie_expires);
 }
 
-$_COOKIE [session_name ()] = session_id ();
+$acl = new acl();
 
-add_hook ( "after_session_start" );
-
-add_hook ( "before_set_language_by_domain" );
-setLanguageByDomain ();
-add_hook ( "after_set_language_by_domain" );
-
-$syslang = getSystemLanguage ();
-if (file_exists ( getLanguageFilePath ( $syslang ) )) {
-	include_once getLanguageFilePath ( $syslang );
-} else if (file_exists ( getLanguageFilePath ( "en" ) )) {
-	include_once getLanguageFilePath ( "en" );
-}
-Translation::loadAllModuleLanguageFiles ( $syslang );
-add_hook ( "before_include_custom_lang_file" );
-Translation::includeCustomLangFile ( $syslang );
-add_hook ( "after_include_custom_lang_file" );
-add_hook ( "before_custom_lang" );
-add_hook ( "custom_lang_" . $syslang );
-
-add_hook ( "after_custom_lang" );
-
-if (logged_in () and $_SERVER ["REQUEST_METHOD"] == "POST" and ! isset ( $_REQUEST ["ajax_cmd"] ) and ! defined ( "NO_ANTI_CSRF" )) {
-	if (! check_csrf_token ()) {
-		die ( "This is probably a CSRF attack!" );
-	}
+if ($acl->hasPermission($_REQUEST["type"]) and ($_REQUEST["type"] == "images" or $_REQUEST["type"] == "files" or $_REQUEST["type"] == "flash")) {
+    $_CONFIG["disabled"] = false;
+    $_SESSION['KCFINDER'] = array();
+    $_SESSION['KCFINDER']['disabled'] = false;
 }
 
-add_hook ( "before_set_locale_by_language" );
-setLocaleByLanguage ();
-add_hook ( "after_set_locale_by_language" );
+add_hook("after_session_start");
+
+add_hook("before_set_language_by_domain");
+setLanguageByDomain();
+add_hook("after_set_language_by_domain");
+
+$syslang = getSystemLanguage();
+if (file_exists(getLanguageFilePath($syslang))) {
+    include_once getLanguageFilePath($syslang);
+} else if (file_exists(getLanguageFilePath("en"))) {
+    include_once getLanguageFilePath("en");
+}
+Translation::loadAllModuleLanguageFiles($syslang);
+add_hook("before_include_custom_lang_file");
+Translation::includeCustomLangFile($syslang);
+add_hook("after_include_custom_lang_file");
+add_hook("before_custom_lang");
+add_hook("custom_lang_" . $syslang);
+
+add_hook("after_custom_lang");
+
+if (logged_in() and $_SERVER["REQUEST_METHOD"] == "POST" and ! isset($_REQUEST["ajax_cmd"]) and ! defined("NO_ANTI_CSRF")) {
+    if (! check_csrf_token()) {
+        die("This is probably a CSRF attack!");
+    }
+}
+
+add_hook("before_set_locale_by_language");
+setLocaleByLanguage();
+add_hook("after_set_locale_by_language");
 
 require_once "../templating.php";
 
-$cfg = new CMSConfig ();
-if (isset ( $cfg->ip_whitelist ) and is_array ( $cfg->ip_whitelist ) and count ( $cfg->ip_whitelist ) > 0 and ! faster_in_array ( get_ip (), $cfg->ip_whitelist )) {
-	translate ( "login_from_ip_not_allowed" );
-	die ();
+$cfg = new CMSConfig();
+if (isset($cfg->ip_whitelist) and is_array($cfg->ip_whitelist) and count($cfg->ip_whitelist) > 0 and ! faster_in_array(get_ip(), $cfg->ip_whitelist)) {
+    translate("login_from_ip_not_allowed");
+    die();
 }
 require_once "inc/queries.php";
 @include_once "inc/sort_direction.php";
@@ -58,83 +63,83 @@ require_once "inc/queries.php";
 require_once "../version.php";
 require_once "inc/logincheck.php";
 
-define ( "_SECURITY", true );
+define("_SECURITY", true);
 
-if ($_GET ["action"] == "ulicms_news") {
-	require_once "inc/ulicms_news.php";
-	exit ();
+if ($_GET["action"] == "ulicms_news") {
+    require_once "inc/ulicms_news.php";
+    exit();
 }
 
-if (isset ( $_SESSION ["ulicms_login"] )) {
-	$eingeloggt = $_SESSION ["ulicms_login"];
-	db_query ( "UPDATE " . tbname ( "users" ) . " SET last_action = " . time () . " WHERE id = " . $_SESSION ["login_id"] );
+if (isset($_SESSION["ulicms_login"])) {
+    $eingeloggt = $_SESSION["ulicms_login"];
+    db_query("UPDATE " . tbname("users") . " SET last_action = " . time() . " WHERE id = " . $_SESSION["login_id"]);
 } else {
-	$eingeloggt = false;
+    $eingeloggt = false;
 }
 
-header ( "Content-Type: text/html; charset=UTF-8" );
+header("Content-Type: text/html; charset=UTF-8");
 
-add_hook ( "before_ajax_handler" );
+add_hook("before_ajax_handler");
 
-if (isset ( $_REQUEST ["ajax_cmd"] )) {
-	include_once "inc/ajax_handler.php";
-	exit ();
+if (isset($_REQUEST["ajax_cmd"])) {
+    include_once "inc/ajax_handler.php";
+    exit();
 }
-add_hook ( "after_ajax_handler" );
+add_hook("after_ajax_handler");
 
-add_hook ( "before_backend_run_methods" );
-ControllerRegistry::runMethods ();
-add_hook ( "after_backend_run_methods" );
+add_hook("before_backend_run_methods");
+ControllerRegistry::runMethods();
+add_hook("after_backend_run_methods");
 
-add_hook ( "before_backend_header" );
+add_hook("before_backend_header");
 require_once "inc/header.php";
-add_hook ( "after_backend_header" );
+add_hook("after_backend_header");
 
 if (! $eingeloggt) {
-	if (isset ( $_GET ["register"] )) {
-		add_hook ( "before_register_form" );
-		require_once "inc/registerform.php";
-		add_hook ( "after_register_form" );
-	} else if (isset ( $_GET ["reset_password"] )) {
-		add_hook ( "before_reset_password_form" );
-		require_once "inc/reset_password.php";
-		add_hook ( "before_after_password_form" );
-	} else {
-		add_hook ( "before_login_form" );
-		require_once "inc/loginform.php";
-		add_hook ( "after_login_form" );
-	}
+    if (isset($_GET["register"])) {
+        add_hook("before_register_form");
+        require_once "inc/registerform.php";
+        add_hook("after_register_form");
+    } else if (isset($_GET["reset_password"])) {
+        add_hook("before_reset_password_form");
+        require_once "inc/reset_password.php";
+        add_hook("before_after_password_form");
+    } else {
+        add_hook("before_login_form");
+        require_once "inc/loginform.php";
+        add_hook("after_login_form");
+    }
 } else {
-	require_once "inc/adminmenu.php";
-	global $actions;
-	$actions = array ();
-	
-	ActionRegistry::loadModuleActions ();
-	
-	add_hook ( "register_actions" );
-	
-	if ($_SESSION ["require_password_change"]) {
-		require_once "inc/change_password.php";
-	} else if (isset ( $actions [get_action ()] )) {
-		$requiredPermission = ActionRegistry::getActionpermission ( get_action () );
-		if ((! $requiredPermission) or ($requiredPermission and $acl->hasPermission ( $requiredPermission ))) {
-			include_once $actions [get_action ()];
-		} else {
-			noperms ();
-		}
-	} else {
-		translate ( "action_not_found" );
-	}
+    require_once "inc/adminmenu.php";
+    global $actions;
+    $actions = array();
+    
+    ActionRegistry::loadModuleActions();
+    
+    add_hook("register_actions");
+    
+    if ($_SESSION["require_password_change"]) {
+        require_once "inc/change_password.php";
+    } else if (isset($actions[get_action()])) {
+        $requiredPermission = ActionRegistry::getActionpermission(get_action());
+        if ((! $requiredPermission) or ($requiredPermission and $acl->hasPermission($requiredPermission))) {
+            include_once $actions[get_action()];
+        } else {
+            noperms();
+        }
+    } else {
+        translate("action_not_found");
+    }
 }
 
-add_hook ( "admin_footer" );
+add_hook("admin_footer");
 
 require_once "inc/footer.php";
 
-add_hook ( "before_admin_cron" );
+add_hook("before_admin_cron");
 require_once "inc/cron.php";
-add_hook ( "after_admin_cron" );
+add_hook("after_admin_cron");
 
-db_close ( $connection );
-exit ();
+db_close($connection);
+exit();
 ?>
--- ulicms/frontend.php
+++ ulicms/frontend.php
@@ -2,284 +2,288 @@
 require_once "init.php";
 global $connection;
 
-add_hook ( "before_session_start" );
+add_hook("before_session_start");
 
 // initialize session
-@session_start ();
-$_COOKIE [session_name ()] = session_id ();
-
-add_hook ( "after_session_start" );
-
-setLanguageByDomain ();
-
-$languages = getAllLanguages ();
-
-if (! empty ( $_GET ["language"] ) and faster_in_array ( $_GET ["language"], $languages )) {
-	$_SESSION ["language"] = Database::escapeValue ( $_GET ["language"], DB_TYPE_STRING );
+@session_start();
+$cfg = new CMSConfig();
+$cookie_expires = (isset($cfg->cookie_expires) and $cfg->cookie_expires > 0) ? time() + intval($cfg->cookie_expires) : 0;
+if (! isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), session_id(), $cookie_expires);
 }
 
-if (! isset ( $_SESSION ["language"] )) {
-	$_SESSION ["language"] = Settings::get ( "default_language" );
+
+add_hook("after_session_start");
+
+setLanguageByDomain();
+
+$languages = getAllLanguages();
+
+if (! empty($_GET["language"]) and faster_in_array($_GET["language"], $languages)) {
+    $_SESSION["language"] = Database::escapeValue($_GET["language"], DB_TYPE_STRING);
 }
 
-setLocaleByLanguage ();
-
-if (faster_in_array ( $_SESSION ["language"], $languages ) && file_exists ( getLanguageFilePath ( $_SESSION ["language"] ) )) {
-	include_once getLanguageFilePath ( $_SESSION ["language"] );
-} else if (file_exists ( getLanguageFilePath ( "en" ) )) {
-	include getLanguageFilePath ( "en" );
+if (! isset($_SESSION["language"])) {
+    $_SESSION["language"] = Settings::get("default_language");
 }
 
-Translation::loadAllModuleLanguageFiles ( $_SESSION ["language"] );
-Translation::includeCustomLangFile ( $_SESSION ["language"] );
+setLocaleByLanguage();
+
+if (faster_in_array($_SESSION["language"], $languages) && file_exists(getLanguageFilePath($_SESSION["language"]))) {
+    include_once getLanguageFilePath($_SESSION["language"]);
+} else if (file_exists(getLanguageFilePath("en"))) {
+    include getLanguageFilePath("en");
+}
+
+Translation::loadAllModuleLanguageFiles($_SESSION["language"]);
+Translation::includeCustomLangFile($_SESSION["language"]);
 
 require_once "templating.php";
-Translation::loadCurrentThemeLanguageFiles ( $_SESSION ["language"] );
-add_hook ( "custom_lang_" . $_SESSION ["language"] );
+Translation::loadCurrentThemeLanguageFiles($_SESSION["language"]);
+add_hook("custom_lang_" . $_SESSION["language"]);
 
-if ($_SERVER ["REQUEST_METHOD"] == "POST" and ! defined ( "NO_ANTI_CSRF" )) {
-	if (! check_csrf_token ()) {
-		die ( "This is probably a CSRF attack!" );
-	}
-	if (Settings::get ( "min_time_to_fill_form", "int" ) > 0) {
-		check_form_timestamp ();
-	}
+if ($_SERVER["REQUEST_METHOD"] == "POST" and ! defined("NO_ANTI_CSRF")) {
+    if (! check_csrf_token()) {
+        die("This is probably a CSRF attack!");
+    }
+    if (Settings::get("min_time_to_fill_form", "int") > 0) {
+        check_form_timestamp();
+    }
 }
 
-$status = check_status ();
+$status = check_status();
 
-if (Settings::get ( "redirection" ) != "" && Settings::get ( "redirection" ) != false) {
-	add_hook ( "before_global_redirection" );
-	header ( "Location: " . Settings::get ( "redirection" ) );
-	exit ();
+if (Settings::get("redirection") != "" && Settings::get("redirection") != false) {
+    add_hook("before_global_redirection");
+    header("Location: " . Settings::get("redirection"));
+    exit();
 }
 
-$theme = get_theme ();
+$theme = get_theme();
 
-if (isMaintenanceMode ()) {
-	add_hook ( "before_maintenance_message" );
-	// Sende HTTP Status 503 und Retry-After im Wartungsmodus
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " 503 Service Temporarily Unavailable" );
-	header ( 'Status: 503 Service Temporarily Unavailable' );
-	header ( 'Retry-After: 60' );
-	header ( "Content-Type: text/html; charset=utf-8" );
-	if (file_exists ( getTemplateDirPath ( $theme ) . "maintenance.php" )) {
-		require_once getTemplateDirPath ( $theme ) . "maintenance.php";
-	} else {
-		die ( get_translation ( "UNDER_MAINTENANCE" ) );
-	}
-	add_hook ( "after_maintenance_message" );
-	die ();
+if (isMaintenanceMode()) {
+    add_hook("before_maintenance_message");
+    // Sende HTTP Status 503 und Retry-After im Wartungsmodus
+    header($_SERVER["SERVER_PROTOCOL"] . " 503 Service Temporarily Unavailable");
+    header('Status: 503 Service Temporarily Unavailable');
+    header('Retry-After: 60');
+    header("Content-Type: text/html; charset=utf-8");
+    if (file_exists(getTemplateDirPath($theme) . "maintenance.php")) {
+        require_once getTemplateDirPath($theme) . "maintenance.php";
+    } else {
+        die(get_translation("UNDER_MAINTENANCE"));
+    }
+    add_hook("after_maintenance_message");
+    die();
 }
 
-if (isset ( $_GET ["format"] ) and ! empty ( $_GET ["format"] )) {
-	$format = trim ( $_GET ["format"] );
+if (isset($_GET["format"]) and ! empty($_GET["format"])) {
+    $format = trim($_GET["format"]);
 } else {
-	$format = "html";
+    $format = "html";
 }
 
-add_hook ( "before_http_header" );
+add_hook("before_http_header");
 
-$redirection = get_redirection ();
+$redirection = get_redirection();
 
-if ($redirection and (is_active () or is_logged_in ())) {
-	Request::redirect ( $redirection, 302 );
+if ($redirection and (is_active() or is_logged_in())) {
+    Request::redirect($redirection, 302);
 }
 try {
-	$page = ContentFactory::getByID ( get_ID () );
-	if (! is_null ( $page->id ) and $page instanceof Language_Link) {
-		$language = new Language ( $page->link_to_language );
-		if (! is_null ( $language->getID () ) and StringHelper::isNotNullOrWhitespace ( $language->getLanguageLink () )) {
-			Request::redirect ( $language->getLanguageLink () );
-		}
-	}
-} catch ( Exception $e ) {
+    $page = ContentFactory::getByID(get_ID());
+    if (! is_null($page->id) and $page instanceof Language_Link) {
+        $language = new Language($page->link_to_language);
+        if (! is_null($language->getID()) and StringHelper::isNotNullOrWhitespace($language->getLanguageLink())) {
+            Request::redirect($language->getLanguageLink());
+        }
+    }
+} catch (Exception $e) {}
+
+if (isset($_GET["goid"])) {
+    $goid = intval($_GET["goid"]);
+    $url = ModuleHelper::getFullPageURLByID($goid);
+    if ($url) {
+        Request::redirect($url, 301);
+    } else {
+        $url = getBaseFolderURL();
+        Request::redirect($url, 301);
+    }
 }
 
-if (isset ( $_GET ["goid"] )) {
-	$goid = intval ( $_GET ["goid"] );
-	$url = ModuleHelper::getFullPageURLByID ( $goid );
-	if ($url) {
-		Request::redirect ( $url, 301 );
-	} else {
-		$url = getBaseFolderURL ();
-		Request::redirect ( $url, 301 );
-	}
-}
+ControllerRegistry::runMethods();
 
-ControllerRegistry::runMethods ();
-
-header ( $_SERVER ["SERVER_PROTOCOL"] . " " . $status );
+header($_SERVER["SERVER_PROTOCOL"] . " " . $status);
 
 if ($format == "html") {
-	header ( "Content-Type: text/html; charset=utf-8" );
+    header("Content-Type: text/html; charset=utf-8");
 } else if ($format == "pdf") {
-	$pdf = new PDFCreator ();
-	$pdf->output ();
+    $pdf = new PDFCreator();
+    $pdf->output();
 } else if ($format == "csv") {
-	$csv = new CSVCreator ();
-	$csv->output ();
+    $csv = new CSVCreator();
+    $csv->output();
 } else if ($format == "json") {
-	$json = new JSONCreator ();
-	$json->output ();
+    $json = new JSONCreator();
+    $json->output();
 } else if ($format == "txt") {
-	$plain = new PlainTextCreator ();
-	$plain->output ();
+    $plain = new PlainTextCreator();
+    $plain->output();
 } else {
-	$format = "html";
+    $format = "html";
 }
 
-add_hook ( "after_http_header" );
+add_hook("after_http_header");
 
-if (count ( getThemeList () ) === 0) {
-	throw new Exception ( "Keine Themes vorhanden!" );
+if (count(getThemeList()) === 0) {
+    throw new Exception("Keine Themes vorhanden!");
 }
 
-if (! is_dir ( getTemplateDirPath ( $theme, true ) )) {
-	throw new Exception ( "The selected theme doesn't exists!" );
+if (! is_dir(getTemplateDirPath($theme, true))) {
+    throw new Exception("The selected theme doesn't exists!");
 }
 
-add_hook ( "before_functions" );
+add_hook("before_functions");
 
-if (file_exists ( getTemplateDirPath ( $theme, true ) . "functions.php" )) {
-	include getTemplateDirPath ( $theme, true ) . "functions.php";
+if (file_exists(getTemplateDirPath($theme, true) . "functions.php")) {
+    include getTemplateDirPath($theme, true) . "functions.php";
 }
 
-add_hook ( "after_functions" );
+add_hook("after_functions");
 
-$hasModul = containsModule ( get_requested_pagename () );
+$hasModul = containsModule(get_requested_pagename());
 
-$cache_control = get_cache_control ();
+$cache_control = get_cache_control();
 switch ($cache_control) {
-	case "auto" :
-	case "force" :
-		Flags::setNoCache ( false );
-		break;
-		break;
-	case "no_cache" :
-		Flags::setNoCache ( true );
-		break;
+    case "auto":
+    case "force":
+        Flags::setNoCache(false);
+        break;
+        break;
+    case "no_cache":
+        Flags::setNoCache(true);
+        break;
 }
 if ($hasModul) {
-	no_cache ();
+    no_cache();
 }
 
 // Kein Caching wenn man eingeloggt ist
-if (is_logged_in () and get_cache_control () == "auto") {
-	no_cache ();
+if (is_logged_in() and get_cache_control() == "auto") {
+    no_cache();
 }
 
-add_hook ( "before_html" );
+add_hook("before_html");
 
 $cacheAdapter = null;
-if (CacheUtil::isCacheEnabled () and Request::isGet () and ! Flags::getNoCache ()) {
-	$cacheAdapter = CacheUtil::getAdapter ();
+if (CacheUtil::isCacheEnabled() and Request::isGet() and ! Flags::getNoCache()) {
+    $cacheAdapter = CacheUtil::getAdapter();
 }
-$uid = CacheUtil::getCurrentUid ();
-if ($cacheAdapter and $cacheAdapter->get ( $uid )) {
-	echo $cacheAdapter->get ( $uid );
-	
-	if (Settings::get ( "no_auto_cron" )) {
-		die ();
-	}
-	
-	add_hook ( "before_cron" );
-	@include 'cron.php';
-	add_hook ( "after_cron" );
-	die ();
-}
-
-if ($cacheAdapter or Settings::get ( "minify_html" )) {
-	ob_start ();
+$uid = CacheUtil::getCurrentUid();
+if ($cacheAdapter and $cacheAdapter->get($uid)) {
+    echo $cacheAdapter->get($uid);
+    
+    if (Settings::get("no_auto_cron")) {
+        die();
+    }
+    
+    add_hook("before_cron");
+    @include 'cron.php';
+    add_hook("after_cron");
+    die();
 }
 
-$html_file = page_has_html_file ( get_requested_pagename () );
+if ($cacheAdapter or Settings::get("minify_html")) {
+    ob_start();
+}
+
+$html_file = page_has_html_file(get_requested_pagename());
 
 if ($html_file) {
-	if (file_exists ( $html_file )) {
-		echo file_get_contents ( $html_file );
-	} else {
-		echo "File Not Found";
-	}
+    if (file_exists($html_file)) {
+        echo file_get_contents($html_file);
+    } else {
+        echo "File Not Found";
+    }
 } else {
-	$top_files = array (
-			"type/" . get_type () . "/oben.php",
-			"type/" . get_type () . "/top.php",
-			"oben.php",
-			"top.php" 
-	);
-	foreach ( $top_files as $file ) {
-		$file = getTemplateDirPath ( $theme, true ) . $file;
-		if (file_exists ( $file )) {
-			require $file;
-			break;
-		}
-	}
-	add_hook ( "before_content" );
-	$text_position = get_text_position ();
-	
-	if ($text_position == "after") {
-		Template::outputContentElement ();
-	}
-	
-	content ();
-	
-	if ($text_position == "before") {
-		Template::outputContentElement ();
-	}
-	
-	add_hook ( "after_content" );
-	
-	add_hook ( "before_edit_button" );
-	
-	edit_button ();
-	add_hook ( "after_edit_button" );
-	$bottom_files = array (
-			"type/" . get_type () . "/unten.php",
-			"type/" . get_type () . "/bottom.php",
-			"unten.php",
-			"bottom.php" 
-	);
-	foreach ( $bottom_files as $file ) {
-		$file = getTemplateDirPath ( $theme, true ) . $file;
-		if (file_exists ( $file )) {
-			require $file;
-			break;
-		}
-	}
+    $top_files = array(
+        "type/" . get_type() . "/oben.php",
+        "type/" . get_type() . "/top.php",
+        "oben.php",
+        "top.php"
+    );
+    foreach ($top_files as $file) {
+        $file = getTemplateDirPath($theme, true) . $file;
+        if (file_exists($file)) {
+            require $file;
+            break;
+        }
+    }
+    add_hook("before_content");
+    $text_position = get_text_position();
+    
+    if ($text_position == "after") {
+        Template::outputContentElement();
+    }
+    
+    content();
+    
+    if ($text_position == "before") {
+        Template::outputContentElement();
+    }
+    
+    add_hook("after_content");
+    
+    add_hook("before_edit_button");
+    
+    edit_button();
+    add_hook("after_edit_button");
+    $bottom_files = array(
+        "type/" . get_type() . "/unten.php",
+        "type/" . get_type() . "/bottom.php",
+        "unten.php",
+        "bottom.php"
+    );
+    foreach ($bottom_files as $file) {
+        $file = getTemplateDirPath($theme, true) . $file;
+        if (file_exists($file)) {
+            require $file;
+            break;
+        }
+    }
 }
 
-add_hook ( "after_html" );
+add_hook("after_html");
 
 // Wenn no_auto_cron gesetzt ist, dann muss cron.php manuell ausgeführt bzw. aufgerufen werden
 
-if ($cacheAdapter or Settings::get ( "minify_html" )) {
-	$generatedHtml = ob_get_clean ();
-	$generatedHtml = normalizeLN ( $generatedHtml, "\n" );
-	if (Settings::get ( "minify_html" )) {
-		$generatedHtml = preg_replace ( '/^\h*\v+/m', '', $generatedHtml );
-		
-		$posBegin = strpos ( $generatedHtml, "<html" );
-		$posEnd = strpos ( $generatedHtml, "</head>" );
-		$posLength = $posEnd - $posBegin;
-		$head = substr ( $generatedHtml, $posBegin, $posLength + strlen ( "</head>" ) );
-		$head = str_replace ( "\n", "", $head );
-		
-		$generatedHtml = $head . substr ( $generatedHtml, $posEnd + strlen ( "</head>" ) + 1 );
-		$generatedHtml = str_replace ( "</body>\n</html>", "</body></html>", $generatedHtml );
-	}
-	echo $generatedHtml;
-	
-	if ($cacheAdapter and ! defined ( "EXCEPTION_OCCURRED" )) {
-		$cacheAdapter->set ( $uid, $generatedHtml, CacheUtil::getCachePeriod () );
-	}
+if ($cacheAdapter or Settings::get("minify_html")) {
+    $generatedHtml = ob_get_clean();
+    $generatedHtml = normalizeLN($generatedHtml, "\n");
+    if (Settings::get("minify_html")) {
+        $generatedHtml = preg_replace('/^\h*\v+/m', '', $generatedHtml);
+        
+        $posBegin = strpos($generatedHtml, "<html");
+        $posEnd = strpos($generatedHtml, "</head>");
+        $posLength = $posEnd - $posBegin;
+        $head = substr($generatedHtml, $posBegin, $posLength + strlen("</head>"));
+        $head = str_replace("\n", "", $head);
+        
+        $generatedHtml = $head . substr($generatedHtml, $posEnd + strlen("</head>") + 1);
+        $generatedHtml = str_replace("</body>\n</html>", "</body></html>", $generatedHtml);
+    }
+    echo $generatedHtml;
+    
+    if ($cacheAdapter and ! defined("EXCEPTION_OCCURRED")) {
+        $cacheAdapter->set($uid, $generatedHtml, CacheUtil::getCachePeriod());
+    }
 }
 
-if (Settings::get ( "no_auto_cron" )) {
-	die ();
+if (Settings::get("no_auto_cron")) {
+    die();
 }
-add_hook ( "before_cron" );
+add_hook("before_cron");
 @include 'cron.php';
-add_hook ( "after_cron" );
-die ();
+add_hook("after_cron");
+die();
 
