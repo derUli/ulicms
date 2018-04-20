cd860213b60612ce519f94f0fa929abed504d213
Ulrich Schmidt <support@ulicms.de>
2018-04-20 23:29:08 +0200

    Mailformulare repariert mailer nun utf8 fähig

--- ulicms/content/modules/core_forms/lang/de.php
+++ ulicms/content/modules/core_forms/lang/de.php
@@ -4,3 +4,4 @@ add_translation ( "chinese_chars_not_allowed", "Chinesische Schriftzeichen sind 
 add_translation ( "cyrillic_charts_not_allowed", "Kyrillische Schriftzeichen sind nicht erlaubt" );
 add_translation ( "request_contains_badword", "Die Anfrage enthalt ein nicht erlaubtes Wort: %word%" );
 add_translation ( "your_country_is_blocked", "Der Zugriff auf diese Funktion ist aus Ihrem Land nicht erlaubt.\nIhr Hostname: %hostname%" );
+add_translation("please_fill_all_required_fields", "Bitte füllen Sie alle Pflichtfelder aus: %field%");
--- ulicms/content/modules/core_forms/objects/forms.php
+++ ulicms/content/modules/core_forms/objects/forms.php
@@ -1,127 +1,136 @@
 <?php
-class Forms {
-	public static function getFormByID($id) {
-		$retval = null;
-		$query = db_query ( "select * from `" . tbname ( "forms" ) . "` WHERE id = " . intval ( $id ) );
-		if (db_num_rows ( $query ) > 0) {
-			$retval = db_fetch_assoc ( $query );
-		}
-		
-		return $retval;
-	}
-	public static function deleteForm($id) {
-		$id = intval ( $id );
-		return db_query ( "DELETE FROM " . tbname ( "forms" ) . " WHERE id = $id" );
-	}
-	public static function createForm($name, $email_to, $subject, $category_id, $fields, $mail_from_field, $target_page_id) {
-		$name = db_escape ( $name );
-		$email_to = db_escape ( $email_to );
-		$subject = db_escape ( $subject );
-		$category_id = intval ( $category_id );
-		$fields = db_escape ( $fields );
-		$mail_from_field = db_escape ( $mail_from_field );
-		$target_page_id = intval ( $target_page_id );
-		$created = time ();
-		$updated = time ();
-		
-		return db_query ( "INSERT INTO `" . tbname ( "forms" ) . "` (name, email_to, subject, category_id, `fields`,
+
+class Forms
+{
+
+    public static function getFormByID($id)
+    {
+        $retval = null;
+        $query = db_query("select * from `" . tbname("forms") . "` WHERE id = " . intval($id));
+        if (db_num_rows($query) > 0) {
+            $retval = db_fetch_assoc($query);
+        }
+        
+        return $retval;
+    }
+
+    public static function deleteForm($id)
+    {
+        $id = intval($id);
+        return db_query("DELETE FROM " . tbname("forms") . " WHERE id = $id");
+    }
+
+    public static function createForm($name, $email_to, $subject, $category_id, $fields, $mail_from_field, $target_page_id)
+    {
+        $name = db_escape($name);
+        $email_to = db_escape($email_to);
+        $subject = db_escape($subject);
+        $category_id = intval($category_id);
+        $fields = db_escape($fields);
+        $mail_from_field = db_escape($mail_from_field);
+        $target_page_id = intval($target_page_id);
+        $created = time();
+        $updated = time();
+        
+        return db_query("INSERT INTO `" . tbname("forms") . "` (name, email_to, subject, category_id, `fields`,
 									 mail_from_field, target_page_id, `created`, `updated`) values ('$name', '$email_to', '$subject', $category_id, '$fields',
-									 '$mail_from_field', $target_page_id, $created, $updated)" );
-	}
-	public static function editForm($id, $name, $email_to, $subject, $category_id, $fields, $mail_from_field, $target_page_id) {
-		$name = db_escape ( $name );
-		$email_to = db_escape ( $email_to );
-		$subject = db_escape ( $subject );
-		$category_id = intval ( $category_id );
-		$fields = db_escape ( $fields );
-		$mail_from_field = db_escape ( $mail_from_field );
-		$target_page_id = intval ( $target_page_id );
-		$updated = time ();
-		$id = intval ( $id );
-		
-		return db_query ( "UPDATE `" . tbname ( "forms" ) . "` set name='$name', email_to = '$email_to', subject = '$subject', category_id = $category_id,
-									 fields = '$fields', mail_from_field = '$mail_from_field', target_page_id = $target_page_id, `updated` = $updated WHERE id = $id" );
-	}
-	public static function getAllForms() {
-		$retval = array ();
-		$query = db_query ( "select * from `" . tbname ( "forms" ) . "` ORDER BY id" );
-		if (db_num_rows ( $query ) > 0) {
-			while ( $row = db_fetch_assoc ( $query ) ) {
-				$retval [] = $row;
-			}
-		}
-		
-		return $retval;
-	}
-	public static function submitForm($id) {
-		$retval = false;
-		$form = self::getFormByID ( $id );
-		if ($form) {
-			$fields = $form ["fields"];
-			$fields = str_replace ( "\r\n", "\n", $fields );
-			$fields = explode ( "\n", $fields );
-			$fields = array_map ( "trim", $fields );
-			$html = "<!DOCTYPE html>";
-			$html .= "<html>";
-			$html .= "<head>";
-			$html .= '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
-			$html .= '<meta charset="utf-8">';
-			$html .= "</head>";
-			$html .= "<body>";
-			$html .= "<table border=\"1\"";
-			foreach ( $fields as $field ) {
-				if (! empty ( $field )) {
-					$field_splitted = explode ( "=>", $field );
-					$field_splitted = array_map ( "trim", $field_splitted );
-					if (count ( $field_splitted ) > 1) {
-						$label = $field_splitted [1];
-					} else {
-						$label = $field_splitted [0];
-					}
-					
-					$value = "";
-					if (isset ( $_POST [$field_splitted [0]] ) and ! empty ( $_POST [$field_splitted [0]] )) {
-						$value = $_POST [$field_splitted [0]];
-					}
-					
-					$html .= "<tr>";
-					$html .= "<td><strong>" . htmlspecialchars ( $label ) . "</strong></td>";
-					$html .= "<td>" . nl2br ( htmlspecialchars ( $value ) ) . "</td>";
-					$html .= "</tr>";
-				}
-			}
-			$html .= "</table>";
-			$html .= "</body>";
-			$html .= "</html>";
-			
-			$email_to = $form ["email_to"];
-			$subject = $form ["subject"];
-			$target_page_id = $form ["target_page_id"];
-			$target_page_systemname = getPageSystemnameByID ( $target_page_id );
-			$redirect_url = buildSEOUrl ( $target_page_systemname );
-			
-			$headers = "Content-Type: text/html; charset=UTF-8";
-			
-			$mail_from_field = $form ["mail_from_field"];
-			
-			$mail_from = StringHelper::isNotNullOrWhitespace ( $mail_from_field ) ? array (
-					$_POST [$mail_from_field] 
-			) : array (
-					Settings::get ( "email" ) 
-			);
-			sanitize ( $mail_from );
-			$headers .= "\n";
-			$headers .= "From: " . $mail_from [0];
-			
-			if (Mailer::send ( $email_to, $subject, $html, $headers )) {
-				Request::redirect ( $redirect_url );
-				$retval = true;
-			} else {
-				// TODO: Add this translation
-				translate ( "error_send_mail_form_failed" );
-				die ();
-			}
-		}
-		return $retval;
-	}
+									 '$mail_from_field', $target_page_id, $created, $updated)");
+    }
+
+    public static function editForm($id, $name, $email_to, $subject, $category_id, $fields, $mail_from_field, $target_page_id)
+    {
+        $name = db_escape($name);
+        $email_to = db_escape($email_to);
+        $subject = db_escape($subject);
+        $category_id = intval($category_id);
+        $fields = db_escape($fields);
+        $mail_from_field = db_escape($mail_from_field);
+        $target_page_id = intval($target_page_id);
+        $updated = time();
+        $id = intval($id);
+        
+        return db_query("UPDATE `" . tbname("forms") . "` set name='$name', email_to = '$email_to', subject = '$subject', category_id = $category_id,
+									 fields = '$fields', mail_from_field = '$mail_from_field', target_page_id = $target_page_id, `updated` = $updated WHERE id = $id");
+    }
+
+    public static function getAllForms()
+    {
+        $retval = array();
+        $query = db_query("select * from `" . tbname("forms") . "` ORDER BY id");
+        if (db_num_rows($query) > 0) {
+            while ($row = db_fetch_assoc($query)) {
+                $retval[] = $row;
+            }
+        }
+        
+        return $retval;
+    }
+
+    public static function submitForm($id)
+    {
+        $retval = false;
+        $form = self::getFormByID($id);
+        if ($form) {
+            $fields = $form["fields"];
+            $fields = Settings::mappingStringToArray($fields);
+            $required_fields = StringHelper::linesFromString($form["required_fields"]);
+            foreach ($required_fields as $field) {
+                $fieldName = isset($fields[$field]) ? $fields[$field] : $field;
+                if (! (isset($_POST[$field]) and ! empty($_POST[$field]))) {
+                    ViewBag::set("exception", get_translation("please_fill_all_required_fields", array(
+                        "%field%" => _esc($fieldName)
+                    )));
+                    $html = Template::executeDefaultOrOwnTemplate("exception.php");
+                    HTMLResult($html, HttpStatusCode::BAD_REQUEST);
+                }
+            }
+            $html = "<!DOCTYPE html>";
+            $html .= "<html>";
+            $html .= "<head>";
+            $html .= '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
+            $html .= '<meta charset="utf-8">';
+            $html .= "</head>";
+            $html .= "<body>";
+            $html .= "<table border=\"1\"";
+            foreach ($fields as $name => $label) {
+                $html .= "<tr>";
+                $html .= "<td><strong>" . _esc($label) . "</strong></td>";
+                $html .= "<td>" . nl2br(_esc($_POST[$name])) . "</td>";
+                $html .= "</tr>";
+            }
+            $html .= "</table>";
+            $html .= "</body>";
+            $html .= "</html>";
+            
+            $email_to = $form["email_to"];
+            $subject = $form["subject"];
+            $target_page_id = $form["target_page_id"];
+            $target_page_systemname = getPageSystemnameByID($target_page_id);
+            $redirect_url = buildSEOUrl($target_page_systemname);
+            
+            $headers = "Content-Type: text/html; charset=UTF-8";
+            
+            $mail_from_field = $form["mail_from_field"];
+            
+            $mail_from = StringHelper::isNotNullOrWhitespace($mail_from_field) ? array(
+                $_POST[$mail_from_field]
+            ) : array(
+                Settings::get("email")
+            );
+            sanitize($mail_from);
+            $headers .= "\n";
+            $headers .= "From: " . $mail_from[0] . "\n";
+            $headers .= "Content-Type: text/html";
+            
+            if (Mailer::send($email_to, $subject, $html, $headers)) {
+                Request::redirect($redirect_url);
+                $retval = true;
+            } else {
+                // TODO: Add this translation
+                translate("error_send_mail_form_failed");
+                die();
+            }
+        }
+        return $retval;
+    }
 }
