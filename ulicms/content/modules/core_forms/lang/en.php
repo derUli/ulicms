3d49c70bfda038ec313661aa9509c2498541567b
Ulrich Schmidt <support@ulicms.de>
2018-04-21 00:04:47 +0200

    Fehlede Übersetzung eingefügt

--- ulicms/content/modules/core_forms/lang/en.php
+++ ulicms/content/modules/core_forms/lang/en.php
@@ -3,5 +3,6 @@ add_translation("honeypot_is_not_empty", "Honeypot is not empty!");
 add_translation("chinese_chars_not_allowed", "Chinese chars are not allowed!");
 add_translation("cyrillic_charts_not_allowed", "Cyrillic chars are not allowed!");
 add_translation("request_contains_badword", "The request contains a bad word: %word%");
-add_translation("required_fields", "Required Fields");
 add_translation("your_country_is_blocked", "Access to this function for your country is blocked!\nYour hostname: %hostname%");
+add_translation("required_fields", "Required Fields");
+add_translation("please_fill_all_required_fields", "Plese fill all required fields: %field%");
