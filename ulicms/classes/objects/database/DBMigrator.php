3132fc673bca0f8e15f4a4ba723c727b321f0bea
Ulrich Schmidt <support@ulicms.de>
2018-04-27 16:31:24 +0200

    db_migrator.php CLI-Tool hinzugefügt

--- ulicms/classes/objects/database/DBMigrator.php
+++ ulicms/classes/objects/database/DBMigrator.php
@@ -1,80 +1,99 @@
 <?php
-class DBMigrator {
-	private $component = null;
-	private $folder = null;
-	public function __construct($component, $folder) {
-		$this->component = $component;
-		$this->folder = $folder;
-	}
-	public function migrate($stop = null) {
-		$this->checkVars ();
-		$files = scandir ( $this->folder );
-		natcasesort ( $files );
-		foreach ( $files as $file ) {
-			if (endsWith ( $file, ".sql" )) {
-				$sql = "SELECT id from {prefix}dbtrack where component = ? and name = ?";
-				$args = array (
-						$this->component,
-						$file 
-				);
-				$result = Database::pQuery ( $sql, $args, true );
-				if (Database::getNumRows ( $result ) == 0) {
-					$path = $this->folder . "/" . $file;
-					$sql = file_get_contents ( $path );
-					$cfg = new CMSConfig ();
-					$sql = str_ireplace ( "{prefix}", $cfg->db_prefix, $sql );
-					Database::query ( $sql, true );
-					$sql = "INSERT INTO {prefix}dbtrack (component, name) values (?,?)";
-					Database::pQuery ( $sql, $args, true );
-				}
-			}
-			if ($file === $stop) {
-				return;
-			}
-		}
-	}
-	public function rollback($stop = null) {
-		$this->checkVars ();
-		$files = scandir ( $this->folder );
-		natcasesort ( $files );
-		$files = array_reverse ( $files );
-		foreach ( $files as $file ) {
-			if (endsWith ( $file, ".sql" )) {
-				$sql = "SELECT id from {prefix}dbtrack where component = ? and name = ?";
-				$args = array (
-						$this->component,
-						$file 
-				);
-				$result = Database::pQuery ( $sql, $args, true );
-				if (Database::getNumRows ( $result ) > 0) {
-					$path = $this->folder . "/" . $file;
-					$sql = file_get_contents ( $path );
-					$cfg = new CMSConfig ();
-					$sql = str_ireplace ( "{prefix}", $cfg->db_prefix, $sql );
-					Database::query ( $sql, true );
-					$sql = "DELETE FROM {prefix}dbtrack where component = ? and name = ?";
-					Database::pQuery ( $sql, $args, true );
-				}
-			}
-			if ($file === $stop) {
-				return;
-			}
-		}
-	}
-	public function resetDBTrack() {
-		return Database::pQuery ( "DELETE FROM {prefix}dbtrack where component = ?", array (
-				$this->component 
-		), true );
-	}
-	private function checkVars() {
-		if (StringHelper::isNullOrEmpty ( $this->component )) {
-			throw new Exception ( "component is null or empty" );
-		}
-		if (StringHelper::isNullOrEmpty ( $this->folder )) {
-			throw new Exception ( "folder is null or empty" );
-		}
-		if (! is_dir ( $this->folder )) {
-			throw new Exception ( "folder not found " . $this->folder );
-		}
-	}
+
+class DBMigrator
+{
+
+    private $component = null;
+
+    private $folder = null;
+
+    public function __construct($component, $folder)
+    {
+        $this->component = $component;
+        $this->folder = $folder;
+    }
+
+    public function migrate($stop = null)
+    {
+        $this->checkVars();
+        $files = scandir($this->folder);
+        natcasesort($files);
+        foreach ($files as $file) {
+            if (endsWith($file, ".sql")) {
+                $sql = "SELECT id from {prefix}dbtrack where component = ? and name = ?";
+                $args = array(
+                    $this->component,
+                    $file
+                );
+                $result = Database::pQuery($sql, $args, true);
+                if (Database::getNumRows($result) == 0) {
+                    $path = $this->folder . "/" . $file;
+                    $sql = file_get_contents($path);
+                    $cfg = new CMSConfig();
+                    $sql = str_ireplace("{prefix}", $cfg->db_prefix, $sql);
+                    Database::query($sql, true);
+                    $sql = "INSERT INTO {prefix}dbtrack (component, name) values (?,?)";
+                    Database::pQuery($sql, $args, true);
+                }
+            }
+            if ($file === $stop) {
+                return;
+            }
+        }
+    }
+
+    public function rollback($stop = null)
+    {
+        $this->checkVars();
+        $files = scandir($this->folder);
+        natcasesort($files);
+        $files = array_reverse($files);
+        foreach ($files as $file) {
+            if (endsWith($file, ".sql")) {
+                $sql = "SELECT id from {prefix}dbtrack where component = ? and name = ?";
+                $args = array(
+                    $this->component,
+                    $file
+                );
+                $result = Database::pQuery($sql, $args, true);
+                if (Database::getNumRows($result) > 0) {
+                    $path = $this->folder . "/" . $file;
+                    $sql = file_get_contents($path);
+                    $cfg = new CMSConfig();
+                    $sql = str_ireplace("{prefix}", $cfg->db_prefix, $sql);
+                    Database::query($sql, true);
+                    $sql = "DELETE FROM {prefix}dbtrack where component = ? and name = ?";
+                    Database::pQuery($sql, $args, true);
+                }
+            }
+            if ($file === $stop) {
+                return;
+            }
+        }
+    }
+
+    public function resetDBTrack()
+    {
+        return Database::pQuery("DELETE FROM {prefix}dbtrack where component = ?", array(
+            $this->component
+        ), true);
+    }
+
+    public function resetDBTrackAll()
+    {
+        Database::truncateTable("dbtrack");
+    }
+
+    private function checkVars()
+    {
+        if (StringHelper::isNullOrEmpty($this->component)) {
+            throw new Exception("component is null or empty");
+        }
+        if (StringHelper::isNullOrEmpty($this->folder)) {
+            throw new Exception("folder is null or empty");
+        }
+        if (! is_dir($this->folder)) {
+            throw new Exception("folder not found " . $this->folder);
+        }
+    }
 }
---
+++ ulicms/shell/db_migrator.php
@@ -0,0 +1,68 @@
+#!/usr/bin/php -q
+<?php
+
+function usage()
+{
+    echo "dg_migrator - Apply database migrations\n";
+    echo "UliCMS Version " . cms_version() . "\n";
+    echo "Copyright (C) 2018 by Ulrich Schmidt";
+    echo "\n\n";
+    echo "Usage php -f db_migrator.php [up|down|list|reset] [component] [sqldir] [stopmigration]\n";
+    exit();
+}
+
+if (php_sapi_name() != "cli") {
+    die("This script can be run from command line only.");
+}
+$parent_path = dirname(__file__) . "/../";
+include $parent_path . "init.php";
+
+array_shift($argv);
+
+if (count($argv) == 0) {
+    usage();
+}
+
+$command = $argv[0];
+$component = count($argv) > 1 ? $argv[1] : null;
+$stop = count($argv) >= 4 ? $argv[3] : null;
+
+if ($command == "list") {
+    $where = $component ? "component='" . Database::escapeValue($component) . "'" : "1=1";
+    $result = Database::query("Select * from {prefix}dbtrack where $where order by component, date", true);
+    while ($row = Database::fetchObject($result)) {
+        echo "{$row->component} | {$row->name} | {$row->date}\n";
+    }
+    exit();
+}
+if ($command == "reset") {
+    
+    $migrator = new DBMigrator($component, getcwd());
+    if ($component) {
+        $migrator->resetDBTrack();
+    } else {
+        $migrator->resetDBTrackAll();
+    }
+    exit();
+}
+if ($command == "up") {
+    if (! $component) {
+        usage();
+    }
+    $folder = Path::resolve($argv[2] . "/up");
+    $migrator = new DBMigrator($component, $folder);
+    $migrator->migrate($stop);
+    exit();
+}
+
+if ($command == "down") {
+    if (! $component) {
+        usage();
+    }
+    $folder = Path::resolve($argv[2] . "/down");
+    $migrator = new DBMigrator($component, $folder);
+    $migrator->rollback($stop);
+    exit();
+}
+
+usage();
