8bb263b6a431379c2f9e5d738a2023917b374aa4
Ulrich Schmidt <support@ulicms.de>
2018-05-01 11:59:12 +0200

    db_schema_version aktualisiert

--- ulicms/installer.aus/sql/patches/001.sql
+++ ulicms/installer.aus/sql/patches/001.sql
@@ -4,4 +4,4 @@ insert into `{prefix}entity_permissions`
 `only_owner_can_edit`, `only_others_can_edit`) 
 select 'content', id, autor, group_id, `only_admins_can_edit`,
 `only_group_can_edit`, `only_owner_can_edit`, `only_others_can_edit` 
-from foo_content
+from {prefix}content
