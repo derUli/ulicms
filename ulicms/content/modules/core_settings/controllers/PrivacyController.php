acf52696c386b028c90983da7adf857e24ff8d6f
Ulrich Schmidt <u.schmidt@velian.de>
2018-05-09 15:43:51 +0200

    dummy save function added

--- ulicms/content/modules/core_settings/controllers/PrivacyController.php
+++ ulicms/content/modules/core_settings/controllers/PrivacyController.php
@@ -1,4 +1,6 @@
 <?php
 class PrivacyController extends Controller{
-    
+    public function savePost(){
+        throw new NotImplementedException("saving privacy policy settings not implemented yet.");
+    }
 }
