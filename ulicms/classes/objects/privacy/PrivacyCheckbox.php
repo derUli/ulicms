b96d42f0c0c319dbc5c7e0138cc6ecc3d0122678
Ulrich Schmidt <support@ulicms.de>
2018-05-09 22:39:43 +0200

    PrivacyCheckbox Klasse erweitert und Unit-Test implementiert

--- ulicms/classes/objects/privacy/PrivacyCheckbox.php
+++ ulicms/classes/objects/privacy/PrivacyCheckbox.php
@@ -6,6 +6,8 @@ class PrivacyCheckbox
 
     private $language;
 
+    const CHECKBOX_NAME = "privacy_policy_checkbox";
+
     public function __construct($language)
     {
         $this->language = $language;
@@ -16,14 +18,40 @@ class PrivacyCheckbox
         return boolval(Settings::get("privacy_policy_checkbox_enable_{$this->language}", "bool"));
     }
 
+    public function getCheckboxName()
+    {
+        return self::CHECKBOX_NAME;
+    }
+
+    Public function isChecked()
+    {
+        return boolval(Request::getVar($this->getCheckboxName(), 0, "bool"));
+    }
+
+    public function check($success = null, $failed = null)
+    {
+        if ($this->isChecked()) {
+            if ($success != null) {
+                $success();
+            }
+        } else {
+            if ($fail) {
+                $fail();
+            } else {
+                ViewBag::set("exception", get_translation("please_accept_privacy_conditions"));
+                echo Template::executeDefaultOrOwnTemplate("exception.php");
+            }
+        }
+    }
+
     public function render()
     {
-        $checkboxHtml = Input::CheckBox("privacy_policy_checkbox", false, "1", array(
+        $checkboxHtml = Input::CheckBox($this->getCheckboxName(), false, "1", array(
             "required" => "required",
-            "id" => "privacy_policy_checkbox"
+            "id" => $this->getCheckboxName()
         ));
-        $fullHtml = Settings::get("privacy_policy_checkbox_text_{$this->language}}");
-        if (! $this->isEnabled || StringHelper::isNullOrWhitespace($fullHtml)) {
+        $fullHtml = Settings::get("privacy_policy_checkbox_text_{$this->language}");
+        if (! $this->isEnabled() || StringHelper::isNullOrWhitespace($fullHtml)) {
             return "";
         }
         $fullHtml = str_ireplace("[checkbox]", $checkboxHtml, $fullHtml);
---
+++ ulicms/tests/PrivacyCheckboxTest.php
@@ -0,0 +1,96 @@
+<?php
+
+class PrivacyCheckboxTest extends PHPUnit_Framework_TestCase
+{
+
+    private $privacy_policy_checkbox_enable_de;
+
+    private $privacy_policy_checkbox_enable_en;
+
+    private $privacy_policy_checkbox_text_de;
+
+    private $privacy_policy_checkbox_text_en;
+
+    public function setUp()
+    {
+        $this->privacy_policy_checkbox_enable_de = Settings::get("privacy_policy_checkbox_enable_de");
+        $this->privacy_policy_checkbox_enable_en = Settings::get("privacy_policy_checkbox_enable_en");
+        $this->$privacy_policy_checkbox_text_de = Settings::get("privacy_policy_checkbox_text_de");
+        $this->$privacy_policy_checkbox_text_en = Settings::get("privacy_policy_checkbox_text_en");
+    }
+
+    public function tearDown()
+    {
+        if ($this->privacy_policy_checkbox_enable_de) {
+            Settings::set("privacy_policy_checkbox_enable_de", "1");
+        } else {
+            Settings::delete("privacy_policy_checkbox_enable_de");
+        }
+        if ($this->privacy_policy_checkbox_enable_en) {
+            Settings::set("privacy_policy_checkbox_enable_en", "1");
+        } else {
+            Settings::delete("privacy_policy_checkbox_enable_en");
+        }
+        Settings::set("privacy_policy_checkbox_text_de", $this->privacy_policy_checkbox_text_de);
+        Settings::set("privacy_policy_checkbox_text_de", $this->privacy_policy_checkbox_text_en);
+        
+        unset($_POST[PrivacyCheckbox::CHECKBOX_NAME]);
+        unset($_GET[PrivacyCheckbox::CHECKBOX_NAME]);
+    }
+
+    public function testIsEnabled()
+    {
+        $languages = array(
+            "de",
+            "en"
+        );
+        foreach ($langauges as $langauge) {
+            Settings::set("privacy_policy_checkbox_enable_{$language}", 1);
+            $checkbox = new PrivacyCheckbox($language);
+            $this->assertTrue($checkbox->isEnabled());
+            Settings::delete("privacy_policy_checkbox_enable_{$language}");
+            $this->assertFalse($checkbox->isEnabled());
+        }
+    }
+
+    public function testRender()
+    {
+        $values = array(
+            "de" => '<div class="checkbox"><label>[checkbox] Lorem Ipsum</label></div>',
+            "en" => '<div class="checkbox"><label>[checkbox] Sit dor amet</label></div>'
+        );
+        $expectedResults = array(
+            "de" => '<div class="checkbox"><label><input type="checkbox" name="privacy_policy_checkbox" value="1" required="required" id="privacy_policy_checkbox"> Lorem Ipsum</label></div>',
+            "en" => '<div class="checkbox"><label><input type="checkbox" name="privacy_policy_checkbox" value="1" required="required" id="privacy_policy_checkbox"> Sit dor amet</label></div>'
+        );
+        foreach ($values as $language => $html) {
+            Settings::delete("privacy_policy_checkbox_text_{$language}");
+            $checkbox = new PrivacyCheckbox($language);
+            $this->assertEmpty($checkbox->render());
+            
+            Settings::set("privacy_policy_checkbox_enable_{$language}", 1);
+            Settings::set("privacy_policy_checkbox_text_{$language}", $html);
+            
+            $this->assertNotEmpty($checkbox->render());
+            $this->assertEquals($expectedResults[$language], $checkbox->render());
+        }
+    }
+
+    public function testIsCheckboxCheckedPost()
+    {
+        $checkbox = new PrivacyCheckbox("en");
+        $this->assertFalse($checkbox->isChecked());
+        $_POST[PrivacyCheckbox::CHECKBOX_NAME] = "1";
+        $this->assertTrue($checkbox->isChecked());
+        unset($_POST[PrivacyCheckbox::CHECKBOX_NAME]);
+    }
+
+    public function testIsCheckboxCheckedGet()
+    {
+        $checkbox = new PrivacyCheckbox("en");
+        $this->assertFalse($checkbox->isChecked());
+        $_GET[PrivacyCheckbox::CHECKBOX_NAME] = "1";
+        $this->assertTrue($checkbox->isChecked());
+        unset($_GET[PrivacyCheckbox::CHECKBOX_NAME]);
+    }
+}
