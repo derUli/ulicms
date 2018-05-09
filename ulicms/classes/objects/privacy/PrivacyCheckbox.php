a2b937628e4745f1a47430ddf01df340ae089a83
Ulrich Schmidt <support@ulicms.de>
2018-05-09 21:03:13 +0200

    render funktion implementiert. noch ungetestet.

--- ulicms/classes/objects/privacy/PrivacyCheckbox.php
+++ ulicms/classes/objects/privacy/PrivacyCheckbox.php
@@ -1,13 +1,31 @@
 <?php
-class PrivacyCheckbox{
+use UliCMS\HTML\Input as Input;
+
+class PrivacyCheckbox
+{
+
     private $language;
-    public function __construct($language){
+
+    public function __construct($language)
+    {
         $this->language = $language;
     }
-    public function isEnabled(){
-        throw new NotImplementedException();
+
+    public function isEnabled()
+    {
+        return boolval(Settings::get("privacy_policy_checkbox_enable_{$this->language}", "bool"));
     }
-    public function render(){
-        throw new NotImplementedException();
+
+    public function render()
+    {
+        $checkboxHtml = Input::CheckBox("privacy_policy_checkbox", false, "1", array(
+            "required" => "required"
+        ));
+        $fullHtml = Settings::get("privacy_policy_checkbox_text_{$this->language}}");
+        if (! $this->isEnabled || StringHelper::isNullOrWhitespace($fullHtml)) {
+            return "";
+        }
+        $fullHtml = str_ireplace("[checkbox]", $checkboxHtml, $fullHtml);
+        return $fullHtml;
     }
 }
