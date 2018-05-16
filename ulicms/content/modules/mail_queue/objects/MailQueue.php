194915f9cccd43e822294dab04f7e04879e44c42
Ulrich Schmidt <support@ulicms.de>
2018-05-16 19:39:23 +0200

    getter für mails implementiert

--- ulicms/content/modules/mail_queue/objects/MailQueue.php
+++ ulicms/content/modules/mail_queue/objects/MailQueue.php
@@ -1,29 +1,56 @@
 <?php
 namespace MailQueue;
-use NotImplementedException;
-class MailQueue{
+
+use Database;
+
+class MailQueue
+{
+
     private static $instance;
-    public static function getInstance(){
-        if(self::$instance == null){
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
-    public function getAllMails(){
-        throw new NotImplementedException();
+    public function getAllMails()
+    {
+        $mails = array();
+        
+        $query = Database::query("select id from `{prefix}mail_queue` order by created asc");
+        
+        while ($row = Database::fetchObject($query)) {
+            $mails[] = new Mail($row->id);
+        }
+        return $mails;
     }
-    public function getNextMail(){
-        throw new NotImplementedException();
+
+    public function getNextMail()
+    {
+        $query = Database::query("select id from `{prefix}mail_queue` order by created asc limit 1");
+        if (Database::getNumRows($query) == 0) {
+            return null;
+        }
+        
+        $result = Database::fetchObject($query);
+        return new Mail($result->id);
     }
-    public function flushMailQueue(){
-        throw new NotImplementedException();
+
+    public function flushMailQueue()
+    {
+        Database::truncateTable("mail_queue");
     }
-    public function addMail($mail){
-        throw new NotImplementedException();
+
+    public function addMail($mail)
+    {
+        $mail->save();
     }
-    public function removeMail($mail){
-        throw new NotImplementedException();
+
+    public function removeMail($mail)
+    {
+        $mail->delete();
     }
 }
-//var_dump(MailQueue::getInstance());
