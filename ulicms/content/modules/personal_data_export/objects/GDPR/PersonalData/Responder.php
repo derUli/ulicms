90916e7bcb28c9902285a48a9229a9da92fc2f08
Ulrich Schmidt <support@ulicms.de>
2018-05-20 13:02:29 +0200

    responder ist nun ein interface

--- ulicms/content/modules/personal_data_export/objects/GDPR/PersonalData/Responder.php
+++ ulicms/content/modules/personal_data_export/objects/GDPR/PersonalData/Responder.php
@@ -1,12 +1,12 @@
 <?php
 namespace GDPR\PersonalData;
 
-abstract class Responder
+interface Responder
 {
 
     // $query is an e-mail address or a name as string
     // must return an array of ResponseBlock
-    public abstract function getData($query);
+    public function getData($query);
 
-    public abstract function deleteData($query);
+    public function deleteData($query);
 }
