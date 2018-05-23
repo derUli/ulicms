d43c9b1a6c09bb5b3401f55826c4f04cb759c354
Ulrich Schmidt <support@ulicms.de>
2018-05-23 07:45:32 +0200

    Template Dateien erstellt

--- ulicms/content/modules/newsletter2/controllers/NewsletterController.php
+++ ulicms/content/modules/newsletter2/controllers/NewsletterController.php
@@ -1,12 +1,20 @@
 <?php
-class NewsletterController extends Controller{
-	private $moduleName = "newsletter2";
 
-	public function render(){
-		return "";
-	}
-	public function uninstall(){
-		
-	}
+class NewsletterController extends Controller
+{
 
+    private $moduleName = "newsletter2";
+
+    public function render()
+    {
+        return Template::executeModuleTemplate($this->moduleName, "subscribe.php");
+    }
+
+    public function settings()
+    {
+        return Template::executeModuleTemplate($this->moduleName, "admin.php");
+    }
+
+    public function uninstall()
+    {}
 }
