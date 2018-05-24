8296ea2f17c31eca48d6eea5bd830febd0858473
Ulrich Schmidt <support@ulicms.de>
2018-05-24 21:22:05 +0200

    Neue API-Funktion ExceptionResult

--- ulicms/classes/objects/content/Results.php
+++ ulicms/classes/objects/content/Results.php
@@ -1,56 +1,77 @@
 <?php
-function JSONResult($data, $status = 200) {
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber ( $status ) );
-	$json = json_encode ( $data );
-	// get string size in Byte
-	$size = getStringLengthInBytes ( $json );
-	header ( 'Content-Type: application/json' );
-	header ( "Content-length: $size" );
-	echo $json;
-	exit ();
-}
-function RawJSONResult($data, $status = 200) {
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber ( $status ) );
-	$size = getStringLengthInBytes ( $data );
-	header ( 'Content-Type: application/json' );
-	header ( "Content-length: $size" );
-	echo $data;
-	exit ();
-}
-function HTMLResult($data, $status = 200) {
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber ( $status ) );
-	$size = getStringLengthInBytes ( $data );
-	header ( 'Content-Type: text/html; charset=UTF-8' );
-	header ( "Content-length: $size" );
-	echo $data;
-	exit ();
-}
-function TextResult($data, $status = 200) {
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber ( $status ) );
-	$size = getStringLengthInBytes ( $data );
-	header ( 'Content-Type: text/plain; charset=utf-8' );
-	header ( "Content-length: $size" );
-	echo $data;
-	exit ();
+
+function JSONResult($data, $status = 200)
+{
+    header($_SERVER["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber($status));
+    $json = json_encode($data);
+    // get string size in Byte
+    $size = getStringLengthInBytes($json);
+    header('Content-Type: application/json');
+    header("Content-length: $size");
+    echo $json;
+    exit();
 }
 
-function Result($data, $status = 200, $type = null){
-	header ( $_SERVER ["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber ( $status ) );
-	$size = getStringLengthInBytes ( $data );
-	if($type){
-		header ( 'Content-Type: $type' );
-	}
-	header ( "Content-length: $size" );
-	echo $data;
-	exit ();
+function RawJSONResult($data, $status = 200)
+{
+    header($_SERVER["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber($status));
+    $size = getStringLengthInBytes($data);
+    header('Content-Type: application/json');
+    header("Content-length: $size");
+    echo $data;
+    exit();
 }
 
-function HTTPStatusCodeResult($status, $description = null) {
-	$header = $_SERVER ["SERVER_PROTOCOL"] . " " . getStatusCodeByNumber ( intval ( $status ) );
+function HTMLResult($data, $status = 200)
+{
+    header($_SERVER["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber($status));
+    $size = getStringLengthInBytes($data);
+    header('Content-Type: text/html; charset=UTF-8');
+    header("Content-length: $size");
+    echo $data;
+    exit();
+}
 
-	if ($description != null and $description != "") {
-		$header = $_SERVER ["SERVER_PROTOCOL"] . " " . intval ( $status ) . " " . $description;
-	}
-	header ( $header );
-	exit ();
+function TextResult($data, $status = 200)
+{
+    header($_SERVER["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber($status));
+    $size = getStringLengthInBytes($data);
+    header('Content-Type: text/plain; charset=utf-8');
+    header("Content-length: $size");
+    echo $data;
+    exit();
+}
+
+function Result($data, $status = 200, $type = null)
+{
+    header($_SERVER["SERVER_PROTOCOL"] . " " . Request::getStatusCodeByNumber($status));
+    $size = getStringLengthInBytes($data);
+    if ($type) {
+        header('Content-Type: $type');
+    }
+    header("Content-length: $size");
+    echo $data;
+    exit();
+}
+
+function HTTPStatusCodeResult($status, $description = null)
+{
+    $header = $_SERVER["SERVER_PROTOCOL"] . " " . getStatusCodeByNumber(intval($status));
+    
+    if ($description != null and $description != "") {
+        $header = $_SERVER["SERVER_PROTOCOL"] . " " . intval($status) . " " . $description;
+    }
+    header($header);
+    exit();
+}
+
+function ExceptionResult($message, $status = 500)
+{
+    ViewBag::set("exception", $message);
+    
+    $header = $_SERVER["SERVER_PROTOCOL"] . " " . getStatusCodeByNumber(intval($status));
+    header($header);
+    
+    echo Template::executeDefaultOrOwnTemplate("exception.php");
+    exit();
 }
