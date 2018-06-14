52e4997a8a57bc3d64a3033f78305cf150dfe33a
Ulrich Schmidt <u.schmidt@velian.de>
2018-06-14 15:39:11 +0200

    privacy.js kommentiert

--- ulicms/admin/scripts/privacy.js
+++ ulicms/admin/scripts/privacy.js
@@ -1,10 +1,14 @@
+// Script for the "privacy settings" page
 $(function() {
+	// Change language
+	// Privacy settings are language specific
 	$("select#language").change(
 			function() {
 				var url = "index.php?action=privacy_settings&language="
 						+ $("select#language option:selected").val();
 				location.replace(url);
 			});
+	// expand privacy policy checkbox options when enabled
 	$("#privacy_policy_checkbox_enable").change(function(e)
 	{
 		var checked = $(this).is(":checked");
