2a6f95eb8b118f7dbfc116c50f1c1f2824e46cbc
Ulrich Schmidt <support@ulicms.de>
2018-06-14 22:48:48 +0200

    global.js kommentiert, javascripts aus banners/list ausgelagert in Datei

---
+++ ulicms/content/modules/core_content/js/banners.js
@@ -0,0 +1,21 @@
+// This the script for the advertisement banners list page
+$(function() {
+	// when select another language
+	$('#category').on(
+			'change',
+			function(e) {
+				var valueSelected = $('#category').val();
+				location.replace("index.php?action=banner&filter_category="
+						+ valueSelected)
+			});
+	// delete button
+	$("form.delete-form").ajaxForm({
+		success : function(responseText, statusText, xhr, $form) {
+			var action = $($form).attr("action");
+			var id = url('?banner', action);
+			var list_item_id = "dataset-" + id
+			var tr = $("tr#" + list_item_id);
+			$(tr).fadeOut();
+		}
+	});
+});
