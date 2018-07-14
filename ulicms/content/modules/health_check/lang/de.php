d42f5fb090d2f0ab677caeb8a22dc02980b25d5f
Ulrich Schmidt <support@ulicms.de>
2018-07-14 14:20:31 +0200

    aufgeräumt

--- ulicms/content/modules/health_check/lang/de.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_translation("object", "Objekt");
-add_translation("expected", "Erwartet");
-add_translation("actual", "Tatsächlich");
-add_translation("ulicms_services_reachable", "UliCMS Dienste erreichbar");
-add_translation("working", "Funktionierend");
-add_translation("url_rewriting_working", "URL Rewriting funktionierend");
-add_translation("php_extension_x_installed", "PHP Erweiterung %extension% installiert");
--- ulicms/content/modules/health_check/lang/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-add_translation("object", "Object");
-add_translation("expected", "Expected");
-add_translation("actual", "Actual");
-add_translation("ulicms_services_reachable", "UliCMS Services Reachable");
-add_translation("url_rewriting_working", "URL Rewriting Working");
-add_translation("php_extension_x_installed", "PHP Extension %extension% installed");
