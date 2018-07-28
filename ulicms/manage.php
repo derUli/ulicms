881728b3ca4e943e91f12bfc7310ef8a82fdac40
Ulrich Schmidt <ulrich1992@web.de>
2018-07-28 17:59:13 +0200

    manage.php script hinzugefügt

---
+++ ulicms/manage.php
@@ -0,0 +1,53 @@
+#!/usr/bin/env php
+<?php
+// wrapper for the tools in the "shell" subfolder
+if (! (php_sapi_name () === 'cli' or defined ( 'STDIN' ))) {
+	header ( "HTTP/1.0 403 Forbidden" );
+	echo "This is a command line only script.";
+	exit ();
+}
+
+$rootDir = dirname ( __FILE__ );
+
+array_shift ( $argv );
+
+$command = array_shift ( $argv );
+
+foreach ( $argv as $key => $value ) {
+	$argv [$key] = '"' . $value . '"';
+}
+
+$fullArgv = implode ( " ", $argv );
+
+$script = $rootDir . "/shell/" . basename ( $command ) . ".php";
+// if there is a script for this command execute it and passthru it's output to the command line
+if (is_file ( $script )) {
+	passthru ( "php \"" . $script . "\" $fullArgv" );
+	exit ();
+}
+
+echo "Usage:\n./" . basename ( __FILE__ ) . " [command] [arguments]\n\n";
+
+echo "Available commands:\n\n";
+
+$scripts = glob ( $rootDir . "/shell/*.php" );
+
+// if we are on Unix / Linux show only scripts which have the executable-bit set
+// Windows doesn't have an execute permission so we skip this on windows
+if (! defined ( 'PHP_WINDOWS_VERSION_MAJOR' )) {
+	$scripts = array_filter ( $scripts, "is_executable" );
+}
+
+foreach ( $scripts as $script ) {
+	$cmd = trim ( str_replace ( ".php", "", basename ( $script ) ) );
+	echo "* {$cmd}\n";
+}
+
+// Show some example usage commands
+
+echo "\nExamples:\n";
+echo "./" . basename ( __FILE__ ) . " sinfo themes\n";
+echo "./" . basename ( __FILE__ ) . " settings_get email\n";
+echo "./" . basename ( __FILE__ ) . ' settings_set foo "My Foobar"' . "\n";
+
+exit ();
