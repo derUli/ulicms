17cf659c4e3a9476575f41a967bb119c2d4aa5bd
Ulrich Schmidt <support@ulicms.de>
2018-08-17 20:38:18 +0200

    sql

---
+++ ulicms/content/modules/gallery2019/sql/up/002.sql
@@ -0,0 +1,8 @@
+CREATE TABLE {prefix}gallery_images` 
+( `id` INT NOT NULL AUTO_INCREMENT , 
+`gallery_id` INT NOT NULL , 
+`path` VARCHAR(255) NOT NULL , 
+`description` TEXT NULL , 
+`order` INT NOT NULL DEFAULT '0' , 
+PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8mb4 
+COLLATE utf8mb4_general_ci;
