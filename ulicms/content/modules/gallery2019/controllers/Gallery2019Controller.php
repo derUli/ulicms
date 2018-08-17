85476c97f5883f6f8b734a5809aa670c3eab78e3
Ulrich Schmidt <support@ulicms.de>
2018-08-17 19:36:10 +0200

    uninstall event

--- ulicms/content/modules/gallery2019/controllers/Gallery2019Controller.php
+++ ulicms/content/modules/gallery2019/controllers/Gallery2019Controller.php
@@ -1,12 +1,16 @@
 <?php
-class Gallery2019Controller extends Controller{
-	private $moduleName = "gallery2019";
 
-		public function uninstall(){
-		
-	}
-	public function clearCache(){
-		
-	}
+class Gallery2019Controller extends Controller
+{
 
+    private $moduleName = "gallery2019";
+
+    public function uninstall()
+    {
+        $migrator = new DBMigrator("module/{$this->moduleName}", ModuleHelper::buildModuleRessourcePath($this->moduleName, "sql/down"));
+        $migrator->migrate();
+    }
+
+    public function clearCache()
+    {}
 }
