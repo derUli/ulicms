05f81d2dffb72b78de00b144416d6a138b839050
Ulrich Schmidt <support@ulicms.de>
2018-08-21 01:08:15 +0200

    Dummy Tabelle erstellt, wird noch nicht gefüllt

--- ulicms/content/modules/gallery2019/lang/en.php
+++ ulicms/content/modules/gallery2019/lang/en.php
@@ -1,2 +1,4 @@
 <?php
 add_translation("galleries", "Galleries");
+add_translation("create_gallery", "Create Gallery");
+add_translation("image_amount", "Image Count");
--- ulicms/content/modules/gallery2019/templates/list.php
+++ ulicms/content/modules/gallery2019/templates/list.php
@@ -1,1 +1,15 @@
-Coming Soon
+<p>
+	<a href="#" class="btn btn-primary"><?php translate("create_gallery");?></a>
+</p>
+
+<table class="tablesorter">
+	<thead>
+		<tr>
+			<th><?php translate("title");?></th>
+			<th><?php translate("image_amount")?></th>
+			<td></td>
+			<td></td>
+		</tr>
+	</thead>
+	<tbody></tbody>
+</table>
