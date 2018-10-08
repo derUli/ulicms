94327d3aefedb1d75d1848b7b57d339150ec2cbb
Ulrich Schmidt <support@ulicms.de>
2018-10-08 08:05:25 +0200

    HTML der Formular E-Mails können nun per Template angepasst werden

---
+++ ulicms/content/modules/core_forms/templates/mails/message.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta charset="utf-8">
+</head>
+<body>
+	<!--  TODO: Output Table with fields -->
+	<table border="1">
+		<tbody>
+	<?php
+
+foreach (ViewBag::get("data") as $label => $value) {
+    ?>
+	  <tr>
+				<td><strong><?php esc($label);?></strong></td>
+				<td><strong><?php esc($value);?></strong></td>
+			</tr>
+	  <?php
+}
+?>
+	
+	</tbody>
+	</table>
+</body>
+</html>
