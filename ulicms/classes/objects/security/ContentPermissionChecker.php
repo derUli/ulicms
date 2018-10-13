ecc95547898d74044e91ac07dbdca9d8c1602db6
Ulrich Schmidt <support@ulicms.de>
2018-10-14 01:10:34 +0200

    ContentPermissionChecker::canWrite könnte nun funktionieren - ungetestet!

--- ulicms/classes/objects/security/ContentPermissionChecker.php
+++ ulicms/classes/objects/security/ContentPermissionChecker.php
@@ -4,6 +4,7 @@ namespace UliCMS\Security;
 use UliCMS\Exceptions\NotImplementedException;
 use ContentFactory;
 use User;
+use Group;
 
 class ContentPermissionChecker implements IDatasetPermissionChecker
 {
@@ -17,7 +18,7 @@ class ContentPermissionChecker implements IDatasetPermissionChecker
 
     public function canRead($dataset)
     {
-        return true;
+        throw new NotImplementedException();
     }
 
     public function canWrite($dataset)
@@ -26,35 +27,64 @@ class ContentPermissionChecker implements IDatasetPermissionChecker
         $permissions = $content->getPermissions();
         
         $contentOwner = $content->autor;
-		$contentGroup  = $content->group_id;
-
-		$user = new User($this->user_id);
-		$userGroups = array();
-		$primaryGroup = $user->getGroupId();
-		if($primaryGroup){
-			$groups[] = new Group($primaryGroup);
-		}
-		
-		$is_owner = $user->getID() ==contentOwner;
-		
-		$groups = array_merge($groups, $user->getSecondaryGroups());
-				
+        $contentGroup = $content->group_id;
+        
+        $user = new User($this->user_id);
+        $permissionChecker = new PermissionChecker($user_id);
+        $userGroups = array();
+        $primaryGroup = $user->getGroupId();
+        
+        if ($primaryGroup) {
+            $userGroups[] = new Group($primaryGroup);
+        }
+        
+        // Is the user the owner of the content dataset?
+        $isOwner = $user->getID() == $contentOwner;
+        
+        $userGroups = array_merge($userGroups, $user->getSecondaryGroups());
+        
+        $groupIds = array();
+        foreach ($userGroups as $group) {
+            $groupIds[] = $group->getID();
+        }
+        
+        // page edit restrictions (booleans)
         $adminsCanEdit = $permissions->getEditRestriction("admins");
         $groupCanEdit = $permissions->getEditRestriction("group");
         $ownerCanEdit = $permissions->getEditRestriction("owner");
         $othersCanEdit = $permissions->getEditRestriction("others");
-		
-		// if there are edit restrictions
-		if($adminsCanEdit or $groupsCanEdit or $ownerCanEdit or $othersCanEdit){
-			 // Work in Progress
-		}
-		
-        $can_edit_this = false;
-
+        
+        $canEditThis = false;
+        
+        // if there are edit restrictions
+        if ($groupCanEdit or $adminsCanEdit or $ownerCanEdit or $othersCanEdit) {
+            if ($groupCanEdit and in_array($contentGroup, $groupIds)) {
+                $canEditThis = true;
+            } else if ($adminsCanEdit and $user->getAdmin()) {
+                $canEditThis = true;
+            } else if ($ownerCanEdit and $isOwner and $permissionChecker->hasPermission("pages_edit_own")) {
+                $canEditThis = true;
+            } else if ($othersCanEdit and ! in_array($contentGroup, $groupIds) and ! $user->getAdmin() and ! $isOwner) {
+                $canEditThis = true;
+            }
+        } else {
+            if (! $isOwner and $permissionChecker->hasPermission("pages_edit_others")) {
+                $canEditThis = true;
+            } else if ($isOwner and $permissionChecker->hasPermission("pages_edit_own")) {
+                $canEditThis = true;
+            }
+        }
+        
+        // admins are gods
+        if ($user->getAdmin()) {
+            $canEditThis = true;
+        }
+        
+        return $canEditThis;
     }
 
     public function canDelete($dataset)
     {
-        throw new NotImplementedException();
+        return $this->canWrite($dataset);
     }
 }
