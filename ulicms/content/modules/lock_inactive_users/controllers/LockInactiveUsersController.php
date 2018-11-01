a79596df3e0d2b621feb525fa78ce525da967437
Ulrich Schmidt <support@ulicms.de>
2018-11-01 06:30:24 +0100

    Einstellungsseite erstellt

--- ulicms/content/modules/lock_inactive_users/controllers/LockInactiveUsersController.php
+++ ulicms/content/modules/lock_inactive_users/controllers/LockInactiveUsersController.php
@@ -3,7 +3,17 @@
 class LockInactiveUsersController extends MainClass
 {
 
-    private $moduleName = "lock_inactive_users";
+    private const MODULE_NAME = "lock_inactive_users";
+
+    public function settings()
+    {
+        return Template::executeModuleTemplate(self::MODULE_NAME, "settings.php");
+    }
+
+    public function getSettingsHeadline()
+    {
+        return get_translation("Lock_inactive_users");
+    }
 
     public function cron()
     {}
--- ulicms/content/modules/lock_inactive_users/lang/en.php
+++ ulicms/content/modules/lock_inactive_users/lang/en.php
@@ -1,1 +1,2 @@
 <?php
+add_translation("Lock_inactive_users", "Lock inactive users");
