3a548729ba05759a8bf93079e9514a8907b7a6a5
Ulrich Schmidt <support@ulicms.de>
2018-11-01 22:08:25 +0100

    Code kommentiert

--- ulicms/content/modules/lock_inactive_users/controllers/LockInactiveUsersController.php
+++ ulicms/content/modules/lock_inactive_users/controllers/LockInactiveUsersController.php
@@ -16,8 +16,11 @@ class LockInactiveUsersController extends MainClass
         $enable = intval(Request::getVar("enable"));
         $days = Request::getVar("days", 30, "int");
         
+        // Save settings
         Settings::set("lock_inactive_users/enable", $enable);
         Settings::set("lock_inactive_users/days", $days);
+        
+        // Redirect to settings page and show success message.
         Response::redirect(ModuleHelper::buildAdminURL(self::MODULE_NAME, "save=1"));
     }
 
@@ -32,8 +35,12 @@ class LockInactiveUsersController extends MainClass
         $daysInSeconds = $days * 60 * 60 * 24;
         
         $userManager = new UserManager();
+        
+        // get all not locked users
         $users = $userManager->getLockedUsers(false);
         foreach ($users as $user) {
+            // lock all users where the time difference between now and the last login
+            // is greater than X days
             if (time() - $user->getLastLogin() >= $daysInSeconds) {
                 $user->setLocked(true);
                 $user->save();
@@ -43,12 +50,16 @@ class LockInactiveUsersController extends MainClass
 
     public function cron()
     {
+        // If the function is enabled
         if (Settings::get("lock_inactive_users/enable")) {
+            // Use better_cron if installed to run the cronjob in a regular interval
             if (class_exists("BetterCron")) {
                 BetterCron::days("lock_inactive_users/cron", 1, function () {
                     $this->deleteExpiredUsers();
                 });
             } else {
+                // if better_cron is not installed run the cronjob on every page load
+                // this may have a negative effect on site performance
                 $this->deleteExpiredUsers();
             }
         }
