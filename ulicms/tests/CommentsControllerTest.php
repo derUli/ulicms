8a4bd0ad6951f5a2bbc8a007ddd0eb3b84d5b216
Ulrich Schmidt <admin@deruli.de>
2018-11-19 20:05:36 +0100

    tests hinzugefügt

--- ulicms/tests/CommentsControllerTest.php
+++ ulicms/tests/CommentsControllerTest.php
@@ -4,9 +4,35 @@ use UliCMS\Exceptions\NotImplementedException;
 class CommentsControllerTest extends \PHPUnit\Framework\TestCase
 {
 
-    public function testGetDefaultStatus()
+    private $initialCommentsMustBeApproved;
+
+    public function setUp()
     {
-        throw new NotImplementedException();
+        $this->initialCommentsMustBeApproved = Settings::get("comments_must_be_approved");
+    }
+
+    public function tearDown()
+    {
+        if (boolval($this->initialCommentsMustBeApproved)) {
+            Settings::set("comments_must_be_approved", "1");
+        } else {
+            Settings::delete("comments_must_be_approved");
+        }
+    }
+
+    public function testGetDefaultStatusExpectPending()
+    {
+        $controller = new CommentsController();
+        Settings::set("comments_must_be_approved", "1");
+        $this->assertEquals(CommentStatus::PENDING, $controller->getDefaultStatus());
+    }
+
+    public function testGetDefaultStatusExpectPublished()
+    {
+        Settings::delete("comments_must_be_approved");
+        
+        $controller = new CommentsController();
+        $this->assertEquals(CommentStatus::PUBLISHED, $controller->getDefaultStatus());
     }
 
     public function testGetResults()
