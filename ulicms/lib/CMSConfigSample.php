4c93b0bf9deeabac7584b872f3bad2b0c7b07d26
Ulrich Schmidt <admin@deruli.de>
2018-12-09 21:16:39 +0100

    comment code

--- ulicms/lib/CMSConfigSample.php
+++ ulicms/lib/CMSConfigSample.php
@@ -1,4 +1,9 @@
 <?php
+// This file file loads the configuration based on the environment
+// Set the environment variable ULICMS_ENVIRONMENT to change the configuration environment
+// If ULICMS_ENVIRONMENT is not set UliCMS fallbacks to "default" Environment
+// configuration files are now located in ULICMS_ROOT/content/configurations
+// Don't edit this file!
 $environment = getenv("ULICMS_ENVIRONMENT") ? getenv("ULICMS_ENVIRONMENT") : "default";
 $environment = basename($environment);
 
