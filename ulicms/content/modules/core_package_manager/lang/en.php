b3cd958ca95a07bfc9245c11da8d7df0634e125a
Ulrich Schmidt <admin@deruli.de>
2018-12-09 21:31:54 +0100

    Benachrichtigung wenn neue Patches verfügbar sind, anpassen

--- ulicms/content/modules/core_package_manager/lang/en.php
+++ ulicms/content/modules/core_package_manager/lang/en.php
@@ -1,2 +1,4 @@
 <?php
 add_translation("download_of_x_failed", "Download of %item% failed.");
+add_translation("show_available_patches", "Show available patches");
+add_translation("patches_will_fix_errors", "Patches fix errors in UliCMS.");
