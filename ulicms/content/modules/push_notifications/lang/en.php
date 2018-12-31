359816ef438380a52c4ddc5e2901b51897657504
Ulrich Schmidt <ulrich1992@web.de>
2019-01-01 00:37:10 +0100

    add help text

--- ulicms/content/modules/push_notifications/lang/en.php
+++ ulicms/content/modules/push_notifications/lang/en.php
@@ -1,4 +1,8 @@
 <?php
 add_translation ( "push_notifications", "Push Notifications" );
-add_translation ( "site_key", "Site-Key" );
+add_translation ( "site_key", "Site Key" );
 add_translation ( "send_push_notifications", "Send Push Notifications" );
+add_translation ( "engagespot_info", "Push Notifications for UliCMS are based on the services of <a href=\"https://engagespot.co\" target=\"_blank\">Engagespot</a>.
+After you registered on Engagespot you have to insert your site key in the text field below.
+The module will include a javascript snippet into your website which adds the notification icon to your website.
+Click the \"Send Push Notifications\" button to open the Engagespot Dashboard Login which enables you to send messages to all subscribers.");
--- ulicms/content/modules/push_notifications/templates/admin.php
+++ ulicms/content/modules/push_notifications/templates/admin.php
@@ -1,9 +1,12 @@
-<div class="alert alert-warning">Work in Progress.</div>
+<div class="alert alert-info">
+<?php echo nl2br(get_translation("engagespot_info"));?>
+</div>
 <p>
 	<a
 		href="<?php esc(PushNotificationsController::ENGAGESPOT_DASHBOARD_URL);?>"
 		class="btn btn-success" target="_blank"><?php translate("send_push_notifications");?></a>
 </p>
+
 <?php echo ModuleHelper::buildMethodCallForm("PushNotificationsController", "saveSettings");?>
 <div class="form-group">
 	<label for="site_key"><?php translate("site_key");?></label> <input
