d68dfa06adf6e9143658b548321dc00169835d4f
Ulrich Schmidt <ulrich1992@web.de>
2018-12-31 18:16:46 +0100

    rename functions to camelCase

--- ulicms/content/modules/odd_and_even/OddAndEven.php
+++ ulicms/content/modules/odd_and_even/OddAndEven.php
@@ -2,13 +2,13 @@
 
 namespace OddAndEven;
 
-function is_even($val) {
+function isEven($val) {
 	if (! is_numeric ( $val )) {
 		throw new \InvalidArgumentException ( "$val is nut numeric." );
 	}
 	$val = round ( $val );
 	return $val % 2 === 0;
 }
-function is_odd($val) {
-	return ! is_even ( $val );
+function isOdd($val) {
+	return ! isEven ( $val );
 }
