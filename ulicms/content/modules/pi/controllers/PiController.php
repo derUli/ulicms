01d3f4b1be826b0eb4fd041d5aebba60e9f3ed60
Ulrich Schmidt <ulrich1992@web.de>
2019-01-01 19:23:50 +0100

    remove pi package

--- ulicms/content/modules/pi/controllers/PiController.php
+++
@@ -1,6 +0,0 @@
-<?php
-class PiController extends MainClass {
-	public function render() {
-		return pi ();
-	}
-}
--- ulicms/content/modules/pi/tests/PiControllerTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-class PiControllerTest extends \PHPUnit\Framework\TestCase {
-	public function testRender() {
-		$controller = ModuleHelper::getMainController ( "pi" );
-		$this->assertEquals ( "3,1415926535898", $controller->render () );
-	}
-}
