82c2638c0a7e2c60a8d95384b074d0ab6f250c05
Ulrich Schmidt <ulrich1992@web.de>
2019-01-01 18:24:08 +0100

    move push_notifications to own repository

--- ulicms/content/modules/push_notifications/lang/de.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_translation ( "push_notifications", "Push Benachrichtigungen" );
-add_translation ( "site_key", "Site Key" );
-add_translation ( "send_push_notifications", "Push Benachrichtigungen senden" );
-add_translation ( "engagespot_info", "Push Benachrichtigungen für UliCMS basieren auf die Dienste von <a href=\"https://engagespot.co\" target=\"_blank\">Engagespot</a>.
-Nach dem Sie sich dort registriert haben, tragen Sie bitte den Site Key unten ein.
-Das Modul fügt ein Javascript-Snippet in Ihre Webseite ein, welches ein Icon um Push Benachrichtigungen zu abonnieren in Ihre Webseite einbindet.
-Klicken Sie auf den \"Push Benachrichtigungen senden\" button um den Login zum Engagespot Dashboard aufzurufen. Dort können Sie Push Benachrichtigungen an Ihre Abonnenten versenden." );
