b15a36fad9a2eb1d2f46e2f84ea22b9595fa59c0
Ulrich Schmidt <admin@deruli.de>
2019-01-06 00:21:33 +0100

    add icons to more buttons, add permission check

--- ulicms/content/modules/core_content/templates/contents.php
+++ ulicms/content/modules/core_content/templates/contents.php
@@ -9,20 +9,25 @@ if ($permissionChecker->hasPermission("pages") or $permissionChecker->hasPermiss
     
     if ($permissionChecker->hasPermission("pages")) {
         ?>
-	<a href="index.php?action=pages" class="btn btn-default"><?php translate("pages");?></a><br />
+	<a href="index.php?action=pages" class="btn btn-default"><i
+		class="fas fa-book"></i> <?php translate("pages");?></a><br />
 	<?php
     }
     ?>
     <?php
     if ($permissionChecker->hasPermission("comments_manage")) {
         ?>
-	<a href="?action=comments_manage" class="btn btn-default voffset2"><?php translate("comments");?></a><br />
+	<a href="?action=comments_manage" class="btn btn-default voffset2"><i
+		class="fa fa-comments" aria-hidden="true"></i>
+	 <?php translate("comments");?></a><br />
 	<?php
     }
     ?>
     <?php
     if ($permissionChecker->hasPermission("forms")) {
-        ?><a href='?action=forms' class="btn btn-default voffset2"><?php
+        ?><a href='?action=forms' class="btn btn-default voffset2"><i
+		class="fab fa-wpforms" aria-hidden="true"></i>
+         <?php
         
         translate("forms");
         ?></a> <br />
@@ -30,14 +35,16 @@ if ($permissionChecker->hasPermission("pages") or $permissionChecker->hasPermiss
     }
     if ($permissionChecker->hasPermission("banners")) {
         ?>
-	<a href="index.php?action=banner" class="btn btn-default voffset2"><?php translate("advertisements");?></a><br />
+	<a href="index.php?action=banner" class="btn btn-default voffset2"><i
+		class="fas fa-bullhorn"></i> <?php translate("advertisements");?></a><br />
 
 	<?php
     }
     if ($permissionChecker->hasPermission("categories")) {
         ?>
-	<a href="index.php?action=categories" class="btn btn-default voffset2"><?php translate("categories");?></a><br />
-	<br />
+	<a href="index.php?action=categories" class="btn btn-default voffset2"><i
+		class="fa fa-list-alt" aria-hidden="true"></i>
+	 <?php translate("categories");?></a><br /> <br />
 	<?php
     }
     do_event("content_type_list_entry");
--- ulicms/content/modules/core_media/templates/icons.php
+++ ulicms/content/modules/core_media/templates/icons.php
@@ -1,4 +1,6 @@
 <?php
+use UliCMS\Security\PermissionChecker;
+
 $currentAction = BackendHelper::getAction();
 $icons = array(
     "images" => "fas fa-images",
@@ -6,6 +8,11 @@ $icons = array(
     "videos" => "fa fa-file-video",
     "audio" => "fa fa-file-audio"
 );
+
+$icons = array_filter($icons, function ($cssClass, $action) {
+    $permissionChecker = new PermissionChecker(get_user_id());
+    return $permissionChecker->hasPermission($action);
+}, ARRAY_FILTER_USE_BOTH);
 
 $selectedButton = "btn btn-primary";
 $notSelectedButton = "btn btn-default"?>
@@ -16,14 +23,14 @@ $notSelectedButton = "btn btn-default"?>
 	<div class="btn-group" role="group">
 		<a href="<?php echo ModuleHelper::buildActionURL("media");?>"
 			class="btn btn-default btn-back" title="<?php translate("back");?>"><i
-			class="fa fa-arrow-left"></i></a>
+			class="fa fa-arrow-left"></i> <?php translate("back");?></a>
 	</div>
 	<?php foreach($icons as $action=>$cssClass){?>
 	  <div class="btn-group" role="group">
 		<a href="<?php echo ModuleHelper::buildActionURL($action);?>"
 			class="<?php echo $action == $currentAction ? $selectedButton : $notSelectedButton; ?>"
 			title="<?php translate($action);?>"> <i
-			class="<?php echo $cssClass?>"></i>
+			class="<?php echo $cssClass?>"></i> <?php translate($action);?>
 		</a>
 	</div>
 	
