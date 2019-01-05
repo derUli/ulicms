743ce0e807314bd81a1f46ef07ef9a1c36bc0d13
Ulrich Schmidt <admin@deruli.de>
2019-01-05 23:49:22 +0100

    oneclick_upgrade add icons

--- ulicms/content/modules/oneclick_upgrade/admin.php
+++ ulicms/content/modules/oneclick_upgrade/admin.php
@@ -1,28 +1,30 @@
 <?php
-define ( "MODULE_ADMIN_HEADLINE", get_translation ( "oneclick_upgrade" ) . " " . get_translation ( "settings" ) );
-function oneclick_upgrade_admin() {
-	if (get_request_method () == "POST") {
-		Settings::set ( "oneclick_upgrade_skip_kcfinder", intval ( isset ( $_POST ["oneclick_upgrade_skip_kcfinder"] ) ) );
-		Settings::set ( "oneclick_upgrade_channel", strval ( $_POST ["oneclick_upgrade_channel"] ) );
-	}
-	$oneclick_upgrade_skip_kcfinder = intval ( Settings::get ( "oneclick_upgrade_skip_kcfinder" ) );
-	$oneclick_upgrade_channel = strval ( Settings::get ( "oneclick_upgrade_channel" ) );
-	$channels = array (
-			"fast",
-			"slow" 
-	);
-	?>
+define("MODULE_ADMIN_HEADLINE", get_translation("oneclick_upgrade") . " " . get_translation("settings"));
+
+function oneclick_upgrade_admin()
+{
+    if (get_request_method() == "POST") {
+        Settings::set("oneclick_upgrade_skip_kcfinder", intval(isset($_POST["oneclick_upgrade_skip_kcfinder"])));
+        Settings::set("oneclick_upgrade_channel", strval($_POST["oneclick_upgrade_channel"]));
+    }
+    $oneclick_upgrade_skip_kcfinder = intval(Settings::get("oneclick_upgrade_skip_kcfinder"));
+    $oneclick_upgrade_channel = strval(Settings::get("oneclick_upgrade_channel"));
+    $channels = array(
+        "fast",
+        "slow"
+    );
+    ?>
 	<?php
-	if (get_request_method () == "POST") {
-		
-		?>
+    if (get_request_method() == "POST") {
+        
+        ?>
 <div class="alert alert-success alert-dismissable fade in">
 	<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 		<?php translate("changes_was_saved")?>
 		</div>
 <?php
-	}
-	?>
+    }
+    ?>
 <form action="<?php Template::escape(getModuleAdminSelfPath());?>"
 	method="post">
 	<?php csrf_token_html();?>
@@ -45,10 +47,10 @@ function oneclick_upgrade_admin() {
 
 
 	</div>
-	</p>
-	<p>
-		<button type="submit" class="btn btn-default"><?php translate("save");?></button>
-	</p>
+
+
+	<button type="submit" class="btn btn-primary voffset3">
+		<i class="fa fa-save"></i> <?php translate("save");?></button>
 </form>
 <?php
 }
