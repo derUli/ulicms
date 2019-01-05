6a2988ed551d1f4978d2cf8b8e20eecde4858658
Ulrich Schmidt <admin@deruli.de>
2019-01-05 23:22:22 +0100

    add icons

--- ulicms/content/modules/core_system_update/templates/system_update.php
+++ ulicms/content/modules/core_system_update/templates/system_update.php
@@ -4,8 +4,9 @@ if ($permissionChecker->hasPermission("update_system")) {
     if (is_file(Path::resolve("ULICMS_ROOT/update.php"))) {
         ?>
 <p>
-	<a href="../update.php" class="btn btn-warning"><i
-		class="fas fa-running"></i>
+	<a href="../update.php" class="btn btn-warning"> <i class="fas fa-sync"></i>
+
+
 	 <?php translate("run_update");?></a>
 </p>
 <p><?php translate("update_notice");?>
