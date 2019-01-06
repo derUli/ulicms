a34159883b7ef127cafb1c7f69151fa142b012d2
Ulrich Schmidt <admin@deruli.de>
2019-01-06 19:32:48 +0100

    add icons to change_password and reset_password

--- ulicms/admin/inc/change_password.php
+++ ulicms/admin/inc/change_password.php
@@ -1,18 +1,18 @@
 <?php
-if (isset ( $_POST ["change_password"] )) {
-	if (! empty ( $_POST ["password"] ) and $_POST ["password"] === $_POST ["password_repeat"]) {
-		$uid = get_user_id ();
-		changePassword ( $_POST ["password"], $uid );
-		db_query ( "UPDATE " . tbname ( "users" ) . " SET `require_password_change` = 0 where id = $uid" );
-		$_SESSION ["require_password_change"] = 0;
-	} else {
-		echo '<p class="ulicms_error">' . get_translation ( "passwords_not_equal" ) . '</p>';
-	}
+if (isset($_POST["change_password"])) {
+    if (! empty($_POST["password"]) and $_POST["password"] === $_POST["password_repeat"]) {
+        $uid = get_user_id();
+        changePassword($_POST["password"], $uid);
+        db_query("UPDATE " . tbname("users") . " SET `require_password_change` = 0 where id = $uid");
+        $_SESSION["require_password_change"] = 0;
+    } else {
+        echo '<p class="ulicms_error">' . get_translation("passwords_not_equal") . '</p>';
+    }
 }
-if (! $_SESSION ["require_password_change"]) {
-	echo '<script type="text/javascript">location.replace(window.location.href);</script>';
+if (! $_SESSION["require_password_change"]) {
+    echo '<script type="text/javascript">location.replace(window.location.href);</script>';
 } else {
-	?>
+    ?>
 <form id="change_password_form" action="index.php" method="post">
 <?php csrf_token_html ();?>
 	<h1><?php translate ( "change_password" );?></h1>
@@ -20,7 +20,8 @@ if (! $_SESSION ["require_password_change"]) {
 	<strong><?php translate ( "password" );?></strong> <input
 		name="password" id="password" type="password"> <br /> <br /> <strong><?php translate ( "password_repeat" );?> </strong>
 	<input name="password_repeat" id="password_repeat" type="password"> <br /></br>
-	<button type="submit" name="change_password" class="btn btn-warning"><?php translate ( "save_changes" );?></button>
+	<button type="submit" name="change_password" class="btn btn-warning">
+		<i class="fa fa-save"></i> <?php translate ( "save_changes" );?></button>
 </form>
 <br />
 <br />
--- ulicms/admin/inc/reset_password.php
+++ ulicms/admin/inc/reset_password.php
@@ -1,59 +1,60 @@
 <?php
-if (Settings::get ( "disable_password_reset" )) {
-	translate ( "function_is_disabled" );
+if (Settings::get("disable_password_reset")) {
+    translate("function_is_disabled");
 } else {
-	?>
+    ?>
 <?php
-	$messame = null;
-	if (isset ( $_POST ["username"] ) and ! empty ( $_POST ["username"] )) {
-		$username = $_POST ["username"];
-		$user = getUserByName ( $username );
-		if ($user) {
-			$passwordReset = new PasswordReset ();
-			$token = $passwordReset->addToken ( $user ["id"] );
-			$passwordReset->sendMail ( $token, $user ["email"], get_ip (), $user ["firstname"], $user ["lastname"] );
-			$message = get_translation ( "PASSWORD_RESET_SUCCESSFULL" );
-		} else {
-			$message = get_translation ( "NO_SUCH_USER" );
-		}
-	}
-	?>
+    $messame = null;
+    if (isset($_POST["username"]) and ! empty($_POST["username"])) {
+        $username = $_POST["username"];
+        $user = getUserByName($username);
+        if ($user) {
+            $passwordReset = new PasswordReset();
+            $token = $passwordReset->addToken($user["id"]);
+            $passwordReset->sendMail($token, $user["email"], get_ip(), $user["firstname"], $user["lastname"]);
+            $message = get_translation("PASSWORD_RESET_SUCCESSFULL");
+        } else {
+            $message = get_translation("NO_SUCH_USER");
+        }
+    }
+    ?>
 <p>
-	<a href="./" class="btn btn-default btn-back"><?php
-	
-	translate ( "back_to_login" );
-	?></a>
+	<a href="./" class="btn btn-default btn-back"><i
+		class="fa fa-arrow-left"></i> <?php
+    
+    translate("back_to_login");
+    ?></a>
 </p>
 <h1>
 <?php translate("reset_password");?>
 </h1>
 <form action="index.php?reset_password" method="post">
 <?php
-	csrf_token_html ();
-	?><p>
+    csrf_token_html();
+    ?><p>
 		<strong><?php
-	
-	translate ( "username" );
-	?>
+    
+    translate("username");
+    ?>
 	</strong> <br /> <input type="text" name="username" value="" required>
 	</p>
 	<p>
-		<button type="submit" class="btn btn-warning"><?php
-	translate ( "reset_password" );
-	?></button>
+		<button type="submit" class="btn btn-warning"><i class="fa fa-lock"></i> <?php
+    translate("reset_password");
+    ?></button>
 	</p>
 		<?php
-	if ($message) {
-		?>
+    if ($message) {
+        ?>
 	<p class="ulicms_error">
 	<?php
-		echo htmlspecialchars ( $message );
-		?>
+        echo htmlspecialchars($message);
+        ?>
 	</p>
 
 	<?php
-	}
-	?>
+    }
+    ?>
 </form>
 <?php
 } 
