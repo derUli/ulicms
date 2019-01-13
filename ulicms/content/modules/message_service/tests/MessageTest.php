42b8895410ea6da5129822fa2e5e8d89b0d93787
Ulrich Schmidt <admin@deruli.de>
2019-01-13 10:00:28 +0100

    setup test users

--- ulicms/content/modules/message_service/tests/MessageTest.php
+++ ulicms/content/modules/message_service/tests/MessageTest.php
@@ -5,13 +5,61 @@ use UliCMS\Exceptions\NotImplementedException;
 class ArrayHelperTest extends TestCase
 {
 
+    public function tearDown()
+    {
+        Database::deleteFrom("users", "username like 'testuser%'");
+    }
+
+    private function setUpTestuser1()
+    {
+        $user = new User();
+        $user->setUsername("testuser1");
+        $user->setFirstname("Max");
+        $user->setLastname("Muster");
+        $user->setGroupId(1);
+        $user->setPassword("password123");
+        $user->setEmail("max@muster.de");
+        $user->setHomepage("http://www.google.de");
+        $user->setSkypeId("deruliimnetz");
+        $user->setDefaultLanguage("fr");
+        $user->setHTMLEditor("ckeditor");
+        $user->setTwitter("ulicms");
+        $user->setAboutMe("hello world");
+        $user->setLastLogin(time());
+        $user->save();
+        return $user;
+    }
+
+    private function setUpTestuser2()
+    {
+        $user = new User();
+        $user->setUsername("testuser2");
+        $user->setFirstname("John");
+        $user->setLastname("Doe");
+        $user->setGroupId(1);
+        $user->setPassword("password123");
+        $user->setEmail("max@muster.de");
+        $user->setHomepage("http://www.google.de");
+        $user->setSkypeId("deruliimnetz");
+        $user->setDefaultLanguage("fr");
+        $user->setHTMLEditor("ckeditor");
+        $user->setTwitter("ulicms");
+        $user->setAboutMe("hello world");
+        $user->setLastLogin(time());
+        $user->save();
+        return $user;
+    }
+
     public function testCreateUpdateAndDeleteMessage()
     {
+        $testUser1 = $this->setUpTestuser1();
+        $testUser2 = $this->setUpTestuser2();
+        
         // Create
         $message = new Message();
         // FIXME. receiver and sender should be different users
-        $message->setSenderId(1);
-        $message->setReceiverId(1);
+        $message->setSenderId($testUser1->getId());
+        $message->setReceiverId($testUser2->getId());
         $message->setMessage("Foo Bar");
         $message->save();
         
@@ -23,12 +71,12 @@ class ArrayHelperTest extends TestCase
         $this->assertNotNull($message->getID());
         $this->assertEquals("Foo Bar", $message->getMessage());
         
-        $this->assertEquals(1, $message->getSenderId());
-        $this->assertEquals(1, $message->getReceiverId());
+        $this->assertEquals($testUser1->getId(), $message->getSenderId());
+        $this->assertEquals($testUser2->getId(), $message->getReceiverId());
         
-        $this->assertEquals(1, $message->getSender()
+        $this->assertEquals($testUser1->getId(), $message->getSender()
             ->getId());
-        $this->assertEquals(1, $message->getReceiver()
+        $this->assertEquals($testUser2->getId(), $message->getReceiver()
             ->getId());
         
         // TODO: do update and compare values
@@ -44,6 +92,8 @@ class ArrayHelperTest extends TestCase
         $this->assertNull($message->getReceiverId());
         $this->assertNull($message->getSender());
         $this->assertNull($message->getReceiver());
+        
+        $this->tearDown();
     }
 
     public function testGetAll()
