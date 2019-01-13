6f0ee1948e6ead2b1cce645cc3be4e1a51f8c5bb
Ulrich Schmidt <admin@deruli.de>
2019-01-13 17:54:59 +0100

    add js

---
+++ ulicms/content/modules/message_service/js/backend.js
@@ -0,0 +1,10 @@
+$(function() {
+	$("#select-all-receivers").click(function() {
+		$("#receivers option").prop('selected', true);
+		$("#receivers").trigger("change");
+	});
+	$("#select-nothing--receivers").click(function() {
+		$("#receivers option").prop('selected', false);
+		$("#receivers").trigger("change");
+	});
+});
