06ede6f35913a53cf8a5858c5474beebfa1f5449
Ulrich Schmidt <admin@deruli.de>
2019-01-16 22:55:22 +0100

    Deutsche Sprachdatei

---
+++ ulicms/content/modules/sql_studio/lang/de.php
@@ -0,0 +1,11 @@
+<?php
+add_translation("tables", "Tabellen");
+add_translation("sql_editor", "SQL Editor");
+add_translation("execute", "Ausführen");
+add_translation("results", "Ergebnisse");
+add_translation("x_rows_affected", "%x Datensätze betroffen.");
+add_translation("table_name_onclick_action", "Aktion beim Anklicken eines Tabellennamens");
+add_translation("generate_select_statement", "Select Statement generieren");
+add_translation("generate_and_execute_select_statement", "Select Statement generieren und ausführen");
+add_translation("replace_placeholders", "Platzhalter ersetzen");
+add_translation("sql_studio_settings", "SQL Studio Einstellungen");
