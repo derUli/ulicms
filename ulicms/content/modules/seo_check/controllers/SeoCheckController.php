1041090ffca37f55524ecdf0e743998b65b818e8
Ulrich Schmidt <admin@deruli.de>
2019-01-20 20:18:20 +0100

    created seo_check module

---
+++ ulicms/content/modules/seo_check/controllers/SeoCheckController.php
@@ -0,0 +1,28 @@
+<?php
+
+use UliCMS\Security\PermissionChecker;
+
+class SeoCheckController extends Controller
+{
+
+    public function adminMenuEntriesFilter($entries)
+    {
+		$checker = new PermissionChecker(get_user_id());
+		if(!$checker->hasPermission("seo_check")){
+			return $entries;
+		}
+		
+        $entry = new MenuEntry('<i class="fas fa-chart-line"></i> SEO Check', $this->getUrl(), "pagespeed", null, array(), true);
+        $entries = ArrayHelper::insertBefore($entries, count($entries) - 2, $entry);
+				
+        return $entries;
+    }
+	
+	private function getUrl(){
+		return "https://freetools.seobility.net/de/seocheck/check?url=".urlencode(get_protocol_and_domain())."&crawltype=1";
+	}
+	
+	public function settings(){
+		Response::javascriptRedirect($this->getURL());
+	}
+}
