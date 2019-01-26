3be03f1eef11bf3b0f5124d2d539fa1921018ed4
Ulrich Schmidt <admin@deruli.de>
2019-01-26 14:11:38 +0100

    add space

--- ulicms/content/modules/core_package_manager/templates/pkginfo.php
+++ ulicms/content/modules/core_package_manager/templates/pkginfo.php
@@ -31,7 +31,7 @@ if (! $permissionChecker->hasPermission("install_packages")) {
             ?>
 <p>
 	<a href="<?php echo ModuleHelper::buildActionURL("upload_package");?>"
-		class="btn btn-default btn-back"><i class="fa fa-arrow-left"></i><?php translate("back")?></a>
+		class="btn btn-default btn-back"><i class="fa fa-arrow-left"></i> <?php translate("back")?></a>
 </p>
 <h1><?php
             
