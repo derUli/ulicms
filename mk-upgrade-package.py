a16b7c4622a978567f22b09110bbc9d983e6003e
Ulrich Schmidt <admin@deruli.de>
2019-02-09 14:23:49 +0100

    ignore phpunit result cache

--- mk-upgrade-package.py
+++ mk-upgrade-package.py
@@ -37,7 +37,7 @@ def main():
               "config.js", "comments", "*~", ".settings", ".project", ".buildpath",
               "tests", "run-tests.sh", "run-tests.bat",
               "run-tests.xampp.mac.sh", ".pydevproject", "CMSConfig.php", "log",
-              "configurations")
+              "configurations", ".phpunit.result.cache")
 
     IGNORE_PATTERNS = shutil.ignore_patterns(*ignore)
     if args.delete and os.path.exists(target):
