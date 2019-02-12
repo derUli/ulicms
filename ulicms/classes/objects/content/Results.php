6364e3d49808f49e55c263c4f73c491aca45baf7
Ulrich Schmidt <admin@deruli.de>
2019-02-12 13:24:37 +0100

    Update Results.php

--- ulicms/classes/objects/content/Results.php
+++ ulicms/classes/objects/content/Results.php
@@ -63,7 +63,7 @@ function ExceptionResult($message, $status = 500) {
 	header ( "Content-Type: text/html; charset=UTF-8" );
 	
 	if ($type) {
-		header ( 'Content-Type: $type' );
+		header ( "Content-Type: $type" );
 	}
 	header ( "Content-length: $size" );
 	
