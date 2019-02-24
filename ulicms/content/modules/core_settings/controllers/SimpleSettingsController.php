0650b3c61da8aef4b605620bda4de0c3e5f012c2
Ulrich Schmidt <admin@deruli.de>
2019-02-24 12:58:49 +0100

    Zeitzonenliste entfernt, nutze DateTimeZonelist::Identifiers

--- ulicms/content/modules/core_settings/controllers/SimpleSettingsController.php
+++ ulicms/content/modules/core_settings/controllers/SimpleSettingsController.php
@@ -1,12 +1,10 @@
 <?php
 
-class SimpleSettingsController extends Controller
-{
+class SimpleSettingsController extends Controller {
 
     private $moduleName = "core_settings";
 
-    public function savePost()
-    {
+    public function savePost() {
         do_event("before_safe_simple_settings");
         Settings::set("homepage_owner", $_POST["homepage_owner"]);
         Settings::set("language", $_POST["language"]);
@@ -16,25 +14,25 @@ class SimpleSettingsController extends Controller
         Settings::set("max_news", (int) $_POST["max_news"]);
         Settings::set("timezone", $_POST["timezone"]);
         Settings::set("robots", $_POST["robots"]);
-        
-        if (! isset($_POST["disable_password_reset"])) {
+
+        if (!isset($_POST["disable_password_reset"])) {
             Settings::set("disable_password_reset", "disable_password_reset");
         } else {
             Settings::delete("disable_password_reset");
         }
-        
-        
+
+
         do_event("after_safe_simple_settings");
-        
+
         // if called by ajax return no content to improve performance
-        if(Request::isAjaxRequest()){
+        if (Request::isAjaxRequest()) {
             HTTPStatusCodeResult(HttpStatusCode::OK);
         }
         Request::redirect(ModuleHelper::buildActionURL("settings_simple"));
     }
 
-    public function getTimezones()
-    {
-        return file(ModuleHelper::buildModuleRessourcePath($this->moduleName, "data/timezones.txt"));
+    public function getTimezones() {
+        return DateTimeZone::listIdentifiers();
     }
+
 }
