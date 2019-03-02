e04f217ddc46cd4343bc7ff78f285911dbdc6eba
Ulrich Schmidt <admin@deruli.de>
2019-03-02 15:08:04 +0100

    Update admin.js

--- ulicms/content/modules/core_comments/js/admin.js
+++ ulicms/content/modules/core_comments/js/admin.js
@@ -8,6 +8,19 @@ $(function () {
         $.ajax({
             url: url,
             success: function (result) {
+                var unread = $(event.target).closest(".unread")
+                if (unread.length) {
+                    unread.removeClass("unread");
+                    var commentCounter = $(".comment-counter .count");
+                    var newCount = commentCounter.data("count") - 1;
+                    commentCounter.data("count", newCount);
+                    commentCounter.text(newCount);
+
+                    if (newCount <= 0) {
+                        commentCounter.hide();
+                    }
+
+                }
                 // show the response to the user in an bootbox alert
                 bootbox.alert({
                     message: result,
