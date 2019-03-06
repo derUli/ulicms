5a9e57f7fb9eebab303077adacb3b650552b97ec
Ulrich Schmidt <admin@deruli.de>
2019-03-06 20:52:34 +0100

    oneclick_upgrade

--- ulicms/content/modules/oneclick_upgrade/templates/Dashboard.php
+++ ulicms/content/modules/oneclick_upgrade/templates/Dashboard.php
@@ -1,14 +1,15 @@
 <?php
-$permissionChecker = new ACL ();
-if ($permissionChecker->hasPermission ( "update_system" )) {
-	$version = ControllerRegistry::get ( "CoreUpgradeController" )->checkForUpgrades ();
-	if ($version) {
-		?>
+$permissionChecker = new ACL();
+if ($permissionChecker->hasPermission("update_system")) {
+    $version = ControllerRegistry::get("CoreUpgradeController")->checkForUpgrades();
+    if ($version) {
+        ?>
 <h2 class="accordion-header"><?php translate("ONECLICK_UPGRADE");?></h2>
 <div class="accordion-content">
-	<?php translate("an_upgrade_is_available", array("%version%" => $version));?> 
-		[<a href="<?= ModuleHelper::buildActionURL("UpgradeCheck");?>"><?php translate("show_more");?></a>]
+	<p><?php translate("an_upgrade_is_available", array("%version%" => $version));?> </p>
+		<p><a href="<?=ModuleHelper::buildActionURL("UpgradeCheck");?>" class="btn btn-info">
+		<i class="fas fa-info-circle"></i> <?php translate("show_more");?></a></p>
 </div>
 <?php
-	}
+}
 }
