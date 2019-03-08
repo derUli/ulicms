3b7dc5dfa3ac325269a058a5d244416b2daa2ec9
Ulrich Schmidt <admin@deruli.de>
2019-03-08 21:49:28 +0100

    Update db-track-reset-for-upgrade-to-2019.3.sql

--- doc/db-track-reset-for-upgrade-to-2019.3.sql
+++ doc/db-track-reset-for-upgrade-to-2019.3.sql
@@ -2,5 +2,4 @@
 -- Don't run this script you are already using UliCMS 2019.2.1
 
 DELETE FROM `{prefix}dbtrack` WHERE component = 'core';
-
 INSERT INTO `{prefix}dbtrack` (`id`, `component`, `name`, `date`) VALUES (NULL, 'core', '001.sql', CURRENT_TIMESTAMP);
