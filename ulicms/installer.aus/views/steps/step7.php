dd46f82a9e3d618822155f9edbffd49032997d75
Ulrich Schmidt <admin@deruli.de>
2019-03-09 22:48:51 +0100

    Dieser Vorgang kann einige Minuten dauern.

--- ulicms/installer.aus/views/steps/step7.php
+++ ulicms/installer.aus/views/steps/step7.php
@@ -1,4 +1,7 @@
+<div class="alert alert-warning">
+    <?php echo TRANSLATION_THIS_PROCEDUDRE_WILL_TAKE_SOME_MINUTES; ?></p>
+</div>
 <form id="setup-database" action="#" method="get">
-	<input type="submit" class="btn btn-primary"
-		value="<?php echo TRANSLATION_BUILD_DATABASE;?>">
+    <input type="submit" class="btn btn-primary"
+           value="<?php echo TRANSLATION_BUILD_DATABASE; ?>">
 </form>
