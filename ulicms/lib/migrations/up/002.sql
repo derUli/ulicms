00361920a95d903267e3e440cd7519cdd397d088
Ulrich Schmidt <admin@deruli.de>
2019-03-09 10:05:29 +0100

    if exists works only in procedures in mysql

--- ulicms/lib/migrations/up/002.sql
+++ ulicms/lib/migrations/up/002.sql
@@ -1,3 +1,8 @@
+DELIMITER $$
+
+CREATE PROCEDURE prepareDbFor2019_2()
+BEGIN
+
 IF EXISTS ( SELECT * FROM information_schema.columns WHERE table_name = '{prefix}users' AND column_name = 'skype_id' AND table_schema = DATABASE() ) THEN
     ALTER TABLE `{prefix}users` DROP COLUMN `skype_id`;
 END IF;
@@ -13,3 +18,11 @@ END IF;
 IF NOT EXISTS ( SELECT * FROM information_schema.columns WHERE table_name = '{prefix}comments' AND column_name = 'read' AND table_schema = DATABASE() ) THEN
     ALTER TABLE `{prefix}comments` add COLUMN `read` tinyint(1) NOT NULL DEFAULT '0';
 END IF;
+
+END $$
+
+DELIMITER ;
+
+CALL prepareDbFor2019_2;
+
+DROP PROCEDURE prepareDbFor2019_2;
