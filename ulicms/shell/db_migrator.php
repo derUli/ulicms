4602ca1f84aa00a2f4573d67eca1699c8e4e4e42
Ulrich Schmidt <admin@deruli.de>
2019-03-09 23:48:21 +0100

    Update db_migrator.php

--- ulicms/shell/db_migrator.php
+++ ulicms/shell/db_migrator.php
@@ -1,9 +1,7 @@
 #!/usr/bin/env php
 <?php
-error_reporting(E_ALL);
 
-function usage()
-{
+function usage() {
     echo "dg_migrator - Apply database migrations\n";
     echo "UliCMS Version " . cms_version() . "\n";
     echo "Copyright (C) 2018 by Ulrich Schmidt";
@@ -50,7 +48,7 @@ if ($command == "reset") {
     exit();
 }
 if ($command == "up") {
-    if (! $component or ! $directory) {
+    if (!$component or ! $directory) {
         usage();
     }
     $folder = Path::resolve($directory . "/up");
@@ -64,7 +62,7 @@ if ($command == "up") {
 }
 
 if ($command == "down") {
-    if (! $component or ! $directory) {
+    if (!$component or ! $directory) {
         usage();
     }
     $folder = Path::resolve($directory . "/down");
