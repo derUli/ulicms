7a7d538a9625781bd1598e3de2aa2e4777112c93
Ulrich Schmidt <admin@deruli.de>
2019-03-14 07:30:17 +0100

    refactor updatePost of User Controller

--- ulicms/content/modules/core_users/controllers/UserController.php
+++ ulicms/content/modules/core_users/controllers/UserController.php
@@ -1,5 +1,7 @@
 <?php
 
+use UliCMS\Security\PermissionChecker;
+
 class UserController extends Controller {
 
     private $logger;
@@ -58,97 +60,69 @@ class UserController extends Controller {
         Request::redirect(ModuleHelper::buildActionURL("admins"));
     }
 
-    /*
-      public function updatePost() {
-      $id = intval($_POST["id"]);
-      $user = new User($id);
-      if (!$user->getId() == $id) {
-      ExceptionResult(get_translation("not_found"), HttpStatusCode::NOT_FOUND);
-      }
-      }
-
-     */
-
     public function updatePost() {
-        $acl = new ACL();
-        if ($acl->hasPermission("users_edit") or $_POST["id"] == $_SESSION["login_id"]) {
+        $permissionChecker = new PermissionChecker(get_user_id());
+        if ($permissionChecker->hasPermission("users_edit") or $_POST["id"] == $_SESSION["login_id"]) {
             $id = intval($_POST["id"]);
-            $username = db_escape($_POST["username"]);
-            $lastname = db_escape($_POST["lastname"]);
-            $firstname = db_escape($_POST["firstname"]);
-            $email = db_escape($_POST["email"]);
+            $username = $_POST["username"];
+            $lastname = $_POST["lastname"];
+            $firstname = $_POST["firstname"];
             $password = $_POST["password"];
-            // User mit eingeschränkten Rechten darf sich nicht selbst zum Admin machen können
-            if ($acl->hasPermission("users")) {
-                $admin = intval(isset($_POST["admin"]));
-                if (isset($_POST["group_id"])) {
-                    $group_id = $_POST["group_id"];
-                    if (!is_numeric($group_id)) {
-                        $group_id = "NULL";
-                    } else {
-                        $group_id = intval($group_id);
-                    }
-                } else {
-                    $group_id = $_SESSION["group_id"];
-                }
-            } else {
-                $user = getUserById($id);
-                $admin = $user["admin"];
-                $group_id = $user["group_id"];
-                if (is_null($group_id)) {
-                    $group_id = "NULL";
-                }
+            $email = $_POST["email"];
+            $default_language = StringHelper::isNotNullOrWhitespace($_POST["default_language"]) ? $_POST["default_language"] : null;
+            $admin = boolval(isset($_POST["admin"]));
+            $locked = boolval(isset($_POST["locked"]));
+
+            $homepage = $_POST["homepage"];
+            $about_me = $_POST["about_me"];
+            $html_editor = $_POST["html_editor"];
+            $group_id = is_numeric($_POST["group_id"]) ? intval($_POST["group_id"]) : null;
+
+            if ($group_id <= 0) {
+                $group_id = null;
             }
-            // FIXME: Das SQL muss raus. Stattdessen das User-Model zum Speichern nutzen.
-            $homepage = db_escape($_POST["homepage"]);
-            $about_me = db_escape($_POST["about_me"]);
-            $html_editor = db_escape($_POST["html_editor"]);
+
             $require_password_change = intval(isset($_POST["require_password_change"]));
-            $locked = intval(isset($_POST["locked"]));
 
-            $default_language = StringHelper::isNotNullOrWhitespace($_POST["default_language"]) ? "'" . Database::escapeValue($_POST["default_language"]) . "'" : "NULL";
-
-            do_event("before_edit_user");
-            $sql = "UPDATE " . tbname("users") . " SET username = '$username', `group_id` = " . $group_id . ", `admin` = $admin, firstname='$firstname',
-      lastname='$lastname', email='$email',
-      about_me = '$about_me', html_editor='$html_editor', require_password_change='$require_password_change', `locked`='$locked', `homepage` = '$homepage' , `default_language` = $default_language WHERE id=$id";
-
-            db_query($sql);
-
-            // save secondary groups
             $user = new User($id);
-            $secondary_groups = $_POST["secondary_groups"];
+            if (!$user->getId() == $id) {
+                ExceptionResult(get_translation("not_found"), HttpStatusCode::NOT_FOUND);
+            }
+            $user->setLastname($lastname);
+            $user->setFirstname($firstname);
+            $user->setPassword($password);
+            $user->setEmail($email);
+            $user->setDefaultLanguage($default_language);
 
-            $user->setSecondaryGroups(array());
-            if (is_array($secondary_groups)) {
-                foreach ($secondary_groups as $group) {
-                    $user->addSecondaryGroup(new Group($group));
+            if ($permissionChecker->hasPermission("users_edit")) {
+                $user->setUsername($username);
+                $user->setAdmin($admin);
+                $user->setLocked($locked);
+                $user->setGroupid($group_id);
+                $user->setSecondaryGroups(array());
+
+                $secondary_groups = $_POST["secondary_groups"];
+                if (is_array($secondary_groups)) {
+                    foreach ($secondary_groups as $group) {
+                        $user->addSecondaryGroup(new Group($group));
+                    }
                 }
             }
 
+            $user->setRequirePasswordChange($require_password_change);
+
+            $user->setHomepage($homepage);
+            $user->setAboutMe($about_me);
+            $user->setHTMLEditor($html_editor);
             $user->save();
 
-            $user = getUserById(get_user_id());
-            $name = isset($user["username"]) ? $user["username"] : AuditLog::UNKNOWN;
-
-            if ($this->logger) {
-                $this->logger->debug("User $name - Edited user ({$username})");
-            }
-
-            if (!empty($password)) {
-                changePassword($password, $id);
-                if ($this->logger) {
-                    $this->logger->debug("User $name - Changed password of user ({$username})");
-                }
-            }
-
-            do_event("after_edit_user");
-
-            if (!$acl->hasPermission("users")) {
+            if (!$permissionChecker->hasPermission("users")) {
                 Request::redirect("index.php");
             } else {
                 Request::redirect(ModuleHelper::buildActionURL("admins"));
             }
+        } else {
+            return ExceptionResult(get_translation("forbidden"), HttpStatusCode::FORBIDDEN);
         }
     }
 
