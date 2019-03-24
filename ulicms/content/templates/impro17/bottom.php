4d155ce3b694bc3305bb576841f5cbd27344407b
Ulrich Schmidt <admin@deruli.de>
2019-03-24 11:03:52 +0100

    css changes

--- ulicms/content/templates/impro17/bottom.php
+++ ulicms/content/templates/impro17/bottom.php
@@ -1,40 +1,27 @@
-<?php Template::comments();?>
+<?php Template::comments(); ?>
 <div class="advertisement">
-<?php random_banner();?>
+    <?php random_banner(); ?>
 </div>
 </main>
 </div>
 <footer class="footer">
-	<div class="row">
-		<div class="col-sm-6">
-			<p>&copy;
-    <?php if(date("Y") > 2016){?>
-    2016 -
-    <?php
-    }
-    ?>
-    <?php year();?> by <?php homepage_owner();?>
-	</p>
-		</div>
-		<div class="col-sm-6">
-			<!--
-			<p class="imprint-right">
-
-				<a href="#"><?php translate("imprint");?></a>
-			</p>
-			-->
-		</div>
-	</div>
+    <p>&copy;
+        <?php if (date("Y") > 2016) { ?>
+            2016 -
+        <?php } ?>
+        <?php year(); ?> by <?php homepage_owner(); ?>
+    </p>
 </footer>
-<?php Template::footer();?>
+<?php Template::footer(); ?>
 </div>
 <?php
 $translation = new JSTranslation();
 $translation->addKey("menu");
 $translation->renderJS();
+
+enqueueScriptFile(getTemplateDirPath(get_theme()) . "main.js");
+combinedScriptHtml();
 ?>
-<script type="text/javascript"
-	src="<?php echo getTemplateDirPath(get_theme());?>main.js"></script>
 </body>
 </html>
 <!--  <?php echo date("r"); ?>-->
