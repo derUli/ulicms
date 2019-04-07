d8bbde46e6059996d16e8e0e4ac0fe389235c06d
Ulrich Schmidt <admin@deruli.de>
2019-04-07 11:51:00 +0200

    Update global.js

--- ulicms/admin/scripts/global.js
+++ ulicms/admin/scripts/global.js
@@ -44,7 +44,7 @@ $(function () {
         showText: true, // shows the text tips
         animate: true, // whether or not to animate the progress bar on input blur/focus
         animateSpeed: 'fast', // the above animation speed
-        username: $("[name=username]"), // select the username field (selector or jQuery instance) for better password checks
+        username: $("[name=username]").length ? $("[name=username]") : false, // select the username field (selector or jQuery instance) for better password checks
         usernamePartialMatch: true, // whether to check for username partials
         minimumLength: 4 // minimum password length (below this threshold, the score is 0)
     });
