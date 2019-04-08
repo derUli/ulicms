69c7f424605483054767bba7f0dce3f0863bf75b
Ulrich Schmidt <admin@deruli.de>
2019-04-08 18:17:27 +0200

    hide forms stuff

--- ulicms/content/modules/core_content/templates/contents.php
+++ ulicms/content/modules/core_content/templates/contents.php
@@ -2,54 +2,54 @@
 $permissionChecker = new ACL();
 if ($permissionChecker->hasPermission("pages") or $permissionChecker->hasPermission("banners") or $permissionChecker->hasPermission("categories") or $permissionChecker->hasPermission("export") or $permissionChecker->hasPermission("forms")) {
     ?>
-<h2><?php translate("contents");?></h2>
-<p>
-	<strong><?php translate("select_content_type");?> </strong><br /> <br />
-	<?php
-    
-    if ($permissionChecker->hasPermission("pages")) {
-        ?>
-	<a href="index.php?action=pages" class="btn btn-default"><i
-		class="fas fa-book"></i> <?php translate("pages");?></a><br />
-	<?php
-    }
-    ?>
-    <?php
-    if ($permissionChecker->hasPermission("comments_manage")) {
-        ?>
-	<a href="?action=comments_manage" class="btn btn-default voffset2"><i
-		class="fa fa-comments" aria-hidden="true"></i>
-	 <?php translate("comments");?></a><br />
-	<?php
-    }
-    ?>
-    <?php
-    if ($permissionChecker->hasPermission("forms")) {
-        ?><a href='?action=forms' class="btn btn-default voffset2"><i
-		class="fab fa-wpforms" aria-hidden="true"></i>
-         <?php
-        
-        translate("forms");
-        ?></a> <br />
-				<?php
-    }
-    if ($permissionChecker->hasPermission("banners")) {
-        ?>
-	<a href="index.php?action=banner" class="btn btn-default voffset2"><i
-		class="fas fa-bullhorn"></i> <?php translate("advertisements");?></a><br />
+    <h2><?php translate("contents"); ?></h2>
+    <p>
+        <strong><?php translate("select_content_type"); ?> </strong><br /> <br />
+        <?php
+        if ($permissionChecker->hasPermission("pages")) {
+            ?>
+            <a href="index.php?action=pages" class="btn btn-default"><i
+                    class="fas fa-book"></i> <?php translate("pages"); ?></a><br />
+                <?php
+            }
+            ?>
+            <?php
+            /* if ($permissionChecker->hasPermission("comments_manage")) {
+              ?>
+              <a href="?action=comments_manage" class="btn btn-default voffset2"><i
+              class="fa fa-comments" aria-hidden="true"></i>
+              <?php translate("comments");?></a><br />
+              <?php
+              }
 
-	<?php
+             */
+            ?>
+            <?php
+            if ($permissionChecker->hasPermission("forms")) {
+                ?><a href='?action=forms' class="btn btn-default voffset2"><i
+                    class="fab fa-wpforms" aria-hidden="true"></i>
+                    <?php
+                    translate("forms");
+                    ?></a> <br />
+                    <?php
+            }
+            if ($permissionChecker->hasPermission("banners")) {
+                ?>
+            <a href="index.php?action=banner" class="btn btn-default voffset2"><i
+                    class="fas fa-bullhorn"></i> <?php translate("advertisements"); ?></a><br />
+
+        <?php
     }
     if ($permissionChecker->hasPermission("categories")) {
         ?>
-	<a href="index.php?action=categories" class="btn btn-default voffset2"><i
-		class="fa fa-list-alt" aria-hidden="true"></i>
-	 <?php translate("categories");?></a><br /> <br />
-	<?php
+            <a href="index.php?action=categories" class="btn btn-default voffset2"><i
+                    class="fa fa-list-alt" aria-hidden="true"></i>
+        <?php translate("categories"); ?></a><br /> <br />
+                <?php
+            }
+            do_event("content_type_list_entry");
+            ?>
+        <?php
+    } else {
+        noPerms();
     }
-    do_event("content_type_list_entry");
-    ?>
-	<?php
-} else {
-    noPerms();
-}
--- ulicms/content/modules/core_content/templates/icons.php
+++ ulicms/content/modules/core_content/templates/icons.php
@@ -1,11 +1,11 @@
 <?php
+
 use UliCMS\Security\PermissionChecker;
 
 $currentAction = BackendHelper::getAction();
 $icons = array(
     "pages" => "fas fa-book",
     "comments_manage" => "fa fa-comment",
-    "forms" => "fab fa-wpforms",
     "banner" => "fas fa-bullhorn",
     "categories" => "fa fa-list-alt"
 );
@@ -18,7 +18,7 @@ $icons = array_filter($icons, function ($cssClass, $action) {
         "banner" => "banners",
         "categories" => "categories"
     );
-    
+
     $permissionChecker = new PermissionChecker(get_user_id());
     return $permissionChecker->hasPermission($permissions[$action]);
 }, ARRAY_FILTER_USE_BOTH);
@@ -28,24 +28,25 @@ $specialLabels = array(
 );
 
 $selectedButton = "btn btn-primary";
-$notSelectedButton = "btn btn-default"?>
+$notSelectedButton = "btn btn-default"
+?>
 
 <div class="btn-toolbar" role="toolbar"
-	aria-label="Toolbar with button groups">
+     aria-label="Toolbar with button groups">
 
-	<div class="btn-group" role="group">
-		<a href="<?php echo ModuleHelper::buildActionURL("contents");?>"
-			class="btn btn-default btn-back"><i class="fa fa-arrow-left"></i>
-		<?php translate("back")?></a>
+    <div class="btn-group" role="group">
+        <a href="<?php echo ModuleHelper::buildActionURL("contents"); ?>"
+           class="btn btn-default btn-back"><i class="fa fa-arrow-left"></i>
+    <?php translate("back") ?></a>
 
-	</div>
-	<?php foreach($icons as $action=>$cssClass){?>
-	  <div class="btn-group" role="group">
-		<a href="<?php echo ModuleHelper::buildActionURL($action);?>"
-			class="<?php echo $action == $currentAction ? $selectedButton : $notSelectedButton; ?>">
-			<i class="<?php echo $cssClass?>"></i><span class="hide-on-820"> <?php (isset($specialLabels[$action]) ? esc($specialLabels[$action]) : translate($action));?></span>
-		</a>
-	</div>
-	
-	<?php }?>
-	</div>
+    </div>
+<?php foreach ($icons as $action => $cssClass) { ?>
+        <div class="btn-group" role="group">
+            <a href="<?php echo ModuleHelper::buildActionURL($action); ?>"
+               class="<?php echo $action == $currentAction ? $selectedButton : $notSelectedButton; ?>">
+                <i class="<?php echo $cssClass ?>"></i><span class="hide-on-820"> <?php (isset($specialLabels[$action]) ? esc($specialLabels[$action]) : translate($action)); ?></span>
+            </a>
+        </div>
+
+<?php } ?>
+</div>
