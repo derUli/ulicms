308c30562a6fd5eaaee7c0198affbbfd9c99cfb9
Ulrich Schmidt <admin@deruli.de>
2019-04-13 18:20:20 +0200

    fix xss

--- ulicms/admin/inc/registerform.php
+++ ulicms/admin/inc/registerform.php
@@ -2,10 +2,10 @@
 if (Settings::get("visitors_can_register") == "off" or ! Settings::get("visitors_can_register")) {
     translate("FUNCTION_IS_DISABLED");
 } else {
-    
+
     $language = getSystemLanguage();
     $checkbox = new PrivacyCheckbox($language);
-    
+
     $errors = false;
     if (isset($_POST["register_user"])) {
         if ($checkbox->isEnabled() and ! $checkbox->isChecked()) {
@@ -20,9 +20,9 @@ if (Settings::get("visitors_can_register") == "off" or ! Settings::get("visitors
             do_event("before_user_registration");
             adduser($_POST["admin_username"], $_POST["admin_lastname"], $_POST["admin_firstname"], $_POST["admin_email"], $_POST["admin_password"], false);
             do_event("after_user_registration");
-            
+
             echo "<p style='color:green;'>" . get_translation("REGISTRATION_SUCCESSFUL") . "</p>";
-            if (! empty($_REQUEST["go"])) {
+            if (!empty($_REQUEST["go"])) {
                 $go = htmlspecialchars($_REQUEST["go"]);
             } else {
                 $go = "index.php";
@@ -31,64 +31,58 @@ if (Settings::get("visitors_can_register") == "off" or ! Settings::get("visitors
         }
     }
     ?>
-<?php
-    
+    <?php
     do_event("before_register_form_title");
     ?>
-<p>
-	<a href="./" class="btn btn-default btn-back"><i
-		class="fa fa-arrow-left"></i> <?php
-    
-    translate("back_to_login");
-    ?></a>
-</p>
-<h1>
-<?php translate("registration");?>
-</h1>
+    <p>
+        <a href="./" class="btn btn-default btn-back"><i
+                class="fa fa-arrow-left"></i> <?php
+                translate("back_to_login");
+                ?></a>
+    </p>
+    <h1>
+        <?php translate("registration"); ?>
+    </h1>
 
-<?php
-    
+    <?php
     do_event("before_register_form");
     ?>
-<form action="index.php?register=register" method="post">
-<?php
-    
-    csrf_token_html();
-    ?>
-	<input type="hidden" name="register_user" value="add_admin">
-	<?php
-    
-    if (! empty($_REQUEST["go"])) {
+    <form action="index.php?register=register" method="post">
+        <?php
+        csrf_token_html();
         ?>
-	<input type="hidden" name="go"
-		value='<?php
-        echo htmlspecialchars($_REQUEST["go"])?>'>
-	<?php
-    }
-    ?>
-	<strong><?php translate("username");?>
-	</strong><br /> <input type="text" required="required"
-		name="admin_username" value=""> <br /> <strong><?php translate("lastname")?>
-	</strong><br /> <input type="text" required="required"
-		name="admin_lastname" value=""> <br /> <strong><?php translate("firstname");?>
-	</strong><br /> <input type="text" required="required"
-		name="admin_firstname" value=""><br /> <strong><?php translate("email");?>
-	</strong><br /> <input type="email" required="required"
-		name="admin_email" value=""><br /> <strong><?php translate("password");?>
-	</strong><br /> <input type="password" required="required"
-		name="admin_password" value=""><br /> <strong><?php translate("password_repeat");?>
-	</strong><br /> <input type="password" required="required"
-		name="admin_password_repeat" value="">
-		<?php do_event ( "register_form_field" );?>
-		<div class="privacy-checkbox">
-		<?php
-    echo $checkbox->render();
-    ?></div>
-	<p class="voffset3">
-		<button type="submit" class="btn btn-primary">
-			<i class="fas fa-user-plus"></i> <?php translate("register");?></button>
-	</p>
-</form>
-<?php
+        <input type="hidden" name="register_user" value="add_admin">
+        <?php
+        if (!empty($_REQUEST["go"])) {
+            ?>
+            <input type="hidden" name="go"
+                   value="<?php esc($_REQUEST["go"]) ?>">
+                   <?php
+               }
+               ?>
+        <strong><?php translate("username"); ?>
+        </strong><br /> <input type="text" required="required"
+                               name="admin_username" value=""> <br /> <strong><?php translate("lastname") ?>
+        </strong><br /> <input type="text" required="required"
+                               name="admin_lastname" value=""> <br /> <strong><?php translate("firstname"); ?>
+        </strong><br /> <input type="text" required="required"
+                               name="admin_firstname" value=""><br /> <strong><?php translate("email"); ?>
+        </strong><br /> <input type="email" required="required"
+                               name="admin_email" value=""><br /> <strong><?php translate("password"); ?>
+        </strong><br /> <input type="password" required="required"
+                               name="admin_password" value=""><br /> <strong><?php translate("password_repeat"); ?>
+        </strong><br /> <input type="password" required="required"
+                               name="admin_password_repeat" value="">
+                               <?php do_event("register_form_field"); ?>
+        <div class="privacy-checkbox voffset3">
+            <?php
+            echo $checkbox->render();
+            ?></div>
+        <p class="voffset3">
+            <button type="submit" class="btn btn-primary">
+                <i class="fas fa-user-plus"></i> <?php translate("register"); ?></button>
+        </p>
+    </form>
+    <?php
     do_event("after_register_form");
 }
