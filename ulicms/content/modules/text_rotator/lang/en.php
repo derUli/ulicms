01ee2fcaa1f8ec990c48bc7007f9e3a1efa9d34d
Ulrich Schmidt <admin@deruli.de>
2019-05-05 21:32:21 +0200

    form wip

--- ulicms/content/modules/text_rotator/lang/en.php
+++ ulicms/content/modules/text_rotator/lang/en.php
@@ -2,3 +2,7 @@
 
 add_translation("text_rotator", "Text Rotator");
 add_translation("create_new_text_rotator", "Create Text Rotator");
+add_translation("words", "Words");
+add_translation("separator", "Separator");
+add_translation("speed", "Speed");
+add_translation("animation", "Animation");
--- ulicms/content/modules/text_rotator/templates/form.php
+++ ulicms/content/modules/text_rotator/templates/form.php
@@ -1,9 +1,78 @@
 <a href="<?php esc(ModuleHelper::buildAdminURL(TextRotatorController::MODULE_NAME)); ?>" class="btn btn-default btn-back">
     <i
         class="fa fa-arrow-left"></i>
-    <?php
-    translate("back");
-    ?></a>
+        <?php
+        translate("back");
+        ?></a>
 <h1><?php translate("create_new_text_rotator"); ?></h1>
 <?php
-echo UliCMS\HTML\Alert::info("Coming Soon", "voffset3");
+echo ModuleHelper::buildMethodCallForm(TextRotatorController::class, "save");
+
+/*
+  private $animation;
+  private $separator = ",";
+  private $speed = 2000;
+  private $words = null;
+
+ */
+?>
+<div class="form-group">
+    <label for="words">
+        <?php translate("words"); ?>
+    </label>
+    <?php
+    echo UliCMS\HTML\Input::TextBox("words", "", "text",
+            array(
+                "required" => "required",
+                "placeholder" => get_translation("words"),
+                "maxlength" => "65536"
+    ));
+    ?>
+</div>
+<div class="form-group">
+    <label for="separator">
+        <?php translate("separator"); ?>
+    </label>
+    <?php
+    echo UliCMS\HTML\Input::TextBox("separator", "", "text",
+            array(
+                "required" => "required",
+                "placeholder" => get_translation("separator"),
+                "maxlength" => 5
+    ));
+    ?>
+</div>
+<div class="form-group">
+    <label for="speed">
+        <?php translate("speed"); ?>
+    </label>
+    <?php
+    echo UliCMS\HTML\Input::TextBox("speed", "", "number",
+            array(
+                "min" => 1,
+                "max" => 9999,
+                "step" => 1,
+                "required" => "required",
+                "placeholder" => get_translation("speed")
+    ));
+    ?>
+</div>
+<div class="form-group">
+    <label for="animation">
+        <?php translate("animation"); ?>
+    </label>
+    <?php
+    echo UliCMS\HTML\Input::TextBox("animation", "", "text",
+            array(
+                "required" => "required",
+                "placeholder" => get_translation("animation")
+    ));
+    ?>
+</div>
+<button type="submit" class="btn btn-primary">
+    <i class="fa fa-save"></i>
+    <?php
+    translate("create");
+    ?></button>
+<?php
+echo ModuleHelper::endForm();
