dbd18213b11a7f148aba0281cf5830ad4c8d75e0
Ulrich Schmidt <admin@deruli.de>
2019-05-07 18:52:05 +0200

    Text Rotator kann erstellt werden

--- ulicms/content/modules/text_rotator/controllers/TextRotatorController.php
+++ ulicms/content/modules/text_rotator/controllers/TextRotatorController.php
@@ -13,4 +13,29 @@ class TextRotatorController extends MainClass {
                         self::MODULE_NAME, "list.php");
     }
 
+    public function savePost() {
+        $id = Request::getVar("id", null, "int");
+
+        $rotating_text = new RotatingText();
+        if ($id) {
+            $rotating_text->loadByID($id);
+        }
+        $words = Request::getVar("words", "", "str");
+        $separator = Request::getVar("separator", ",", "str");
+        $speed = Request::getVar("speed", 2000, "int");
+        $animation = Request::getVar("animation", "", "str");
+
+        $rotating_text->setWords($words);
+        $rotating_text->setSeparator($separator);
+        $rotating_text->setSpeed($speed);
+        $rotating_text->setAnimation($animation);
+
+        $rotating_text->save();
+        Response::redirect(
+                ModuleHelper::buildAdminURL(
+                        self::MODULE_NAME
+                )
+        );
+    }
+
 }
