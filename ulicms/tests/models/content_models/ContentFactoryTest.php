825a41e7aafe680aeb55e52de01cd41a4909d6fb
Ulrich Schmidt <admin@deruli.de>
2019-05-21 18:52:19 +0200

    Update ContentFactoryTest.php

--- ulicms/tests/models/content_models/ContentFactoryTest.php
+++ ulicms/tests/models/content_models/ContentFactoryTest.php
@@ -2,7 +2,7 @@
 
 class ContentFactoryTest extends \PHPUnit\Framework\TestCase {
 
-    public function testCreateModels() {
+    public function testGetAllbyType() {
         $types = TypeMapper::getMappings();
         $this->assertGreaterThanOrEqual(11, count($types));
 
@@ -11,6 +11,46 @@ class ContentFactoryTest extends \PHPUnit\Framework\TestCase {
             foreach ($content as $page) {
                 $this->assertInstanceOf($modelClass, $page);
             }
+        }
+    }
+
+    public function testGetAllByLanguage() {
+        $languages = getAllLanguages();
+
+        foreach ($languages as $language) {
+            $content = ContentFactory::getAllByLanguage($language);
+            foreach ($content as $page) {
+                $this->assertEquals($language, $page->language);
+            }
+        }
+    }
+
+    public function testGetAllbyMenu() {
+        $menus = getAllMenus();
+
+        foreach ($menus as $menu) {
+            $content = ContentFactory::getAllByMenu($menu);
+            foreach ($content as $page) {
+                $this->assertEquals($menu, $page->menu);
+            }
+        }
+    }
+
+    public function testGetAll() {
+        $content = ContentFactory::getAll();
+        $query = Database::pQuery("select id from {prefix}content", array(), true);
+        $this->assertEquals(count($content), Database::getNumRows($query));
+
+        foreach ($content as $page) {
+            $this->assertInstanceOf(Content::class, $page);
+        }
+    }
+
+    public function testGetAllRegular() {
+        $content = ContentFactory::getAllRegular();
+
+        foreach ($content as $page) {
+            $this->assertTrue($page->isRegular());
         }
     }
 
