1b0c43e10c4fa7c1d864b0f7d0962c41ca5d0b28
Ulrich Schmidt <admin@deruli.de>
2019-05-24 07:57:14 +0200

    add form

--- ulicms/content/modules/telegram/lang/en.php
+++ ulicms/content/modules/telegram/lang/en.php
@@ -1,3 +1,6 @@
 <?php
 
 add_translation("telegram_connect_failed", "Connection to Telegram failed. Please check your authentication credentials.");
+add_translation("telegram_bot_token", "Bot Token");
+add_translation("telegram_channel_name", "Channel Name");
+add_translation("telegram_channel_placeholder", "Channel Name (Including @ (e.g. @mychannel)");
--- ulicms/content/modules/telegram/templates/settings.php
+++ ulicms/content/modules/telegram/templates/settings.php
@@ -1,6 +1,5 @@
 <?php
-
-echo UliCMS\HTML\Alert::warning("Coming soon!");
+echo ModuleHelper::buildMethodCallForm(TelegramController::class, "save");
 
 if (Request::getVar("save")) {
     echo UliCMS\HTML\Alert::success(get_translation("changes_was_saved"));
@@ -10,3 +9,24 @@ if (Request::getVar("error")) {
     echo UliCMS\HTML\Alert::danger(get_translation(
                     Request::getVar("error")));
 }
+?>
+<div class="form-group">
+    <label for="text"><?php translate("telegram_channel_name"); ?></label>
+    <input type="text" class="form-control"
+           name="channel_name" id="text"
+           placeholder="<?php translate("telegram_channel_name"); ?>" autocomplete="new-password" value="<?php esc(Settings::get("telegram/channel_name")); ?>">
+</div>
+<div class="form-group">
+    <label for="username"><?php translate("telegram_bot_token"); ?></label>
+    <input type="text" class="form-control"
+           name="bot_token" id="username"
+
+           placeholder="<?php translate("telegram_bot_token_placeholder"); ?>" autocomplete="new-password" value="<?php esc(Settings::get("telegram/bot_token")); ?>">
+</div>
+
+<button type="submit" class="btn btn-primary">
+    <i class="fa fa-save"></i>
+    <?php translate("save"); ?>
+</button>
+<?php
+echo ModuleHelper::endForm();
