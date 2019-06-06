c79f207189c5dbebefe06253b99079b0c9f97198
Ulrich Schmidt <admin@deruli.de>
2019-06-06 07:50:40 +0200

    Update Database.php

--- ulicms/classes/objects/database/Database.php
+++ ulicms/classes/objects/database/Database.php
@@ -17,7 +17,16 @@ class Database {
         }
         self::query("SET NAMES 'utf8mb4'");
         // sql_mode auf leer setzen, da sich UliCMS nicht im strict_mode betreiben lässt
-        if (!$db_strict_mode) {
+        if ($db_strict_mode) {
+            Database::pQuery("SET SESSION sql_mode =
+                'ONLY_FULL_GROUP_BY,
+                STRICT_TRANS_TABLES,
+                NO_ZERO_IN_DATE,
+                NO_ZERO_DATE',
+                ERROR_FOR_DIVISION_BY_ZERO,
+                NO_AUTO_CREATE_USER,
+                NO_ENGINE_SUBSTITUTION");
+        } else {
             self::query("SET SESSION sql_mode = ''");
         }
 
