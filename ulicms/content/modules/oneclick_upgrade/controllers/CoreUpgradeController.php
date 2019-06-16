d6bd5eff852ae77de65d68a9b0dfdbb2883e7efe
Ulrich Schmidt <admin@deruli.de>
2019-06-16 21:02:13 +0200

    fix url for corrupt download error page

--- ulicms/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
+++ ulicms/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
@@ -65,7 +65,7 @@ class CoreUpgradeController extends Controller {
         try {
             $data = file_get_contents_wrapper($jsonData->file, false, $jsonData->hashsum);
         } catch (CorruptDownloadException $e) {
-            Request::redirect("admin/" . ModuleHelper::buildActionURL("CorruptedDownloadError"));
+            Request::redirect(ModuleHelper::buildActionURL("CorruptedDownloadError"));
         }
         if ($data) {
             file_put_contents($tmpArchive, $data);
