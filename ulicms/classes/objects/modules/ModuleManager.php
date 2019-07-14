adf0f0cf27060be91bd58190dd4ae6da5d7acbbb
Ulrich Schmidt <admin@deruli.de>
2019-07-14 12:33:49 +0200

    and types to ModuleManager

--- ulicms/classes/objects/modules/ModuleManager.php
+++ ulicms/classes/objects/modules/ModuleManager.php
@@ -1,48 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 class ModuleManager {
 
-    public function getAllModules() {
+    public function getAllModules(): array {
+        $modules = [];
         $sql = "select name from {prefix}modules";
         $query = Database::query($sql, true);
-        $modules = [];
         while ($row = Database::fetchObject($query)) {
             $modules [] = new Module($row->name);
         }
         return $modules;
     }
 
-    public function getEnabledModuleNames() {
+    public function getEnabledModuleNames(): array {
+        $modules = [];
         $sql = "select name from {prefix}modules where enabled = 1";
         $query = Database::query($sql, true);
-        $modules = [];
         while ($row = Database::fetchObject($query)) {
             $modules [] = $row->name;
         }
         return $modules;
     }
 
-    public function getDisabledModuleNames() {
+    public function getDisabledModuleNames(): array {
+        $modules = [];
         $sql = "select name from {prefix}modules where enabled = 0";
         $query = Database::query($sql, true);
-        $modules = [];
         while ($row = Database::fetchObject($query)) {
             $modules [] = $row->name;
         }
         return $modules;
     }
 
-    public function getAllModuleNames() {
+    public function getAllModuleNames(): array {
+        $modules = [];
         $sql = "select name from {prefix}modules";
         $query = Database::query($sql, true);
-        $modules = [];
         while ($row = Database::fetchObject($query)) {
             $modules [] = $row->name;
         }
         return $modules;
     }
 
-    public function getDependencies($module, $allDeps = []) {
+    public function getDependencies(?string $module, array $allDeps = []): array {
         $dependencies = getModuleMeta($module, "dependencies");
         if ($dependencies) {
             foreach ($dependencies as $dep) {
@@ -54,13 +56,13 @@ class ModuleManager {
         return $allDeps;
     }
 
-    public function getDependentModules($module, $allDeps = []) {
+    public function getDependentModules(?string $module, array $allDeps = []): array {
         $allModules = $this->getEnabledModuleNames();
         foreach ($allModules as $mod) {
             $dependencies = getModuleMeta($mod, "dependencies");
             if ($dependencies && faster_in_array($module, $dependencies)) {
                 $allDeps [] = $mod;
-                $allDeps = array_combine($allDeps, $this->getDependentModules($dep, $allDeps));
+                $allDeps = array_combine($allDeps, $this->getDependentModules($mod, $allDeps));
             }
         }
         $allDeps = array_unique($allDeps);
@@ -73,7 +75,12 @@ class ModuleManager {
     // - Nicht mehr vorhandene Module aus Datenbank löschen
     // - neue Module sollen erst mal deaktiviert sein
     // - Diese Funktion aufrufen beim installieren von Modulen, beim leeren des Caches und beim deinstallieren von Modulen
-    public function sync() {
+    public function sync(): void {
+        $this->removeDeletedModules();
+        $this->addNewModules();
+    }
+
+    protected function removeDeletedModules() {
         $realModules = getAllModules();
 
         $dataBaseModules = $this->getAllModuleNames();
@@ -84,34 +91,46 @@ class ModuleManager {
                 $module->delete();
             }
         }
+    }
+
+    protected function addNewModules() {
+        $realModules = getAllModules();
         $dataBaseModules = $this->getAllModuleNames();
+        // Settings aller aktiven Module auslesen und registrieren
         foreach ($realModules as $realModule) {
             $version = getModuleMeta($realModule, "version");
             if (faster_in_array($realModule, $dataBaseModules)) {
-                $module = new Module($realModule);
-                if ($module->getVersion() != $version) {
-                    $module->setVersion($version);
-                }
-                $module->save();
-            } else {
-                $module = new Module ();
-                $module->setName($realModule);
-                $module->setVersion($version);
-                $module->save();
-                $module->enable();
+                $this->updateModuleVersion($version, $realModule);
+                continue;
             }
+            $module = new Module ();
+            $module->setName($realModule);
+            $module->setVersion($version);
+            $module->save();
+            $module->enable();
         }
+    }
 
-        // Settings aller aktiven Module auslesen und registrieren
+    protected function initModulesDefaultSettings(): void {
         $enabledModules = $this->getEnabledModuleNames();
         foreach ($enabledModules as $module) {
             $settings = getModuleMeta($module, "settings");
-            if ($settings and is_array($settings)) {
-                foreach ($settings as $key => $value) {
-                    Settings::register($key, $value);
-                }
+            if (!($settings and is_array($settings))) {
+                continue;
+            }
+            foreach ($settings as $key => $value) {
+                Settings::register($key, $value);
             }
         }
+    }
+
+    protected function updateModuleVersion(string $version, string $realModule): void {
+
+        $module = new Module($realModule);
+        if ($module->getVersion() !== $version) {
+            $module->setVersion($version);
+        }
+        $module->save();
     }
 
 }
