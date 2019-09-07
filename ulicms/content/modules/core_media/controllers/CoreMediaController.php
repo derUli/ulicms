0eae12db4c6fabb5c42d30a40ac3c6635d0c70da
Ulrich Schmidt <admin@deruli.de>
2019-09-07 20:19:10 +0200

    fixes replace media

--- ulicms/content/modules/core_media/controllers/CoreMediaController.php
+++ ulicms/content/modules/core_media/controllers/CoreMediaController.php
@@ -2,7 +2,7 @@
 
 use MediaEmbed\MediaEmbed;
 
-class CoreMediaEmbedController extends MainClass {
+class CoreMediaController extends MainClass {
 
     public function beforeContentFilter($input) {
         $data = CustomData::get();
@@ -14,31 +14,35 @@ class CoreMediaEmbedController extends MainClass {
 
     public function replaceLinks($input) {
         $content = mb_convert_encoding($input, 'HTML-ENTITIES', "UTF-8");
+
         $dom = new DOMDocument();
         @$dom->loadHTML($content);
 
-        $linksToReplace = [];
-
         $elements = $dom->getElementsByTagName("a");
 
-        for ($i = 0; $i < $elements->count(); $i++) {
-            $oldNode = $elements->item($i);
+        $linksToReplace = [];
+
+        foreach ($elements as $oldNode) {
             $href = $oldNode->getAttribute('href');
-            $text = $oldNode->textContent;
 
-            if ($href !== $text) {
-                continue;
-            }
+            $embedCode = $this->embedCodeFromUrl($href);
 
-            $url = $text;
-            $embedCode = $this->embedCodeFromUrl($url);
             if ($embedCode) {
                 $newNode = $this->createElementFromHTML($embedCode, $dom);
                 $importNode = $dom->importNode($newNode, true);
-                $oldNode->parentNode->replaceChild($importNode, $oldNode);
+                $link = new stdClass();
+                $link->oldNode = $oldNode;
+                $link->newNode = $importNode;
+                $linksToReplace[] = $link;
             }
         }
+        foreach ($linksToReplace as $link) {
+            $link->oldNode->parentNode->replaceChild($link->newNode, $link->oldNode);
+        }
+        return $this->getBodyContent($dom->saveHTML());
+    }
 
+    private function getBodyContent($html) {
         return preg_replace('/^<!DOCTYPE.+?>/', '', str_replace(array(
             '<html>',
             '</html>',
@@ -49,7 +53,7 @@ class CoreMediaEmbedController extends MainClass {
             '',
             '',
             ''
-                        ), $dom->saveHTML()));
+                        ), $html));
     }
 
     public function embedCodeFromUrl($url) {
@@ -64,13 +68,13 @@ class CoreMediaEmbedController extends MainClass {
         return null;
     }
 
-    public function createElementFromHTML($str, $dom) {
+    private function createElementFromHTML($str, $dom) {
         $element = $dom->createElement("span");
         $this->appendHTML($element, $str);
         return $element;
     }
 
-    function appendHTML(DOMNode $parent, $html) {
+    private function appendHTML(DOMNode $parent, $html) {
         $tmpDoc = new DOMDocument();
         $tmpDoc->loadHTML($html);
         foreach ($tmpDoc->getElementsByTagName('body')->item(0)->childNodes as $node) {
