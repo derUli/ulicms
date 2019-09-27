70065c6881f9dbfee7cada0ae77104883f0dbc1d
Ulrich Schmidt <admin@deruli.de>
2019-09-27 08:01:17 +0200

    add infos about channels to oneclick upgrade

--- ulicms/content/modules/oneclick_upgrade/admin.php
+++ ulicms/content/modules/oneclick_upgrade/admin.php
@@ -1,4 +1,6 @@
 <?php
+use UliCMS\HTML\Alert;
+use function UliCMS\HTML\text;
 define("MODULE_ADMIN_HEADLINE", get_translation("oneclick_upgrade") . " " . get_translation("settings"));
 
 function oneclick_upgrade_admin() {
@@ -26,8 +28,11 @@ function oneclick_upgrade_admin() {
               <?php csrf_token_html(); ?>
         <div>
             <label for="oneclick_upgrade_channel"><?php translate("channel") ?></label><br />
-            <select name="oneclick_upgrade_channel" size=1>
-                <?php for ($i = 0; $i < count($channels); $i++) { ?>
+            <select
+                name="oneclick_upgrade_channel"
+                size=1
+                id="oneclick_upgrade_channel">
+                    <?php for ($i = 0; $i < count($channels); $i++) { ?>
                     <option value="<?php Template::escape($channels[$i]) ?>"
                     <?php
                     if ($oneclick_upgrade_channel == $channels[$i]) {
@@ -38,11 +43,22 @@ function oneclick_upgrade_admin() {
 
             </select>
         </div>
+        <div id="help-texts" class="voffset3 alert alert-info">
+            <div data-channel="fast">
+                <?php echo text(get_translation("fast_description")); ?>
+            </div>
 
-
-        <button type="submit" class="btn btn-primary voffset3">
+            <div data-channel="slow">
+                <?php echo text(get_translation("slow_description")); ?>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">
             <i class="fa fa-save"></i> <?php translate("save"); ?></button>
     </form>
     <?php
+    enqueueScriptFile(
+            ModuleHelper::buildRessourcePath("oneclick_upgrade",
+                    "js/settings.js")
+    );
+	combinedScriptHtml();
 }
-?>
