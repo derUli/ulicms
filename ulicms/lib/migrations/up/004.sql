982bfc197054308d1f27622dcfa4435ad0a993d1
Ulrich Schmidt <admin@deruli.de>
2019-10-07 19:18:23 +0200

    make it compatible again with a fucking old MariaDB Version

--- ulicms/lib/migrations/up/004.sql
+++ ulicms/lib/migrations/up/004.sql
@@ -1,2 +1,27 @@
 ALTER TABLE {prefix}content CHANGE `autor` `author_id` int(11) DEFAULT NULL;
 
+ALTER TABLE `{prefix}content`
+  ADD KEY `systemname` (`systemname`(191)),
+  ADD KEY `language` (`language`),
+  ADD KEY `menu` (`menu`),
+  ADD KEY `parent` (`parent`),
+  ADD KEY `active` (`active`),
+  ADD KEY `deleted_at` (`deleted_at`),
+  ADD KEY `hidden` (`hidden`),
+  ADD KEY `type` (`type`),
+  ADD KEY `fk_category` (`category`),
+  ADD KEY `fk_autor` (`autor`),
+  ADD KEY `fk_video` (`video`),
+  ADD KEY `fk_audio` (`audio`),
+  ADD KEY `fk_link_to_language` (`link_to_language`),
+  ADD KEY `fk_content_group_id` (`group_id`);
+  
+  ALTER TABLE `{prefix}content`
+  ADD CONSTRAINT `fk_audio` FOREIGN KEY (`audio`) REFERENCES `{prefix}audio` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_autor` FOREIGN KEY (`autor`) REFERENCES `{prefix}users` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_category` FOREIGN KEY (`category`) REFERENCES `{prefix}categories` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_content_group_id` FOREIGN KEY (`group_id`) REFERENCES `{prefix}groups` (`id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `fk_content_language` FOREIGN KEY (`language`) REFERENCES `{prefix}languages` (`language_code`) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_link_to_language` FOREIGN KEY (`link_to_language`) REFERENCES `{prefix}languages` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_parent_content` FOREIGN KEY (`parent`) REFERENCES `{prefix}content` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_video` FOREIGN KEY (`video`) REFERENCES `{prefix}videos` (`id`) ON DELETE SET NULL;
--- ulicms/lib/migrations/up/006.sql
+++ ulicms/lib/migrations/up/006.sql
@@ -1,1 +1,4 @@
 ALTER TABLE {prefix}banner CHANGE `category` `category_id` int(11) DEFAULT NULL;
+
+ALTER TABLE `{prefix}banner`
+  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `{prefix}categories` (`id`) ON DELETE SET NULL;
