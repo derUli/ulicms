c8622ae3a7868a5b2dce9540de6bc7faa8c71b53
Ulrich Schmidt <admin@deruli.de>
2019-11-15 21:12:36 +0100

    update oneclick_upgrade, support for upgrade paths

--- ulicms/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
+++ ulicms/content/modules/oneclick_upgrade/controllers/CoreUpgradeController.php
@@ -4,92 +4,94 @@ use UliCMS\Exceptions\CorruptDownloadException;
 
 class CoreUpgradeController extends Controller {
 
-    public function getCheckURL() {
-        return "http://channels.ulicms.de/" . Settings::get("oneclick_upgrade_channel") . ".json";
-    }
+	public function getCheckURL() {
+		$version = cms_version();
+		$channel = Settings::get("oneclick_upgrade_channel");
+		return "https://channels.ulicms.de/$version/$channel.json";
+	}
 
-    public function __construct() {
-        parent::__construct();
-        $this->checkURL = $this->getCheckURL();
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->checkURL = $this->getCheckURL();
+	}
 
-    public function setCheckURL($url) {
-        $this->checkURL = $url;
-    }
+	public function setCheckURL($url) {
+		$this->checkURL = $url;
+	}
 
-    public function getJSON() {
-        $data = file_get_contents_wrapper($this->getCheckURL(), true);
-        if (!$data) {
-            return null;
-        }
-        $data = json_decode($data);
-        return $data;
-    }
+	public function getJSON() {
+		$data = file_get_contents_wrapper($this->getCheckURL(), true);
+		if (!$data) {
+			return null;
+		}
+		$data = json_decode($data);
+		return $data;
+	}
 
-    public function checkForUpgrades() {
-        $data = $this->getJSON();
-        if (!$data) {
-            return null;
-        }
-        $version = $data->version;
-        $cfg = new UliCMSVersion();
-        $oldVersion = $cfg->getInternalVersionAsString();
-        if (version_compare($oldVersion, $data->version, "<")) {
-            return $data->version;
-        }
-        return null;
-    }
+	public function checkForUpgrades() {
+		$data = $this->getJSON();
+		if (!$data) {
+			return null;
+		}
+		$version = $data->version;
+		$cfg = new UliCMSVersion();
+		$oldVersion = $cfg->getInternalVersionAsString();
+		if (version_compare($oldVersion, $data->version, "<")) {
+			return $data->version;
+		}
+		return null;
+	}
 
-    public function runUpgrade($skipPermissions = false) {
-        @set_time_limit(0);
-        @ignore_user_abort(1);
-        $acl = new ACL();
-        if ((!$skipPermissions and ( !$acl->hasPermission("update_system")) or ! $this->checkForUpgrades() or get_request_method() != "post")) {
-            return false;
-        }
+	public function runUpgrade($skipPermissions = false) {
+		@set_time_limit(0);
+		@ignore_user_abort(1);
+		$acl = new ACL();
+		if ((!$skipPermissions and ( !$acl->hasPermission("update_system")) or ! $this->checkForUpgrades() or get_request_method() != "post")) {
+			return false;
+		}
 
-        $jsonData = $this->getJSON();
-        if (!$jsonData) {
-            return null;
-        }
+		$jsonData = $this->getJSON();
+		if (!$jsonData) {
+			return null;
+		}
 
-        $tmpDir = Path::resolve("ULICMS_TMP/upgrade");
-        $tmpArchive = Path::resolve("$tmpDir/upgrade.zip");
+		$tmpDir = Path::resolve("ULICMS_TMP/upgrade");
+		$tmpArchive = Path::resolve("$tmpDir/upgrade.zip");
 
-        if (is_dir($tmpDir)) {
-            sureRemoveDir($tmpDir, true);
-        }
+		if (is_dir($tmpDir)) {
+			sureRemoveDir($tmpDir, true);
+		}
 
-        if (!is_dir($tmpDir)) {
-            mkdir($tmpDir, 0777, true);
-        }
-        try {
-            $data = file_get_contents_wrapper($jsonData->file, false, $jsonData->hashsum);
-        } catch (CorruptDownloadException $e) {
-            Request::redirect(ModuleHelper::buildActionURL("CorruptedDownloadError"));
-        }
-        if ($data) {
-            file_put_contents($tmpArchive, $data);
-            $zip = new ZipArchive();
-            if ($zip->open($tmpArchive) === true) {
-                $zip->extractTo($tmpDir);
-                $zip->close();
-            }
+		if (!is_dir($tmpDir)) {
+			mkdir($tmpDir, 0777, true);
+		}
+		try {
+			$data = file_get_contents_wrapper($jsonData->file, false, $jsonData->hashsum);
+		} catch (CorruptDownloadException $e) {
+			Request::redirect(ModuleHelper::buildActionURL("CorruptedDownloadError"));
+		}
+		if ($data) {
+			file_put_contents($tmpArchive, $data);
+			$zip = new ZipArchive();
+			if ($zip->open($tmpArchive) === true) {
+				$zip->extractTo($tmpDir);
+				$zip->close();
+			}
 
-            $upgradeCodeDir = Path::resolve("$tmpDir/ulicms");
+			$upgradeCodeDir = Path::resolve("$tmpDir/ulicms");
 
-            if (is_dir($upgradeCodeDir)) {
-                recurse_copy($upgradeCodeDir, ULICMS_ROOT);
-                sureRemoveDir($tmpDir, true);
+			if (is_dir($upgradeCodeDir)) {
+				recurse_copy($upgradeCodeDir, ULICMS_ROOT);
+				sureRemoveDir($tmpDir, true);
 
-                response::redirect("../update.php");
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+				response::redirect("../update.php");
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 
 }
