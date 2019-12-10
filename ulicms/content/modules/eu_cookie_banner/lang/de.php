aa73d6044b3029c52814bfbd105df682c65c7d39
Ulrich Schmidt <admin@deruli.de>
2019-12-10 19:09:19 +0100

    add settings

--- ulicms/content/modules/eu_cookie_banner/lang/de.php
+++ ulicms/content/modules/eu_cookie_banner/lang/de.php
@@ -2,3 +2,7 @@
 
 add_translation("eu_cookie_banner", "EU Cookie Banner");
 add_translation("html_tracking_codes", "HTML Tracking Codes");
+
+add_translation("eu_cookie_banner_help_text", "Cookie-Hinweistext");
+add_translation("eu_cookie_banner_accept", "Button zum Akzeptieren");
+add_translation("eu_cookie_banner_reject", "Buttom zum Ablehnen");
