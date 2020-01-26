6ca598fd5ec5558935328d3b9079b59d00631192
Ulrich Schmidt <admin@deruli.de>
2020-01-26 14:00:53 +0100

    Update UliCMSVersion.php

--- ulicms/UliCMSVersion.php
+++ ulicms/UliCMSVersion.php
@@ -6,7 +6,7 @@ class UliCMSVersion {
 
 	function __construct() {
 		$this->codename = "Lurking Anaconda";
-		$this->releaseYear = 2019;
+		$this->releaseYear = 2020;
 		$this->buildDate = 0; // {InsertBuildDate}
 		$this->internalVersion = [
 			2020,
