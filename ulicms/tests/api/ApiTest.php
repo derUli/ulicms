e4e83350c31453d86c57dc447be210a5fb82bedb
Ulrich Schmidt <admin@deruli.de>
2020-02-12 17:23:09 +0100

    fix failing tests

--- ulicms/tests/api/ApiTest.php
+++ ulicms/tests/api/ApiTest.php
@@ -128,7 +128,7 @@ class ApiTest extends \PHPUnit\Framework\TestCase {
     public function testGetThemeMeta() {
         $meta = getThemeMeta("impro17");
         $this->assertIsArray($meta);
-        $this->assertEquals("2.1.3", $meta["version"]);
+        $this->assertEquals("2.1.4", $meta["version"]);
     }
 
     public function testBool2YesNo() {
