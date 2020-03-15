81e697e56db78dd9584ce4810675698f3cabf64e
Der Uli im Netz <admin@deruli.de>
2020-03-15 21:06:09 +0100

    Update UliCMSVersion.php

--- ulicms/UliCMSVersion.php
+++ ulicms/UliCMSVersion.php
@@ -4,45 +4,44 @@ declare(strict_types=1);
 
 class UliCMSVersion {
 
-	function __construct() {
-		$this->codename = "Playful Amazon River Dolphin";
-		$this->releaseYear = 2020;
-		$this->buildDate = 0; // {InsertBuildDate}
-		$this->internalVersion = [
-			2020,
-			1,
-                        1
-		];
+    function __construct() {
+        $this->codename = "Playful Amazon River Dolphin";
+        $this->releaseYear = 2020;
+        $this->buildDate = 0; // {InsertBuildDate}
+        $this->internalVersion = [
+            2020,
+            2
+        ];
 
-		$this->update = "";
-	}
+        $this->update = "";
+    }
 
-	public function getReleaseYear(): string {
-		return strval($this->releaseYear);
-	}
+    public function getReleaseYear(): string {
+        return strval($this->releaseYear);
+    }
 
-	//  returns the codename of this UliCMS release
-	public function getCodeName(): string {
-		return $this->codename;
-	}
+    //  returns the codename of this UliCMS release
+    public function getCodeName(): string {
+        return $this->codename;
+    }
 
-	// returns the version number
-	public function getInternalVersion(): array {
-		return $this->internalVersion;
-	}
+    // returns the version number
+    public function getInternalVersion(): array {
+        return $this->internalVersion;
+    }
 
-	// Returns the full version number as string
-	public function getInternalVersionAsString(): string {
-		return implode(".", $this->internalVersion);
-	}
+    // Returns the full version number as string
+    public function getInternalVersionAsString(): string {
+        return implode(".", $this->internalVersion);
+    }
 
-	public function getBuildTimestamp(): int {
-		return $this->buildDate;
-	}
+    public function getBuildTimestamp(): int {
+        return $this->buildDate;
+    }
 
-	public function getBuildDate(): string {
-		return strftime("%x %X", $this->getBuildTimestamp());
-	}
+    public function getBuildDate(): string {
+        return strftime("%x %X", $this->getBuildTimestamp());
+    }
 
 }
 
