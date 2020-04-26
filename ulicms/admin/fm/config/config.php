708955218a02d79c0a8f513e1a2c17a158f02e06
Der Uli im Netz <admin@deruli.de>
2020-04-26 14:45:54 +0200

    Update config.php

--- ulicms/admin/fm/config/config.php
+++ ulicms/admin/fm/config/config.php
@@ -3,6 +3,7 @@
 define("RESPONSIVE_FM", true);
 include_once "../../init.php";
 $version = "9.14.0";
+
 if (session_id() == '') {
     session_start();
 }
@@ -263,7 +264,7 @@ $config = array(
     // set maximum pixel width and/or maximum pixel height for all images
     // If you set a maximum width or height, oversized images are converted to those limits. Images smaller than the limit(s) are unaffected
     // if you don't need a limit set both to 0
-    'image_max_width' => 2560 ,
+    'image_max_width' => 2560,
     'image_max_height' => 0,
     'image_max_mode' => 'auto',
     /*
