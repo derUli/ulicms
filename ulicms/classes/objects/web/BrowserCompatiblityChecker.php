38f96f892a29d210bb4cab62a24ded690d38c72b
Der Uli im Netz <admin@deruli.de>
2020-05-16 15:53:20 +0200

    incompatible browser blacklist

--- ulicms/classes/objects/web/BrowserCompatiblityChecker.php
+++ ulicms/classes/objects/web/BrowserCompatiblityChecker.php
@@ -3,6 +3,7 @@
 namespace UliCMS\Backend\Utils;
 
 class BrowserCompatiblityChecker {
+
     private $unsupportedBrowserName = null;
     private $useragent = '';
 
@@ -10,14 +11,26 @@ class BrowserCompatiblityChecker {
         $this->useragent = $useragent;
     }
 
+    // All current browsers except Internet Explorer, Opera Mini
+    // and Text Mode browsers are supported
     public function isCompatible(): bool {
         if ($this->isIE()) {
             $this->unsupportedBrowserName = "Microsoft Internet Explorer";
+        } else if ($this->isOperaMini()) {
+            $this->unsupportedBrowserName = "Opera Mini";
+        } else if ($this->isLynx()) {
+            $this->unsupportedBrowserName = "Lynx";
+        } else if ($this->isELinks()) {
+            $this->unsupportedBrowserName = "ELinks";
+        } else if ($this->isLinks()) {
+            $this->unsupportedBrowserName = "Links";
+        } else if ($this->isW3M()) {
+            $this->unsupportedBrowserName = "w3m";
         }
 
         return empty($this->unsupportedBrowserName);
     }
-    
+
     public function getUnsupportedBrowserName(): ?string {
         return $this->unsupportedBrowserName;
     }
@@ -25,6 +38,27 @@ class BrowserCompatiblityChecker {
     public function isIE(): bool {
         return (preg_match('~MSIE|Internet Explorer~i', $this->useragent) ||
                 (strpos($this->useragent, 'Trident/7.0') !== false));
+    }
+
+    public function isLynx(): bool {
+        return strpos($this->useragent, 'Lynx/') !== false;
+    }
+
+    public function isELinks(): bool {
+        return strpos($this->useragent, 'ELinks/') !== false;
+    }
+    
+    public function isOperaMini(): bool {
+        return strpos($this->useragent, 'Opera Mini') !== false;
+    }
+
+    public function isLinks(): bool {
+        return strpos($this->useragent, 'Links (2') !== false ||
+        strpos($this->useragent, 'Links (1') !== false;
+    }
+    
+    public function isW3M(): bool {
+        return strpos($this->useragent, 'w3m/') !== false;
     }
 
 }
--- ulicms/tests/utils/backend/BrowserCompatiblityCheckerTest.php
+++ ulicms/tests/utils/backend/BrowserCompatiblityCheckerTest.php
@@ -23,6 +23,17 @@ class BrowserCompatiblityCheckerTest extends \PHPUnit\Framework\TestCase {
         $this->assertNull($checker->getUnsupportedBrowserName());
     }
 
+    public function testIsSafari() {
+        $checker = new BrowserCompatiblityChecker(
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) "
+                . "AppleWebKit/537.36 (KHTML, like Gecko) "
+                . "Chrome/81.0.4044.141 Safari/537.36"
+        );
+
+        $this->assertTrue($checker->isCompatible());
+        $this->assertNull($checker->getUnsupportedBrowserName());
+    }
+
     public function testIE() {
         $checker = new BrowserCompatiblityChecker(
                 "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; "
@@ -44,6 +55,70 @@ class BrowserCompatiblityCheckerTest extends \PHPUnit\Framework\TestCase {
         );
         $this->assertTrue($checker->isCompatible());
         $this->assertNull($checker->getUnsupportedBrowserName());
+    }
+
+    public function testLynx() {
+        $checker = new BrowserCompatiblityChecker(
+                "Lynx/2.8.9dev.16 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/3.5.17"
+        );
+        $this->assertFalse($checker->isCompatible());
+        $this->assertEquals(
+                "Lynx",
+                $checker->getUnsupportedBrowserName()
+        );
+    }
+
+    public function testElinks() {
+        $checker = new BrowserCompatiblityChecker(
+                "ELinks/0.12pre6 (textmode; Linux 4.15.0-13-generic x86_64; 204x55-2)"
+        );
+        $this->assertFalse($checker->isCompatible());
+        $this->assertEquals(
+                "ELinks",
+                $checker->getUnsupportedBrowserName()
+        );
+    }
+
+    public function testLinks() {
+        $checker = new BrowserCompatiblityChecker(
+                "Links (2.1pre11; Linux 2.4.22 i686; fb)"
+        );
+        $this->assertFalse($checker->isCompatible());
+        $this->assertEquals(
+                "Links",
+                $checker->getUnsupportedBrowserName()
+        );
+    }
+
+    public function testW3m() {
+        $checker = new BrowserCompatiblityChecker(
+                "w3m/0.5.3+git20180125"
+        );
+        $this->assertFalse($checker->isCompatible());
+        $this->assertEquals(
+                "w3m",
+                $checker->getUnsupportedBrowserName()
+        );
+    }
+
+    public function testOpera() {
+        $checker = new BrowserCompatiblityChecker(
+                "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18"
+        );
+        $this->assertTrue($checker->isCompatible());
+        $this->assertNull($checker->getUnsupportedBrowserName());
+    }
+
+    public function testOperaMini() {
+        $checker = new BrowserCompatiblityChecker(
+                "Opera/9.80 (Android; Opera Mini/12.0.1987/37.7327; U; pl) "
+                . "Presto/2.12.423 Version/12.16	"
+        );
+        $this->assertFalse($checker->isCompatible());
+        $this->assertEquals(
+                "Opera Mini",
+                $checker->getUnsupportedBrowserName()
+        );
     }
 
 }
