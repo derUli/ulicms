82cd2d7404124496e7edcd1e75f01221ffc2b12c
Der Uli im Netz <admin@deruli.de>
2020-06-03 14:04:02 +0200

    more test coverage

--- ulicms/content/modules/core_package_manager/controllers/PkgInfoController.php
+++ ulicms/content/modules/core_package_manager/controllers/PkgInfoController.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 class PkgInfoController extends Controller {
 
     public function installPost(): void {
+        // TODO: handle incomplete requests and errors
         if (StringHelper::isNotNullOrEmpty($_REQUEST["file"])) {
             $file = basename($_POST["file"]);
-            $path = Path::resolve("ULICMS_TMP/$file");
-            $pkg = new SinPackageInstaller($path);
-            if (file_exists($path)) {
-                $pkg->installPackage();
-            }
+            $this->_installPost($file);
             Request::redirect(
                     ModuleHelper::buildActionURL(
                             "sin_package_install_ok", "file=$file"
                     )
             );
         }
+    }
+
+    public function _installPost($file): bool {
+        $path = Path::resolve("ULICMS_TMP/$file");
+        $pkg = new SinPackageInstaller($path);
+        if (file_exists($path)) {
+            return $pkg->installPackage();
+        }
+        return false;
     }
 
 }
