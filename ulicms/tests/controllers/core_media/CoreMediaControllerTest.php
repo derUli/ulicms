16c346257dcef928a1134612e599ef27c5f7be04
Der Uli im Netz <admin@deruli.de>
2020-08-15 01:20:42 +0200

    fix tests

--- ulicms/tests/controllers/core_media/CoreMediaControllerTest.php
+++ ulicms/tests/controllers/core_media/CoreMediaControllerTest.php
@@ -1,39 +1,30 @@
 <?php
 
 use UliCMS\HTML\Link;
+use Spatie\Snapshots\MatchesSnapshots;
 
-class CoreMediaControllerTest extends \PHPUnit\Framework\TestCase
-{
-    public function testReplaceLinks()
-    {
+class CoreMediaControllerTest extends \PHPUnit\Framework\TestCase {
+
+    use MatchesSnapshots;
+
+    public function testReplaceLinks() {
         $input = $this->getExampleHtml();
 
         $controller = new CoreMediaController();
         $actual = $controller->_replaceLinks($input);
-        $expected = file_get_contents(
-            Path::resolve(
-                "ULICMS_ROOT/tests/fixtures/embed-media.expected.txt"
-            )
-        );
-        $this->assertEquals(normalizeLN($actual), normalizeLN($expected));
+
+        $this->assertMatchesHtmlSnapshot($actual);
     }
 
-    public function testBeforeContentFilterEnabled()
-    {
+    public function testBeforeContentFilterEnabled() {
         $input = $this->getExampleHtml();
 
         $controller = new CoreMediaController();
         $actual = $controller->beforeContentFilter($input);
-        $expected = file_get_contents(
-            Path::resolve(
-                "ULICMS_ROOT/tests/fixtures/embed-media.expected.txt"
-            )
-        );
-        $this->assertEquals(normalizeLN($actual), normalizeLN($expected));
+        $this->assertMatchesHtmlSnapshot($actual);
     }
-    
-    private function getExampleHtml()
-    {
+
+    private function getExampleHtml() {
         $urls = [
             "http://example.org/",
             "https://youtu.be/7b-B1-xs6Og",
@@ -46,11 +37,11 @@ class CoreMediaControllerTest extends \PHPUnit\Framework\TestCase
 
         return implode("\n", $links);
     }
-    
-    public function testReplaceLinksWithEmpty()
-    {
+
+    public function testReplaceLinksWithEmpty() {
         $controller = new CoreMediaController();
         $output = $controller->_replaceLinks("");
         $this->assertEmpty($output);
     }
+
 }
