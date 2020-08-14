2da30c38b5a8682a7a1d22acc41486cc4c0d43d8
Der Uli im Netz <admin@deruli.de>
2020-08-14 18:00:58 +0200

    add lazy loading

---
+++ ulicms/lib/migrations/up/022.sql
@@ -0,0 +1,7 @@
+INSERT INTO `{prefix}settings` 
+            (name, 
+             value) 
+VALUES      ('lazy_loading_img', 
+             '1'), 
+            ('lazy_loading_iframe', 
+             '1') 
