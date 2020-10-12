37c18e9b92f95e8e4b35a696c077a6b734fec924
derUli <admin@deruli.de>
2020-10-12 18:08:22 +0200

    test coverage

---
+++ ulicms/tests/controllers/core_settings/DefaultAccessRestrictionsControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+class DefaultAccessRestrictionsControllerTest extends \PHPUnit\Framework\TestCase {
+
+    protected function tearDown(): void {
+        $_POST = [];
+        Settings::set(
+                "only_admins_can_edit",
+                0
+        );
+        Settings::set(
+                "only_group_can_edit",
+                0
+        );
+        Settings::set(
+                "only_owner_can_edit",
+                0
+        );
+        Settings::set(
+                "only_others_can_edit",
+                0
+        );
+    }
+
+    public function testSavePost() {
+        $_POST = [
+            "only_admins_can_edit" => "1",
+            "only_owner_can_edit" => "1",
+        ];
+
+        $controller = new DefaultAccessRestrictionsController();
+        $controller->_savePost();
+
+        $this->assertEquals(1, Settings::get("only_admins_can_edit", "int"));
+        $this->assertEquals(1, Settings::get("only_owner_can_edit", "int"));
+
+        $this->assertEquals(0, Settings::get("only_group_can_edit", "int"));
+        $this->assertEquals(0, Settings::get("only_others_can_edit", "int"));
+    }
+
+}
