ec028643e593ace91fb8c3e9dfdce3bd04d7810f
derUli <admin@deruli.de>
2020-10-23 00:35:31 +0200

    frontpagesettings

--- ulicms/content/modules/core_settings/controllers/FrontPageSettingsController.php
+++ ulicms/content/modules/core_settings/controllers/FrontPageSettingsController.php
@@ -4,26 +4,30 @@ declare(strict_types=1);
 
 use UliCMS\Utils\CacheUtil;
 
-class FrontPageSettingsController extends Controller
-{
-    public function savePost(): void
-    {
+class FrontPageSettingsController extends Controller {
+
+    public function _savePost(): void {
         $languages = getAllLanguages();
-        for ($i = 0; $i < count($languages); $i ++) {
+        for ($i = 0; $i < count($languages); $i++) {
             $lang = $languages[$i];
             if (isset($_POST["frontpage_" . $lang])) {
                 $page = $_POST["frontpage_" . $lang];
                 Settings::set("frontpage_" . $lang, $page);
-                if ($lang == Settings::get("default_language")) {
+                if ($lang === Settings::get("default_language")) {
                     Settings::set("frontpage", $page);
                 }
             }
         }
         CacheUtil::clearPageCache();
+    }
+
+    public function savePost(): void {
+        $this->_savePost();
         // if called by ajax return no content to improve performance
         if (Request::isAjaxRequest()) {
             HTTPStatusCodeResult(HttpStatusCode::OK);
         }
         Request::redirect(ModuleHelper::buildActionURL("frontpage_settings"));
     }
+
 }
---
+++ ulicms/tests/controllers/core_settings/FrontPageSettingsControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+class FrontPageSettingsControllerTest extends \PHPUnit\Framework\TestCase {
+
+    private $defaultSettings = [];
+    
+    protected function setUp(): void {
+        $this->defaultSettings = [
+            "default_language" => Settings::get("default_language"),
+            "frontpage" => Settings::get("frontpage_de"),
+            "frontpage_en" => Settings::get("frontpage_en"),
+            "frontpag" => Settings::get("meta_description"),
+        ];
+    }
+    
+    protected function tearDown(): void {
+        $_POST = [];
+        
+        foreach($this->defaultSettings as $key => $value){
+            Settings::set($key, $value);
+        }
+    }
+
+    public function testSavePost(): void {
+        $_POST["frontpage_de"] = "willkommen";
+        $_POST["frontpage_en"] = "welcome";
+        Settings::set("default_language", "en");
+
+        $controller = new FrontPageSettingsController();
+        $controller->_savePost();
+
+        $this->assertEquals(
+                "willkommen",
+                Settings::get('frontpage_de')
+        );
+
+        $this->assertEquals(
+                "welcome",
+                Settings::get('frontpage_en')
+        );
+         $this->assertEquals(
+                "welcome",
+                Settings::get('frontpage')
+        );
+    }
+
+}
