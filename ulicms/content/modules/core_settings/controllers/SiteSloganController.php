6f333c61125c5fd1f34af3d1400ab2467eaf81e1
derUli <admin@deruli.de>
2020-10-24 21:04:44 +0200

    add test

--- ulicms/content/modules/core_settings/controllers/SiteSloganController.php
+++ ulicms/content/modules/core_settings/controllers/SiteSloganController.php
@@ -4,19 +4,18 @@ declare(strict_types=1);
 
 use UliCMS\Utils\CacheUtil;
 
-class SiteSloganController extends Controller
-{
-    public function savePost(): void
-    {
+class SiteSloganController extends Controller {
+
+    public function _savePost(): void {
         $languages = getAllLanguages();
 
         if (isset($_POST["submit"])) {
-            for ($i = 0; $i < count($languages); $i ++) {
+            for ($i = 0; $i < count($languages); $i++) {
                 $lang = $languages[$i];
                 if (isset($_POST["site_slogan_" . $lang])) {
                     $page = $_POST["site_slogan_" . $lang];
                     Settings::set("site_slogan_" . $lang, $page);
-                    if ($lang == Settings::get("default_language")) {
+                    if ($lang === Settings::get("default_language")) {
                         Settings::set("site_slogan", $page);
                     }
                 }
@@ -24,11 +23,15 @@ class SiteSloganController extends Controller
         }
 
         CacheUtil::clearPageCache();
+    }
 
+    public function savePost(): void {
+        $this->_savePost();
         // if called by ajax return no content to improve performance
         if (Request::isAjaxRequest()) {
             HTTPStatusCodeResult(HttpStatusCode::OK);
         }
         Request::redirect(ModuleHelper::buildActionURL("site_slogan"));
     }
+
 }
