0fabf6160364ef531bb554d9722d2f11746bfff8
derUli <admin@deruli.de>
2020-10-27 00:41:11 +0100

    tests

--- ulicms/content/modules/core_settings/controllers/ErrorPagesController.php
+++ ulicms/content/modules/core_settings/controllers/ErrorPagesController.php
@@ -4,19 +4,18 @@ declare(strict_types=1);
 
 use UliCMS\Utils\CacheUtil;
 
-class ErrorPagesController extends Controller
-{
-    public function savePost(): void
-    {
+class ErrorPagesController extends Controller {
+
+    public function _savePost(): void {
         $errorPages = $_POST["error_page"];
         if (is_array($errorPages)) {
             foreach ($errorPages as $code => $languages) {
                 foreach ($languages as $language => $page_id) {
                     if ($page_id > 0) {
                         Settings::setLanguageSetting(
-                            "error_page_{$code}",
-                            $page_id,
-                            $language
+                                "error_page_{$code}",
+                                $page_id,
+                                $language
                         );
                     } else {
                         Settings::delete("error_page_{$code}_{$language}");
@@ -26,10 +25,15 @@ class ErrorPagesController extends Controller
         }
 
         CacheUtil::clearPageCache();
+    }
+
+    public function savePost(): void {
+        $this->_savePost();
 
         Response::sendHttpStatusCodeResultIfAjax(
-            HttpStatusCode::OK,
-            ModuleHelper::buildActionURL("error_pages")
+                HttpStatusCode::OK,
+                ModuleHelper::buildActionURL("error_pages")
         );
     }
+
 }
