07f51f89aded6b3477108d5232c3e92a16d6e00b
derUli <admin@deruli.de>
2020-11-28 01:40:59 +0100

    fix failing tests

--- ulicms/classes/objects/privacy/PrivacyCheckbox.php
+++ ulicms/classes/objects/privacy/PrivacyCheckbox.php
@@ -5,51 +5,46 @@ declare(strict_types=1);
 use UliCMS\HTML\Input as Input;
 
 // Class with DSGVO / GDPR related functions
-class PrivacyCheckbox
-{
+class PrivacyCheckbox {
+
     private $language;
 
     const CHECKBOX_NAME = "accept_privacy_policy";
 
-    public function __construct(string $language)
-    {
+    public function __construct(string $language) {
         $this->language = $language;
     }
 
     // the gdpr checkbox must be enabled and configured by language
-    public function isEnabled(): bool
-    {
+    public function isEnabled(): bool {
         return boolval(
-            Settings::get(
-                "privacy_policy_checkbox_enable_{$this->language}",
-                "bool"
-            )
+                Settings::get(
+                        "privacy_policy_checkbox_enable_{$this->language}",
+                        "bool"
+                )
         );
     }
 
     // returns the name of the checkbox input
-    public function getCheckboxName(): string
-    {
+    public function getCheckboxName(): string {
         return self::CHECKBOX_NAME;
     }
 
     // returns true if the checkbox is checked
-    public function isChecked(): bool
-    {
-        return StringHelper::isNotNullOrWhitespace(
-            Request::getVar(
-                $this->getCheckboxName(),
-                "",
-                "str"
-            )
+    public function isChecked(): bool {
+        $value = Request::getVar(
+                        $this->getCheckboxName(),
+                        "",
+                        "str"
         );
+        return StringHelper::isNotNullOrWhitespace($value);
     }
 
     // check if the gdpr checkbox is checked
     // after that execute success or failed callback
     public function check(
-        ?callable $success = null,
-        ?callable $failed = null
+            ?callable $success = null,
+            ?callable $failed = null
     ): void {
         if ($this->isChecked()) {
             if ($success != null) {
@@ -69,19 +64,18 @@ class PrivacyCheckbox
     // render the dgpr checkbox input
     // the dgpr accept text can be written in a html editor
     // this method replaces the [checkbox] placeholder with the checkbox input
-    public function render(): string
-    {
+    public function render(): string {
         $checkboxHtml = Input::checkBox(
-            $this->getCheckboxName(),
-            false,
-            "✔",
-            [
+                        $this->getCheckboxName(),
+                        false,
+                        "✔",
+                        [
                             "required" => "required",
                             "id" => $this->getCheckboxName()
                         ]
         );
         $fullHtml = Settings::get(
-            "privacy_policy_checkbox_text_{$this->language}"
+                        "privacy_policy_checkbox_text_{$this->language}"
         );
         if (!$this->isEnabled() or
                 StringHelper::isNullOrWhitespace($fullHtml)
@@ -89,10 +83,11 @@ class PrivacyCheckbox
             return "";
         }
         $fullHtml = str_ireplace(
-            "[checkbox]",
-            $checkboxHtml,
-            $fullHtml
+                "[checkbox]",
+                $checkboxHtml,
+                $fullHtml
         );
         return $fullHtml;
     }
+
 }
