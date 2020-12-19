853e84a067fd721591b771370e018926b7aae6a7
derUli <admin@deruli.de>
2020-12-19 02:00:30 +0100

    Update CsvWriterTest.php

--- ulicms/tests/api/CsvWriterTest.php
+++ ulicms/tests/api/CsvWriterTest.php
@@ -1,9 +1,19 @@
 <?php
 
-class CsvWriterTest extends \PHPUnit\Framework\TestCase
-{
-    public function testGetCSV()
-    {
-        $this->assertEquals('foo,123,"29,95"', getCSV(array("foo", 123, 29.95)));
+class CsvWriterTest extends \PHPUnit\Framework\TestCase {
+
+    public function testGetCSV() {
+        $this->assertEquals(
+                'foo,123,29.95,"foo bar"',
+                getCSV(
+                        [
+                            "foo",
+                            123,
+                            29.95,
+                            "foo bar"
+                        ]
+                )
+        );
     }
+
 }
