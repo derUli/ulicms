54bbbc2d69f85950314b7b1c90517041b47de186
derUli <admin@deruli.de>
2021-03-28 10:37:47 +0200

    fix all tests for php 8

--- ulicms/lib/translation.php
+++ ulicms/lib/translation.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-function get_translation(string $name, array $placeholders = []): string
-{
+function get_translation(string $name, array $placeholders = []): string {
     $iname = strtoupper($name);
     foreach (get_defined_constants() as $key => $value) {
         if (startsWith($key, "TRANSLATION_")
@@ -25,57 +24,48 @@ function get_translation(string $name, array $placeholders = []): string
     return $name;
 }
 
-function _t(string $name, array $placeholders = []): string
-{
+function _t(string $name, array $placeholders = []): string {
     return get_translation($name, $placeholders);
 }
 
-function t(string $name, array $placeholders = []): void
-{
+function t(string $name, array $placeholders = []): void {
     echo _t($name, $placeholders);
 }
 
-function singularOrPlural(int $count, string $singular, string $plural)
-{
+function singularOrPlural(int $count, string $singular, string $plural) {
     return $count === 1 ?
-            str_ireplace("%number%", $count, $singular) :
-            str_ireplace("%number%", $count, $plural);
+            str_ireplace("%number%", strval($count), strval($singular)) :
+            str_ireplace("%number%", strval($count), strval($plural));
 }
 
-function translation(string $name, array $placeholders = []): void
-{
+function translation(string $name, array $placeholders = []): void {
     echo get_translation($name, $placeholders);
 }
 
-function translate(string $name, array $placeholders = [])
-{
+function translate(string $name, array $placeholders = []) {
     translation($name, $placeholders);
 }
 
 function get_secure_translation(
-    string $name,
-    array $placeholders = []
+        string $name,
+        array $placeholders = []
 ): string {
     return Template::getEscape(get_translation($name, $placeholders));
 }
 
-function secure_translation(string $name, array $placeholders = []): void
-{
+function secure_translation(string $name, array $placeholders = []): void {
     echo get_secure_translation($name, $placeholders);
 }
 
-function secure_translate(string $name, array $placeholders = []): void
-{
+function secure_translate(string $name, array $placeholders = []): void {
     secure_translation($name, $placeholders);
 }
 
-function add_translation(string $key, string $value): void
-{
+function add_translation(string $key, string $value): void {
     register_translation($key, $value);
 }
 
-function register_translation(string $key, string $value): void
-{
+function register_translation(string $key, string $value): void {
     $key = strtoupper($key);
     if (!startsWith($key, "TRANSLATION_")) {
         $key = "TRANSLATION_" . $key;
@@ -83,12 +73,11 @@ function register_translation(string $key, string $value): void
     idefine($key, $value);
 }
 
-function getFrontendLanguage()
-{
+function getFrontendLanguage() {
     $domainLanguage = get_domain() ?
             getDomainByLanguage(get_domain()) : null;
     $fallbackLanguage = $domainLanguage ?
             $domainLanguage : Settings::get("language");
-    
+
     return isset($_SESSION["language"]) ? $_SESSION["language"] : $fallbackLanguage;
 }
--- ulicms/tests/utils/PreparedStatementsTest.php
+++ ulicms/tests/utils/PreparedStatementsTest.php
@@ -1,17 +1,18 @@
 <?php
 
-class PreparedStatementsTest extends \PHPUnit\Framework\TestCase
-{
-    public function testPreparedStatements()
-    {
-        $result = Database::pQuery("SELECT ? as wert1, ? as wert2, ? as wert3, ? as wert4, ? as wert5, ? as wert6", array(
-                    123,
-                    1.85,
-                    "My Text",
-                    "2014-11-22 13:23:44.657",
-                    true,
-                    false
-        ));
+class PreparedStatementsTest extends \PHPUnit\Framework\TestCase {
+
+    public function testPreparedStatements() {
+        $result = Database::pQuery(
+                        "SELECT ? as wert1, ? as wert2, ? as wert3, ? as wert4, ? as wert5, ? as wert6",
+                        [
+                            123,
+                            1.85,
+                            "My Text",
+                            "2014-11-22 13:23:44.657",
+                            true,
+                            false
+        ]);
         $dataset = Database::fetchObject($result);
         $this->assertEquals(123, $dataset->wert1);
         $this->assertEquals("1.85", $dataset->wert2);
@@ -20,4 +21,5 @@ class PreparedStatementsTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(1, $dataset->wert5);
         $this->assertEquals(0, $dataset->wert6);
     }
+
 }
