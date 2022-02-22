215faa96620764887309efcae50f823c036b0974
Uli <admin@deruli.de>
2022-02-22 11:50:41 +0100

    Update constants.php

--- ulicms/lib/constants.php
+++ ulicms/lib/constants.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/**
+ * Defines a constant if it isn't defined yet
+ * @param string $key Constant Name
+ * @param type $value Value
+ * @return bool returns false if the constant was already set
+ */
 function idefine(string $key, $value): bool
 {
     $key = strtoupper($key);
