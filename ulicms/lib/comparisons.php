e5973ac1f1309d27d7f9a173ac5755570a2694a5
Uli <admin@deruli.de>
2022-02-27 15:48:41 +0100

    Update comparisons.php

--- ulicms/lib/comparisons.php
+++ ulicms/lib/comparisons.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 use Carbon\Carbon;
 
-function faster_in_array($needle, $haystack): bool
-{
+function faster_in_array($needle, $haystack): bool {
     if (!is_array($haystack)) {
         return false;
     }
@@ -14,113 +13,96 @@ function faster_in_array($needle, $haystack): bool
 }
 
 // is $val a decimal number or a integer?
-function is_decimal($val): bool
-{
+function is_decimal($val): bool {
     return is_numeric($val) && !ctype_digit(strval($val));
 }
 
-function is_zero($val): bool
-{
+function is_zero($val): bool {
     return is_numeric($val) && $val == 0;
 }
 
-function is_today($datetime = null): bool
-{
+function is_today($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isToday();
 }
 
-function midnight($datetime = null)
-{
+function midnight($datetime = null) {
     $carbon = get_carbon($datetime);
     $midnight = $carbon->startOfDay();
     return $midnight->getTimestamp();
 }
 
-function midday($datetime = null)
-{
+function midday($datetime = null) {
     $carbon = get_carbon($datetime);
     $mdiday = $carbon->midday();
     return $mdiday->getTimestamp();
 }
 
-function end_of_day($datetime = null)
-{
+function end_of_day($datetime = null) {
     $carbon = get_carbon($datetime);
     $endOfDay = $carbon->endOfDay();
     return $endOfDay->getTimestamp();
 }
 
-function is_midnight($datetime = null): bool
-{
+function is_midnight($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isStartOfDay();
 }
 
-function is_end_of_day($datetime = null): bool
-{
+function is_end_of_day($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isEndOfDay();
 }
 
-function is_midday($datetime = null): bool
-{
+function is_midday($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isMidday();
 }
 
-function is_tomorrow($datetime = null): bool
-{
+function is_tomorrow($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isTomorrow();
 }
 
-function is_yesterday($datetime = null): bool
-{
+function is_yesterday($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isYesterday();
 }
 
-function is_past($datetime = null): bool
-{
+function is_past($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isPast();
 }
 
-function is_future($datetime = null): bool
-{
+function is_future($datetime = null): bool {
     $carbon = get_carbon($datetime);
     return $carbon->isFuture();
 }
 
-function get_carbon($datetime = null): Carbon
-{
+function get_carbon($datetime = null): Carbon {
     return new Carbon($datetime ?? time(), date_default_timezone_get());
 }
 
-function is_blank($val = null): bool
-{
+function is_blank($val = null): bool {
     return isset($val) && (is_string($val) &&
             StringHelper::isNullOrWhitespace($val)) ||
             empty($val);
 }
 
-function is_present($val = null): bool
-{
+function is_present($val = null): bool {
     return isset($val) && !is_blank($val);
 }
+
 /**
  * Checks if a string is valid JSON
  * @param string|null $str input string
  * @return bool
  */
-function is_json(?string $str): bool
-{
+function is_json(?string $str): bool {
     return !is_null($str) ? json_decode($str) != null : false;
 }
 
-function is_numeric_array($var): bool
-{
+function is_numeric_array($var): bool {
     if (!is_array($var)) {
         return false;
     }
@@ -132,29 +114,24 @@ function is_numeric_array($var): bool
     return true;
 }
 
-function is_true($var): bool
-{
+function is_true($var): bool {
     return (isset($var) and $var);
 }
 
-function is_false($var): bool
-{
+function is_false($var): bool {
     return !(isset($var) and $var);
 }
 
 // sind wir gerade im Adminordner?
-function is_admin_dir(): bool
-{
+function is_admin_dir(): bool {
     return basename(getcwd()) === "admin";
 }
 
-function is_desktop(): bool
-{
+function is_desktop(): bool {
     return !is_mobile();
 }
 
-function is_crawler(?string $useragent = null): bool
-{
+function is_crawler(?string $useragent = null): bool {
     if (!$useragent && Request::getUserAgent()) {
         $useragent = Request::getUserAgent();
     }
@@ -177,8 +154,7 @@ function is_crawler(?string $useragent = null): bool
 
 // 21. Februar 2015
 // Nutzt nun die Klasse Mobile_Detect
-function is_mobile(): bool
-{
+function is_mobile(): bool {
     $result = false;
 
     if (class_exists("Mobile_Detect")) {
@@ -186,8 +162,7 @@ function is_mobile(): bool
         $result = $detect->isMobile();
     }
 
-    if (Settings::get("no_mobile_design_on_tablet")
-            && $result && $detect->isTablet()) {
+    if (Settings::get("no_mobile_design_on_tablet") && $result && $detect->isTablet()) {
         $result = false;
     }
 
@@ -202,8 +177,7 @@ function is_mobile(): bool
  * Checks if the maintenance mode is enabled
  * @return bool
  */
-function isMaintenanceMode(): bool
-{
+function isMaintenanceMode(): bool {
     if (!is_string(Settings::get("maintenance_mode"))) {
         return false;
     }
@@ -212,8 +186,7 @@ function isMaintenanceMode(): bool
             Settings::get("maintenance_mode") == "1");
 }
 
-function is_tablet(): bool
-{
+function is_tablet(): bool {
     $result = false;
 
     if (class_exists("Mobile_Detect")) {
@@ -229,8 +202,7 @@ function is_tablet(): bool
  * to the system
  * @return bool
  */
-function is_admin(): bool
-{
+function is_admin(): bool {
     $isAdmin = false;
     $user_id = get_user_id();
     if ($user_id) {
@@ -241,15 +213,13 @@ function is_admin(): bool
 }
 
 // Check if it is night (current hour between 0 and 4 o'Clock AM)
-function is_night(?int $time = null): bool
-{
+function is_night(?int $time = null): bool {
     $time = $time ? $time : time();
     $hour = (int) date("G", $time);
     return ($hour >= 0 and $hour <= 4);
 }
 
-function is_debug_mode(): bool
-{
+function is_debug_mode(): bool {
     $config = new CMSConfig();
     return (defined("ULICMS_DEBUG") and ULICMS_DEBUG)
             or (isset($config->debug) and $config->debug);
@@ -259,35 +229,31 @@ function is_debug_mode(): bool
  * Checks if the application is running from command line
  * @return bool
  */
-function isCLI(): bool
-{
+function isCLI(): bool {
     return php_sapi_name() == "cli";
 }
 
-function startsWith(string $haystack, string $needle, bool $case = true): bool
-{
+function startsWith(string $haystack, string $needle, bool $case = true): bool {
     if ($case) {
         return (strcmp(substr($haystack, 0, strlen($needle)), $needle) === 0);
     }
     return (strcasecmp(substr($haystack, 0, strlen($needle)), $needle) === 0);
 }
 
-function endsWith(string $haystack, string $needle, bool $case = true): bool
-{
+function endsWith(string $haystack, string $needle, bool $case = true): bool {
     if ($case) {
         return (strcmp(
-            substr($haystack, strlen($haystack) - strlen($needle)),
-            $needle
-        ) === 0);
+                        substr($haystack, strlen($haystack) - strlen($needle)),
+                        $needle
+                ) === 0);
     }
     return (strcasecmp(
-        substr($haystack, strlen($haystack) - strlen($needle)),
-        $needle
-    ) === 0);
+                    substr($haystack, strlen($haystack) - strlen($needle)),
+                    $needle
+            ) === 0);
 }
 
-function var_is_type($var, $type, $required = false): bool
-{
+function var_is_type($var, $type, $required = false): bool {
     $methodName = "is_{$type}";
 
     if ($var === null or $var === "") {
@@ -305,7 +271,6 @@ function var_is_type($var, $type, $required = false): bool
  * @param string|null $input
  * @return bool
  */
-function is_version_number(?string $input): bool
-{
+function is_version_number(?string $input): bool {
     return ($input and version_compare($input, '0.0.1', '>='));
 }
