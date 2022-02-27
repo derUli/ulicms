a0a11e0c4e1bfd520c6d644e8a2677a048e4b6c7
Uli <admin@deruli.de>
2022-02-27 15:59:49 +0100

    Kommentare

--- ulicms/lib/csrf_token.php
+++ ulicms/lib/csrf_token.php
@@ -1,9 +1,10 @@
 <?php
 
-// HTML Code für Anti CSRF Token zurückgeben
-// Siehe http://de.wikipedia.org/wiki/Cross-Site-Request-Forgery
-function get_csrf_token_html(): string
-{
+/**
+ * Gibt ein Input Field mit dem CSRF Token zurück
+ * @return string
+ */
+function get_csrf_token_html(): string {
     $html = '<input type="hidden" name="csrf_token" value="' .
             get_csrf_token() . '">';
     if (Settings::get("min_time_to_fill_form", "int") > 0) {
@@ -14,31 +15,44 @@ function get_csrf_token_html(): string
     return optimizeHtml($html);
 }
 
-function csrf_token_html(): void
-{
+/**
+ * Gibt ein Input Field mit dem CSRF Token aus
+ * @return void
+ */
+function csrf_token_html(): void {
     echo get_csrf_token_html();
 }
 
-function get_csrf_token(): string
-{
+/**
+ * Gibt den CSRF Token für die aktuelle Sitzung zurück
+ * Wenn noch keiner vorhanden ist, wird dieser generiert
+ * @return string CSRF Token
+ */
+function get_csrf_token(): string {
     if (!isset($_SESSION["csrf_token"])) {
         $_SESSION["csrf_token"] = md5(uniqid());
     }
+
     return $_SESSION["csrf_token"];
 }
 
-// Prüfen, ob Anti CSRF Token vorhanden ist
-// Siehe http://de.wikipedia.org/wiki/Cross-Site-Request-Forgery
-function check_csrf_token(): bool
-{
+/**
+ * Prüft, ob der übergebene CSRF Token korrekt ist.
+ * @return bool
+ */
+function check_csrf_token(): bool {
     if (!isset($_REQUEST["csrf_token"])) {
         return false;
     }
+
     return $_REQUEST["csrf_token"] == $_SESSION["csrf_token"];
 }
 
-function no_anti_csrf(): void
-{
+/**
+ * Deaktiviert den CSRF Check für diesen Request
+ * @return void
+ */
+function no_anti_csrf(): void {
     if (!defined("NO_ANTI_CSRF")) {
         define("NO_ANTI_CSRF", true);
     }
