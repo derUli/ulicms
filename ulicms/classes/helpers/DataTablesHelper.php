c563b7d98f096c99d650d2aa1432dbb9ef1f8659
Uli <admin@deruli.de>
2022-03-02 21:28:00 +0100

    Update DataTablesHelper.php

--- ulicms/classes/helpers/DataTablesHelper.php
+++ ulicms/classes/helpers/DataTablesHelper.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace UliCMS\Helpers;
 
+/**
+ * Methods for DataTables
+ */
 class DataTablesHelper extends \Helper {
 
-    // returns the path of the datatables language file
-    // if there is no language file for the current language
-    // it returns the path to the english language file
+    /**
+     * Returns the path of the datatables language file
+     * if there is no language file for the current language
+     * it returns the path to the english language file
+     * @param string $lang Language Code
+     * @return string Language File URL 
+     */
     public static function getLanguageFileURL(string $lang): string {
         $baseUrl = "scripts/datatables/lang";
         $file = "{$baseUrl}/{$lang}.lang";
