190a5ed5b60640e7e46b68d2d4df82e3f578831e
Uli <admin@deruli.de>
2022-12-07 14:47:47 +0100

    Update RoboBaseTest.php

--- ulicms/tests/robo/RoboBaseTest.php
+++ ulicms/tests/robo/RoboBaseTest.php
@@ -8,10 +8,15 @@ require_once __DIR__ . "/RoboTestFile.php";
 abstract class RoboBaseTest extends \PHPUnit\Framework\TestCase {
 
     protected function runRoboCommand(array $command): string {
-        $runner = new Robo\Runner(RoboTestFile::class);
-        array_unshift($command, "vendor/bin/robo");
         ob_start();
-        $runner->execute($command);
+        passthru(
+                "vendor" .
+                DIRECTORY_SEPARATOR .
+                "bin" .
+                DIRECTORY_SEPARATOR .
+                "robo " .
+                $command[0]
+        );
         return trim(ob_get_clean());
     }
 
