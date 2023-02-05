09a353538f40f92d90fcbea44852618b2278617b
Uli <admin@deruli.de>
2023-02-05 20:45:34 +0100

    Update cache.php

--- ulicms/lib/cache.php
+++ ulicms/lib/cache.php
@@ -7,13 +7,11 @@ use UliCMS\Utils\CacheUtil;
 // Alle Caches leeren
 // Sowohl den Seiten-Cache, den Download/Paketmanager Cache
 // als auch den APC Bytecode Cache
-function clearCache(): void
-{
+function clearCache(): void {
     CacheUtil::clearCache();
 }
 
-function no_cache($do = false): void
-{
+function no_cache($do = false): void {
     if ($do) {
         Flags::setNoCache(true);
     } elseif (in_array(get_cache_control(), ["auto", "no_cache"])) {
