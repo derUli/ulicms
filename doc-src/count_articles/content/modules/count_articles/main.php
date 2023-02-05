c245b2b1d13fd3cb73bb967254969acfdbee3a45
Uli <admin@deruli.de>
2023-02-05 21:47:40 +0100

    Veraltete Dokudateien gelöscht

--- doc-src/count_articles/content/modules/count_articles/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-function count_articles_render(){
-   $args = array("article", getCurrentLanguage(true), 1);
-   $data = Database::pQuery("select id from {prefix}content where type = ? and language = ? and active = ?", $args, true);
-   return Database::getNumRows($data);
-}
