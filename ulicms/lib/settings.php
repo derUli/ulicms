67541b1dea4f7fda584482e7be3f8d31de4a24f8
Uli <admin@deruli.de>
2023-02-05 21:31:56 +0100

    Update settings.php

--- ulicms/lib/settings.php
+++ ulicms/lib/settings.php
@@ -4,17 +4,34 @@ declare(strict_types=1);
 
 use UliCMS\Constants\AuditLog;
 
-// get a config variable
+/**
+ * Returns a setting
+ * @param string $key
+ * @deprecated since 2023.1
+ * @return type
+ */
 function getconfig(string $key) {
     return Settings::get($key);
 }
 
+/**
+ * @deprecated since 2023.1
+ * Deletes a setting
+ * @param string $key
+ * @return bool
+ */
 // Remove an configuration variable
 function deleteconfig(string $key): bool {
     return Settings::delete($key);
 }
 
-// Set a configuration Variable;
+/**
+ * 
+ * Sets a setting
+ * @param string $key
+ * @param type $value
+ * @deprecated since 2023.1
+ */
 function setconfig(string $key, $value) {
     $result = db_query("SELECT id FROM " . tbname("settings") .
             " WHERE name='$key'");
@@ -39,6 +56,13 @@ function setconfig(string $key, $value) {
     }
 }
 
+/**
+ * Adds a setting if not exist yet
+ * @param string $key
+ * @param type $value
+ * @deprecated since 2023.1
+ * @return bool
+ */
 function initconfig(string $key, $value): bool {
     $success = false;
     if (!Settings::get($key)) {
