78c5b605c67b437e367720fe7e767a679bd0bf27
Uli <admin@deruli.de>
2023-02-06 01:20:03 +0100

    Update translation.php

--- ulicms/lib/translation.php
+++ ulicms/lib/translation.php
@@ -6,7 +6,7 @@ function get_translation(string $name, array $placeholders = []): string {
     $iname = strtoupper($name);
     foreach (get_defined_constants() as $key => $value) {
         if (startsWith($key, "TRANSLATION_")
-                and $key == "TRANSLATION_" . $iname) {
+                && $key == "TRANSLATION_" . $iname) {
             $custom_translation = Translation::get($key);
             if ($custom_translation !== null) {
                 $value = $custom_translation;
@@ -15,7 +15,7 @@ function get_translation(string $name, array $placeholders = []): string {
             // als assoziatives Array als zweiter Parameter
             // dem Funktionsaufruf mitgegeben werden
             foreach ($placeholders as $placeholder => $replacement) {
-                $value = str_ireplace($placeholder, $replacement, $value);
+                $value = str_ireplace($placeholder, (string)$replacement, $value);
             }
 
             return $value;
