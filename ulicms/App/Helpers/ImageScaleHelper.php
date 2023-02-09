ec799ff6ef0ca7c26bb0cb5278739c5d1e93cd69
Uli <admin@deruli.de>
2023-02-09 22:38:19 +0100

    Update ImageScaleHelper.php

--- ulicms/App/Helpers/ImageScaleHelper.php
+++ ulicms/App/Helpers/ImageScaleHelper.php
@@ -10,34 +10,35 @@ use ImagineHelper;
 use Imagine\Image\Box;
 use Imagine\Image\ImageInterface;
 
-class ImageScaleHelper extends Helper
-{
-    public static function getMaxImageDimensions(): ?array
-    {
+class ImageScaleHelper extends Helper {
+
+    public static function getMaxImageDimensions(): ?array {
         $dimensions = null;
         $scale = strtolower(Settings::get("max_image_dimensions") ?? '');
 
         $explodedScale = explode("x", $scale);
 
         if (count($explodedScale) === 2) {
+
             if (!empty($explodedScale[0])) {
-                $width = intval($explodedScale[0]);
+                $width = (int) $explodedScale[0];
             }
+
             if (!empty($explodedScale[1])) {
-                $height = intval($explodedScale[1]);
+                $height = (int) $explodedScale[1];
             }
         }
 
         if ($width && $height) {
             $dimensions = [$width, $height];
         }
-        
+
         return $dimensions;
     }
 
     public static function scaleDown(
-        string $file,
-        ?string $outputFile = null
+            string $file,
+            ?string $outputFile = null
     ): bool {
         $dimensions = ImageScaleHelper::getMaxImageDimensions();
 
@@ -52,8 +53,11 @@ class ImageScaleHelper extends Helper
             $imagine->open($file)
                     ->thumbnail($size, $mode)
                     ->save($outputFile ? $outputFile : $file);
+
             $scaled = true;
         }
+
         return $scaled;
     }
+
 }
