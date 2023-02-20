38f571d0e38f88732acb84347f4dd1cba152d3c9
Uli <admin@deruli.de>
2023-02-20 20:54:17 +0100

    Update update.php

--- dist/update.php
+++ dist/update.php
@@ -4,6 +4,9 @@ const CORE_COMPONENT = 'update';
 
 require_once dirname(__FILE__) . '/init.php';
 
+// Enable maintenance mode
+Settings::set('maintenance_mode', "1");
+
 use App\Packages\PatchManager;
 use App\Utils\CacheUtil;
 
@@ -18,11 +21,17 @@ $version = new UliCMSVersion();
 $versionNumber = $version->getInternalVersionAsString();
 Settings::set("db_schema_version", $versionNumber);
 
+Settings::set("maintenance_mode", "0");
+
 // Reset tracking of installed patches
 $patchManager = new PatchManager();
 $patchManager->truncateInstalledPatches();
 
+// Clear Cache
 CacheUtil::clearCache();
+
+// Disable maintenance mode
+Settings::set('maintenance_mode', "0");
 
 // The line below will be uncommented by the mk-upgrade-package.py deploy script
 // The script will delete itself after execution.
