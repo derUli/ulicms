6863cb51526fc48729ed3618883cb63e1387029b
Uli <admin@deruli.de>
2023-03-26 15:15:47 +0200

    Update DefaultContentTypes.php

--- dist/App/Models/Content/Types/DefaultContentTypes.php
+++ dist/App/Models/Content/Types/DefaultContentTypes.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models\Content\Types;
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 use App\Models\Content\Types\ContentType;
 
 class DefaultContentTypes
