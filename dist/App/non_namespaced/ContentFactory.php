a7694806a3e29223ea1a7c7491933ddb6eecde08
Uli <admin@deruli.de>
2023-03-28 23:31:46 +0200

    Update ContentFactory.php

--- dist/App/non_namespaced/ContentFactory.php
+++ dist/App/non_namespaced/ContentFactory.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 use App\Exceptions\DatasetNotFoundException;
 use App\Exceptions\UnknownContentTypeException;
 use App\Models\Content\TypeMapper;
