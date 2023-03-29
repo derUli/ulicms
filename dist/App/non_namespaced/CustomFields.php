5bfcb564bce461ba600baed0d9fbcec75369e137
Uli <admin@deruli.de>
2023-03-29 20:02:44 +0200

    Update CustomFields.php

--- dist/App/non_namespaced/CustomFields.php
+++ dist/App/non_namespaced/CustomFields.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 // This class contains methods to manipulate CustomFields
 // defined by modules
 class CustomFields
