12fc45585c1a426704ba8075f299784874b93370
Uli <admin@deruli.de>
2023-03-30 23:10:22 +0200

    Update ModuleHelper.php

--- dist/App/non_namespaced/ModuleHelper.php
+++ dist/App/non_namespaced/ModuleHelper.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 use App\Models\Content\Language;
 use App\HTML\Form;
 use App\Constants\RequestMethod;
