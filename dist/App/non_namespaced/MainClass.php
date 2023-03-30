7ae45782137c2f6570171c9688155082a2c82649
Uli <admin@deruli.de>
2023-03-30 22:31:58 +0200

    Update MainClass.php

--- dist/App/non_namespaced/MainClass.php
+++ dist/App/non_namespaced/MainClass.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 // Use this as base class for the MainClass of your module
 abstract class MainClass extends Controller
 {
