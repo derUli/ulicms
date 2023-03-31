2052532aa7386229e05e02d8534d4b5510537edf
Uli <admin@deruli.de>
2023-03-31 12:00:20 +0200

    Update MultilineTextField.php

--- dist/App/non_namespaced/MultilineTextField.php
+++ dist/App/non_namespaced/MultilineTextField.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 class MultilineTextField extends CustomField
 {
     public function render($value = null): string
