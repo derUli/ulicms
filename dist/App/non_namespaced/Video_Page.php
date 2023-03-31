023761251fafda3df38818cd64aacf2ff02d8163
Uli <admin@deruli.de>
2023-03-31 14:45:39 +0200

    Update Video_Page.php

--- dist/App/non_namespaced/Video_Page.php
+++ dist/App/non_namespaced/Video_Page.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') or exit('no direct script access allowed');
+
 use App\Models\Media\Video;
 
 // video pages are pages that are linked to video files
