673d96d5a1198f714de21e3517a0a59f5c2826e8
Uli <admin@deruli.de>
2023-04-07 13:45:27 +0200

    Update list.php

--- dist/content/modules/core_content/templates/banners/list.php
+++ dist/content/modules/core_content/templates/banners/list.php
@@ -10,9 +10,11 @@ if ($permissionChecker->hasPermission("banners")) {
     if (!isset($_SESSION["filter_category"])) {
         $_SESSION["filter_category"] = 0;
     }
+    
     if (isset($_GET["filter_category"])) {
-        $_SESSION["filter_category"] = intval($_GET["filter_category"]);
+        $_SESSION["filter_category"] = (int)$_GET["filter_category"];
     }
+
     if ($_SESSION["filter_category"] == 0) {
         $banners = Banners::getAll();
     } else {
