cce7e99b3f84f354325ecbd0d85e6795dd4cfd96
Uli <admin@deruli.de>
2023-04-07 14:38:41 +0200

    Update CoreFormsControllerTest.php

--- dist/tests/controllers/core_forms/CoreFormsControllerTest.php
+++ dist/tests/controllers/core_forms/CoreFormsControllerTest.php
@@ -32,10 +32,10 @@ class CoreFormsControllerTest extends \PHPUnit\Framework\TestCase
     public function testIncSpamCount()
     {
         $controller = new CoreFormsController();
-        $initialCount = intval(Settings::get("contact_form_refused_spam_mails"));
+        $initialCount = (int)Settings::get("contact_form_refused_spam_mails");
 
         for ($i = 1; $i <= 3; $i++) {
-            $oldCount = intval(Settings::get("contact_form_refused_spam_mails"));
+            $oldCount = (int)Settings::get("contact_form_refused_spam_mails");
             $newCount = $controller->_incSpamCount();
             $this->assertIsInt($newCount);
             $this->assertGreaterThan($oldCount, $newCount);
@@ -43,7 +43,7 @@ class CoreFormsControllerTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals(
             $initialCount + 3,
-            intval(Settings::get("contact_form_refused_spam_mails"))
+            (int)Settings::get("contact_form_refused_spam_mails")
         );
     }
 
