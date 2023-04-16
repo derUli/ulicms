7e88ab8df5ca1da163fc730297b86396a1986b35
Ulrich Schmidt <support@ulicms.de>
2023-04-16 23:49:46 +0200

    test

---
+++ dist/tests/robo/RoboBuildTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Nette\Utils\FileSystem;
+
+require_once __DIR__ . '/RoboTestFile.php';
+require_once __DIR__ . '/RoboTestBase.php';
+
+class RoboBuildTest extends RoboTestBase
+{
+    public function testBuildCopyChangelog()
+    {
+        $source = ULICMS_ROOT . '/../doc/changelog.txt';
+        $target = ULICMS_CONTENT . '/modules/core_info/changelog.txt';
+
+        FileSystem::delete($target);
+        $this->assertFileDoesNotExist($target);
+
+        $this->runRoboCommand(['build:copy-changelog']);
+        $this->assertFileEquals($target, $source);
+    }
+
+    public function testBuildLicenses()
+    {
+        $file1 = ULICMS_ROOT . '/licenses.md';
+        $file2 = ULICMS_ROOT . '/licenses.json';
+
+        FileSystem::delete($file1);
+        FileSystem::delete($file2);
+
+        $this->assertFileDoesNotExist($file1);
+        $this->assertFileDoesNotExist($file2);
+
+        $this->runRoboCommand(['build:licenses']);
+
+        $this->assertFileExists($file1);
+        $this->assertFileExists($file2);
+    }
+}
