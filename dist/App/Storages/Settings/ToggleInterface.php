0531d000f5c9f71e20644e41cc1290dd1ba4ba7d
Ulrich Schmidt <support@ulicms.de>
2023-04-29 17:05:21 +0200

    ToggleInterface Klasse erstellt

---
+++ dist/App/Storages/Settings/ToggleInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Storages\Settings;
+
+defined('ULICMS_ROOT') || exit('No direct script access allowed');
+
+/**
+ * Use this interface to implement On/Off toggle switches
+ */
+interface ToggleInterface {
+
+    public function isEnabled(): bool;
+
+    public function enable(): void;
+
+    public function disable(): void;
+
+    public function toggle(): bool;
+}
