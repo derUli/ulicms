259ebb1169a7941472d10d96fa5cea00098d6bd0
Ulrich Schmidt <support@ulicms.de>
2023-04-30 15:16:07 +0200

    System requirements check with composer

--- dist/installer.aus/views/steps/step3.php
+++ dist/installer.aus/views/steps/step3.php
@@ -2,47 +2,11 @@
 defined('ULICMS_ROOT') || exit('No direct script access allowed');
 ?>
 <?php
-use App\Utils\VersionComparison;
-
-$minPhpRequired = '8.1.0';
-
-$phpVersion = PHP_VERSION;
-
-$phpVersionCompatible = VersionComparison::compare(
-    $phpVersion,
-    $minPhpRequired,
-    '>='
-);
 
 $isWritable = is_writable(ULICMS_ROOT);
-
-$phpModules = [
-    'mysqli',
-    'gd',
-    'json',
-    'mbstring',
-    'openssl',
-    'dom',
-    'xml',
-    'curl',
-    'intl'
-];
-
-sort($phpModules);
 ?>
 
 <ul>
-    <li>
-        <?php echo TRANSLATION_PHP_VERSION; ?>
-        <?php echo $minPhpRequired; ?>
-        <?php
-        if ($phpVersionCompatible) {
-            echo '<i class="fa fa-check text-green" aria-hidden="true"></i>';
-        } else {
-            echo '<i class="fa fa-exclamation-triangle text-red" aria-hidden="true"></i>';
-        }
-?>
-    </li>
     <li>
     <?php
 
@@ -55,21 +19,6 @@ sort($phpModules);
         }
 ?>
     </li>
-    <?php
-    foreach ($phpModules as $module) {
-        $check = extension_loaded($module);
-        echo '<li>';
-        echo TRANSLATION_PHP_MODULE . ' ' . htmlspecialchars($module) . ' ';
-
-        if ($check) {
-            echo '<i class="fa fa-check text-green" aria-hidden="true"></i>';
-        } else {
-            echo '<i class="fa fa-exclamation-triangle text-red" aria-hidden="true"></i>';
-        }
-
-        echo '</li>';
-    }
-?>
 </ul>
 
 <p>
--- dist/tests/environment/SystemRequirementsTest.php
+++ dist/tests/environment/SystemRequirementsTest.php
@@ -3,10 +3,6 @@
 use App\Helpers\TestHelper;
 
 class SystemRequirementsTest extends \PHPUnit\Framework\TestCase {
-    public function testPhpVersion(): void {
-        $this->assertTrue(\App\Utils\VersionComparison::compare(PHP_VERSION, '8.1.0', '>='));
-    }
-
     public function testMySQLVersion(): void {
         $this->assertTrue(
             \App\Utils\VersionComparison::compare($this->getMySQLVersion(), '5.5.3', '>=')
@@ -17,48 +13,8 @@ class SystemRequirementsTest extends \PHPUnit\Framework\TestCase {
         $this->assertDirectoryIsWritable(ULICMS_ROOT);
     }
 
-    public function testPhpModuleMySqli(): void {
-        $this->assertTrue(extension_loaded('mysqli'));
-    }
-
-    public function testPhpModuleGd(): void {
-        $this->assertTrue(extension_loaded('gd'));
-    }
-
-    public function testPhpModuleJson(): void {
-        $this->assertTrue(extension_loaded('json'));
-    }
-
-    public function testPhpModuleMbString(): void {
-        $this->assertTrue(extension_loaded('mbstring'));
-    }
-
-    public function testPhpModuleOpenSSL(): void {
-        $this->assertTrue(extension_loaded('openssl'));
-    }
-
-    public function testPhpModuleDom(): void {
-        $this->assertTrue(extension_loaded('dom'));
-    }
-
-    public function testPhpModuleXml(): void {
-        $this->assertTrue(extension_loaded('xml'));
-    }
-
-    public function testPhpModuleIntl(): void {
-        $this->assertTrue(extension_loaded('intl'));
-    }
-
-    public function testPhpModuleCurl(): void {
-        $this->assertTrue(extension_loaded('curl'));
-    }
-
     public function testConnectToUliCMSServices(): void {
         $this->assertNotNull(file_get_contents_wrapper('https://www.ulicms.de/', true));
-    }
-
-    public function testPhpIcoInstalled(): void {
-        $this->assertTrue(class_exists('PHP_ICO'));
     }
 
     public function testIsRunningPHPUnit(): void {
