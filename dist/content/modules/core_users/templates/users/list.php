221a243b7a742500167d99057e8cbce349feb098
Ulrich Schmidt <support@ulicms.de>
2023-05-05 15:22:49 +0200

    Berechtigungsprüfungen

--- dist/content/modules/core_users/templates/users/list.php
+++ dist/content/modules/core_users/templates/users/list.php
@@ -10,149 +10,145 @@ use function App\HTML\imageTag;
 
 $permissionChecker = PermissionChecker::fromCurrentUSer();
 
-if ($permissionChecker->hasPermission('users')) {
-    if (! isset($_SESSION['admins_filter_group'])) {
-        $_SESSION['admins_filter_group'] = 0;
-    }
-    if (Request::getVar('admins_filter_group') !== null) {
-        $_SESSION['admins_filter_group'] = Request::getVar('admins_filter_group', 0, 'int');
-    }
-    $manager = new UserManager();
-    if ($_SESSION['admins_filter_group'] > 0) {
-        $users = $manager->getUsersByGroupId($_SESSION['admins_filter_group']);
-    } else {
-        $users = $manager->getAllUsers();
-    }
-
-    $groups = Group::getAll();
-    ?>
-    <?php echo Template::executeModuleTemplate('core_users', 'icons.php'); ?>
-    <h2><?php translate('users'); ?></h2>
-    <?php if ($permissionChecker->hasPermission('users_create')) { ?>
-        <?php
-        echo Alert::info(
-            get_translation('users_infotext')
-        );
-        ?>
-        <div class="voffset2">    
-            <a href="index.php?action=admin_new&ref=admins"
-               class="btn btn-default is-not-ajax">
-                <i class="fa fa-plus"></i> 
-                <?php translate('create_user'); ?></a>
-        </div>
-    <?php } ?>
-    <form action="index.php" method="get" class="voffset2">
-        <strong>
-            <?php translate('primary_group'); ?>
-        </strong>
-
-        <input type="hidden" name="action" value="admins"> <select
-            name="admins_filter_group" size="1"
-            onchange="$(this).closest('form').submit();">
-            <option value="0"
-            <?php
-            if ($_SESSION ['admins_filter_group'] <= 0) {
-                echo 'selected';
-            }
-    ?>>[<?php translate('every'); ?>]</option>
-                    <?php foreach ($groups as $group) { ?>
-                <option
-                <?php
-        if ($group->getId() == $_SESSION ['admins_filter_group']) {
-            echo 'selected ';
-        }
-                        ?>
-                    value="<?php Template::escape($group->getId()); ?>"><?php Template::escape($group->getName()); ?></option>
-                <?php } ?>
-        </select>
-    </form>    
-    <?php if (count($users) > 0) { ?>
-        <div class="scroll voffset2">
-            <table class="tablesorter">
-                <thead>
-                    <tr style="font-weight: bold;">
-                        <th class="no-sort"></th>
-                        <th><?php translate('username'); ?></th>
-                        <th class="hide-on-mobile"><?php translate('lastname'); ?></th>
-                        <th class="hide-on-mobile"><?php translate('firstname'); ?></th>
-                        <th class="hide-on-mobile"><?php translate('email'); ?></th>
-                        <th class="hide-on-mobile"><?php translate('primary_group'); ?></th>
-                        <?php if ($permissionChecker->hasPermission('users_edit')) { ?>
-                            <td class="no-sort text-center">
-                                <?php translate('edit'); ?>
-                            </td>
-                            <td class="no-sort text-center">
-                                <?php translate('delete'); ?>
-                            </td>
-                        <?php } ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                            foreach ($users as $user) {
-                                $groupName = '[' . get_translation('none') . ']';
-
-                                $primaryGroup = $user->getPrimaryGroup();
-
-                                $groupName = $primaryGroup ? $primaryGroup->getName() : '[' . get_translation('none') . ']';
-                                ?>
-                        <?php
-                                $avatar = imageTag(
-                                    $user->getAvatar(),
-                                    [
-                                        'class' => 'avatar has-pointer',
-                                        'alt' => get_translation('show_in_original_size'),
-                                        'title' => get_translation('show_in_original_size'),
-                                        'data-name' => _esc($user->getUserName())
-                                    ]
-                                );
-                                echo '<tr id="dataset-' . $user->getId() . '">';
-                                echo '<td class="text-center">' . $avatar . '</td>';
-                                echo '<td>';
-
-                                echo _esc($user->getUsername()) . '</td>';
-                                echo '<td class="hide-on-mobile">' .
-                                _esc($user->getLastName()) .
-                                '</td>';
-
-                                echo '<td class="hide-on-mobile">' .
-                                _esc($user->getFirstname()) .
-                                '</td>';
-
-                                echo '<td class="hide-on-mobile">' .
-                                _esc($user->getEmail()) . '</td>';
-                                echo '<td class="hide-on-mobile">';
-                                $id = $user->getPrimaryGroupId();
-                                if ($id && $permissionChecker->hasPermission('groups_edit')) {
-                                    $url = \App\Helpers\ModuleHelper::buildActionURL('groups', "edit={$id}");
-                                    echo '<a href="' . Template::getEscape($url) . '" class="is-not-ajax">';
-                                }
-                                esc($groupName);
-
-                                if ($id && $permissionChecker->hasPermission('groups_edit')) {
-                                    echo '</a>';
-                                }
-                                echo '</td>';
-                                if ($permissionChecker->hasPermission('users_edit')) {
-                                    echo '<td class="text-center">' . '<a href="index.php?action=admin_edit&id=' . $user->getId() . '" class="is-not-ajax"><img class="mobile-big-image" src="gfx/edit.png" alt="' . get_translation('edit') . '" title="' . get_translation('edit') . '"></a></td>';
-
-                                    if ($user->getId() == $_SESSION['login_id']) {
-                                        echo '<td class="text-center"></td>';
-                                    } else {
-                                        echo '<td class="text-center">' . '<form action="index.php?sClass=UserController&sMethod=delete&id=' . $user->getId() . '" method="post" class="delete-form">' . get_csrf_token_html() . '<input type="image" class="mobile-big-image" src="gfx/delete.png"></form></td>';
-                                    }
-                                }
-                                echo '</tr>';
-                            }
-        ?>
-                </tbody>
-            </table>
-        <?php } ?>
-    </div>
-    <?php
-} else {
-    noPerms();
+if (! isset($_SESSION['admins_filter_group'])) {
+    $_SESSION['admins_filter_group'] = 0;
 }
+if (Request::getVar('admins_filter_group') !== null) {
+    $_SESSION['admins_filter_group'] = Request::getVar('admins_filter_group', 0, 'int');
+}
+$manager = new UserManager();
+if ($_SESSION['admins_filter_group'] > 0) {
+    $users = $manager->getUsersByGroupId($_SESSION['admins_filter_group']);
+} else {
+    $users = $manager->getAllUsers();
+}
+
+$groups = Group::getAll();
+?>
+<?php echo Template::executeModuleTemplate('core_users', 'icons.php'); ?>
+<h2><?php translate('users'); ?></h2>
+<?php if ($permissionChecker->hasPermission('users_create')) { ?>
+    <?php
+    echo Alert::info(
+        get_translation('users_infotext')
+    );
+    ?>
+    <div class="voffset2">    
+        <a href="index.php?action=admin_new&ref=admins"
+            class="btn btn-default is-not-ajax">
+            <i class="fa fa-plus"></i> 
+            <?php translate('create_user'); ?></a>
+    </div>
+<?php } ?>
+<form action="index.php" method="get" class="voffset2">
+    <strong>
+        <?php translate('primary_group'); ?>
+    </strong>
+
+    <input type="hidden" name="action" value="admins"> <select
+        name="admins_filter_group" size="1"
+        onchange="$(this).closest('form').submit();">
+        <option value="0"
+        <?php
+        if ($_SESSION ['admins_filter_group'] <= 0) {
+            echo 'selected';
+        }
+?>>[<?php translate('every'); ?>]</option>
+                <?php foreach ($groups as $group) { ?>
+            <option
+            <?php
+    if ($group->getId() == $_SESSION ['admins_filter_group']) {
+        echo 'selected ';
+    }
+                    ?>
+                value="<?php Template::escape($group->getId()); ?>"><?php Template::escape($group->getName()); ?></option>
+            <?php } ?>
+    </select>
+</form>    
+<?php if (count($users) > 0) { ?>
+    <div class="scroll voffset2">
+        <table class="tablesorter">
+            <thead>
+                <tr style="font-weight: bold;">
+                    <th class="no-sort"></th>
+                    <th><?php translate('username'); ?></th>
+                    <th class="hide-on-mobile"><?php translate('lastname'); ?></th>
+                    <th class="hide-on-mobile"><?php translate('firstname'); ?></th>
+                    <th class="hide-on-mobile"><?php translate('email'); ?></th>
+                    <th class="hide-on-mobile"><?php translate('primary_group'); ?></th>
+                    <?php if ($permissionChecker->hasPermission('users_edit')) { ?>
+                        <td class="no-sort text-center">
+                            <?php translate('edit'); ?>
+                        </td>
+                        <td class="no-sort text-center">
+                            <?php translate('delete'); ?>
+                        </td>
+                    <?php } ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                        foreach ($users as $user) {
+                            $groupName = '[' . get_translation('none') . ']';
+
+                            $primaryGroup = $user->getPrimaryGroup();
+
+                            $groupName = $primaryGroup ? $primaryGroup->getName() : '[' . get_translation('none') . ']';
+                            ?>
+                    <?php
+                            $avatar = imageTag(
+                                $user->getAvatar(),
+                                [
+                                    'class' => 'avatar has-pointer',
+                                    'alt' => get_translation('show_in_original_size'),
+                                    'title' => get_translation('show_in_original_size'),
+                                    'data-name' => _esc($user->getUserName())
+                                ]
+                            );
+                            echo '<tr id="dataset-' . $user->getId() . '">';
+                            echo '<td class="text-center">' . $avatar . '</td>';
+                            echo '<td>';
+
+                            echo _esc($user->getUsername()) . '</td>';
+                            echo '<td class="hide-on-mobile">' .
+                            _esc($user->getLastName()) .
+                            '</td>';
+
+                            echo '<td class="hide-on-mobile">' .
+                            _esc($user->getFirstname()) .
+                            '</td>';
+
+                            echo '<td class="hide-on-mobile">' .
+                            _esc($user->getEmail()) . '</td>';
+                            echo '<td class="hide-on-mobile">';
+                            $id = $user->getPrimaryGroupId();
+                            if ($id && $permissionChecker->hasPermission('groups_edit')) {
+                                $url = \App\Helpers\ModuleHelper::buildActionURL('groups', "edit={$id}");
+                                echo '<a href="' . Template::getEscape($url) . '" class="is-not-ajax">';
+                            }
+                            esc($groupName);
+
+                            if ($id && $permissionChecker->hasPermission('groups_edit')) {
+                                echo '</a>';
+                            }
+                            echo '</td>';
+                            if ($permissionChecker->hasPermission('users_edit')) {
+                                echo '<td class="text-center">' . '<a href="index.php?action=admin_edit&id=' . $user->getId() . '" class="is-not-ajax"><img class="mobile-big-image" src="gfx/edit.png" alt="' . get_translation('edit') . '" title="' . get_translation('edit') . '"></a></td>';
+
+                                if ($user->getId() == $_SESSION['login_id']) {
+                                    echo '<td class="text-center"></td>';
+                                } else {
+                                    echo '<td class="text-center">' . '<form action="index.php?sClass=UserController&sMethod=delete&id=' . $user->getId() . '" method="post" class="delete-form">' . get_csrf_token_html() . '<input type="image" class="mobile-big-image" src="gfx/delete.png"></form></td>';
+                                }
+                            }
+                            echo '</tr>';
+                        }
+    ?>
+            </tbody>
+        </table>
+    <?php } ?>
+</div>
+<?php
 
 $translation = new JSTranslation(
     [
--- dist/content/modules/core_users/templates/users/new.php
+++ dist/content/modules/core_users/templates/users/new.php
@@ -2,173 +2,167 @@
 
 defined('ULICMS_ROOT') || exit('No direct script access allowed');
 
-use App\Security\Permissions\PermissionChecker;
 use App\Translations\JSTranslation;
 
-$permissionChecker = PermissionChecker::fromCurrentUser();
 $groups = Group::getAll('name');
 
-if ($permissionChecker->hasPermission('users') && $permissionChecker->hasPermission('users_create')) {
-    $languages = getAvailableBackendLanguages();
-    $default_language = getSystemLanguage();
-    $ref = _esc(Request::getVar('ref', 'home'));
-    ?>
-    <div class="btn-toolbar">
-        <a href="<?php echo \App\Helpers\ModuleHelper::buildActionURL('admins'); ?>"
-           class="btn btn-default btn-back is-not-ajax"><i class="fa fa-arrow-left"></i> <?php translate('back'); ?></a>
+$languages = getAvailableBackendLanguages();
+$default_language = getSystemLanguage();
+$ref = _esc(Request::getVar('ref', 'home'));
+?>
+<div class="btn-toolbar">
+    <a href="<?php echo \App\Helpers\ModuleHelper::buildActionURL('admins'); ?>"
+        class="btn btn-default btn-back is-not-ajax"><i class="fa fa-arrow-left"></i> <?php translate('back'); ?></a>
+</div>
+<form action="index.php?sClass=UserController&sMethod=create"
+        method="post" id="edit-user" class="voffset2">
+            <?php csrf_token_html(); ?>
+    <input type="hidden" name="add_admin" value="add_admin">
+    <div class="field">
+        <strong class="field-label">
+            <?php translate('username'); ?>*
+        </strong>
+        <input type="text" required="required" name="username" value="">
     </div>
-    <form action="index.php?sClass=UserController&sMethod=create"
-          method="post" id="edit-user" class="voffset2">
-              <?php csrf_token_html(); ?>
-        <input type="hidden" name="add_admin" value="add_admin">
-        <div class="field">
+    <div class="row field">
+        <div class="col-xs-12 col-md-6">
             <strong class="field-label">
-                <?php translate('username'); ?>*
+                <?php translate('firstname'); ?>
             </strong>
-            <input type="text" required="required" name="username" value="">
+            <input type="text" name="firstname" value="">
         </div>
-        <div class="row field">
-            <div class="col-xs-12 col-md-6">
-                <strong class="field-label">
-                    <?php translate('firstname'); ?>
-                </strong>
-                <input type="text" name="firstname" value="">
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <strong class="field-label">
-                    <?php translate('lastname'); ?>
-                </strong> <input
-                    type="text" name="lastname" value="">
-            </div>
-        </div>
-        <div class="field">
+        <div class="col-xs-12 col-md-6">
             <strong class="field-label">
-                <?php translate('email'); ?>
-            </strong>
-            <input type="email" name="email" value="">
+                <?php translate('lastname'); ?>
+            </strong> <input
+                type="text" name="lastname" value="">
         </div>
-        <div class="row field">
-            <div class="col-xs-12 col-md-6">
-                <strong class="field-label">
-                    <?php translate('password'); ?>*
-                </strong>
-                <input type="password" required="required" name="password"
-                       class="password-security-check"
-                       id="password" value="" autocomplete="new-password">
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <strong class="field-label">
-                    <?php translate('password_repeat'); ?>*
-                </strong>
-                <input type="password" required="required" name="password_repeat"
-                       id="password_repeat" value="" autocomplete="new-password">
+    </div>
+    <div class="field">
+        <strong class="field-label">
+            <?php translate('email'); ?>
+        </strong>
+        <input type="email" name="email" value="">
+    </div>
+    <div class="row field">
+        <div class="col-xs-12 col-md-6">
+            <strong class="field-label">
+                <?php translate('password'); ?>*
+            </strong>
+            <input type="password" required="required" name="password"
+                    class="password-security-check"
+                    id="password" value="" autocomplete="new-password">
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <strong class="field-label">
+                <?php translate('password_repeat'); ?>*
+            </strong>
+            <input type="password" required="required" name="password_repeat"
+                    id="password_repeat" value="" autocomplete="new-password">
 
-            </div>
         </div>
-        <?php
-
-    ?>
-        <div class="field">
-            <strong class="field-label">
-                <?php translate('primary_group'); ?>
-            </strong>
-            <select
-                name="group_id">
-                <option value="-">[<?php translate('none'); ?>]</option>
-                <?php
-            foreach ($groups as $group) {
-                ?>
-                    <option value="<?php echo $group->getId(); ?>"
-                    <?php
-                if (Settings::get('default_acl_group') == $group->getId()) {
-                    echo 'selected';
-                }
-                ?>>
-                                <?php esc($group->getName()); ?>
-                    </option>
-                <?php }
-            ?>
-            </select>
-        </div>
-        <div class="field">
-            <strong class="field-label">
-                <?php translate('secondary_groups'); ?>
-            </strong>
-            <select name="secondary_groups[]" multiple>
-                <?php
-            foreach ($groups as $group) {
-                ?>
-                    <option value="<?php echo $group->getId(); ?>">
-                        <?php esc($group->getName()); ?>
-                    </option>
-                <?php }
-            ?>
-            </select>
-        </div>
-        <div class="checkbox block">
-            <label>
-                <input type="checkbox" value="1"
-                       name="require_password_change"
-                       id="require_password_change"
-                       class="js-switch"><?php translate('REQUIRE_PASSWORD_CHANGE_ON_NEXT_LOGIN'); ?> </label>
-        </div>
-        <div class="checkbox block">
-            <label><input type="checkbox" id="send_mail" name="send_mail"
-                          value="sendmail"
-                          class="js-switch">
-                <?php translate('SEND_LOGINDATA_BY_MAIL'); ?></label>
-        </div>
-        <div class="checkbox block">
-            <label><input type="checkbox" value="1" name="admin" id="admin"
-                          class="js-switch">
-                <?php translate('is_admin'); ?> </label>
-            <span class="has-help"
-                  onclick="$('div#is_admin').slideToggle()">
-                <i class="fa fa-question-circle text-info" aria-hidden="true"></i>
-            </span>
-        </div>
-        <div id="is_admin" class="help" style="display: none">
-            <?php echo nl2br(get_translation('HELP_IS_ADMIN')); ?>
-        </div>
-        <div class="checkbox block">
-            <div class="field">
-                <label><input type="checkbox" value="1" name="locked" id="locked"
-                              class="js-switch">
-                    <?php translate('locked'); ?> </label>
-            </div>
-        </div>
-        <div class="field">
-            <strong class="field-label">
-                <?php translate('default_language'); ?>
-            </strong>
-            <select name="default_language">
-                <option value="" selected>[<?php translate('standard'); ?>]</option>
-                <?php
-            $languageCount = count($languages);
-    for ($i = 0; $i < $languageCount; $i++) {
-        echo '<option value="' . $languages[$i] . '">' . getLanguageNameByCode($languages[$i]) . '</option>';
-    }
-    ?>
-            </select>
-        </div>
-        <div class="voffset2">
-            <button type="submit" class="btn btn-primary">
-                <i class="fa fa-save"></i>
-                <?php translate('save'); ?></button>
-        </div>
-    </form>
+    </div>
     <?php
-    $translation = new JSTranslation([], 'UserTranslation');
-    $translation->addKey('passwords_not_equal');
-    $translation->render();
-    enqueueScriptFile(
-        \App\Helpers\ModuleHelper::buildRessourcePath(
-            'core_users',
-            'js/users.js'
-        )
-    );
-    enqueueScriptFile('../node_modules/password-strength-meter/dist/password.min.js');
-    combinedScriptHtml();
-} else {
-    noPerms();
+
+?>
+    <div class="field">
+        <strong class="field-label">
+            <?php translate('primary_group'); ?>
+        </strong>
+        <select
+            name="group_id">
+            <option value="-">[<?php translate('none'); ?>]</option>
+            <?php
+        foreach ($groups as $group) {
+            ?>
+                <option value="<?php echo $group->getId(); ?>"
+                <?php
+            if (Settings::get('default_acl_group') == $group->getId()) {
+                echo 'selected';
+            }
+            ?>>
+                            <?php esc($group->getName()); ?>
+                </option>
+            <?php }
+        ?>
+        </select>
+    </div>
+    <div class="field">
+        <strong class="field-label">
+            <?php translate('secondary_groups'); ?>
+        </strong>
+        <select name="secondary_groups[]" multiple>
+            <?php
+        foreach ($groups as $group) {
+            ?>
+                <option value="<?php echo $group->getId(); ?>">
+                    <?php esc($group->getName()); ?>
+                </option>
+            <?php }
+        ?>
+        </select>
+    </div>
+    <div class="checkbox block">
+        <label>
+            <input type="checkbox" value="1"
+                    name="require_password_change"
+                    id="require_password_change"
+                    class="js-switch"><?php translate('REQUIRE_PASSWORD_CHANGE_ON_NEXT_LOGIN'); ?> </label>
+    </div>
+    <div class="checkbox block">
+        <label><input type="checkbox" id="send_mail" name="send_mail"
+                        value="sendmail"
+                        class="js-switch">
+            <?php translate('SEND_LOGINDATA_BY_MAIL'); ?></label>
+    </div>
+    <div class="checkbox block">
+        <label><input type="checkbox" value="1" name="admin" id="admin"
+                        class="js-switch">
+            <?php translate('is_admin'); ?> </label>
+        <span class="has-help"
+                onclick="$('div#is_admin').slideToggle()">
+            <i class="fa fa-question-circle text-info" aria-hidden="true"></i>
+        </span>
+    </div>
+    <div id="is_admin" class="help" style="display: none">
+        <?php echo nl2br(get_translation('HELP_IS_ADMIN')); ?>
+    </div>
+    <div class="checkbox block">
+        <div class="field">
+            <label><input type="checkbox" value="1" name="locked" id="locked"
+                            class="js-switch">
+                <?php translate('locked'); ?> </label>
+        </div>
+    </div>
+    <div class="field">
+        <strong class="field-label">
+            <?php translate('default_language'); ?>
+        </strong>
+        <select name="default_language">
+            <option value="" selected>[<?php translate('standard'); ?>]</option>
+            <?php
+        $languageCount = count($languages);
+for ($i = 0; $i < $languageCount; $i++) {
+    echo '<option value="' . $languages[$i] . '">' . getLanguageNameByCode($languages[$i]) . '</option>';
 }
+?>
+        </select>
+    </div>
+    <div class="voffset2">
+        <button type="submit" class="btn btn-primary">
+            <i class="fa fa-save"></i>
+            <?php translate('save'); ?></button>
+    </div>
+</form>
+<?php
+$translation = new JSTranslation([], 'UserTranslation');
+$translation->addKey('passwords_not_equal');
+$translation->render();
+enqueueScriptFile(
+    \App\Helpers\ModuleHelper::buildRessourcePath(
+        'core_users',
+        'js/users.js'
+    )
+);
+enqueueScriptFile('../node_modules/password-strength-meter/dist/password.min.js');
+combinedScriptHtml();
